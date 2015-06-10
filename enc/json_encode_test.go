7bed29bcde5aa178f47a65aace87fdd7a1afd91c
Aaron Boodman <aaron@aaronboodman.com>
2015-06-09 23:32:15 -0700

    Rest of tests for json_encode.go

--- enc/json_encode_test.go
+++ enc/json_encode_test.go
@@ -1,7 +1,9 @@
 package enc
 
 import (
+	"bytes"
 	"crypto/sha1"
+	"io/ioutil"
 	"testing"
 
 	"github.com/attic-labs/noms/ref"
@@ -24,6 +26,15 @@ func TestJsonEncode(t *testing.T) {
 		return r
 	}
 
+	assertExists := func(refStr, expected string) {
+		ref := ref.MustParse(refStr)
+		r, err := s.Get(ref)
+		assert.NoError(err)
+		b, err := ioutil.ReadAll(r)
+		assert.NoError(err)
+		assert.True(bytes.Equal([]byte(expected), b), "Expected ref %s to be %s", refStr, expected)
+	}
+
 	// booleans
 	testEncode("j false\n", types.Bool(false))
 	testEncode("j true\n", types.Bool(true))
@@ -36,9 +47,27 @@ func TestJsonEncode(t *testing.T) {
 	testEncode("j {\"uint32\":0}\n", types.UInt32(0))
 	testEncode("j {\"uint64\":9223372036854775808}\n", types.UInt64(1<<63))
 
-	// blobs we delegate to the blob codec. That's tested in blob_codec_test.go, but let's just make sure the delegation is working.
+	// Blobs we delegate to the blob codec. That's tested in blob_codec_test.go, but let's just make sure the delegation is working.
 	testEncode(string([]byte{'b', ' ', 0x00, 0x01, 0x02}), types.NewBlob([]byte{0x00, 0x01, 0x02}))
 
-	// strings - todo
-	//testEncode("j 'Hello, World!'", types.NewString("Hello, World!"))
+	// Strings
+	testEncode("j \"\"\n", types.NewString(""))
+	testEncode("j \"Hello, World!\"\n", types.NewString("Hello, World!"))
+
+	// Lists
+	testEncode("j {\"list\":[]}\n", types.NewList())
+	testEncode("j {\"list\":[\"foo\",true,{\"uint16\":42},{\"ref\":\"sha1-58bdf8e374b39f9b1e8a64784cf5c09601f4b7ea\"},{\"ref\":\"sha1-fa8026bf44f60b64ab674c49cda31a697467973c\"}]}\n",
+		types.NewList(types.NewString("foo"), types.Bool(true), types.UInt16(42), types.NewList(), types.NewMap()))
+	assertChildVals := func() {
+		assert.Equal(3, s.Len())
+		assertExists("sha1-58bdf8e374b39f9b1e8a64784cf5c09601f4b7ea", "j {\"list\":[]}\n")
+		assertExists("sha1-fa8026bf44f60b64ab674c49cda31a697467973c", "j {\"map\":{}}\n")
+	}
+	assertChildVals()
+
+	// Maps
+	testEncode("j {\"map\":{}}\n", types.NewMap())
+	testEncode("j {\"map\":{\"bool\":false,\"int32\":{\"int32\":42},\"list\":{\"ref\":\"sha1-58bdf8e374b39f9b1e8a64784cf5c09601f4b7ea\"},\"map\":{\"ref\":\"sha1-fa8026bf44f60b64ab674c49cda31a697467973c\"},\"string\":\"hotdog\"}}\n",
+		types.NewMap("bool", types.Bool(false), "int32", types.Int32(42), "string", types.NewString("hotdog"), "list", types.NewList(), "map", types.NewMap()))
+	assertChildVals()
 }
--- store/memory_store.go
+++ store/memory_store.go
@@ -5,7 +5,6 @@ import (
 	"crypto/sha1"
 	"io"
 	"io/ioutil"
-
 	"github.com/attic-labs/noms/ref"
 )
 
