bc210df1ddc0385a9fd2cf4edbda0c0ff418611b
Aaron Boodman <aaron@aaronboodman.com>
2015-06-18 12:05:48 -0700

    Remove dead code

--- user/user_test.go
+++ user/user_test.go
@@ -9,21 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-/*
-func TestInitDataStore(t *testing.T) {
-	assert := assert.New(t)
-	ds := datastore.NewDataStore(&chunks.MemoryStore{})
-	assert.EqualValues(0, ds.GetRoots().Len())
-	InitDataStore(ds)
-	assert.EqualValues(1, ds.GetRoots().Len())
-	users := ds.GetRoots().Any().(types.Map).Get(types.NewString("value")).(types.Set)
-	assert.EqualValues(0, users.Len())
-
-	// It's OK to call it twice -- the commit will just fail.
-	InitDataStore(ds)
-}
-*/
-
 func TestInsertUser(t *testing.T) {
 	assert := assert.New(t)
 	oldDs := datastore.NewDataStore(&chunks.MemoryStore{})
