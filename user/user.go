b5e6d920c6d3c9f1c704f83e92579df56c396f09
Aaron Boodman <aaron@aaronboodman.com>
2015-06-17 18:25:50 -0700

    Add user.CreateUser()

--- user/user.go
+++ user/user.go
@@ -2,21 +2,38 @@ package user
 
 import (
 	"github.com/attic-labs/noms/datastore"
+	. "github.com/attic-labs/noms/dbg"
 	"github.com/attic-labs/noms/types"
 )
 
-func InitDataStore(ds datastore.DataStore) {
-	if ds.GetRoots().Len() > 0 {
-		return
-	}
+func commit(ds datastore.DataStore, oldRoots types.Set, users types.Set) {
 	ds.Commit(types.NewSet(
 		types.NewMap(
-			types.NewString("$type"),
-			types.NewString("noms.Root"),
-			types.NewString("parents"),
-			types.NewSet(),
-			types.NewString("value"),
-			types.NewSet(), // empty set of noms.User
-		),
-	))
+			types.NewString("$type"), types.NewString("noms.Root"),
+			types.NewString("parents"), oldRoots,
+			types.NewString("value"), users)))
+}
+
+func InitDataStore(ds datastore.DataStore) types.Set {
+	roots := ds.GetRoots()
+	if roots.Len() == 0 {
+		commit(ds, roots, types.NewSet())
+		roots = ds.GetRoots()
+	}
+	Chk.EqualValues(1, roots.Len())
+	return roots
+}
+
+func CreateUser(ds datastore.DataStore, email string) {
+	roots := InitDataStore(ds)
+	user := types.NewMap(
+		types.NewString("$type"), types.NewString("noms.User"),
+		types.NewString("email"), types.NewString(email),
+		// TODO: Need nil Value so that we can put nil appRoot in here now?
+	)
+
+	// TODO: What if the user exists with a set appRoot? Need GetUser() check above.
+	users := roots.Any().(types.Map).Get(types.NewString("value")).(types.Set)
+	users = users.Insert(user)
+	commit(ds, roots, users)
 }
--- user/user_test.go
+++ user/user_test.go
@@ -17,4 +17,17 @@ func TestInitDataStore(t *testing.T) {
 	assert.EqualValues(1, ds.GetRoots().Len())
 	users := ds.GetRoots().Any().(types.Map).Get(types.NewString("value")).(types.Set)
 	assert.EqualValues(0, users.Len())
+
+	// It's OK to call it twice -- the commit will just fail.
+	InitDataStore(ds)
+}
+
+func TestCreateUser(t *testing.T) {
+	assert := assert.New(t)
+	ds := datastore.NewDataStore(&chunks.MemoryStore{})
+	CreateUser(ds, "foo@bar.com")
+	users := ds.GetRoots().Any().(types.Map).Get(types.NewString("value")).(types.Set)
+	assert.EqualValues(1, users.Len())
+	user := users.Any().(types.Map)
+	assert.True(types.NewString("foo@bar.com").Equals(user.Get(types.NewString("email"))))
 }
