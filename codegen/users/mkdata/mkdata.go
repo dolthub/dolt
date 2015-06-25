fbacb93510ff108f3fb9888b8138b91356751026
Aaron Boodman <aaron@aaronboodman.com>
2015-06-24 17:15:44 -0700

    Add codegen/users/mkdata: a program to generate noms data describing the user-related types.

---
+++ codegen/users/mkdata/mkdata.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/attic-labs/noms/chunks"
+	"github.com/attic-labs/noms/enc"
+	"github.com/attic-labs/noms/types"
+)
+
+var (
+	outDir = flag.String("out", "", "blah")
+)
+
+func main() {
+	flag.Parse()
+	if *outDir == "" {
+		flag.Usage()
+		return
+	}
+
+	cs := chunks.NewFileStore(*outDir, "root")
+
+	app := types.NewMap(
+		types.NewString("$type"), types.NewString("noms.StructDef"),
+		types.NewString("$name"), types.NewString("App"),
+		types.NewString("id"), types.NewString("string"),
+		types.NewString("value"), types.NewString("value"),
+	)
+
+	user := types.NewMap(
+		types.NewString("$name"), types.NewString("User"),
+		types.NewString("email"), types.NewString("string"),
+		types.NewString("apps"), types.NewMap(
+			types.NewString("$type"), types.NewString("noms.SetDef"),
+			types.NewString("elem"), app,
+		),
+	)
+
+	enc.WriteValue(user, cs)
+}
