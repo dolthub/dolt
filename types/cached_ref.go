cd9656cdb97bb359fb9112bae76a38f8f467f1ed
Chris Masone <cmasone@atticlabs.io>
2015-07-08 15:53:10 -0700

    Add friendly message to Chk for Reffer != nil
    Now you get told you're holding it wrong when you fail
    to import the enc package and get the global definition for
    Reffer.

--- types/cached_ref.go
+++ types/cached_ref.go
@@ -9,7 +9,7 @@ type cachedRef ref.Ref
 
 func (cr *cachedRef) Ref(v Value) ref.Ref {
 	if ref.Ref(*cr) == (ref.Ref{}) {
-		Chk.NotNil(Reffer)
+		Chk.NotNil(Reffer, "Reffer is nil; you probably need to import 'enc'.")
 		*cr = cachedRef(Reffer(v))
 	}
 	return ref.Ref(*cr)
