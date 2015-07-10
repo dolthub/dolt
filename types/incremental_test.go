fd75f33c8cefcf63d9d4c9f516cd57aee1dadfac
Aaron Boodman <aaron@aaronboodman.com>
2015-07-09 18:57:26 -0700

    comment cleanup

--- types/incremental_test.go
+++ types/incremental_test.go
@@ -34,8 +34,6 @@ func TestIncrementalLoad(t *testing.T) {
 	assert.NoError(err)
 	actual := actualVar.(List)
 
-	// NOTE: No equals test here because Ref() ends up expanding entire tree, which would kill the point of the test.
-
 	prev := cs.count
 	assert.Equal(1, prev)
 	for i := uint64(0); i < expected.Len(); i++ {
@@ -45,12 +43,13 @@ func TestIncrementalLoad(t *testing.T) {
 		next := prev
 		switch v.(type) {
 		case Blob, Set, List, Map:
+			// These are the types that are out-of-line in our current encoding. So we expect them to cause a load.
 			next += 1
 		}
 
 		assert.Equal(next, cs.count)
 
-		// Do it again to make sure multiple derefs don't change anything.
+		// Do it again to make sure multiple derefs don't do multiple loads.
 		v = actual.Get(i)
 		assert.Equal(next, cs.count)
 
