6db73c518e4984bed0698450b53b3d7fa9fcaca2
Rafael Weinstein <rafael@atticlabs.io>
2015-07-22 15:54:33 -0700

    Rate-limit concurrent noms XHR requests

--- js/src/noms_store.js
+++ js/src/noms_store.js
@@ -13,31 +13,59 @@ var rpc = {
   root: nomsServer + '/root',
 };
 
-// TODO: Use whatwg-fetch
-function fetch(url) {
+// TODO: Chrome seems to start spitting out uncatchable errors if we queue too many XHRs. This limit probably doesn't actually slow us down because the user agent has its own queue of fetches to service.
+var maxConnections = 512;
+var activeFetches = 0;
+var pendingFetches = [];
+
+function requestFetch(url) {
   return new Promise((resolve, reject) => {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = (e) => {
-      resolve(e.target.responseText);
-    };
-    xhr.onerror = (e) => {
-      reject(e.target.statusText);
-    };
-    xhr.open('get', url, true);
-    xhr.send();
+    pendingFetches.push({ url, resolve, reject });
+    pumpFetchQueue();
   });
 }
 
+function beginFetch(req) {
+  activeFetches++;
+  fetch(req.url, req.resolve, req.reject);
+}
+
+function endFetch() {
+  activeFetches--;
+  pumpFetchQueue();
+}
+
+function pumpFetchQueue() {
+  while (pendingFetches.length && activeFetches < maxConnections) {
+    beginFetch(pendingFetches.shift())
+  }
+}
+
+// TODO: Use whatwg-fetch
+function fetch(url, resolve, reject) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = (e) => {
+    endFetch();
+    resolve(e.target.responseText);
+  };
+  xhr.onerror = (e) => {
+    endFetch();
+    reject(e.target.statusText);
+  };
+  xhr.open('get', url, true);
+  xhr.send();
+}
+
 function getChunk(ref) {
-  return fetch(rpc.get + '?ref=' + ref);
+  return requestFetch(rpc.get + '?ref=' + ref);
 }
 
 function getRoot() {
-  return fetch(rpc.root);
+  return requestFetch(rpc.root);
 }
 
 function getDataset(id) {
-  return fetch(rpc.dataset + '?id=' + id)
+  return requestFetch(rpc.dataset + '?id=' + id)
 }
 
 module.exports = {
