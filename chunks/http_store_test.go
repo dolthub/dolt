b58e20eb8c5a4d70641c09e53e23be407630ed3e
Rafael Weinstein <rafael@atticlabs.io>
2015-09-02 13:20:34 -0700

    fix http_store test

--- chunks/http_store_test.go
+++ chunks/http_store_test.go
@@ -20,13 +20,20 @@ func (suite *HttpStoreTestSuite) SetupTest() {
 	suite.store = NewHttpStoreClient("http://localhost:8000")
 	suite.server = NewHttpStoreServer(&MemoryStore{}, 8000)
 	go suite.server.Run()
+
+	// This call to a non-existing URL allows us to exit being sure that the server started. Otherwise, we sometimes get races with Stop() below.
+	req, err := http.NewRequest("GET", "http://localhost:8000/notHere", nil)
+	suite.NoError(err)
+	res, err := http.DefaultClient.Do(req)
+	suite.NoError(err)
+	suite.Equal(res.StatusCode, http.StatusNotFound)
 }
 
 func (suite *HttpStoreTestSuite) TearDownTest() {
 	suite.server.Stop()
 
 	// Stop will have closed it's side of an existing KeepAlive socket. The next request will fail.
-	req, err := http.NewRequest("GET", "http://localhost:8000", nil)
+	req, err := http.NewRequest("GET", "http://localhost:8000/notHere", nil)
 	suite.NoError(err)
 	_, err = http.DefaultClient.Do(req)
 	suite.Error(err)
