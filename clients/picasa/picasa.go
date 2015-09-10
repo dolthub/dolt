4ff11d1f6ec71ec863caf20d353c952e3464f2a2
Dan Willhite <willhite@gmail.com>
2015-09-10 11:11:08 -0700

    Add concurrency to photo fetching.
    Other improvements are:
    1) Handle albums with more than 1000 photos correctly
    2) Fetch original or small images (-small flag)
    3) Progress logging and -quiet flag
    4) Addition of -force-auth flag
    5) Improve handling of http errors
    6) Used go fmt and golint on source code
    7) If fetching 1 album, merge in albums from previous commit

--- clients/picasa/picasa.go
+++ clients/picasa/picasa.go
@@ -11,7 +11,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/attic-labs/noms/Godeps/_workspace/src/golang.org/x/oauth2"
@@ -21,42 +23,70 @@ import (
 	"github.com/attic-labs/noms/dataset"
 	"github.com/attic-labs/noms/marshal"
 	"github.com/attic-labs/noms/types"
+	"strconv"
 )
+
+const maxProcs = 25
 
 var (
-	apiKeyFlag       = flag.String("api-key", "", "API key from Google credentials. See below for help creating credentials:")
-	apiKeySecretFlag = flag.String("api-key-secret", "", "API secret from Google credentials. See below for help creating credentials")
-	albumIdFlag      = flag.String("album-id", "", "Import a specific album, identified by id")
-	ds               *dataset.Dataset
-	httpClient       *http.Client
+	albumIDFlag       = flag.String("album-id", "", "Import a specific album, identified by id")
+	apiKeyFlag        = flag.String("api-key", "", "API keys for Google can be created at https://console.developers.google.com")
+	apiKeySecretFlag  = flag.String("api-key-secret", "", "API keys for Google can be created at https://console.developers.google.com")
+	authHTTPClient    *http.Client
+	cachingHTTPClient *http.Client
+	ds                *dataset.Dataset
+	forceAuthFlag     = flag.Bool("force-auth", false, "Force re-authentication")
+	quietFlag         = flag.Bool("quiet", false, "Don't print progress information")
+	smallFlag         = flag.Bool("small", false, "Fetch lower resolution images from picasa")
+	start             time.Time
 )
 
-type Photo struct {
-	NomsName string          `noms:"$name"`
-	Height   string          `noms:"height"`
-	Id       string          `noms:"id"`
-	Image    *bytes.Reader   `noms:"image"`
-	Tags     map[string]bool `noms:"tags"`
-	Title    string          `noms:"title"`
-	Url      string          `noms:"url"`
-	Width    string          `noms:"width"`
-}
+func main() {
+	flag.Usage = picasaUsage
+	dsFlags := dataset.NewFlags()
+	flag.Parse()
+	cachingHTTPClient = util.CachingHttpClient()
 
-type Album struct {
-	Id        string
-	Title     string
-	NumPhotos int
-	Photos    []Photo
-}
+	if *apiKeyFlag == "" || *apiKeySecretFlag == "" || cachingHTTPClient == nil {
+		flag.Usage()
+		return
+	}
 
-type User struct {
-	Id     string
-	Name   string
-	Albums []Album
-}
+	ds = dsFlags.CreateDataset()
+	if ds == nil {
+		flag.Usage()
+		return
+	}
+	defer ds.Close()
 
-type OauthToken struct {
-	RefreshToken string
+	var cu types.Value
+	if commit, ok := ds.MaybeHead(); ok {
+		cu = commit.Value()
+	}
+
+	c, refreshToken := doAuthentication(cu)
+	authHTTPClient = c
+
+	// set start after authentication so we don't count that time
+	start = time.Now()
+
+	var nomsUser types.Value
+	if *albumIDFlag != "" {
+		newNomsUser, newNomsAlbum, _ := getAlbum(*albumIDFlag)
+		if cu != nil {
+			nomsUser = mergeInCurrentAlbums(cu, newNomsUser, newNomsAlbum)
+		} else {
+			nomsUser = newNomsUser
+		}
+		printStats(newNomsUser)
+	} else {
+		nomsUser = getAlbums()
+		printStats(nomsUser)
+	}
+
+	nomsUser = setValueInNomsMap(nomsUser, "RefreshToken", types.NewString(refreshToken))
+	_, ok := ds.Commit(nomsUser)
+	d.Exp.True(ok, "Could not commit due to conflicting edit")
 }
 
 func picasaUsage() {
@@ -80,217 +110,231 @@ func picasaUsage() {
 	fmt.Fprintf(os.Stderr, "\n%s\n\n", credentialSteps)
 }
 
-func main() {
-	flag.Usage = picasaUsage
-	dsFlags := dataset.NewFlags()
-	flag.Parse()
-	httpClient = util.CachingHttpClient()
-
-	if *apiKeyFlag == "" || *apiKeySecretFlag == "" || httpClient == nil {
-		flag.Usage()
-		return
-	}
-
-	ds = dsFlags.CreateDataset()
-	if ds == nil {
-		flag.Usage()
-		return
-	}
-	defer ds.Close()
-
-	var client *http.Client
-	refreshToken := getRefreshToken()
-	client = tryRefreshToken(refreshToken)
-	if client == nil {
-		client, refreshToken = googleOAuth()
-	}
-	var nomsUser types.Value
-	if *albumIdFlag != "" {
-		nomsUser, _, _ = getAlbum(client, *albumIdFlag)
-	} else {
-		nomsUser = getAlbums(client)
-	}
-	nomsUser = setValueInNomsMap(nomsUser, "RefreshToken", types.NewString(refreshToken))
-
-	_, ok := ds.Commit(nomsUser)
-	d.Exp.True(ok, "Could not commit due to conflicting edit")
+func getAlbum(albumID string) (nomsUser types.Value, nomsAlbum types.Value, nomsPhotoList types.Value) {
+	aj := AlbumJSON{}
+	path := fmt.Sprintf("user/default/albumid/%s?alt=json&max-results=0", albumID)
+	callPicasaAPI(authHTTPClient, path, &aj)
+	u := User{ID: aj.Feed.UserID.V, Name: aj.Feed.UserName.V}
+	a := Album{ID: aj.Feed.ID.V, Title: aj.Feed.Title.V, NumPhotos: aj.Feed.NumPhotos.V}
+	nomsPhotoList = getPhotos(a, 1)
+	nomsAlbum = marshal.Marshal(a)
+	nomsAlbum = setValueInNomsMap(nomsAlbum, "Photos", nomsPhotoList)
+	nomsUser = marshal.Marshal(u)
+	nomsUser = setValueInNomsMap(nomsUser, "Albums", types.NewList(nomsAlbum))
+	return nomsUser, nomsAlbum, nomsPhotoList
 }
 
-func getRefreshToken() string {
-	oauthToken := &OauthToken{}
-	if commit, ok := ds.MaybeHead(); ok {
-		marshal.Unmarshal(commit.Value(), &oauthToken)
+func getAlbums() (nomsUser types.Value) {
+	aj := AlbumListJSON{}
+	callPicasaAPI(authHTTPClient, "user/default?alt=json", &aj)
+	user := User{ID: aj.Feed.UserID.V, Name: aj.Feed.UserName.V}
+
+	if !*quietFlag {
+		fmt.Printf("Found %d albums\n", len(aj.Feed.Entry))
 	}
-	return oauthToken.RefreshToken
-}
-
-func tryRefreshToken(refreshToken string) *http.Client {
-	var client *http.Client
-
-	if refreshToken != "" {
-		tok := oauth2.Token{}
-		conf := baseConfig("")
-		contentType := "application/x-www-form-urlencoded"
-		body := fmt.Sprintf("client_id=%s&client_secret=%s&grant_type=refresh_token&refresh_token=%s", *apiKeyFlag, *apiKeySecretFlag, refreshToken)
-		resp, err := httpClient.Post(google.Endpoint.TokenURL, contentType, strings.NewReader(body))
-		d.Chk.NoError(err)
-		if resp.StatusCode == 200 {
-			buf, err := ioutil.ReadAll(resp.Body)
-			d.Chk.NoError(err)
-			json.Unmarshal(buf, &tok)
-			client = conf.Client(oauth2.NoContext, &tok)
-		}
-	}
-	return client
-}
-
-func getAlbum(client *http.Client, albumId string) (types.Value, types.Value, types.Value) {
-	response := struct {
-		Feed struct {
-			UserName struct {
-				V string `json:"$t"`
-			} `json:"gphoto$nickname"`
-			Id struct {
-				V string `json:"$t"`
-			} `json:"gphoto$id"`
-			NumPhotos struct {
-				V int `json:"$t"`
-			} `json:"gphoto$numphotos"`
-			Title struct {
-				V string `json:"$t"`
-			}
-			UserId struct {
-				V string `json:"$t"`
-			} `json:"gphoto$user"`
-			Entry []struct {
-				Content struct {
-					Src  string
-					Type string
-				}
-				Height struct {
-					V string `json:"$t"`
-				} `json:"gphoto$height"`
-				Id struct {
-					V string `json:"$t"`
-				} `json:"gphoto$id"`
-				Size struct {
-					V string `json:"$t"`
-				} `json:"gphoto$size"`
-				MediaGroup struct {
-					Tags struct {
-						V string `json:"$t"`
-					} `json:"media$keywords"`
-				} `json:"media$group"`
-				Timestamp struct {
-					V string `json:"$t"`
-				} `json:"gphoto$timestamp"`
-				Title struct {
-					V string `json:"$t"`
-				}
-				Width struct {
-					V string `json:"$t"`
-				} `json:"gphoto$width"`
-			}
-		}
-	}{}
-
-	path := fmt.Sprintf("user/default/albumid/%s?alt=json", albumId)
-	callPicasaApi(client, path, &response)
-	feed := response.Feed
-	var nomsPhotos = types.NewList()
-	for _, entry := range feed.Entry {
-		image := getPhoto(entry.Content.Src)
-		tags := map[string]bool{}
-		for _, s := range strings.Split(entry.MediaGroup.Tags.V, ",") {
-			s1 := strings.Trim(s, " ")
-			if s1 != "" {
-				tags[s1] = true
-			}
-		}
-		photo := Photo{
-			NomsName: "Photo",
-			Height:   entry.Height.V,
-			Id:       entry.Id.V,
-			Image:    image,
-			Tags:     tags,
-			Title:    entry.Title.V,
-			Url:      entry.Content.Src,
-			Width:    entry.Width.V,
-		}
-		nomsPhoto := marshal.Marshal(photo)
-		r := types.WriteValue(nomsPhoto, ds.Store())
-		nomsPhotos = nomsPhotos.Append(types.Ref{r})
+	var nomsAlbumList = types.NewList()
+	for i, entry := range aj.Feed.Entry {
+		a := Album{ID: entry.ID.V, Title: entry.Title.V, NumPhotos: entry.NumPhotos.V}
+		nomsPhotoList := getPhotos(a, i)
+		nomsAlbum := marshal.Marshal(a)
+		nomsAlbum = setValueInNomsMap(nomsAlbum, "Photos", nomsPhotoList)
+		nomsAlbumList = nomsAlbumList.Append(nomsAlbum)
 	}
 
-	nomsAlbum := marshal.Marshal(Album{Id: feed.Id.V, Title: feed.Title.V, NumPhotos: feed.NumPhotos.V})
-	nomsAlbum = setValueInNomsMap(nomsAlbum, "Photos", nomsPhotos)
-	nomsUser := marshal.Marshal(User{Id: feed.UserId.V, Name: feed.UserName.V})
-	nomsUser = setValueInNomsMap(nomsUser, "Albums", types.NewSet(nomsAlbum))
-	return nomsUser, nomsAlbum, nomsPhotos
-}
-
-func getAlbums(client *http.Client) types.Value {
-	response := struct {
-		Feed struct {
-			UserName struct {
-				V string `json:"$t"`
-			} `json:"gphoto$nickname"`
-			Entry []struct {
-				Id struct {
-					V string `json:"$t"`
-				} `json:"gphoto$id"`
-				NumPhotos struct {
-					V int `json:"$t"`
-				} `json:"gphoto$numphotos"`
-				Title struct {
-					V string `json:"$t"`
-				}
-			}
-			UserId struct {
-				V string `json:"$t"`
-			} `json:"gphoto$user"`
-		}
-	}{}
-
-	callPicasaApi(client, "user/default?alt=json", &response)
-	feed := response.Feed
-
-	var nomsAlbums = types.NewList()
-	for _, entry := range feed.Entry {
-		_, _, nomsPhotos := getAlbum(client, entry.Id.V)
-		nomsAlbum := marshal.Marshal(Album{Id: entry.Id.V, Title: entry.Title.V, NumPhotos: entry.NumPhotos.V})
-		nomsAlbum = setValueInNomsMap(nomsAlbum, "Photos", nomsPhotos)
-		nomsAlbums = nomsAlbums.Append(nomsAlbum)
-	}
-
-	nomsUser := marshal.Marshal(User{Id: feed.UserId.V, Name: feed.UserName.V})
-	nomsUser = setValueInNomsMap(nomsUser, "Albums", nomsAlbums)
+	nomsUser = marshal.Marshal(user)
+	nomsUser = setValueInNomsMap(nomsUser, "Albums", nomsAlbumList)
 
 	return nomsUser
+}
+
+func getPhotos(album Album, albumIndex int) (nomsPhotoList types.List) {
+	if album.NumPhotos <= 0 {
+		return nil
+	}
+	photos := make([]Photo, 0, album.NumPhotos)
+	if !*quietFlag {
+		fmt.Printf("Album #%d: %q contains %d photos... ", albumIndex, album.Title, album.NumPhotos)
+	}
+	for startIndex, foundPhotos := 0, true; album.NumPhotos > len(photos) && foundPhotos; startIndex += 1000 {
+		foundPhotos = false
+		aj := AlbumJSON{}
+		path := fmt.Sprintf("user/default/albumid/%s?alt=json&max-results=1000", album.ID)
+		if !*smallFlag {
+			path = fmt.Sprintf("%s%s", path, "&imgmax=d")
+		}
+		if startIndex > 0 {
+			path = fmt.Sprintf("%s%s%d", path, "&start-index=", startIndex)
+		}
+		callPicasaAPI(authHTTPClient, path, &aj)
+		for _, e := range aj.Feed.Entry {
+			foundPhotos = true
+			tags := splitTags(e.MediaGroup.Tags.V)
+			height, _ := strconv.Atoi(e.Height.V)
+			width, _ := strconv.Atoi(e.Width.V)
+			p := Photo{
+				NomsName: "Photo",
+				Height:   int(height),
+				ID:       e.ID.V,
+				Tags:     tags,
+				Title:    e.Title.V,
+				URL:      e.Content.Src,
+				Width:    int(width),
+			}
+			photos = append(photos, p)
+		}
+	}
+
+	pChan, rChan := getImageFetcher(len(photos))
+	for i, p := range photos {
+		pChan <- PhotoMessage{i, p}
+	}
+	close(pChan)
+
+	refMessages := make([]RefMessage, 0, album.NumPhotos)
+	for rm := range rChan {
+		refMessages = append(refMessages, rm)
+	}
+	sort.Sort(ByIndex(refMessages))
+	nomsPhotoList = types.NewList()
+	for _, refMsg := range refMessages {
+		nomsPhotoList = nomsPhotoList.Append(types.Ref{R: refMsg.Ref})
+	}
+
+	if !*quietFlag {
+		fmt.Printf("fetched %d, elapsed time: %.2f secs\n", nomsPhotoList.Len(), time.Now().Sub(start).Seconds())
+	}
+	return nomsPhotoList
+}
+
+func getImageFetcher(numPhotos int) (pChan chan PhotoMessage, rChan chan RefMessage) {
+	pChan = make(chan PhotoMessage, numPhotos)
+	rChan = make(chan RefMessage)
+	var wg sync.WaitGroup
+	n := min(numPhotos, maxProcs)
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func(fid int) {
+			for msg := range pChan {
+				msg.Photo.Image = getImage(msg.Photo.URL)
+				nomsPhoto := marshal.Marshal(msg.Photo)
+				ref := types.WriteValue(nomsPhoto, ds.Store())
+				rChan <- RefMessage{msg.Index, ref}
+			}
+			wg.Done()
+		}(i)
+	}
+
+	go func() {
+		wg.Wait()
+		close(rChan)
+	}()
+	return
+}
+
+func getImage(url string) *bytes.Reader {
+	pr := getImageReader(url)
+	defer pr.Close()
+	buf, err := ioutil.ReadAll(pr)
+	d.Chk.NoError(err)
+	return bytes.NewReader(buf)
+}
+
+func getImageReader(url string) io.ReadCloser {
+	r, err := cachingHTTPClient.Get(url)
+	d.Chk.NoError(err)
+	return r.Body
+}
+
+func printStats(nomsUser types.Value) {
+	if !*quietFlag {
+		numPhotos := uint64(0)
+		nomsAlbums := getValueInNomsMap(nomsUser, "Albums").(types.List)
+		for i := uint64(0); i < nomsAlbums.Len(); i++ {
+			nomsAlbum := nomsAlbums.Get(i)
+			nomsPhotos := getValueInNomsMap(nomsAlbum, "Photos").(types.List)
+			numPhotos = numPhotos + nomsPhotos.Len()
+		}
+
+		fmt.Printf("Imported %d album(s), %d photo(s), time: %.2f\n", nomsAlbums.Len(), numPhotos, time.Now().Sub(start).Seconds())
+	}
+}
+
+func mergeInCurrentAlbums(cu, newNomsUser, newNomsAlbum types.Value) (nomsUser types.Value) {
+	newAlbumID := getValueInNomsMap(newNomsAlbum, "ID").(types.String).String()
+	oldNomsAlbums := getValueInNomsMap(cu, "Albums").(types.List)
+	newNomsAlbums := types.NewList()
+	inserted := false
+	for i := uint64(0); i < uint64(oldNomsAlbums.Len()); i++ {
+		oldNomsAlbum := oldNomsAlbums.Get(i)
+		oldAlbumID := getValueInNomsMap(oldNomsAlbum, "ID").(types.String).String()
+		if newAlbumID != oldAlbumID {
+			newNomsAlbums = newNomsAlbums.Append(oldNomsAlbum)
+		} else {
+			inserted = true
+			newNomsAlbums = newNomsAlbums.Append(newNomsAlbum)
+		}
+	}
+	if !inserted {
+		newNomsAlbums = newNomsAlbums.Append(newNomsAlbum)
+	}
+	nomsUser = setValueInNomsMap(newNomsUser, "Albums", newNomsAlbums)
+	return
+}
+
+func doAuthentication(currentUser types.Value) (c *http.Client, rt string) {
+	if !*forceAuthFlag && currentUser != nil {
+		rt = getValueInNomsMap(currentUser, "RefreshToken").(types.String).String()
+		c = tryRefreshToken(rt)
+	}
+	if c == nil {
+		c, rt = googleOAuth()
+	}
+	return c, rt
+}
+
+func tryRefreshToken(rt string) *http.Client {
+	var c *http.Client
+
+	if rt != "" {
+		t := oauth2.Token{}
+		conf := baseConfig("")
+		ct := "application/x-www-form-urlencoded"
+		body := fmt.Sprintf("client_id=%s&client_secret=%s&grant_type=refresh_token&refresh_token=%s", *apiKeyFlag, *apiKeySecretFlag, rt)
+		r, err := cachingHTTPClient.Post(google.Endpoint.TokenURL, ct, strings.NewReader(body))
+		d.Chk.NoError(err)
+		if r.StatusCode == 200 {
+			buf, err := ioutil.ReadAll(r.Body)
+			d.Chk.NoError(err)
+			json.Unmarshal(buf, &t)
+			c = conf.Client(oauth2.NoContext, &t)
+		}
+	}
+	return c
 }
 
 func googleOAuth() (*http.Client, string) {
 	l, err := net.Listen("tcp", "127.0.0.1:0")
 	d.Chk.NoError(err)
 
-	redirectUrl := "http://" + l.Addr().String()
-	conf := baseConfig(redirectUrl)
-	rand1 := rand.New(rand.NewSource(time.Now().UnixNano()))
-	state := fmt.Sprintf("%v", rand1.Uint32())
-	url := conf.AuthCodeURL(state)
+	redirectURL := "http://" + l.Addr().String()
+	conf := baseConfig(redirectURL)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	state := fmt.Sprintf("%v", r.Uint32())
+	u := conf.AuthCodeURL(state)
 
 	// Redirect user to Google's consent page to ask for permission
 	// for the scopes specified above.
-	fmt.Printf("Visit the following URL to authorize access to your Picasa data: %v\n", url)
+	fmt.Printf("Visit the following URL to authorize access to your Picasa data: %v\n", u)
 	code, returnedState := awaitOAuthResponse(l)
 	d.Chk.Equal(state, returnedState, "Oauth state is not correct")
 
 	// Handle the exchange code to initiate a transport.
-	tok, err := conf.Exchange(oauth2.NoContext, code)
+	t, err := conf.Exchange(oauth2.NoContext, code)
 	d.Chk.NoError(err)
 
-	client := conf.Client(oauth2.NoContext, tok)
-	return client, tok.RefreshToken
+	client := conf.Client(oauth2.NoContext, t)
+	return client, t.RefreshToken
 }
 
 func awaitOAuthResponse(l net.Listener) (string, string) {
@@ -303,6 +347,9 @@ func awaitOAuthResponse(l net.Listener) (string, string) {
 			w.Header().Add("content-type", "text/plain")
 			fmt.Fprintf(w, "Authorized")
 			l.Close()
+		} else if err := r.URL.Query().Get("error"); err == "access_denied" {
+			fmt.Fprintln(os.Stderr, "Request for authorization was denied.")
+			os.Exit(0)
 		} else if err := r.URL.Query().Get("error"); err != "" {
 			l.Close()
 			d.Chk.Fail(err)
@@ -313,41 +360,41 @@ func awaitOAuthResponse(l net.Listener) (string, string) {
 	return code, state
 }
 
-func callPicasaApi(client *http.Client, path string, response interface{}) {
-	url := "https://picasaweb.google.com/data/feed/api/" + path
-	req, err := http.NewRequest("GET", url, nil)
+func callPicasaAPI(client *http.Client, path string, response interface{}) {
+	u := "https://picasaweb.google.com/data/feed/api/" + path
+	req, err := http.NewRequest("GET", u, nil)
 	d.Chk.NoError(err)
 
 	req.Header.Add("GData-Version", "2")
 	resp, err := client.Do(req)
 	d.Chk.NoError(err)
 
+	msg := func() string {
+		body := &bytes.Buffer{}
+		_, err := io.Copy(body, resp.Body)
+		d.Chk.NoError(err)
+		return fmt.Sprintf("could not load %s: %d: %s", u, resp.StatusCode, body)
+	}
+
+	switch resp.StatusCode / 100 {
+	case 4:
+		d.Exp.Fail(msg())
+	case 5:
+		d.Chk.Fail(msg())
+	}
+
 	err = json.NewDecoder(resp.Body).Decode(response)
 	d.Chk.NoError(err)
 }
 
-func baseConfig(redirectUrl string) *oauth2.Config {
+func baseConfig(redirectURL string) *oauth2.Config {
 	return &oauth2.Config{
 		ClientID:     *apiKeyFlag,
 		ClientSecret: *apiKeySecretFlag,
-		RedirectURL:  redirectUrl,
+		RedirectURL:  redirectURL,
 		Scopes:       []string{"https://picasaweb.google.com/data"},
 		Endpoint:     google.Endpoint,
 	}
-}
-
-func getPhotoReader(url string) io.ReadCloser {
-	resp, err := httpClient.Get(url)
-	d.Chk.NoError(err)
-	return resp.Body
-}
-
-func getPhoto(url string) *bytes.Reader {
-	photoReader := getPhotoReader(url)
-	defer photoReader.Close()
-	buf, err := ioutil.ReadAll(photoReader)
-	d.Chk.NoError(err)
-	return bytes.NewReader(buf)
 }
 
 // General utility functions
@@ -360,8 +407,26 @@ func setValueInNomsMap(m types.Value, field string, value types.Value) types.Val
 	return m.(types.Map).Set(types.NewString(field), value)
 }
 
-func toJson(str interface{}) string {
+func toJSON(str interface{}) string {
 	v, err := json.Marshal(str)
 	d.Chk.NoError(err)
 	return string(v)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func splitTags(s string) map[string]bool {
+	tags := map[string]bool{}
+	for _, s := range strings.Split(s, ",") {
+		s1 := strings.Trim(s, " ")
+		if s1 != "" {
+			tags[s1] = true
+		}
+	}
+	return tags
 }
