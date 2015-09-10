e70c473e377d34f896c8a9b27068309a95470407
Chris Masone <cmasone@atticlabs.io>
2015-09-10 10:51:58 -0700

    Further WIP, using TypeRef

--- types/parser/parser.go
+++ types/parser/parser.go
@@ -2,8 +2,86 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"strings"
+
+	"github.com/attic-labs/noms/d"
 )
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: grammar 'EXPR'")
+	}
+
+	err := d.Try(func() {
+		file, err := os.Open(os.Args[1])
+		d.Exp.NoError(err)
+		defer file.Close()
+		pkg := ParsePackage(os.Args[1], file)
+		// lookup := buildLookupTable(pkg)
+		//resolvePackage(pkg, lookup)
+
+		for _, decl := range pkg.UsingDeclarations {
+			d.Exp.IsType(CompoundDesc{}, decl.Desc)
+			fmt.Println(decl.Explain())
+		}
+		for _, named := range pkg.NamedTypes {
+			fmt.Println(named.Explain())
+		}
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func ParsePackage(logname string, r io.Reader) Package {
+	got, err := ParseReader(logname, r)
+	d.Exp.NoError(err)
+	return got.(Package)
+}
+
+// A Package has a Name, and a map of import Aliases that are not yet really used.
+// UsingDeclarations is kind of a hack to indicate specializations of Noms containers that need to be generated. These should all be one of ListKind, SetKind, MapKind or RefKind, and Desc should be a CompoundDesc instance.
+// NamedTypes is a lookup table for types defined in this package. These should all be EnumKind or StructKind. When traversing the definition of a given type, you may run into a TypeRef that IsUnresolved(). In that case, look it up by name in the NamedTypes of the appropriate package.
+type Package struct {
+	Name              string
+	Aliases           map[string]string
+	UsingDeclarations []TypeRef
+	NamedTypes        map[string]TypeRef
+}
+
+type TypeRef struct {
+	PkgRef string // Not yet used.
+	Name   string
+	Desc   TypeDesc // When Noms-ified, the TypeRef handling code will use Kind information to know how to deserialize Desc.
+}
+
+func (t TypeRef) IsUnresolved() bool {
+	return t.Desc == nil
+}
+
+func (t TypeRef) Kind() NomsKind {
+	return t.Desc.Kind()
+}
+
+func (t TypeRef) Explain() string {
+	seg := []string{}
+	if t.Name != "" {
+		seg = append(seg, fmt.Sprintf("NamedType %s: ", t.Name))
+	}
+	if t.Desc != nil {
+		seg = append(seg, t.Desc.Describe())
+	}
+	return strings.Join(seg, "\n")
+}
+
+type TypeDesc interface {
+	Kind() NomsKind
+	Describe() string
+}
 
 type NomsKind int
 
@@ -28,23 +106,21 @@ const (
 	SetKind
 	UnionKind
 	EnumKind
-	NamedKind
 	StructKind
 )
 
-type Type interface {
-	Kind() NomsKind
-	Describe() string
+func MakePrimitiveTypeRef(p string) TypeRef {
+	return TypeRef{Desc: PrimitiveToDesc[p]}
 }
 
-type PrimitiveType NomsKind
+type PrimitiveDesc NomsKind
 
-func (p PrimitiveType) Kind() NomsKind {
+func (p PrimitiveDesc) Kind() NomsKind {
 	return NomsKind(p)
 }
 
-func (p PrimitiveType) Describe() string {
-	for k, v := range PrimitiveToKind {
+func (p PrimitiveDesc) Describe() string {
+	for k, v := range PrimitiveToDesc {
 		if p == v {
 			return k
 		}
@@ -52,111 +128,40 @@ func (p PrimitiveType) Describe() string {
 	panic("wha")
 }
 
-var PrimitiveToKind = map[string]PrimitiveType{
-	"Bool":    PrimitiveType(BoolKind),
-	"UInt64":  PrimitiveType(UInt64Kind),
-	"UInt32":  PrimitiveType(UInt32Kind),
-	"UInt16":  PrimitiveType(UInt16Kind),
-	"UInt8":   PrimitiveType(UInt8Kind),
-	"Int64":   PrimitiveType(Int64Kind),
-	"Int32":   PrimitiveType(Int32Kind),
-	"Int16":   PrimitiveType(Int16Kind),
-	"Int8":    PrimitiveType(Int8Kind),
-	"Float64": PrimitiveType(Float64Kind),
-	"Float32": PrimitiveType(Float32Kind),
-	"String":  PrimitiveType(StringKind),
-	"Blob":    PrimitiveType(BlobKind),
-	"Value":   PrimitiveType(ValueKind),
+var PrimitiveToDesc = map[string]PrimitiveDesc{
+	"Bool":    PrimitiveDesc(BoolKind),
+	"UInt64":  PrimitiveDesc(UInt64Kind),
+	"UInt32":  PrimitiveDesc(UInt32Kind),
+	"UInt16":  PrimitiveDesc(UInt16Kind),
+	"UInt8":   PrimitiveDesc(UInt8Kind),
+	"Int64":   PrimitiveDesc(Int64Kind),
+	"Int32":   PrimitiveDesc(Int32Kind),
+	"Int16":   PrimitiveDesc(Int16Kind),
+	"Int8":    PrimitiveDesc(Int8Kind),
+	"Float64": PrimitiveDesc(Float64Kind),
+	"Float32": PrimitiveDesc(Float32Kind),
+	"String":  PrimitiveDesc(StringKind),
+	"Blob":    PrimitiveDesc(BlobKind),
+	"Value":   PrimitiveDesc(ValueKind),
 }
 
-type Package struct {
-	packageName       string
-	aliases           map[string]string
-	usingDeclarations []CompoundType
-	structs           map[string]StructType
-	enums             map[string]EnumType
+func MakeCompoundTypeRef(k NomsKind, e []TypeRef) TypeRef {
+	return TypeRef{Desc: CompoundDesc{k, e}}
 }
 
-type CompoundType struct {
+type CompoundDesc struct {
 	kind      NomsKind
-	elemTypes []Type
+	elemTypes []TypeRef
 }
 
-type EnumType struct {
-	name string
-	ids  []string
-}
-
-type NamedType struct {
-	name string
-}
-
-type UnionType struct {
-	choices []Field
-}
-
-type Field struct {
-	name string
-	t    Type
-}
-
-type StructType struct {
-	name   string
-	fields []Field
-	union  UnionType
-}
-
-func (e StructType) Kind() NomsKind {
-	return StructKind
-}
-
-func (s StructType) Describe() (out string) {
-	out = fmt.Sprintf("Custom type %s\n", s.name)
-	if len(s.union.choices) != 0 {
-		out += fmt.Sprintf("  anon %s\n", s.union.Describe())
-	}
-	for _, f := range s.fields {
-		out += fmt.Sprintf("  %s: %s\n", f.name, f.t.Describe())
-	}
-	return
-}
-
-func (e EnumType) Kind() NomsKind {
-	return EnumKind
-}
-
-func (e EnumType) Describe() string {
-	return "Enum: " + strings.Join(e.ids, ", ")
-}
-
-func (n NamedType) Kind() NomsKind {
-	return NamedKind
-}
-
-func (n NamedType) Describe() string {
-	return "Struct " + n.name
-}
-
-func (n UnionType) Kind() NomsKind {
-	return UnionKind
-}
-
-func (u UnionType) Describe() (out string) {
-	out = "Union of {\n"
-	for _, c := range u.choices {
-		out += fmt.Sprintf("  %s, %s\n", c.name, c.t.Describe())
-	}
-	return out + "  }"
-}
-
-func (c CompoundType) Kind() NomsKind {
+func (c CompoundDesc) Kind() NomsKind {
 	return c.kind
 }
 
-func (c CompoundType) Describe() string {
+func (c CompoundDesc) Describe() string {
 	descElems := func() (out string) {
 		for _, e := range c.elemTypes {
-			out += fmt.Sprintf("%v (%T); ", e, e)
+			out += fmt.Sprintf("%v (%T); ", e, e.Desc)
 		}
 		return out
 	}
@@ -174,7 +179,117 @@ func (c CompoundType) Describe() string {
 	}
 }
 
-type Alias struct {
-	name   string
-	target string
+func MakeEnumTypeRef(n string, ids []string) TypeRef {
+	return TypeRef{Name: n, Desc: EnumDesc{ids}}
 }
+
+type EnumDesc struct {
+	ids []string
+}
+
+func (e EnumDesc) Kind() NomsKind {
+	return EnumKind
+}
+
+func (e EnumDesc) Describe() string {
+	return "Enum: " + strings.Join(e.ids, ", ")
+}
+
+func MakeUnionTypeRef(c []Field) TypeRef {
+	return TypeRef{Desc: UnionDesc{c}}
+}
+
+type UnionDesc struct {
+	choices []Field
+}
+
+func (n UnionDesc) Kind() NomsKind {
+	return UnionKind
+}
+
+func (u UnionDesc) Describe() (out string) {
+	out = "Union of {\n"
+	for _, c := range u.choices {
+		out += fmt.Sprintf("  %s, %s\n", c.Name, c.t.Explain())
+	}
+	return out + "  }"
+}
+
+type Field struct {
+	Name string
+	t    TypeRef
+}
+
+type StructDesc struct {
+	fields []Field
+	union  UnionDesc
+}
+
+func MakeStructTypeRef(n string, f []Field, u UnionDesc) TypeRef {
+	return TypeRef{Name: n, Desc: StructDesc{f, u}}
+}
+
+func (e StructDesc) Kind() NomsKind {
+	return StructKind
+}
+
+func (s StructDesc) Describe() (out string) {
+	out = ""
+	if len(s.union.choices) != 0 {
+		out += fmt.Sprintf("  anon %s\n", s.union.Describe())
+	}
+	for _, f := range s.fields {
+		out += fmt.Sprintf("  %s: %s\n", f.Name, f.t.Explain())
+	}
+	return
+}
+
+type alias struct {
+	Name   string
+	Target string
+}
+
+// I think these aren't the right appproach
+/*func resolvePackage(pkg *Package, lookup map[string]*TypeRef) {
+	d.Chk.NotNil(pkg)
+	for _, decl := range pkg.UsingDeclarations {
+		d.Exp.IsType(CompoundDesc{}, decl.Desc)
+		for i, e := range decl.Desc.(CompoundDesc).elemTypes {
+			if e.IsUnresolved() {
+				if tr, ok := lookup[e.Name]; ok {
+					decl.Desc.(CompoundDesc).elemTypes[i] = *tr
+				}
+			}
+		}
+	}
+	for _, named := range pkg.NamedTypes {
+		if named.Kind() == StructKind {
+			d.Exp.IsType(StructDesc{}, named.Desc)
+			resolveFields(named.Desc.(StructDesc).fields, lookup)
+			resolveFields(named.Desc.(StructDesc).union.choices, lookup)
+		}
+	}
+}
+
+func buildLookupTable(pkgs ...Package) map[string]*TypeRef {
+	out := map[string]*TypeRef{}
+	for _, pkg := range pkgs {
+		for i, n := range pkg.NamedTypes {
+			out[n.Name] = &pkg.NamedTypes[i]
+		}
+	}
+	return out
+}
+
+func resolveFields(fields []Field, lookup map[string]*TypeRef) {
+	for i, f := range fields {
+		if f.t.IsUnresolved() {
+			if tr, ok := lookup[f.t.Name]; ok {
+				fields[i] = Field{f.Name, *tr}
+			}
+		} else if f.t.Kind() == UnionKind {
+			d.Exp.IsType(UnionDesc{}, f.t.Desc)
+			resolveFields(f.t.Desc.(UnionDesc).choices, lookup)
+		}
+	}
+}*/
