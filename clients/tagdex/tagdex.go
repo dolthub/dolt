235a1e09b6abfe951ee1160583c7a1e98351ac05
Erik Arvidsson <erik.arvidsson@gmail.com>
2015-11-12 15:52:07 -0500

    Update tagdex to search a whole dataset again

--- clients/tagdex/tagdex.go
+++ clients/tagdex/tagdex.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"runtime"
 	"time"
 
 	"github.com/attic-labs/noms/d"
@@ -11,12 +13,13 @@ import (
 	"github.com/attic-labs/noms/dataset"
 	"github.com/attic-labs/noms/ref"
 	"github.com/attic-labs/noms/types"
+	"github.com/attic-labs/noms/walk"
 )
 
 var (
 	flags    = datas.NewFlags()
-	inputRef = flag.String("input-ref", "", "ref to root of a set of photos")
-	outputID = flag.String("output-ds", "", "dataset to store index in")
+	inputID  = flag.String("in", "", "dataset to find photos in")
+	outputID = flag.String("out", "", "dataset to store index in")
 )
 
 type targetRef interface {
@@ -24,50 +27,55 @@ type targetRef interface {
 }
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s -ldb=/path/to/db -in=flickr -out=tagdex\n\n", os.Args[0])
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
 	store, ok := flags.CreateDataStore()
-	if !ok || *inputRef == "" || *outputID == "" {
+	if !ok || *inputID == "" || *outputID == "" {
 		flag.Usage()
 		return
 	}
 	defer store.Close()
 
-	var photoSet SetOfRefOfRemotePhoto
-	if d.Try(func() {
-		r := ref.Parse(*inputRef)
-		photoSet = types.ReadValue(r, store).(SetOfRefOfRemotePhoto)
-	}) != nil {
-		log.Fatal("Invalid value for -input-ref: %s\n", *inputRef)
+	inputDS := dataset.NewDataset(store, *inputID)
+	commit, ok := inputDS.MaybeHead()
+	if !ok {
+		log.Fatalf("No dataset named %s", *inputID)
 	}
-
 	outputDS := dataset.NewDataset(store, *outputID)
 
 	out := NewMapOfStringToSetOfRefOfRemotePhoto()
 
 	t0 := time.Now()
-	numRefs := 0
+	numValues := 0
 	numPhotos := 0
 
-	photoSet.IterAll(func(r RefOfRemotePhoto) {
-		numRefs++
+	walk.AllP(commit.Value(), store, func(v types.Value) {
+		numValues++
+		if p, ok := v.(RemotePhoto); ok {
+			tags := p.Tags()
+			if !tags.Empty() {
+				numPhotos++
+				fmt.Println("Indexing", p.Ref())
 
-		p := r.TargetValue(store)
-		tags := p.Tags()
-
-		if !tags.Empty() {
-			numPhotos++
-			fmt.Println("Indexing", p.Ref())
-
-			tags.IterAll(func(item string) {
-				s, _ := out.MaybeGet(item)
-				out = out.Set(item, s.Insert(r))
-			})
+				r := NewRefOfRemotePhoto(p.Ref())
+				tags.IterAll(func(item string) {
+					s, _ := out.MaybeGet(item)
+					out = out.Set(item, s.Insert(r))
+				})
+			}
 		}
-	})
+
+		// AllP gives inconsistent results. BUG 604
+	}, 1)
 
 	_, ok = outputDS.Commit(out)
 	d.Exp.True(ok, "Could not commit due to conflicting edit")
 
-	fmt.Printf("Indexed %v photos from %v refs in %v\n", numPhotos, numRefs, time.Now().Sub(t0))
+	fmt.Printf("Indexed %v photos from %v values in %v\n", numPhotos, numValues, time.Now().Sub(t0))
 }
