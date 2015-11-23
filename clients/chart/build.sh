af23cc0dfe043f4d26fc2f228c9268e808f994d7
Erik Arvidsson <erik.arvidsson@gmail.com>
2015-11-23 13:01:12 -0500

    Chart too

--- clients/chart/build.sh
+++ clients/chart/build.sh
@@ -1,7 +1,10 @@
-SRC="node_modules/babel-regenerator-runtime/runtime.js main.js"
+SRC="node_modules/babel-regenerator-runtime/runtime.js src/main.js"
 OUT="out.js"
 
 export NODE_ENV=production
+
+cp node_modules/nvd3/build/nv.d3.min.css nv.d3.css
+
 node_modules/.bin/browserify \
     -p bundle-collapser/plugin \
     $SRC \
