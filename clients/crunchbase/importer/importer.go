3c29fdc0be0713f64fd0e3f51a9be09c05e31161
Aaron Boodman <aaron@aaronboodman.com>
2015-11-30 17:52:02 -0800

    Make it possible to run crunchbase importer under cron
    - fetch from a url rather than file
    - use http caching to fetch the file
    - compare hashes as a fallback for when caching fails us

--- clients/crunchbase/importer/importer.go
+++ clients/crunchbase/importer/importer.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/sha1"
 	"flag"
 	"fmt"
+	"hash"
+	"io"
+	"io/ioutil"
 	"os"
 	"strconv"
 	"time"
@@ -10,37 +14,76 @@ import (
 	"strings"
 
 	"github.com/attic-labs/noms/Godeps/_workspace/src/github.com/tealeg/xlsx"
+	"github.com/attic-labs/noms/clients/util"
 	"github.com/attic-labs/noms/d"
 	"github.com/attic-labs/noms/dataset"
+	"github.com/attic-labs/noms/ref"
 	"github.com/attic-labs/noms/types"
 )
 
-var date1904 bool = false
+var (
+	date1904 = false
+	dsFlags  = dataset.NewFlags()
+)
 
 func main() {
 	var ds *dataset.Dataset
 
 	flag.Usage = func() {
-		fmt.Println("Usage: crunchbase [options] file\n")
+		fmt.Println("Usage: crunchbase [options] url\n")
 		flag.PrintDefaults()
 	}
 
-	dsFlags := dataset.NewFlags()
 	flag.Parse()
 
+	httpClient := util.CachingHttpClient()
 	ds = dsFlags.CreateDataset()
-	path := flag.Arg(0)
-	if ds == nil || path == "" {
+	url := flag.Arg(0)
+	if httpClient == nil || ds == nil || url == "" {
 		flag.Usage()
 		return
 	}
 	defer ds.Close()
 
-	xlFile, err := xlsx.OpenFile(path)
+	fmt.Print("Fetching excel file - this can take a minute or so...")
+	resp, err := httpClient.Get(url)
+	d.Exp.NoError(err)
+	defer resp.Body.Close()
+
+	tempFile, err := ioutil.TempFile(os.TempDir(), "")
+	defer tempFile.Close()
+	d.Chk.NoError(err)
+
+	h := sha1.New()
+	_, err = io.Copy(io.MultiWriter(h, tempFile), resp.Body)
+	d.Chk.NoError(err)
+
+	companiesRef := getExistingCompaniesRef(*ds, h)
+	if !companiesRef.IsEmpty() {
+		fmt.Println("\rExcel file hasn't changed since last run, nothing to do.")
+	} else {
+		companiesRef = importCompanies(*ds, tempFile.Name())
+	}
+
+	// Commit ref of the companiesRef list
+	_, ok := ds.Commit(ImportDef{
+		ref.FromHash(h).String(),
+		DateDef{time.Now().Format(time.RFC3339)},
+		companiesRef,
+	}.New())
+	d.Exp.True(ok, "Could not commit due to conflicting edit")
+}
+
+func importCompanies(ds dataset.Dataset, fileName string) ref.Ref {
+	fmt.Print("\rOpening excel file - this can take a minute or so...")
+
+	xlFile, err := xlsx.OpenFile(fileName)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(-1)
 	}
+
+	fmt.Print("\rImporting...")
 
 	date1904 = xlFile.Date1904
 
@@ -61,6 +104,7 @@ func main() {
 	companyRefs := NewMapOfStringToRefOfCompany()
 	companySheet := xlFile.Sheet["Companies"]
 	for i, row := range companySheet.Rows {
+		fmt.Printf("\rImporting %d of %d rounds... (%.2f%%)", i, len(companySheet.Rows), float64(i)/float64(len(companySheet.Rows))*float64(100))
 		if i != 0 {
 			company := NewCompanyFromRow(row)
 			permalink := company.Permalink()
@@ -78,14 +122,21 @@ func main() {
 		}
 	}
 
+	fmt.Printf("\r### imported %d companies with %d rounds\n", companyRefs.Len(), numRounds)
+
 	// Write the list of companyRefs
-	companiesRef := types.WriteValue(companyRefs, ds.Store())
+	return types.WriteValue(companyRefs, ds.Store())
+}
 
-	// Commit ref of the companiesRef list
-	_, ok := ds.Commit(types.NewRef(companiesRef))
-	d.Exp.True(ok, "Could not commit due to conflicting edit")
-
-	fmt.Printf("### imported %d companies with %d rounds\n", companyRefs.Len(), numRounds)
+func getExistingCompaniesRef(ds dataset.Dataset, h hash.Hash) ref.Ref {
+	if head, ok := ds.MaybeHead(); ok {
+		if imp, ok := head.Value().(Import); ok {
+			if imp.FileSHA1() == ref.FromHash(h).String() {
+				return imp.Companies().TargetRef()
+			}
+		}
+	}
+	return ref.Ref{}
 }
 
 func NewCompanyFromRow(row *xlsx.Row) Company {
