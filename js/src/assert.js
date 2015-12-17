de0632be77f79be2b5ea219b92997041c278f906
Erik Arvidsson <erik.arvidsson@gmail.com>
2015-12-17 11:43:48 -0500

    JS: Strip invariant calls in production
    Fixes #742

--- js/src/assert.js
+++ js/src/assert.js
@@ -1,9 +1,8 @@
 // @flow
 
-export function invariant(exp: boolean, message: string = 'Invariant violated') {
-  if (!exp) {
-    throw new Error(message);
-  }
+export function invariant(exp: any, message: string = 'Invariant violated') {
+  if (process.env.NODE_ENV === 'production') return;
+  if (!exp) throw new Error(message);
 }
 
 export function notNull<T>(v: ?T): T {
