ec3163c7f8f9adb7d03a4a561e0280835abe842f
Chris Masone <cmasone@atticlabs.io>
2016-03-10 15:42:35 -0800

    Some small fixes in chunks/
    Mostly 'go vet' complaints.

--- chunks/http_store.go
+++ chunks/http_store.go
@@ -46,8 +46,8 @@ type HTTPStore struct {
 	hasQueue      chan hasRequest
 	writeQueue    chan Chunk
 	finishedChan  chan struct{}
-	wg            *sync.WaitGroup
-	wgFinished    *sync.WaitGroup
+	requestWg     *sync.WaitGroup
+	workerWg      *sync.WaitGroup
 	unwrittenPuts *unwrittenPutCache
 }
 
@@ -64,8 +64,8 @@ func NewHTTPStore(baseURL, auth string) *HTTPStore {
 		hasQueue:      make(chan hasRequest, hasBufferSize),
 		writeQueue:    make(chan Chunk, writeBufferSize),
 		finishedChan:  make(chan struct{}),
-		wg:            &sync.WaitGroup{},
-		wgFinished:    &sync.WaitGroup{},
+		requestWg:     &sync.WaitGroup{},
+		workerWg:      &sync.WaitGroup{},
 		unwrittenPuts: newUnwrittenPutCache(),
 	}
 
@@ -76,124 +76,124 @@ func NewHTTPStore(baseURL, auth string) *HTTPStore {
 	return client
 }
 
-func (c *HTTPStore) Host() *url.URL {
-	return &url.URL{Host: c.host.Host, Scheme: c.host.Scheme}
+func (h *HTTPStore) Host() *url.URL {
+	return &url.URL{Host: h.host.Host, Scheme: h.host.Scheme}
 }
 
-func (c *HTTPStore) Get(r ref.Ref) Chunk {
-	pending := c.unwrittenPuts.Get(r)
+func (h *HTTPStore) Get(r ref.Ref) Chunk {
+	pending := h.unwrittenPuts.Get(r)
 	if !pending.IsEmpty() {
 		return pending
 	}
 
 	ch := make(chan Chunk)
-	c.wg.Add(1)
-	c.getQueue <- getRequest{r, ch}
+	h.requestWg.Add(1)
+	h.getQueue <- getRequest{r, ch}
 	return <-ch
 }
 
-func (c *HTTPStore) sendReadRequests(req getRequest) {
+func (h *HTTPStore) sendReadRequests(req getRequest) {
 	batch := getBatch{}
 	refs := map[ref.Ref]bool{}
 
 	addReq := func(req getRequest) {
 		batch[req.r] = append(batch[req.r], req.ch)
 		refs[req.r] = true
-		c.wg.Done()
+		h.requestWg.Done()
 	}
 
 	addReq(req)
 	for done := false; !done; {
 		select {
-		case req := <-c.getQueue:
+		case req := <-h.getQueue:
 			addReq(req)
 		default:
 			done = true
 		}
 	}
-	c.getRefs(refs, &batch)
+	h.getRefs(refs, &batch)
 	batch.Close()
 }
 
-func (c *HTTPStore) Has(ref ref.Ref) bool {
-	pending := c.unwrittenPuts.Get(ref)
+func (h *HTTPStore) Has(ref ref.Ref) bool {
+	pending := h.unwrittenPuts.Get(ref)
 	if !pending.IsEmpty() {
 		return true
 	}
 
 	ch := make(chan bool)
-	c.wg.Add(1)
-	c.hasQueue <- hasRequest{ref, ch}
+	h.requestWg.Add(1)
+	h.hasQueue <- hasRequest{ref, ch}
 	return <-ch
 }
 
-func (c *HTTPStore) sendHasRequests(req hasRequest) {
+func (h *HTTPStore) sendHasRequests(req hasRequest) {
 	batch := hasBatch{}
 	refs := map[ref.Ref]bool{}
 
 	addReq := func(req hasRequest) {
 		batch[req.r] = append(batch[req.r], req.ch)
 		refs[req.r] = true
-		c.wg.Done()
+		h.requestWg.Done()
 	}
 
 	addReq(req)
 	for done := false; !done; {
 		select {
-		case req := <-c.hasQueue:
+		case req := <-h.hasQueue:
 			addReq(req)
 		default:
 			done = true
 		}
 	}
-	c.getHasRefs(refs, batch)
+	h.getHasRefs(refs, batch)
 }
 
-func (c *HTTPStore) Put(chunk Chunk) {
-	if !c.unwrittenPuts.Add(chunk) {
+func (h *HTTPStore) Put(chunk Chunk) {
+	if !h.unwrittenPuts.Add(chunk) {
 		return
 	}
 
-	c.wg.Add(1)
-	c.writeQueue <- chunk
+	h.requestWg.Add(1)
+	h.writeQueue <- chunk
 }
 
-func (c *HTTPStore) sendWriteRequests(chunk Chunk) {
+func (h *HTTPStore) sendWriteRequests(chunk Chunk) {
 	chunks := []Chunk{}
 
 	chunks = append(chunks, chunk)
 	for done := false; !done; {
 		select {
-		case chunk := <-c.writeQueue:
+		case chunk := <-h.writeQueue:
 			chunks = append(chunks, chunk)
 		default:
 			done = true
 		}
 	}
 
-	c.wg.Add(-len(chunks))
-	c.postRefs(chunks)
+	h.requestWg.Add(-len(chunks))
+	h.postRefs(chunks)
 }
 
-func (c *HTTPStore) batchRequests() {
-	c.wgFinished.Add(1)
-	defer c.wgFinished.Done()
+func (h *HTTPStore) batchRequests() {
+	h.workerWg.Add(1)
+	defer h.workerWg.Done()
 
 	for done := false; !done; {
 		select {
-		case req := <-c.hasQueue:
-			c.sendHasRequests(req)
-		case req := <-c.getQueue:
-			c.sendReadRequests(req)
-		case chunk := <-c.writeQueue:
-			c.sendWriteRequests(chunk)
-		case <-c.finishedChan:
+		case req := <-h.hasQueue:
+			h.sendHasRequests(req)
+		case req := <-h.getQueue:
+			h.sendReadRequests(req)
+		case chunk := <-h.writeQueue:
+			h.sendWriteRequests(chunk)
+		case <-h.finishedChan:
 			done = true
 		}
 	}
 }
 
-func (c *HTTPStore) newRequest(method, url string, body io.Reader, header http.Header) *http.Request {
+func (h *HTTPStore) newRequest(method, url string, body io.Reader, header http.Header) *http.Request {
 	req, err := http.NewRequest(method, url, body)
 	d.Chk.NoError(err)
 	for k, vals := range header {
@@ -201,13 +201,13 @@ func (c *HTTPStore) newRequest(method, url string, body io.Reader, header http.H
 			req.Header.Add(k, v)
 		}
 	}
-	if c.auth != "" {
-		req.Header.Set("Authorization", c.auth)
+	if h.auth != "" {
+		req.Header.Set("Authorization", h.auth)
 	}
 	return req
 }
 
-func (c *HTTPStore) postRefs(chs []Chunk) {
+func (h *HTTPStore) postRefs(chs []Chunk) {
 	body := &bytes.Buffer{}
 	gw := gzip.NewWriter(body)
 	sz := NewSerializer(gw)
@@ -217,24 +217,24 @@ func (c *HTTPStore) postRefs(chs []Chunk) {
 	sz.Close()
 	gw.Close()
 
-	url := *c.host
-	url.Path = httprouter.CleanPath(c.host.Path + constants.PostRefsPath)
-	req := c.newRequest("POST", url.String(), body, http.Header{
+	url := *h.host
+	url.Path = httprouter.CleanPath(h.host.Path + constants.PostRefsPath)
+	req := h.newRequest("POST", url.String(), body, http.Header{
 		"Content-Encoding": {"gzip"},
 		"Content-Type":     {"application/octet-stream"},
 	})
 
-	res, err := c.httpClient.Do(req)
+	res, err := h.httpClient.Do(req)
 	d.Chk.NoError(err)
 
 	d.Chk.Equal(res.StatusCode, http.StatusCreated, "Unexpected response: %s", http.StatusText(res.StatusCode))
 	closeResponse(res)
-	c.unwrittenPuts.Clear(chs)
+	h.unwrittenPuts.Clear(chs)
 }
 
-func (c *HTTPStore) requestRef(r ref.Ref, method string, body io.Reader) *http.Response {
-	url := *c.host
-	url.Path = httprouter.CleanPath(c.host.Path + constants.RefPath)
+func (h *HTTPStore) requestRef(r ref.Ref, method string, body io.Reader) *http.Response {
+	url := *h.host
+	url.Path = httprouter.CleanPath(h.host.Path + constants.RefPath)
 	if !r.IsEmpty() {
 		url.Path = path.Join(url.Path, r.String())
 	}
@@ -243,28 +243,28 @@ func (c *HTTPStore) requestRef(r ref.Ref, method string, body io.Reader) *http.R
 	if body != nil {
 		header.Set("Content-Type", "application/octet-stream")
 	}
-	req := c.newRequest(method, url.String(), body, header)
+	req := h.newRequest(method, url.String(), body, header)
 
-	res, err := c.httpClient.Do(req)
+	res, err := h.httpClient.Do(req)
 	d.Chk.NoError(err)
 	return res
 }
 
-func (c *HTTPStore) getHasRefs(refs map[ref.Ref]bool, reqs hasBatch) {
+func (h *HTTPStore) getHasRefs(refs map[ref.Ref]bool, reqs hasBatch) {
 	// POST http://<host>/getHasRefs/. Post body: ref=sha1---&ref=sha1---& Response will be text of lines containing "|ref| |bool|"
-	u := *c.host
-	u.Path = httprouter.CleanPath(c.host.Path + constants.GetHasPath)
+	u := *h.host
+	u.Path = httprouter.CleanPath(h.host.Path + constants.GetHasPath)
 	values := &url.Values{}
 	for r, _ := range refs {
 		values.Add("ref", r.String())
 	}
 
-	req := c.newRequest("POST", u.String(), strings.NewReader(values.Encode()), http.Header{
+	req := h.newRequest("POST", u.String(), strings.NewReader(values.Encode()), http.Header{
 		"Accept-Encoding": {"gzip"},
 		"Content-Type":    {"application/x-www-form-urlencoded"},
 	})
 
-	res, err := c.httpClient.Do(req)
+	res, err := h.httpClient.Do(req)
 	d.Chk.NoError(err)
 	defer closeResponse(res)
 	d.Chk.Equal(http.StatusOK, res.StatusCode, "Unexpected response: %s", http.StatusText(res.StatusCode))
@@ -289,21 +289,21 @@ func (c *HTTPStore) getHasRefs(refs map[ref.Ref]bool, reqs hasBatch) {
 	}
 }
 
-func (c *HTTPStore) getRefs(refs map[ref.Ref]bool, cs ChunkSink) {
+func (h *HTTPStore) getRefs(refs map[ref.Ref]bool, cs ChunkSink) {
 	// POST http://<host>/getRefs/. Post body: ref=sha1---&ref=sha1---& Response will be chunk data if present, 404 if absent.
-	u := *c.host
-	u.Path = httprouter.CleanPath(c.host.Path + constants.GetRefsPath)
+	u := *h.host
+	u.Path = httprouter.CleanPath(h.host.Path + constants.GetRefsPath)
 	values := &url.Values{}
 	for r, _ := range refs {
 		values.Add("ref", r.String())
 	}
 
-	req := c.newRequest("POST", u.String(), strings.NewReader(values.Encode()), http.Header{
+	req := h.newRequest("POST", u.String(), strings.NewReader(values.Encode()), http.Header{
 		"Accept-Encoding": {"gzip"},
 		"Content-Type":    {"application/x-www-form-urlencoded"},
 	})
 
-	res, err := c.httpClient.Do(req)
+	res, err := h.httpClient.Do(req)
 	d.Chk.NoError(err)
 	defer closeResponse(res)
 	d.Chk.Equal(http.StatusOK, res.StatusCode, "Unexpected response: %s", http.StatusText(res.StatusCode))
@@ -320,9 +320,9 @@ func (c *HTTPStore) getRefs(refs map[ref.Ref]bool, cs ChunkSink) {
 	Deserialize(reader, cs, rl)
 }
 
-func (c *HTTPStore) Root() ref.Ref {
+func (h *HTTPStore) Root() ref.Ref {
 	// GET http://<host>/root. Response will be ref of root.
-	res := c.requestRoot("GET", ref.Ref{}, ref.Ref{})
+	res := h.requestRoot("GET", ref.Ref{}, ref.Ref{})
 	defer closeResponse(res)
 
 	d.Chk.Equal(http.StatusOK, res.StatusCode, "Unexpected response: %s", http.StatusText(res.StatusCode))
@@ -331,20 +331,20 @@ func (c *HTTPStore) Root() ref.Ref {
 	return ref.Parse(string(data))
 }
 
-func (c *HTTPStore) UpdateRoot(current, last ref.Ref) bool {
+func (h *HTTPStore) UpdateRoot(current, last ref.Ref) bool {
 	// POST http://<host>root?current=<ref>&last=<ref>. Response will be 200 on success, 409 if current is outdated.
-	c.wg.Wait()
+	h.requestWg.Wait()
 
-	res := c.requestRoot("POST", current, last)
+	res := h.requestRoot("POST", current, last)
 	defer closeResponse(res)
 
 	d.Chk.True(res.StatusCode == http.StatusOK || res.StatusCode == http.StatusConflict, "Unexpected response: %s", http.StatusText(res.StatusCode))
 	return res.StatusCode == http.StatusOK
 }
 
-func (c *HTTPStore) requestRoot(method string, current, last ref.Ref) *http.Response {
-	u := *c.host
-	u.Path = httprouter.CleanPath(c.host.Path + constants.RootPath)
+func (h *HTTPStore) requestRoot(method string, current, last ref.Ref) *http.Response {
+	u := *h.host
+	u.Path = httprouter.CleanPath(h.host.Path + constants.RootPath)
 	if method == "POST" {
 		d.Exp.False(current.IsEmpty())
 		params := url.Values{}
@@ -353,23 +353,23 @@ func (c *HTTPStore) requestRoot(method string, current, last ref.Ref) *http.Resp
 		u.RawQuery = params.Encode()
 	}
 
-	req := c.newRequest(method, u.String(), nil, nil)
+	req := h.newRequest(method, u.String(), nil, nil)
 
-	res, err := c.httpClient.Do(req)
+	res, err := h.httpClient.Do(req)
 	d.Chk.NoError(err)
 
 	return res
 }
 
-func (c *HTTPStore) Close() error {
-	c.wg.Wait()
+func (h *HTTPStore) Close() error {
+	h.requestWg.Wait()
 
-	close(c.finishedChan)
-	c.wgFinished.Wait()
+	close(h.finishedChan)
+	h.workerWg.Wait()
 
-	close(c.hasQueue)
-	close(c.getQueue)
-	close(c.writeQueue)
+	close(h.hasQueue)
+	close(h.getQueue)
+	close(h.writeQueue)
 	return nil
 }
 
