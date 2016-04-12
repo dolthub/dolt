fd10c789d68b667d34b8c007fe496b02457b7567
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-04-11 17:36:31 -0700

    Auto deploy JS SDK (#1214)
    Use rev-list instead?
    
    Issue #1200

--- tools/publish-js-sdk.py
+++ tools/publish-js-sdk.py
@@ -7,11 +7,14 @@ def main():
   os.chdir('js')
   deployed_version = LooseVersion(
     subprocess.check_output(['npm', 'info', '@attic/noms', 'version']).strip())
-  new_version = LooseVersion(
-    subprocess.check_output(['npm', 'info', '.', 'version']).strip())
+  changes = subprocess.check_output(
+    ['git', 'rev-list', '--no-merges', '--count', 'HEAD', '--', './']).strip()
+  new_version = LooseVersion('%s.0.0' % changes)
   print 'Old version: %s, New version: %s' % (deployed_version, new_version)
   if new_version > deployed_version:
-    subprocess.check_call(['npm', 'publish'])
+    # subprocess.check_call(['npm', 'version', str(new_version)])
+    # subprocess.check_call(['npm', 'publish'])
+    print 'Not publishing until I know it works'
 
 if __name__ == '__main__':
   main()
