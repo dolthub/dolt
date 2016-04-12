3b76423cd0663c41ea55565ee5099a7570557e0f
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-04-11 18:05:00 -0700

    Auto deploy JS (#1215)
    Trying to use the version in package.json again
    
    Issue #1200

--- tools/publish-js-sdk.py
+++ tools/publish-js-sdk.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 
-import os, subprocess
+import os, subprocess, json
 from distutils.version import LooseVersion
 
 def main():
   os.chdir('js')
   deployed_version = LooseVersion(
     subprocess.check_output(['npm', 'info', '@attic/noms', 'version']).strip())
-  changes = subprocess.check_output(
-    ['git', 'rev-list', '--no-merges', '--count', 'HEAD', '--', './']).strip()
-  new_version = LooseVersion('%s.0.0' % changes)
+  with open('package.json') as pkg:
+    data = json.load(pkg)
+  new_version = LooseVersion(data['version'])
   print 'Old version: %s, New version: %s' % (deployed_version, new_version)
   if new_version > deployed_version:
-    # subprocess.check_call(['npm', 'version', str(new_version)])
-    # subprocess.check_call(['npm', 'publish'])
-    print 'Not publishing until I know it works'
+    subprocess.check_call(['npm', 'publish'])
 
 if __name__ == '__main__':
   main()
