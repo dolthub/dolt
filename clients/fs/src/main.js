cbe97a054a5cdebce4855955a4f4412da0c5dd44
Mike Gray <mike@mikegray.org>
2016-04-22 16:04:52 -0700

    using existing format-file-size; fixing nits with progress output

--- clients/fs/src/main.js
+++ clients/fs/src/main.js
@@ -3,6 +3,7 @@
 import fs from 'mz/fs';
 import path from 'path';
 import argv from 'yargs';
+import formatFileSize from './format-file-size.js';
 import {
   newMapOfStringToRefOfDirectoryEntry,
   Directory,
@@ -125,24 +126,10 @@ function processBlob(p: string, store: DataStore): Promise<RefValue<NomsBlob>> {
   });
 }
 
-function humanFileSize(bytes, si) {
-  const thresh = si ? 1000 : 1024;
-  if (Math.abs(bytes) < thresh) {
-    return bytes + ' B';
-  }
-  const units = si
-    ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
-    : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
-  let u = -1;
-  do {
-    bytes /= thresh;
-    ++u;
-  } while (Math.abs(bytes) >= thresh && u < units.length - 1);
-  return bytes.toFixed(1) + ' ' + units[u];
-}
-
 function updateProgress() {
-  process.stdout.write(`\r${numFilesComplete} of ${numFilesFound} entries processed... ${humanFileSize(sizeFilesComplete,true)} of ${humanFileSize(sizeFilesFound,true)} contents processed`);  // eslint-disable-line max-len
+  process.stdout.write(`\r${numFilesComplete} of ${numFilesFound} entries \
+(${formatFileSize(sizeFilesComplete)} of ${formatFileSize(sizeFilesFound)}) \
+processed...`);
 }
 
 function parseArgs() {
