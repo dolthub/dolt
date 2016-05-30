01eb7023107496dd222b86b277016039dfdd773d
Aaron Boodman <aaron@aaronboodman.com>
2016-05-30 14:18:07 -0700

    Licensify chunks/

--- chunks/memory_root_tracker.go
+++ chunks/memory_root_tracker.go
@@ -1,3 +1,7 @@
+// Copyright 2016 The Noms Authors. All rights reserved.
+// Licensed under the Apache License, version 2.0:
+// http://www.apache.org/licenses/LICENSE-2.0
+
 package chunks
 
 import "github.com/attic-labs/noms/hash"
