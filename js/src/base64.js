e5f288b752081f9786b2749b15269ac4d2d4a9b6
Aaron Boodman <aaron@aaronboodman.com>
2016-05-30 14:18:38 -0700

    Licensify js/

--- js/src/base64.js
+++ js/src/base64.js
@@ -1,3 +1,7 @@
+// Copyright 2016 The Noms Authors. All rights reserved.
+// Licensed under the Apache License, version 2.0:
+// http://www.apache.org/licenses/LICENSE-2.0
+
 // @flow
 
 // Based on https://github.com/niklasvh/base64-arraybuffer
--- js/src/browser/sha1.js
+++ js/src/browser/sha1.js
@@ -1,3 +1,7 @@
+// Copyright 2016 The Noms Authors. All rights reserved.
+// Licensed under the Apache License, version 2.0:
+// http://www.apache.org/licenses/LICENSE-2.0
+
 // @flow
 
 // This is the browser version. The Node.js version is in ../sha1.js.
