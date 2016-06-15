8d112992c66aa4f44afd873a91b3d1c3ec704cd7
Dan Willhite <willhite@gmail.com>
2016-06-15 13:55:14 -0700

    Move demo-server from it's own project into noms.

---
+++ samples/go/demo-server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/attic-labs/noms/go/chunks"
+)
+
+var (
+	portFlag = flag.Int("port", 8000, "port to listen on")
+	ldbDir   = flag.String("ldb-dir", "", "directory for ldb database")
+	authKeyFlag   = flag.String("authkey", "", "directory for ldb database")
+)
+
+func usage() {
+	fmt.Println("Usage: demo-server -authkey <authkey> [options]")
+	flag.PrintDefaults()
+}
+
+func main() {
+	chunks.RegisterLevelDBFlags()
+	dynFlags := chunks.DynamoFlags("")
+
+	flag.Usage = usage
+	flag.Parse()
+
+	if *portFlag == 0 || *authKeyFlag == "" {
+		usage()
+		return
+	}
+
+	var factory chunks.Factory
+	if factory = dynFlags.CreateFactory(); factory != nil {
+		fmt.Printf("Using dynamo ...\n")
+	} else if *ldbDir != "" {
+		factory = chunks.NewLevelDBStoreFactoryUseFlags(*ldbDir)
+		fmt.Printf("Using leveldb ...\n")
+	} else {
+		factory = chunks.NewMemoryStoreFactory()
+		fmt.Printf("Using mem ...\n")
+	}
+	defer factory.Shutter()
+
+    
+	startWebServer(factory, *authKeyFlag)
+}
---
+++ samples/go/demo-server/web_server_test.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/attic-labs/noms/go/chunks"
+	"github.com/attic-labs/noms/go/constants"
+	"github.com/attic-labs/noms/go/datas"
+	"github.com/attic-labs/noms/go/hash"
+	"github.com/attic-labs/noms/go/types"
+	"github.com/attic-labs/testify/assert"
+)
+
+var (
+	dbName = "/test/db"
+)
+
+func TestRoot(t *testing.T) {
+	assert := assert.New(t)
+
+	factory := chunks.NewMemoryStoreFactory()
+	defer factory.Shutter()
+
+	router = setupWebServer(factory)
+	defer func() { router = nil }()
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", dbName+constants.RootPath, nil)
+	router.ServeHTTP(w, r)
+	assert.Equal("sha1-0000000000000000000000000000000000000000", w.Body.String())
+
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("OPTIONS", dbName+constants.RootPath, nil)
+	r.Header.Add("Origin", "http://www.noms.io")
+	router.ServeHTTP(w, r)
+	assert.Equal(w.HeaderMap["Access-Control-Allow-Origin"][0], "http://www.noms.io")
+}
+
+func buildGetRefsRequestBody(hashes map[hash.Hash]struct{}) io.Reader {
+	values := &url.Values{}
+	for h := range hashes {
+		values.Add("ref", h.String())
+	}
+	return strings.NewReader(values.Encode())
+}
+
+func TestWriteValue(t *testing.T) {
+	assert := assert.New(t)
+	factory := chunks.NewMemoryStoreFactory()
+	defer factory.Shutter()
+
+	router = setupWebServer(factory)
+	defer func() { router = nil }()
+
+	testString := "Now, what?"
+	authKey = "anauthkeyvalue"
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", dbName+constants.RootPath, nil)
+	assert.NoError(err)
+	router.ServeHTTP(w, r)
+	lastRoot := w.Body
+	assert.Equal(http.StatusOK, w.Code)
+
+	tval := types.Bool(true)
+	wval := types.NewString(testString)
+	chunk1 := types.EncodeValue(tval, nil)
+	chunk2 := types.EncodeValue(wval, nil)
+	refList := types.NewList(types.NewRef(tval), types.NewRef(wval))
+	chunk3 := types.EncodeValue(refList, nil)
+
+	body := &bytes.Buffer{}
+	// we would use this func, but it's private so use next line instead: serializeHints(body, map[ref.Ref]struct{}{hint: struct{}{}})
+	err = binary.Write(body, binary.BigEndian, uint32(0))
+	assert.NoError(err)
+
+	sz := chunks.NewSerializer(body)
+	sz.Put(chunk1)
+	sz.Put(chunk2)
+	sz.Put(chunk3)
+	sz.Close()
+
+	w = httptest.NewRecorder()
+	r, err = http.NewRequest("POST", dbName+constants.WriteValuePath+"?access_token="+authKey, ioutil.NopCloser(body))
+	assert.NoError(err)
+	router.ServeHTTP(w, r)
+	assert.Equal(http.StatusCreated, w.Code)
+
+	w = httptest.NewRecorder()
+	args := fmt.Sprintf("&last=%s&current=%s", lastRoot, types.NewRef(refList).TargetHash())
+	r, _ = http.NewRequest("POST", dbName+constants.RootPath+"?access_token="+authKey+args, ioutil.NopCloser(body))
+	router.ServeHTTP(w, r)
+	assert.Equal(http.StatusOK, w.Code)
+
+	whash := wval.Hash()
+	hints := map[hash.Hash]struct{}{whash: struct{}{}}
+	rdr := buildGetRefsRequestBody(hints)
+	r, _ = http.NewRequest("POST", dbName+constants.GetRefsPath, rdr)
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	router.ServeHTTP(w, r)
+	assert.Equal(http.StatusOK, w.Code)
+
+	ms := chunks.NewMemoryStore()
+	chunks.Deserialize(w.Body, ms, nil)
+	v := types.DecodeValue(ms.Get(whash), datas.NewDatabase(ms))
+	assert.Equal(testString, v.(types.String).String())
+}
