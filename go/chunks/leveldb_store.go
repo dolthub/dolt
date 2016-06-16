2492c02c8d7fc45829079fe5182d9ab7caa10d85
Rafael Weinstein <rafael@atticlabs.io>
2016-06-15 17:12:20 -0700

    Turn off leveldb-snappy compression (#1833)

--- go/chunks/leveldb_store.go
+++ go/chunks/leveldb_store.go
@@ -130,7 +130,7 @@ func newBackingStore(dir string, maxFileHandles int, dumpStats bool) *internalLe
 	d.Exp.NotEmpty(dir)
 	d.Exp.NoError(os.MkdirAll(dir, 0700))
 	db, err := leveldb.OpenFile(dir, &opt.Options{
-		Compression:            opt.SnappyCompression,
+		Compression:            opt.NoCompression,
 		Filter:                 filter.NewBloomFilter(10), // 10 bits/key
 		OpenFilesCacheCapacity: maxFileHandles,
 		WriteBuffer:            1 << 24, // 16MiB,
