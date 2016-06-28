ba88abfa9b442bcffda655fb97a44c7fc885370f
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-06-28 13:04:24 -0700

    Remove nomdl parser (#1923)
    We are not using this any more...

--- go/nomdl/pkg/gen/generate_parser.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-"$GOPATH/bin/pigeon" "${1}" | "$GOPATH/bin/goimports" > "${1}".go
--- go/nomdl/pkg/rungen.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2016 The Noms Authors. All rights reserved.
-// Licensed under the Apache License, version 2.0:
-// http://www.apache.org/licenses/LICENSE-2.0
-
-package pkg
-
-//go:generate gen/generate_parser.sh grammar.peg
