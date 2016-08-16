3320518f8809f3f84d371892562067a588d5a9b7
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-08-16 11:58:57 -0700

    Fix shell script flags (#2377)

--- samples/go/hr/build_test_data.sh
+++ samples/go/hr/build_test_data.sh
@@ -4,6 +4,5 @@ if [ -d test-data ]; then
 	mv test-data test-data.bak
 fi
 
-./hr -ds test-data::hr add-person 7 "Aaron Boodman" "Chief Evangelism Officer"
-./hr -ds test-data::hr add-person 13 "Samuel Boodman" "VP, Culture"
-
+./hr --ds test-data::hr add-person 7 "Aaron Boodman" "Chief Evangelism Officer"
+./hr --ds test-data::hr add-person 13 "Samuel Boodman" "VP, Culture"
