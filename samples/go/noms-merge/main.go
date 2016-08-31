49ea5ec3c0212f205d9617e2b272507b7f04089a
cmasone-attic <cmasone@atticlabs.io>
2016-08-31 14:20:21 -0700

    Introduce noms-merge, a standalone noms tool for merging datasets (#2470)
    This is a first pass at exposing the new merge package to users.  The
    tool is very basic, and currently only works on datasets in the same
    database. It requires the 'parent' (i.e. a common ancestor of the two
    datasets being merged) to be provided by a commandline option; a
    follow-on patch will make the code discover this ancestor automatically.
    
    Toward #2445

---
+++ samples/go/noms-merge/main.go
@@ -0,0 +1,85 @@
+// Copyright 2016 Attic Labs, Inc. All rights reserved.
+// Licensed under the Apache License, version 2.0:
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/attic-labs/noms/go/d"
+	"github.com/attic-labs/noms/go/dataset"
+	"github.com/attic-labs/noms/go/merge"
+	"github.com/attic-labs/noms/go/spec"
+	"github.com/attic-labs/noms/go/types"
+	flag "github.com/juju/gnuflag"
+)
+
+var datasetRe = regexp.MustCompile("^" + dataset.DatasetRe.String() + "$")
+
+func main() {
+	if err := nomsMerge(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func nomsMerge() error {
+	outDSStr := flag.String("out-ds-name", "", "output dataset to write to - if empty, defaults to <right-ds-name>")
+	parentStr := flag.String("parent", "", "common ancestor of <left-ds-name> and <right-ds-name> (currently required; soon to be optional)")
+	flag.Usage = usage
+
+	return d.Unwrap(d.Try(func() {
+		flag.Parse(false)
+
+		if flag.NArg() == 0 {
+			flag.Usage()
+			d.PanicIfTrue(true, "")
+		}
+
+		d.PanicIfTrue(flag.NArg() != 3, "Incorrect number of arguments\n")
+
+		db, err := spec.GetDatabase(flag.Arg(0))
+		defer db.Close()
+		d.PanicIfError(err)
+
+		makeDS := func(dsName string) dataset.Dataset {
+			d.PanicIfTrue(!datasetRe.MatchString(dsName), "Invalid dataset %s, must match %s\n", dsName, dataset.DatasetRe.String())
+			return dataset.NewDataset(db, dsName)
+		}
+
+		leftDS := makeDS(flag.Arg(1))
+		rightDS := makeDS(flag.Arg(2))
+		parentDS := makeDS(*parentStr)
+
+		parent, ok := parentDS.MaybeHeadValue()
+		d.PanicIfTrue(!ok, "Parent dataset has no data\n")
+		left, ok := leftDS.MaybeHeadValue()
+		d.PanicIfTrue(!ok, "left dataset has no data\n")
+		right, ok := rightDS.MaybeHeadValue()
+		d.PanicIfTrue(!ok, "right dataset has no data\n")
+
+		outDS := rightDS
+		if *outDSStr != "" {
+			outDS = makeDS(*outDSStr)
+		}
+
+		merged, err := merge.ThreeWay(left, right, parent, db)
+		d.PanicIfError(err)
+
+		_, err = outDS.Commit(merged, dataset.CommitOptions{Parents: types.NewSet(leftDS.HeadRef(), rightDS.HeadRef())})
+		d.PanicIfError(err)
+	}))
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Attempts to merge the two datasets in the provided database and commit the merge to either <right-ds-name> or another dataset of your choice.\n\n")
+	fmt.Fprintf(os.Stderr, "Usage: %s [--out-ds-name=<name>] [--parent=<name>] <db-spec> <left-ds-name> <right-ds-name>\n\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "  <db-spec>       : database in which named datasets live\n")
+	fmt.Fprintf(os.Stderr, "  <left-ds-name>  : name of a dataset descending from <parent>\n")
+	fmt.Fprintf(os.Stderr, "  <right-ds-name> : name of another dataset descending from <parent>\n\n")
+	fmt.Fprintf(os.Stderr, "Flags:\n\n")
+	flag.PrintDefaults()
+}
---
+++ samples/go/noms-merge/main_test.go
@@ -0,0 +1,81 @@
+// Copyright 2016 Attic Labs, Inc. All rights reserved.
+// Licensed under the Apache License, version 2.0:
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package main
+
+import (
+	"testing"
+
+	"github.com/attic-labs/noms/go/spec"
+	"github.com/attic-labs/noms/go/types"
+	"github.com/attic-labs/noms/go/util/clienttest"
+	"github.com/attic-labs/testify/suite"
+)
+
+func TestBasics(t *testing.T) {
+	suite.Run(t, &testSuite{})
+}
+
+type testSuite struct {
+	clienttest.ClientTestSuite
+}
+
+func (s *testSuite) TestWin() {
+	prep := func(name string, data types.StructData) {
+		ds, _ := spec.GetDataset(spec.CreateValueSpecString("ldb", s.LdbDir, name))
+		defer ds.Database().Close()
+		ds.CommitValue(types.NewStruct("", data))
+	}
+
+	p := "parent"
+	prep(p, types.StructData{
+		"num": types.Number(42),
+		"str": types.String("foobar"),
+		"lst": types.NewList(types.Number(1), types.String("foo")),
+		"map": types.NewMap(types.Number(1), types.String("foo"),
+			types.String("foo"), types.Number(1)),
+	})
+
+	l := "left"
+	prep(l, types.StructData{
+		"num": types.Number(42),
+		"str": types.String("foobaz"),
+		"lst": types.NewList(types.Number(1), types.String("foo")),
+		"map": types.NewMap(types.Number(1), types.String("foo"),
+			types.String("foo"), types.Number(1)),
+	})
+
+	r := "right"
+	prep(r, types.StructData{
+		"num": types.Number(42),
+		"str": types.String("foobar"),
+		"lst": types.NewList(types.Number(1), types.String("foo")),
+		"map": types.NewMap(types.Number(1), types.String("foo"),
+			types.String("foo"), types.Number(1), types.Number(2), types.String("bar")),
+	})
+
+	expected := types.NewStruct("", types.StructData{
+		"num": types.Number(42),
+		"str": types.String("foobaz"),
+		"lst": types.NewList(types.Number(1), types.String("foo")),
+		"map": types.NewMap(types.Number(1), types.String("foo"),
+			types.String("foo"), types.Number(1), types.Number(2), types.String("bar")),
+	})
+
+	var mainErr error
+	stdout, stderr := s.Run(func() { mainErr = nomsMerge() }, []string{"--parent=" + p, s.LdbDir, l, r})
+	if s.NoError(mainErr, "%s", mainErr) {
+		s.Equal("", stdout)
+		s.Equal("", stderr)
+
+		ds, err := spec.GetDataset(spec.CreateValueSpecString("ldb", s.LdbDir, r))
+		if s.NoError(err) {
+			merged := ds.HeadValue()
+			s.True(expected.Equals(merged), "%s != %s", types.EncodedValue(expected), types.EncodedValue(merged))
+		}
+		defer ds.Database().Close()
+	}
+}
+
+// TODO failure tests
