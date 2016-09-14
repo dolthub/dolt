68b3de35bfe86dad217ab047c0a4b99d598c68c5
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-09-14 10:59:22 -0700

    More jsdoc for value (#2555)
    Towards #2297

--- js/noms/src/primitives.js
+++ js/noms/src/primitives.js
@@ -4,11 +4,17 @@
 // Licensed under the Apache License, version 2.0:
 // http://www.apache.org/licenses/LICENSE-2.0
 
+/**
+ * The primitive types supported by Noms.
+ */
 export type primitive =
     number |
     string |
     boolean;
 
+/**
+ * Whether a value is considered to be a primitive Noms value.
+ */
 export function isPrimitive(v: any): boolean {
   switch (typeof v) {
     case 'string':
--- js/noms/src/value.js
+++ js/noms/src/value.js
@@ -10,6 +10,9 @@ import {ensureHash} from './get-hash.js';
 import type {Type} from './type.js';
 import type Ref from './ref.js';
 
+/**
+ * ValueBase is the base class for non primitive Noms values.
+ */
 export class ValueBase {
   _hash: ?Hash;
 
@@ -17,22 +20,40 @@ export class ValueBase {
     init(this);
   }
 
+  /**
+   * The Noms type of the Noms value.
+   */
   get type(): Type<any> {
     throw new Error('abstract');
   }
 
+  /**
+   * The hash of a Noms value. All Noms values have a unique hash and if two values have the same
+   * hash they must be equal.
+   */
   get hash(): Hash {
     return this._hash = ensureHash(this._hash, this);
   }
 
+  /**
+   * This represents the refs to the underlying chunks. If this value is a collection that has been
+   * chunked then this will return the refs of th sub trees of the prolly-tree.
+   */
   get chunks(): Array<Ref<any>> {
     return [];
   }
 }
 
+/**
+ * Value is the union of types supported in Noms.
+ */
 type Value = primitive | ValueBase;
 export type {Value as default};
 
+/**
+ * This returns the refs to the underlying chunks. If this value is a collection that has been
+ * chunked then this will return the refs of th sub trees of the prolly-tree.
+ */
 export function getChunksOfValue(v: Value): Array<Ref<any>> {
   if (v instanceof ValueBase) {
     return v.chunks;
