8320cb6d289ac898163c2572ac314f16bf43075c
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-10-27 13:28:26 -0700

    Use the eslint-config-noms instead (#2767)

--- js/noms/.eslintrc.js
+++ js/noms/.eslintrc.js
@@ -2,4 +2,6 @@
 // Licensed under the Apache License, version 2.0:
 // http://www.apache.org/licenses/LICENSE-2.0
 
-module.exports = require('@attic/eslintrc');
+module.exports = {
+  extends: 'noms',
+};
--- js/perf/.eslintrc.js
+++ js/perf/.eslintrc.js
@@ -2,8 +2,12 @@
 // Licensed under the Apache License, version 2.0:
 // http://www.apache.org/licenses/LICENSE-2.0
 
-module.exports = require('@attic/eslintrc');
-// Allow console
-module.exports.rules['no-console'] = 0;
-// Used to distinguish between user errors and exceptions.
-module.exports.rules['no-throw-literal'] = 0;
+module.exports = {
+  rules: {
+    // Allow console
+    'no-console': 0,
+    // Used to distinguish between user errors and exceptions.
+    'no-throw-literal': 0,
+  },
+  extends: 'noms',
+};
