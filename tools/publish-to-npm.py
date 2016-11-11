13a188cc89a307f25bee7e3ddd13ee0a25026eca
Ben Kalman <kalman@users.noreply.github.com>
2016-11-10 17:10:56 -0800

    Change publish-to-npm to work relative to GOPATH not pwd (#2821)
    Jenkins is now running from a different pwd. This changes fixes that and
    makes it immune to future changes.

--- tools/publish-to-npm.py
+++ tools/publish-to-npm.py
@@ -4,21 +4,31 @@
 # Licensed under the Apache License, version 2.0:
 # http://www.apache.org/licenses/LICENSE-2.0
 
-import os, subprocess, json
 from distutils.version import LooseVersion
+import json
+import os
+import subprocess
 
 def main():
-  os.chdir(os.path.join('js', 'noms'))
-  deployed_version = LooseVersion(
-    subprocess.check_output(['npm', 'info', '@attic/noms', 'version']).strip())
-  with open('package.json') as pkg:
-    data = json.load(pkg)
-  new_version = LooseVersion(data['version'])
-  print 'Old version: %s, New version: %s' % (deployed_version, new_version)
-  if new_version > deployed_version:
-    subprocess.check_call(['npm', 'whoami'])
-    subprocess.check_call(['npm', 'install'])
-    subprocess.check_call(['npm', 'publish'])
+    '''Publishes the latest version of the Noms npm package.
+    '''
+    gopath = os.getenv('GOPATH')
+    if gopath is None:
+        raise 'GOPATH not found in environemnt'
+
+    os.chdir(os.path.join(gopath, 'src', 'github.com', 'attic-labs', 'noms', 'js', 'noms'))
+
+    deployed_version = LooseVersion(
+        subprocess.check_output(['npm', 'info', '@attic/noms', 'version']).strip())
+    with open('package.json') as pkg:
+        data = json.load(pkg)
+    new_version = LooseVersion(data['version'])
+
+    print 'Old version: %s, New version: %s' % (deployed_version, new_version)
+    if new_version > deployed_version:
+        subprocess.check_call(['npm', 'whoami'])
+        subprocess.check_call(['npm', 'install'])
+        subprocess.check_call(['npm', 'publish'])
 
 if __name__ == '__main__':
-  main()
+    main()
