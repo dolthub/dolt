f6c3f91cc3819772573e7348e136587e13b419aa
Erik Arvidsson <erik.arvidsson@gmail.com>
2016-11-11 16:11:23 -0800

    Make publish-js-sdk.py publish all packages (#2822)

--- tools/publish-to-npm.py
+++ tools/publish-to-npm.py
@@ -8,23 +8,35 @@ from distutils.version import LooseVersion
 import json
 import os
 import subprocess
+from noms.pushd import pushd
+
+packages = [
+    'babel-preset-noms',
+    'eslint-config-noms',
+    'noms',
+    'webpack-config',
+]
 
 def main():
-    '''Publishes the latest version of the Noms npm package.
+    '''Publishes the latest version of the js npm packages.
     '''
+
     gopath = os.getenv('GOPATH')
     if gopath is None:
         raise 'GOPATH not found in environemnt'
 
-    os.chdir(os.path.join(gopath, 'src', 'github.com', 'attic-labs', 'noms', 'js', 'noms'))
+    for p in packages:
+        with pushd(os.path.join(gopath, 'src', 'github.com', 'attic-labs', 'noms', 'js', p)):
+            npm_publish()
 
-    deployed_version = LooseVersion(
-        subprocess.check_output(['npm', 'info', '@attic/noms', 'version']).strip())
+def npm_publish():
     with open('package.json') as pkg:
         data = json.load(pkg)
+    package_name = data['name']
+    deployed_version = LooseVersion(
+        subprocess.check_output(['npm', 'info', package_name, 'version']).strip())
     new_version = LooseVersion(data['version'])
-
-    print 'Old version: %s, New version: %s' % (deployed_version, new_version)
+    print '%s: Old version: %s, New version: %s' % (package_name, deployed_version, new_version)
     if new_version > deployed_version:
         subprocess.check_call(['npm', 'whoami'])
         subprocess.check_call(['npm', 'install'])
