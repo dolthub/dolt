1898f2a588d86e9bdea57f19167e6e5d202feabc
zcstarr <zcstarr@gmail.com>
2017-02-13 11:31:17 -0800

    Fix graphql css reference (#3188)

--- samples/js/graphiql/stage.py
+++ samples/js/graphiql/stage.py
@@ -11,5 +11,5 @@ sys.path.append(os.path.abspath('../../../tools'))
 import noms.staging as staging
 
 if __name__ == '__main__':
-    staging.Main('graphiql', staging.GlobCopier('out.js', 'styles.css',
+    staging.Main('graphiql', staging.GlobCopier('out.js', 'styles.css', 'graphiql.css',
         index_file='index.html', rename=True))
