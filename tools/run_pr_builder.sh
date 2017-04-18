f234913943f60cefa41121a5bae85aa934dc56de
Eric Halpern <eric.halpern@gmail.com>
2017-04-17 18:17:24 -0700

    Remove code coverage (#3395)
    * Remove code coverage
    
    * Remove node --version

--- tools/run_pr_builder.sh
+++ tools/run_pr_builder.sh
@@ -12,23 +12,10 @@ export GOPATH=${WORKSPACE}
 NOMS_DIR=${WORKSPACE}/src/github.com/attic-labs/noms
 
 go version
-node --version
 
 # go list is expensive, only do it once.
 GO_LIST="$(go list ./... | grep -v /vendor/ | grep -v /samples/js/)"
 go build ${GO_LIST}
-
-# go test plus build coverage data for upload codecov.io
-rm -rf coverage.txt
-touch coverage.txt
-for d in ${GO_LIST}; do
-    go test -coverprofile=profile.out -covermode=atomic $d
-    if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
-        rm profile.out
-    fi
-done
+go test ${GO_LIST}
 
 python -m unittest discover -p "*_test.py" -s $GOPATH/src/github.com/attic-labs/noms/tools
-
-bash <(curl -s https://codecov.io/bash) -t ${COVERALLS_TOKEN}
