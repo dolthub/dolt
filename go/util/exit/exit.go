f186bb9e290a6549349b64730f08c804882ef7be
Aaron Boodman <aaron@aaronboodman.com>
2017-06-01 12:13:31 -0700

    Add exit.Reset helper to reset to default value (#3512)

--- go/util/exit/exit.go
+++ go/util/exit/exit.go
@@ -10,14 +10,23 @@ import (
 	"os"
 )
 
-var Exit = func(code int) {
+var def = func(code int) {
 	os.Exit(code)
 }
 
+var Exit = def
+
+// Sets the implementation of Exit() to the default.
+func Reset() {
+	Exit = def
+}
+
+// Exits with a failure status.
 func Fail() {
 	Exit(1)
 }
 
+// Exits with a success status.
 func Success() {
 	Exit(0)
 }
