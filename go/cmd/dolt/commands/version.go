32115b715eecdd919aa6db55e97afd28d8902005
Matt Jesuele <matt@liquidata.co>
2019-03-18 12:14:29 -0700

    dolt/go/cmd/dolt/commands/version.go: Match git version output
    Resolves #737

--- go/cmd/dolt/commands/version.go
+++ go/cmd/dolt/commands/version.go
@@ -5,9 +5,10 @@ import (
 	"github.com/liquidata-inc/ld/dolt/go/libraries/doltcore/env"
 )
 
+// Version displays the version of the running dolt client
 func Version(version string) cli.CommandFunc {
 	return func(commandStr string, args []string, dEnv *env.DoltEnv) int {
-		cli.Println("The current dolt version is", version)
+		cli.Println("dolt version", version)
 
 		return 0
 	}
