3e6757164d7eb6b4154a92ca3c329066341f7e83
Zach Musgrave <zach@liquidata.co>
2019-03-18 17:23:58 -0700

    Fixing broken build caused by changing return type of PullChunks

--- go/libraries/doltcore/env/actions/remotes.go
+++ go/libraries/doltcore/env/actions/remotes.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/attic-labs/noms/go/datas"
 	"github.com/liquidata-inc/ld/dolt/go/libraries/doltcore/doltdb"
+	"github.com/liquidata-inc/ld/dolt/go/libraries/utils/pantoerr"
 )
 
 var ErrCantFF = errors.New("can't fast forward merge.")
@@ -17,7 +18,9 @@ func Push(branch, remoteBranch string, srcDB, destDB *doltdb.DoltDB, commit *dol
 		return ErrCantFF
 	}
 
-	err = destDB.PullChunks(srcDB, commit, progChan)
+	err = pantoerr.PanicToErrorNil("error pulling chunks", func() {
+		destDB.PullChunks(srcDB, commit, progChan)
+	})
 
 	if err != nil {
 		return err
@@ -35,7 +38,9 @@ func Push(branch, remoteBranch string, srcDB, destDB *doltdb.DoltDB, commit *dol
 }
 
 func Fetch(branch string, srcDB, destDB *doltdb.DoltDB, commit *doltdb.Commit, progChan chan datas.PullProgress) error {
-	err := destDB.PullChunks(srcDB, commit, progChan)
+	err := pantoerr.PanicToErrorNil("error pulling chunks", func() {
+		destDB.PullChunks(srcDB, commit, progChan)
+	})
 
 	if err != nil {
 		return err
