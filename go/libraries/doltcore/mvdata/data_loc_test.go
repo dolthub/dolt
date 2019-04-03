11cdcdfbfe87a017d047d365da3a352c8b15cb4e
Aaron Son <reltuk@gmail.com>
2019-04-03 16:34:48 -0700

    dolt/go/libraries/doltcore/mvdata: Fix compile error in data_loc_test.go.

--- go/libraries/doltcore/mvdata/data_loc_test.go
+++ go/libraries/doltcore/mvdata/data_loc_test.go
@@ -170,7 +170,7 @@ func TestCreateRdWr(t *testing.T) {
 		}
 
 		// TODO (oo): fix this for json path test
-		rd, _, err := loc.CreateReader(root, fs, "")
+		rd, _, err := loc.CreateReader(root, fs, "", "")
 
 		if err != nil {
 			t.Fatal("Unexpected error creating writer", err)
