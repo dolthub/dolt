1223dd213033d032be5691080d253dfd0eeba5ca
bheni <bheni@users.noreply.github.com>
2019-04-23 12:12:01 -0700

    support setting a parent directory where the global .dolt directory should be placed

--- go/libraries/doltcore/env/paths.go
+++ go/libraries/doltcore/env/paths.go
@@ -2,12 +2,14 @@ package env
 
 import (
 	"github.com/liquidata-inc/ld/dolt/go/libraries/doltcore/doltdb"
+	"os"
 	"os/user"
 	"path/filepath"
 )
 
 const (
-	credsDir = "creds"
+	doltRootParentDirEnvVar = "PARENT_OF_DOLT_ROOT_DIR"
+	credsDir                = "creds"
 
 	configFile   = "config.json"
 	globalConfig = "config_global.json"
@@ -15,9 +17,18 @@ const (
 	repoStateFile = "repo_state.json"
 )
 
+// HomeDirProvider is a function that returns the users home directory.  This is where global dolt state is stored for
+// the current user
 type HomeDirProvider func() (string, error)
 
+// GetCurrentUserHomeDir will return the current user's home directory by default.  This directory is where global dolt
+// state will be stored inside of the .dolt directory.  The environment variable PARENT_OF_DOLT_ROOT_DIR can be used to
+// provide a different directory where the root .dolt directory should be located and global state will be stored there.
 func GetCurrentUserHomeDir() (string, error) {
+	if parentOfDoltRootDir, ok := os.LookupEnv(doltRootParentDirEnvVar); ok && parentOfDoltRootDir != "" {
+		return parentOfDoltRootDir, nil
+	}
+
 	if usr, err := user.Current(); err != nil {
 		return "", err
 	} else {
