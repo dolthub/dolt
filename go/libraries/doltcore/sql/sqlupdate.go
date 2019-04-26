d57cc4f92ae04ee1e309573917ee3a43de375b16
Zach Musgrave <zach@liquidata.co>
2019-04-26 14:40:02 -0700

    Working joins. This won't work for all possible queries and needs more tests, but it's a good start. In particular, it won't work when a join condition references a column that isn't selected. The main issue at this point is that the fields used in comparisons don't have the same tag numbers as the result set in many cases.

--- go/libraries/doltcore/sql/sqlupdate.go
+++ go/libraries/doltcore/sql/sqlupdate.go
@@ -8,6 +8,7 @@ import (
 	"github.com/liquidata-inc/ld/dolt/go/libraries/doltcore/row"
 	"github.com/liquidata-inc/ld/dolt/go/libraries/doltcore/schema"
 	"github.com/liquidata-inc/ld/dolt/go/libraries/doltcore/table/typed/noms"
+	"github.com/liquidata-inc/ld/dolt/go/libraries/doltcore/table/untyped/resultset"
 	"github.com/xwb1989/sqlparser"
 	"io"
 )
@@ -134,7 +135,7 @@ func ExecuteUpdate(db *doltdb.DoltDB, root *doltdb.RootValue, s *sqlparser.Updat
 	}
 
 	// TODO: support aliases in update
-	filter, err := createFilterForWhere(s.Where, map[string]schema.Schema{tableName: tableSch}, NewAliases())
+	filter, err := createFilterForWhere(s.Where, map[string]schema.Schema{tableName: tableSch}, NewAliases(), resultset.Identity(tableSch))
 	if err != nil {
 		return errUpdate(err.Error())
 	}
