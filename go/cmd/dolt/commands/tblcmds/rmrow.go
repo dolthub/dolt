735869b8eae4d11bbd72643e480a18cf8a7e8b1b
bheni <bheni@users.noreply.github.com>
2019-05-02 15:26:26 -0700

    output fix

--- go/cmd/dolt/commands/tblcmds/rmrow.go
+++ go/cmd/dolt/commands/tblcmds/rmrow.go
@@ -98,7 +98,7 @@ func updateTableWithRowsRemoved(root *doltdb.RootValue, tbl *doltdb.Table, tblNa
 		_, ok := m.MaybeGet(context.TODO(), pk)
 
 		if !ok {
-			cli.PrintErrln(color.YellowString(`No row with %s equal to %s was found.`, types.EncodedValue(context.TODO(), pk)))
+			cli.PrintErrln(color.YellowString(`No row with pk equal to %s was found.`, types.EncodedValue(context.TODO(), pk)))
 			continue
 		}
 
