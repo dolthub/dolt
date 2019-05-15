c5b3ae3c6be73fe9af8a910b7d2b9a7cc96f2a77
bheni <bheni@users.noreply.github.com>
2019-05-14 22:35:01 -0700

    revert changes

--- go/types/compare_test.go
+++ go/types/compare_test.go
@@ -79,13 +79,10 @@ func TestCompareDifferentPrimitiveTypes(t *testing.T) {
 		for j, v2 := range vals {
 			iBytes := [1024]byte{}
 			jBytes := [1024]byte{}
+			res := compareEncodedKey(encodeGraphKey(iBytes[:0], v1), encodeGraphKey(jBytes[:0], v2))
+			expectedRes := compareInts(i, j)
 
-			if i == 3 && j == 4 {
-				res := compareEncodedKey(encodeGraphKey(iBytes[:0], v1), encodeGraphKey(jBytes[:0], v2))
-				expectedRes := compareInts(i, j)
-
-				assert.Equal(expectedRes, res, "%d:%d", i, j)
-			}
+			assert.Equal(expectedRes, res, "%d:%d", i, j)
 		}
 	}
 }
