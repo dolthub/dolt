9cedb50393ea58412eb35e5421cd6689a4d6fa64
Brian Hendriks <brian@liquidata.co>
2019-06-02 14:05:52 -0700

    test fix

--- go/libraries/doltcore/remotestorage/map_chunk_cache_test.go
+++ go/libraries/doltcore/remotestorage/map_chunk_cache_test.go
@@ -10,13 +10,11 @@ import (
 	"time"
 )
 
-var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
-
-func genRandomChunks(n int) (hash.HashSet, []chunks.Chunk) {
+func genRandomChunks(rng *rand.Rand, n int) (hash.HashSet, []chunks.Chunk) {
 	chks := make([]chunks.Chunk, n)
 	hashes := make(hash.HashSet)
 	for i := 0; i < n; i++ {
-		size := int(rng.Int31n(100))
+		size := int(rng.Int31n(99) + 1)
 		bytes := make([]byte, size)
 		for j := 0; j < size; j++ {
 			bytes[j] = byte(rng.Int31n(255))
@@ -34,23 +32,25 @@ func genRandomChunks(n int) (hash.HashSet, []chunks.Chunk) {
 func TestMapChunkCache(t *testing.T) {
 	const chunkBatchSize = 10
 
-	hashes, chks := genRandomChunks(chunkBatchSize)
+	seed := time.Now().UnixNano()
+	rng := rand.New(rand.NewSource(seed))
+	hashes, chks := genRandomChunks(rng, chunkBatchSize)
 
 	mapChunkCache := newMapChunkCache()
 	mapChunkCache.Put(chks)
 	hashToChunk := mapChunkCache.Get(hashes)
 
-	assert.Equal(t, len(hashToChunk), chunkBatchSize, "Did not read back all chunks")
+	assert.Equal(t, len(hashToChunk), chunkBatchSize, "Did not read back all chunks (seed %d)", seed)
 
 	absent := mapChunkCache.Has(hashes)
 
-	assert.Equal(t, len(absent), 0, "Missing chunks that were added")
+	assert.Equal(t, len(absent), 0, "Missing chunks that were added (seed %d)", seed)
 
 	toFlush := mapChunkCache.GetAndClearChunksToFlush()
 
-	assert.True(t, reflect.DeepEqual(toFlush, hashToChunk), "unexpected or missing chunks to flush")
+	assert.True(t, reflect.DeepEqual(toFlush, hashToChunk), "unexpected or missing chunks to flush (seed %d)", seed)
 
-	moreHashes, moreChks := genRandomChunks(chunkBatchSize)
+	moreHashes, moreChks := genRandomChunks(rng, chunkBatchSize)
 
 	joinedHashes := make(hash.HashSet)
 
@@ -64,11 +64,11 @@ func TestMapChunkCache(t *testing.T) {
 
 	absent = mapChunkCache.Has(joinedHashes)
 
-	assert.True(t, reflect.DeepEqual(absent, moreHashes), "unexpected absent hashset")
-	assert.False(t, mapChunkCache.PutChunk(&chks[0]), "existing chunk should return false")
-	assert.True(t, mapChunkCache.PutChunk(&moreChks[0]), "new chunk should return true")
+	assert.True(t, reflect.DeepEqual(absent, moreHashes), "unexpected absent hashset (seed %d)", seed)
+	assert.False(t, mapChunkCache.PutChunk(&chks[0]), "existing chunk should return false (seed %d)", seed)
+	assert.True(t, mapChunkCache.PutChunk(&moreChks[0]), "new chunk should return true (seed %d)", seed)
 
 	toFlush = mapChunkCache.GetAndClearChunksToFlush()
 
-	assert.True(t, reflect.DeepEqual(toFlush, map[hash.Hash]chunks.Chunk{moreChks[0].Hash(): moreChks[0]}), "Missing or unexpected chunks to flush")
+	assert.True(t, reflect.DeepEqual(toFlush, map[hash.Hash]chunks.Chunk{moreChks[0].Hash(): moreChks[0]}), "Missing or unexpected chunks to flush (seed %d)", seed)
 }
