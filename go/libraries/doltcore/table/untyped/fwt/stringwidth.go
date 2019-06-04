1f6dc7b7703ea9827066109b082048d336990e42
Zach Musgrave <zach@liquidata.co>
2019-06-04 12:05:50 -0700

    Made StringWidth consider newlines. Also added tests for auto fixed width transformer

--- go/libraries/doltcore/table/untyped/fwt/stringwidth.go
+++ go/libraries/doltcore/table/untyped/fwt/stringwidth.go
@@ -3,21 +3,35 @@ package fwt
 import (
 	"github.com/mattn/go-runewidth"
   "github.com/rivo/uniseg"
+	"strings"
 )
 
-// StringWidth returns the number of horizontal cells needed to print the given text. It splits the text into its
-// grapheme clusters, calculates each cluster's width, and adds them up to a total.
-func StringWidth(text string) (width int) {
-	g := uniseg.NewGraphemes(text)
-	for g.Next() {
-		var chWidth int
-		for _, r := range g.Runes() {
-			chWidth = runewidth.RuneWidth(r)
-			if chWidth > 0 {
-				break // Our best guess at this point is to use the width of the first non-zero-width rune.
+// StringWidth returns the number of horizontal cells needed to print the given text. It splits the text into lines,
+// each line into grapheme clusters, calculates each cluster's width, sums them, and returns the width of the
+// longest line
+func StringWidth(text string) int {
+	var maxWidth int
+	lines := strings.Split(text, "\n")
+
+	for _, line := range lines {
+		var width int
+		g := uniseg.NewGraphemes(line)
+
+		for g.Next() {
+			var chWidth int
+			for _, r := range g.Runes() {
+				chWidth = runewidth.RuneWidth(r)
+				if chWidth > 0 {
+					break // Our best guess at this point is to use the width of the first non-zero-width rune.
+				}
 			}
+			width += chWidth
 		}
-		width += chWidth
+
+		if width > maxWidth {
+			maxWidth = width
+		}
 	}
-	return
+
+	return maxWidth
 }
