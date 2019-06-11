feb5b8402c7fe11683b91a77c8b85bcfab81ebf9
bheni <bheni@users.noreply.github.com>
2019-06-11 16:53:48 -0700

    fixes

--- go/cmd/dolt/commands/remote_test.go
+++ go/cmd/dolt/commands/remote_test.go
@@ -66,7 +66,7 @@ func TestGetAbsRemoteUrl(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.str, func(t *testing.T) {
-			actualScheme, actualUrl, err := getAbsRemoteUrl(test.cfg, test.str)
+			actualScheme, actualUrl, err := getAbsRemoteUrl(nil, test.cfg, test.str)
 
 			if test.expectErr {
 				assert.Error(t, err)
