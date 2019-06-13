f4182ee2db6ec4a9a0ad4cf471b9d384943d9a66
Zach Musgrave <zach@liquidata.co>
2019-06-12 17:30:49 -0700

    Fixed alias tests

--- go/libraries/doltcore/sql/aliases_test.go
+++ go/libraries/doltcore/sql/aliases_test.go
@@ -1,31 +1,47 @@
 
 package sql
 
-// TODO: fix me
-/**
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
 func TestTableAliases(t *testing.T) {
 	a := NewAliases()
 	alias1 := "alias"
-	table1 := "table1"
+	table1 := "Table1"
 	require.NoError(t, a.AddTableAlias(table1, alias1))
 	alias2 := "alias2"
 	alias3 := "alias3"
-	table2 := "table2"
+	table2 := "Table2"
 	require.NoError(t, a.AddTableAlias(table2, alias2))
 	require.NoError(t, a.AddTableAlias(table2, alias3))
 
-	assert.Equal(t, table1, a.TablesByAlias[alias1])
-	assert.Equal(t, table2, a.TablesByAlias[alias2])
+	var tableName string
+	var ok bool
 
-	assert.Equal(t, 1, len(a.AliasesByTable[table1]))
-	assert.Equal(t, alias1, a.AliasesByTable[table1][0])
+	tableName, ok = a.GetTableByAlias(alias1)
+	assert.True(t, ok)
+	assert.Equal(t, table1, tableName)
 
-	assert.Equal(t, 2, len(a.AliasesByTable[table2]))
-	assert.Equal(t, alias2, a.AliasesByTable[table2][0])
-	assert.Equal(t, alias3, a.AliasesByTable[table2][1])
+	tableName, ok = a.GetTableByAlias(alias2)
+	assert.True(t, ok)
+	assert.Equal(t, table2, tableName)
 
-	err := a.AddTableAlias("table3", alias1)
+	tableName, ok = a.GetTableByAlias(alias3)
+	assert.True(t, ok)
+	assert.Equal(t, table2, tableName)
+
+	// aliases are case-insensitive
+	tableName, ok = a.GetTableByAlias("ALIAS3")
+	assert.True(t, ok)
+	assert.Equal(t, table2, tableName)
+
+	tableName, ok = a.GetTableByAlias("not a thing")
+	assert.False(t, ok)
+
+	err := a.AddTableAlias("Table3", alias1)
 	assert.Error(t, err)
-	assert.Contains(t, err.Error(), "Duplicate table alias")
+	assert.Contains(t, err.Error(), "Non-unique")
 }
-*/
