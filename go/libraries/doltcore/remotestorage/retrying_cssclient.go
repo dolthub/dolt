8fff8378300fe9c9881aca485e5ab2f9ad6210ba
Brian Hendriks <brian@liquidata.co>
2019-06-19 14:05:35 -0700

    use github.com/cenkalti/backoff

--- go/libraries/doltcore/remotestorage/retrying_cssclient.go
+++ go/libraries/doltcore/remotestorage/retrying_cssclient.go
@@ -3,18 +3,17 @@ package remotestorage
 import (
 	"context"
 	"errors"
+	"github.com/cenkalti/backoff"
 	remotesapi "github.com/liquidata-inc/ld/dolt/go/gen/proto/dolt/services/remotesapi_v1alpha1"
-	"github.com/liquidata-inc/ld/dolt/go/libraries/utils/retry"
 	"google.golang.org/grpc"
-	"time"
 )
 
 var ErrRCSSCUnknown = errors.New("unknown error occurred")
 
-var csRetryParams = retry.RetryParams{
-	NumRetries: 5,
-	MaxDelay:   2 * time.Second,
-	Backoff:    200 * time.Millisecond,
+var csRetryParams backoff.BackOff
+
+func init() {
+	csRetryParams = backoff.WithMaxRetries(backoff.NewExponentialBackOff(), 5)
 }
 
 type RetryingChunkStoreServiceClient struct {
@@ -23,90 +22,78 @@ type RetryingChunkStoreServiceClient struct {
 
 func (rcssc RetryingChunkStoreServiceClient) HasChunks(ctx context.Context, in *remotesapi.HasChunksRequest, opts ...grpc.CallOption) (*remotesapi.HasChunksResponse, error) {
 	var resp *remotesapi.HasChunksResponse
-	var err error
-	success := retry.CallWithRetries(csRetryParams, func() retry.RetriableCallState {
+	op := func() error {
+		var err error
 		resp, err = rcssc.client.HasChunks(ctx, in, opts...)
-		return retry.ProcessGrpcErr(err)
-	})
-
-	if err == nil && !success {
-		err = ErrRCSSCUnknown
+		return processGrpcErr(err)
 	}
+
+	err := backoff.Retry(op, csRetryParams)
 
 	return resp, err
 }
 
 func (rcssc RetryingChunkStoreServiceClient) GetDownloadLocations(ctx context.Context, in *remotesapi.GetDownloadLocsRequest, opts ...grpc.CallOption) (*remotesapi.GetDownloadLocsResponse, error) {
 	var resp *remotesapi.GetDownloadLocsResponse
-	var err error
-	success := retry.CallWithRetries(csRetryParams, func() retry.RetriableCallState {
+	op := func() error {
+		var err error
 		resp, err = rcssc.client.GetDownloadLocations(ctx, in, opts...)
-		return retry.ProcessGrpcErr(err)
-	})
-
-	if err == nil && !success {
-		err = ErrRCSSCUnknown
+		return processGrpcErr(err)
 	}
+
+	err := backoff.Retry(op, csRetryParams)
 
 	return resp, err
 }
 
 func (rcssc RetryingChunkStoreServiceClient) GetUploadLocations(ctx context.Context, in *remotesapi.GetUploadLocsRequest, opts ...grpc.CallOption) (*remotesapi.GetUploadLocsResponse, error) {
 	var resp *remotesapi.GetUploadLocsResponse
-	var err error
-	success := retry.CallWithRetries(csRetryParams, func() retry.RetriableCallState {
+	op := func() error {
+		var err error
 		resp, err = rcssc.client.GetUploadLocations(ctx, in, opts...)
-		return retry.ProcessGrpcErr(err)
-	})
-
-	if err == nil && !success {
-		err = ErrRCSSCUnknown
+		return processGrpcErr(err)
 	}
+
+	err := backoff.Retry(op, csRetryParams)
 
 	return resp, err
 }
 
 func (rcssc RetryingChunkStoreServiceClient) Rebase(ctx context.Context, in *remotesapi.RebaseRequest, opts ...grpc.CallOption) (*remotesapi.RebaseResponse, error) {
 	var resp *remotesapi.RebaseResponse
-	var err error
-	success := retry.CallWithRetries(csRetryParams, func() retry.RetriableCallState {
+	op := func() error {
+		var err error
 		resp, err = rcssc.client.Rebase(ctx, in, opts...)
-		return retry.ProcessGrpcErr(err)
-	})
-
-	if err == nil && !success {
-		err = ErrRCSSCUnknown
+		return processGrpcErr(err)
 	}
+
+	err := backoff.Retry(op, csRetryParams)
 
 	return resp, err
 }
 
 func (rcssc RetryingChunkStoreServiceClient) Root(ctx context.Context, in *remotesapi.RootRequest, opts ...grpc.CallOption) (*remotesapi.RootResponse, error) {
 	var resp *remotesapi.RootResponse
-	var err error
-	success := retry.CallWithRetries(csRetryParams, func() retry.RetriableCallState {
+	op := func() error {
+		var err error
 		resp, err = rcssc.client.Root(ctx, in, opts...)
-		return retry.ProcessGrpcErr(err)
-	})
-
-	if err == nil && !success {
-		err = ErrRCSSCUnknown
+		return processGrpcErr(err)
 	}
+
+	err := backoff.Retry(op, csRetryParams)
 
 	return resp, err
 }
 
 func (rcssc RetryingChunkStoreServiceClient) Commit(ctx context.Context, in *remotesapi.CommitRequest, opts ...grpc.CallOption) (*remotesapi.CommitResponse, error) {
 	var resp *remotesapi.CommitResponse
-	var err error
-	success := retry.CallWithRetries(csRetryParams, func() retry.RetriableCallState {
+	op := func() error {
+		var err error
 		resp, err = rcssc.client.Commit(ctx, in, opts...)
-		return retry.ProcessGrpcErr(err)
-	})
-
-	if err == nil && !success {
-		err = ErrRCSSCUnknown
+		return processGrpcErr(err)
 	}
+
+	err := backoff.Retry(op, csRetryParams)
 
 	return resp, err
 }
