fae35d91ced14f149b8b3b9e2fc9346be8b3a2bc
Zach Musgrave <zach@liquidata.co>
2019-06-20 14:13:31 -0700

    Almost working table / row iterators

--- go/libraries/doltcore/sqle/types.go
+++ go/libraries/doltcore/sqle/types.go
@@ -15,7 +15,8 @@ func nomsTypeToSqlType(kind types.NomsKind) sql.Type {
 	case types.StringKind:
 		return sql.Text
 	case types.UUIDKind:
-		panic("TODO")
+		// TODO: make an actual uuid
+		return sql.Text
 	case types.IntKind:
 		return sql.Int64
 	case types.UintKind:
@@ -23,4 +24,47 @@ func nomsTypeToSqlType(kind types.NomsKind) sql.Type {
 	default:
 		panic(fmt.Sprintf("Unexpected kind %v", kind))
 	}
+}
+
+func nomsValToSqlVal(val types.Value) interface{} {
+	switch val.Kind() {
+	case types.BoolKind:
+		return convertBool(val.(types.Bool))
+	case types.FloatKind:
+		return convertFloat(val.(types.Float))
+	case types.StringKind:
+		return convertString(val.(types.String))
+	case types.UUIDKind:
+		return convertUUID(val.(types.UUID))
+	case types.IntKind:
+		return convertInt(val.(types.Int))
+	case types.UintKind:
+		return convertUint(val.(types.Uint))
+	default:
+		panic(fmt.Sprintf("Unexpected kind %v", val.Kind()))
+	}
+}
+
+func convertUUID(u types.UUID) interface{} {
+	return u.String()
+}
+
+func convertUint(i types.Uint) interface{} {
+	return uint64(i)
+}
+
+func convertInt(i types.Int) interface{} {
+	return int64(i)
+}
+
+func convertString(i types.String) interface{} {
+	return string(i)
+}
+
+func convertFloat(f types.Float) interface{} {
+	return float64(f)
+}
+
+func convertBool(b types.Bool) interface{} {
+	return bool(b)
 }
