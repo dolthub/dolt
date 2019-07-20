bc59893f6651855e1d204d9e03c8510fd47c8239
Daylon Wilkins <daylon@liquidata.co>
2019-07-19 17:22:41 -0700

    Changed bats tests on Windows to use WSL

--- bats/helper/windows-compat.bash
+++ bats/helper/windows-compat.bash
@@ -1,26 +1,11 @@
 skiponwindows() { :; }
-	
-unameOut="$(uname -s)"
-case "${unameOut}" in
-	CYGWIN*)    machine=Windows;;
-	MINGW*)     machine=Windows;;
-	*)          machine=Unix;;
-esac
+nativepath() { echo "$1"; }
 
-if [ ${machine} = "Windows" ]; then
-	skiponwindows() {
-		skip "$1"
+if [ -d /mnt/*/Windows/System32 ]; then
+	nativepath() {
+		wslpath -w "$1"
 	}
-fi
-
-if ! [ -x "$(command -v pkill)" ]; then
-	pkill() {
-		taskkill -fi "IMAGENAME eq $2*" -f
-	}
-fi
-
-if ! [ -x "$(command -v pgrep)" ]; then
-	pgrep() {
-		tasklist -fi "IMAGENAME eq $1*"
-	}
+    skiponwindows() {
+        skip "$1"
+    }
 fi
