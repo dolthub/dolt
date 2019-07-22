2d2673eca396e9e97177343efd3cdc396cb8252f
Daylon Wilkins <daylon@liquidata.co>
2019-07-22 16:08:56 -0700

    Made config.bats WSL-compatible and fixed #1801

--- bats/helper/windows-compat.bash
+++ bats/helper/windows-compat.bash
@@ -1,11 +1,19 @@
-skiponwindows() { :; }
 nativepath() { echo "$1"; }
+nativevar() { eval "$1"='$2'; }
+skiponwindows() { :; }
+
+IS_WINDOWS=false
 
 if [ -d /mnt/*/Windows/System32 ]; then
+	IS_WINDOWS=true
 	nativepath() {
 		wslpath -w "$1"
 	}
-    skiponwindows() {
-        skip "$1"
-    }
+	nativevar() {
+		eval export "$1"='$2'
+		export WSLENV="$1$3"
+	}
+	skiponwindows() {
+		skip "$1"
+	}
 fi
