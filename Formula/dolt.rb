9058ff9aec5de3f6eb7145e394a3ac4597c576b4
FanaHOVA <fana@alessiofanelli.com>
2019-08-13 10:15:30 -0400

    Install from source

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -1,15 +1,16 @@
 class Dolt < Formula
   desc "Dolt - It's git for data"
   homepage "https://github.com/liquidata-inc/dolt"
-  url "https://github.com/liquidata-inc/dolt/releases/download/v0.9.7/dolt-darwin-amd64.tar.gz"
+  url "https://github.com/liquidata-inc/dolt"
   sha256 "2c13dfcf594f5c52780ccc97e932bcfd3128b07ff43018e1622a066b4ff36a3a"
 
   bottle :unneeded
 
   def install
-    bin.install 'dolt'
-    bin.install 'git-dolt'
-    bin.install 'git-dolt-smudge'
+    system 'cd go'
+    system 'go install ./cmd/dolt'
+    system 'go install ./cmd/git-dolt'
+    system 'go install ./cmd/git-dolt-smudge'
   end
 
   test do
