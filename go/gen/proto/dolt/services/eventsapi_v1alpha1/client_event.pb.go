9d6e3bca03872c6a88fb423f8d285aeb11e68252
Brian Hendriks <brian@liquidata.co>
2019-08-20 15:39:08 -0700

    fix formatting

--- go/gen/proto/dolt/services/eventsapi_v1alpha1/client_event.pb.go
+++ go/gen/proto/dolt/services/eventsapi_v1alpha1/client_event.pb.go
@@ -6,13 +6,14 @@ package eventsapi
 import (
 	context "context"
 	fmt "fmt"
+	math "math"
+
 	proto "github.com/golang/protobuf/proto"
 	duration "github.com/golang/protobuf/ptypes/duration"
 	timestamp "github.com/golang/protobuf/ptypes/timestamp"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
