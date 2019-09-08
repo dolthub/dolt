c476673048d6f448ff52bfc03a04e758a672e473
Dustin Brown <dustin@liquidata.co>
2019-09-08 16:59:40 -0700

    cleaning up PR

--- go/libraries/utils/filesys/lock.go
+++ go/libraries/utils/filesys/lock.go
@@ -10,13 +10,16 @@ import (
 const originalStateValue int32 = 0
 const newStateValue int32 = 1
 
+// errLockUnlock occurs if there is an error unlocking the lock
 var errLockUnlock = errors.New("unable to unlock the lock")
 
+// FilesysLock is an interface for locking and unlocking filesystems
 type FilesysLock interface {
 	TryLock() (bool, error)
 	Unlock() error
 }
 
+// CreateFilesysLock creates a new FilesysLock
 func CreateFilesysLock(fs Filesys, filename string) FilesysLock {
 	switch fs.(type) {
 	case *InMemFS:
@@ -28,14 +31,17 @@ func CreateFilesysLock(fs Filesys, filename string) FilesysLock {
 	}
 }
 
+// InMemFileLock is a lock for the InMemFS
 type InMemFileLock struct {
 	state int32
 }
 
+// NewInMemFileLock creates a new InMemFileLock
 func NewInMemFileLock(fs Filesys) *InMemFileLock {
 	return &InMemFileLock{originalStateValue}
 }
 
+// TryLock attempts to lock the lock or fails if it is already locked
 func (memLock *InMemFileLock) TryLock() (bool, error) {
 	if atomic.CompareAndSwapInt32(&memLock.state, originalStateValue, newStateValue) {
 		return true, nil
@@ -43,18 +49,13 @@ func (memLock *InMemFileLock) TryLock() (bool, error) {
 	return false, nil
 }
 
+// Unlock unlocks the lock
 func (memLock *InMemFileLock) Unlock() error {
-	// if memLock.state == originalStateValue {
 	if memLock.state == 0 {
 		return nil
 	}
 
-	// old := newStateValue
 	new := atomic.AddInt32(&memLock.state, -newStateValue)
-
-	// if atomic.CompareAndSwapInt32(&memLock.state, old, new) {
-	// 	return nil
-	// }
 
 	if new != 0 {
 		return errLockUnlock
@@ -63,15 +64,18 @@ func (memLock *InMemFileLock) Unlock() error {
 	return nil
 }
 
+// LocalFileLock is the lock for the localFS
 type LocalFileLock struct {
 	lck *fslock.Lock
 }
 
+// NewLocalFileLock creates a new LocalFileLock
 func NewLocalFileLock(fs Filesys, filename string) *LocalFileLock {
 	lck := fslock.New(filename)
 	return &LocalFileLock{lck: lck}
 }
 
+// TryLock attempts to lock the lock or fails if it is already locked
 func (locLock *LocalFileLock) TryLock() (bool, error) {
 	err := locLock.lck.TryLock()
 	if err != nil {
@@ -80,6 +84,7 @@ func (locLock *LocalFileLock) TryLock() (bool, error) {
 	return true, nil
 }
 
+// Unlock unlocks the lock
 func (locLock *LocalFileLock) Unlock() error {
 	err := locLock.lck.Unlock()
 	if err != nil {
