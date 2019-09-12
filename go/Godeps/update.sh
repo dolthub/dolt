28dab53376a1c63ccf31fddd8df0aac43618c3ce
Aaron Son <aaron@liquidata.co>
2019-09-11 17:10:45 -0700

    go/Godeps: Make sure to include windows-only third party software in Godeps/LICENSES.

--- go/Godeps/update.sh
+++ go/Godeps/update.sh
@@ -5,4 +5,6 @@ set -eo pipefail
 script_dir=$(dirname "$0")
 cd $script_dir/..
 
-go list -deps -json ./cmd/dolt/. ./cmd/git-dolt/. ./cmd/git-dolt-smudge/. | go run ./utils/3pdeps/. > ./Godeps/LICENSES
+( go list -deps -json ./cmd/dolt/. ./cmd/git-dolt/. ./cmd/git-dolt-smudge/. && \
+  GOOS=windows go list -deps -json ./cmd/dolt/. ./cmd/git-dolt/. ./cmd/git-dolt-smudge/.) \
+  | go run ./utils/3pdeps/. > ./Godeps/LICENSES
