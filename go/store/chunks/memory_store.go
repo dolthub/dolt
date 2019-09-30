95952fc1e730d2d3990b74128e61174e13a518e6
Brian Hendriks <brian@liquidata.co>
2019-09-30 10:53:20 -0700

    fix version

--- go/store/chunks/memory_store.go
+++ go/store/chunks/memory_store.go
@@ -48,7 +48,7 @@ type MemoryStorage struct {
 func (ms *MemoryStorage) NewView() ChunkStore {
 	version := ms.version
 	if version == "" {
-		version = constants.FormatDefaultString
+		version = constants.NomsVersion
 	}
 
 	return &MemoryStoreView{storage: ms, rootHash: ms.rootHash, version: version}
