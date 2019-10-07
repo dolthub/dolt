85f9500de45261b57c0f178010184edf299f5fc7
Timothy Sehn <tim@liquidata.co>
2019-10-07 16:13:56 -0700

    Formatted per script

--- go/cmd/dolt/commands/sql.go
+++ go/cmd/dolt/commands/sql.go
@@ -165,7 +165,7 @@ func scanStatements(data []byte, atEOF bool) (advance int, token []byte, err err
 // runBatchMode processes queries until EOF and returns the resulting root value
 func runBatchMode(ctx context.Context, dEnv *env.DoltEnv, root *doltdb.RootValue) (*doltdb.RootValue, error) {
 	scanner := bufio.NewScanner(os.Stdin)
-	const maxCapacity = 512*1024
+	const maxCapacity = 512 * 1024
 	buf := make([]byte, maxCapacity)
 	scanner.Buffer(buf, maxCapacity)
 	scanner.Split(scanStatements)
