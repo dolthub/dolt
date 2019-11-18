8bddfddd7b1d94b3c77f06507bafdd8927b54743
Andy Arthur <andy@liquidata.co>
2019-11-18 08:52:36 -0800

    updated unittests

--- go/libraries/doltcore/diff/sql_diff_test.go
+++ go/libraries/doltcore/diff/sql_diff_test.go
@@ -19,15 +19,19 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/liquidata-inc/dolt/go/libraries/doltcore/dtestutils"
+	"github.com/liquidata-inc/dolt/go/libraries/doltcore/env"
 	"github.com/liquidata-inc/dolt/go/libraries/doltcore/row"
 	"github.com/liquidata-inc/dolt/go/libraries/doltcore/schema"
 	"github.com/liquidata-inc/dolt/go/libraries/doltcore/schema/alterschema"
 	"github.com/liquidata-inc/dolt/go/libraries/doltcore/sql"
-	"github.com/liquidata-inc/dolt/go/store/types"
 )
+
+const name = "Jeffery Williams"
+const email = "meet.me@the.london"
 
 type StringBuilderCloser struct {
 	strings.Builder
@@ -37,77 +41,116 @@ func (*StringBuilderCloser) Close() error {
 	return nil
 }
 
-func TestPrintSqlTableDiffs(t *testing.T) {
-	dEnv := dtestutils.CreateTestEnv()
-
-	dropCols := []schema.Column{
-		schema.NewColumn("pk", 0, types.StringKind, true, schema.NotNullConstraint{}),
-		schema.NewColumn("data", 1, types.IntKind, false),
-	}
-	dropColColl, _ := schema.NewColCollection(dropCols...)
-	dropSch := schema.SchemaFromCols(dropColColl)
-
-	renameCols := []schema.Column{
-		schema.NewColumn("pk", 0, types.StringKind, true, schema.NotNullConstraint{}),
-		schema.NewColumn("data", 1, types.IntKind, false),
-	}
-	renameColColl, _ := schema.NewColCollection(renameCols...)
-	renameSch := schema.SchemaFromCols(renameColColl)
-
-	addCols := []schema.Column{
-		schema.NewColumn("pk", 0, types.StringKind, true, schema.NotNullConstraint{}),
-		schema.NewColumn("data", 1, types.IntKind, false),
-	}
-	addColColl, _ := schema.NewColCollection(addCols...)
-	addSch := schema.SchemaFromCols(addColColl)
-
-	const name = "Jeffery Williams"
-	const email = "meet.me@the.london"
+func setupSchema() (context.Context, schema.Schema, *env.DoltEnv) {
 	ctx := context.Background()
+	sch := dtestutils.TypedSchema
+	dEnv := dtestutils.CreateTestEnv()
 	_ = dEnv.DoltDB.WriteEmptyRepo(ctx, name, email)
-	{
-		ctx := context.Background()
-		_ = dEnv.DoltDB.WriteEmptyRepo(ctx, name, email)
-		oldRoot, _ := dEnv.WorkingRoot(ctx)
-		dtestutils.CreateTestTable(t, dEnv, "addTable", addSch, []row.Row{}...)
-		newRoot, _ := dEnv.WorkingRoot(ctx)
-		added, _, removed, _ := newRoot.TableDiff(ctx, oldRoot)
-		adds, _, _, _ := findRenames(ctx, newRoot, oldRoot, added, removed)
-		assert.Equal(t, []string{"addTable"}, adds)
+	return ctx, sch, dEnv
+}
 
-		expectedOutput := sql.SchemaAsCreateStmt("addTable", addSch) + "\n"
-		var stringWr StringBuilderCloser
-		_ = PrintSqlTableDiffs(ctx, newRoot, oldRoot, &stringWr)
-		assert.Equal(t, expectedOutput, stringWr.String())
-	}
-	{
-		ctx := context.Background()
-		_ = dEnv.DoltDB.WriteEmptyRepo(ctx, name, email)
-		dtestutils.CreateTestTable(t, dEnv, "dropTable", dropSch, []row.Row{}...)
-		oldRoot, _ := dEnv.WorkingRoot(ctx)
-		newRoot, _ := oldRoot.RemoveTables(ctx, []string{"dropTable"}...)
-		added, _, removed, _ := newRoot.TableDiff(ctx, oldRoot)
-		_, drops, _, _ := findRenames(ctx, newRoot, oldRoot, added, removed)
-		assert.Equal(t, []string{"dropTable"}, drops)
+func strPointer(s string) *string {
+	return &s
+}
 
-		expectedOutput := "DROP TABLE `dropTable`;\n"
-		var stringWr StringBuilderCloser
-		_ = PrintSqlTableDiffs(ctx, newRoot, oldRoot, &stringWr)
-		assert.Equal(t, expectedOutput, stringWr.String())
-	}
-	{
-		ctx := context.Background()
-		_ = dEnv.DoltDB.WriteEmptyRepo(ctx, name, email)
-		dtestutils.CreateTestTable(t, dEnv, "renameTable", renameSch, []row.Row{}...)
-		oldRoot, _ := dEnv.WorkingRoot(ctx)
-		newRoot, _ := alterschema.RenameTable(ctx, dEnv.DoltDB, oldRoot, "renameTable", "newTableName")
-		added, _, removed, _ := newRoot.TableDiff(ctx, oldRoot)
-		_, _, renames, _ := findRenames(ctx, newRoot, oldRoot, added, removed)
-		assert.Equal(t, map[string]string{"renameTable": "newTableName"}, renames)
+func TestSqlTableDiffAdd(t *testing.T) {
+	ctx, sch, dEnv := setupSchema()
 
-		expectedOutput := "RENAME TABLE `renameTable` TO `newTableName`\n"
-		var stringWr StringBuilderCloser
-		_ = PrintSqlTableDiffs(ctx, newRoot, oldRoot, &stringWr)
-		assert.Equal(t, expectedOutput, stringWr.String())
-	}
+	oldRoot, _ := dEnv.WorkingRoot(ctx)
+	dtestutils.CreateTestTable(t, dEnv, "addTable", sch, []row.Row{}...)
+	newRoot, _ := dEnv.WorkingRoot(ctx)
+	a, _, rm, _ := newRoot.TableDiff(ctx, oldRoot)
+	adds, removed, renamed, _ := findRenames(ctx, newRoot, oldRoot, a, rm)
+	assert.Equal(t, []string{"addTable"}, adds)
+	assert.Equal(t, []string{}, removed)
+	assert.Equal(t, map[string]string{}, renamed)
+
+	var stringWr StringBuilderCloser
+	_ = PrintSqlTableDiffs(ctx, newRoot, oldRoot, &stringWr)
+	expectedOutput := sql.SchemaAsCreateStmt("addTable", sch) + "\n"
+	assert.Equal(t, expectedOutput, stringWr.String())
+}
+
+func TestSqlTableDiffAddThenInsert(t *testing.T) {
+	id := uuid.MustParse("00000000-0000-0000-0000-000000000000")
+	ctx, sch, dEnv := setupSchema()
+
+	oldRoot, _ := dEnv.WorkingRoot(ctx)
+	dtestutils.CreateTestTable(t, dEnv, "addTable", sch, []row.Row{}...)
+	r := dtestutils.NewTypedRow(id, "Big Billy", 77, false, strPointer("Doctor"))
+	newRoot, _ := dEnv.WorkingRoot(ctx)
+	newRoot, _ = dtestutils.AddRowToRoot(dEnv, ctx, newRoot, "addTable", r)
+	a, _, rm, _ := newRoot.TableDiff(ctx, oldRoot)
+	added, removed, renamed, _ := findRenames(ctx, newRoot, oldRoot, a, rm)
+	assert.Equal(t, []string{"addTable"}, added)
+	assert.Equal(t, []string{}, removed)
+	assert.Equal(t, map[string]string{}, renamed)
+
+	var stringWr StringBuilderCloser
+	_ = PrintSqlTableDiffs(ctx, newRoot, oldRoot, &stringWr)
+	expectedOutput := sql.SchemaAsCreateStmt("addTable", sch) + "\n"
+	expectedOutput = expectedOutput +
+		"INSERT INTO `addTable` (`id`,`name`,`age`,`is_married`,`title`) " +
+		"VALUES (\"00000000-0000-0000-0000-000000000000\",\"Big Billy\",77,FALSE,\"Doctor\");\n"
+	assert.Equal(t, expectedOutput, stringWr.String())
+}
+
+func TestSqlTableDiffsDrop(t *testing.T) {
+	ctx, sch, dEnv := setupSchema()
+
+	dtestutils.CreateTestTable(t, dEnv, "dropTable", sch, []row.Row{}...)
+	oldRoot, _ := dEnv.WorkingRoot(ctx)
+	newRoot, _ := oldRoot.RemoveTables(ctx, []string{"dropTable"}...)
+	a, _, rm, _ := newRoot.TableDiff(ctx, oldRoot)
+	added, drops, renamed, _ := findRenames(ctx, newRoot, oldRoot, a, rm)
+	assert.Equal(t, []string{"dropTable"}, drops)
+	assert.Equal(t, []string{}, added)
+	assert.Equal(t, map[string]string{}, renamed)
+
+	var stringWr StringBuilderCloser
+	_ = PrintSqlTableDiffs(ctx, newRoot, oldRoot, &stringWr)
+	expectedOutput := "DROP TABLE `dropTable`;\n"
+	assert.Equal(t, expectedOutput, stringWr.String())
+}
+
+func TestSqlTableDiffRename(t *testing.T) {
+	ctx, sch, dEnv := setupSchema()
+	dtestutils.CreateTestTable(t, dEnv, "renameTable", sch, []row.Row{}...)
+	oldRoot, _ := dEnv.WorkingRoot(ctx)
+	newRoot, _ := alterschema.RenameTable(ctx, dEnv.DoltDB, oldRoot, "renameTable", "newTableName")
+	a, _, rm, _ := newRoot.TableDiff(ctx, oldRoot)
+	added, removed, renames, _ := findRenames(ctx, newRoot, oldRoot, a, rm)
+	assert.Equal(t, map[string]string{"renameTable": "newTableName"}, renames)
+	assert.Equal(t, []string{}, removed)
+	assert.Equal(t, []string{}, added)
+
+	expectedOutput := "RENAME TABLE `renameTable` TO `newTableName`\n"
+	var stringWr StringBuilderCloser
+	_ = PrintSqlTableDiffs(ctx, newRoot, oldRoot, &stringWr)
+	assert.Equal(t, expectedOutput, stringWr.String())
+}
+
+func TestSqlTableDiffRenameChangedTable(t *testing.T) {
+	id := uuid.MustParse("00000000-0000-0000-0000-000000000000")
+	ctx, sch, dEnv := setupSchema()
+
+	dtestutils.CreateTestTable(t, dEnv, "renameTable", sch, []row.Row{}...)
+	oldRoot, _ := dEnv.WorkingRoot(ctx)
+	newRoot, _ := alterschema.RenameTable(ctx, dEnv.DoltDB, oldRoot, "renameTable", "newTableName")
+	r := dtestutils.NewTypedRow(id, "Big Billy", 77, false, strPointer("Doctor"))
+	newRoot, _ = dtestutils.AddRowToRoot(dEnv, ctx, newRoot, "newTableName", r)
+	a, _, rm, _ := newRoot.TableDiff(ctx, oldRoot)
+	added, removed, renamed, _ := findRenames(ctx, newRoot, oldRoot, a, rm)
+	assert.Equal(t, []string{"renameTable"}, removed)
+	assert.Equal(t, []string{"newTableName"}, added)
+	assert.Equal(t, map[string]string{}, renamed)
+
+	var stringWr StringBuilderCloser
+	_ = PrintSqlTableDiffs(ctx, newRoot, oldRoot, &stringWr)
+	expectedOutput := "DROP TABLE `renameTable`;\n"
+	expectedOutput = expectedOutput +
+		sql.SchemaAsCreateStmt("newTableName", sch) + "\n" +
+		"INSERT INTO `newTableName` (`id`,`name`,`age`,`is_married`,`title`) " +
+		"VALUES (\"00000000-0000-0000-0000-000000000000\",\"Big Billy\",77,FALSE,\"Doctor\");\n"
+	assert.Equal(t, expectedOutput, stringWr.String())
 }
