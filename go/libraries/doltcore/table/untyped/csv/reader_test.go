2c683f34676b53a2e730046b1a3abc30fbdcbd67
Andy Arthur <andy@liquidata.co>
2020-01-23 15:09:48 -0800

    adding field with newline to csv read tests

--- go/libraries/doltcore/table/untyped/csv/reader_test.go
+++ go/libraries/doltcore/table/untyped/csv/reader_test.go
@@ -29,11 +29,12 @@ import (
 
 var PersonDB1 = `name, Age, Title
 Bill Billerson, 32, Senior Dufus
-Rob Robertson, 25, Dufus
-John Johnson, 21, Intern Dufus`
+Rob Robertson, 25, "Assistant, Dufus"
+John Johnson, 21, "Intern
+Dufus"`
 
 var PersonDB2 = PersonDB1 + "\n"
-var PersonDB3 = strings.Replace(PersonDB2, "\n", "\n\n", -1)
+var PersonDB3 = strings.Replace(PersonDB2, "\n", "\n\n", 2) // don't replace quoted newline
 
 var PersonDBWithBadRow = `name, Age, Title
 Bill Billerson, 32, Senior Dufus
@@ -44,13 +45,15 @@ var PersonDBWithBadRow2 = PersonDBWithBadRow + "\n"
 var PersonDBWithBadRow3 = strings.Replace(PersonDBWithBadRow2, "\n", "\n\n", -1)
 
 var PersonDBWithoutHeaders = `Bill Billerson, 32, Senior Dufus
-Rob Robertson, 25, Dufus
-John Johnson, 21, Intern Dufus`
+Rob Robertson, 25, "Assistant, Dufus"
+John Johnson, 21, "Intern
+Dufus"`
 
 var PersonDBDifferentHeaders = `n, a, t
 Bill Billerson, 32, Senior Dufus
-Rob Robertson, 25, Dufus
-John Johnson, 21, Intern Dufus`
+Rob Robertson, 25, "Assistant, Dufus"
+John Johnson, 21, "Intern
+Dufus"`
 
 func mustRow(r row.Row, err error) row.Row {
 	if err != nil {
@@ -65,8 +68,8 @@ func TestReader(t *testing.T) {
 	_, sch := untyped.NewUntypedSchema(colNames...)
 	goodExpectedRows := []row.Row{
 		mustRow(untyped.NewRowFromStrings(types.Format_7_18, sch, []string{"Bill Billerson", "32", "Senior Dufus"})),
-		mustRow(untyped.NewRowFromStrings(types.Format_7_18, sch, []string{"Rob Robertson", "25", "Dufus"})),
-		mustRow(untyped.NewRowFromStrings(types.Format_7_18, sch, []string{"John Johnson", "21", "Intern Dufus"})),
+		mustRow(untyped.NewRowFromStrings(types.Format_7_18, sch, []string{"Rob Robertson", "25", "Assistant, Dufus"})),
+		mustRow(untyped.NewRowFromStrings(types.Format_7_18, sch, []string{"John Johnson", "21", "Intern\nDufus"})),
 	}
 	badExpectedRows := []row.Row{
 		mustRow(untyped.NewRowFromStrings(types.Format_7_18, sch, []string{"Bill Billerson", "32", "Senior Dufus"})),
