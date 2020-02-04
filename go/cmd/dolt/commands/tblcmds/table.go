bad4e83b52a9845764d17c2dd2f6ae8cee274588
Zach Musgrave <zach@liquidata.co>
2020-02-04 11:30:04 -0800

    Added bats test for name validation on table import. Fixed a bug in validation logic.:
    Signed-off-by: Zach Musgrave <zach@liquidata.co>

--- go/cmd/dolt/commands/tblcmds/table.go
+++ go/cmd/dolt/commands/tblcmds/table.go
@@ -33,7 +33,7 @@ func ValidateTableNameForCreate(tableName string) errhand.VerboseError {
 	if !doltdb.IsValidTableName(tableName) {
 		return errhand.BuildDError("'%s' is not a valid table name\ntable names must match the regular expression: %s",
 			tableName, doltdb.TableNameRegexStr).Build()
-	} else if doltdb.IsSystemTable(tableName) {
+	} else if doltdb.HasDoltPrefix(tableName) {
 		return errhand.BuildDError("'%s' is not a valid table name\ntable names beginning with dolt_ are reserved for internal use", tableName).Build()
 	}
 	return nil
