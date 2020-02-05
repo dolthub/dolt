b4315d6cd3342755e9e37fca803e4f57ad476886
Zach Musgrave <zach@liquidata.co>
2020-02-05 15:50:51 -0800

    Formatting
    Signed-off-by: Zach Musgrave <zach@liquidata.co>

--- go/cmd/dolt/commands/tblcmds/mv.go
+++ go/cmd/dolt/commands/tblcmds/mv.go
@@ -16,16 +16,15 @@ package tblcmds
 
 import (
 	"context"
-	"github.com/liquidata-inc/dolt/go/libraries/doltcore/doltdb"
-
-	eventsapi "github.com/liquidata-inc/dolt/go/gen/proto/dolt/services/eventsapi/v1alpha1"
-	"github.com/liquidata-inc/dolt/go/libraries/utils/filesys"
 
 	"github.com/liquidata-inc/dolt/go/cmd/dolt/cli"
 	"github.com/liquidata-inc/dolt/go/cmd/dolt/commands"
 	"github.com/liquidata-inc/dolt/go/cmd/dolt/errhand"
+	eventsapi "github.com/liquidata-inc/dolt/go/gen/proto/dolt/services/eventsapi/v1alpha1"
+	"github.com/liquidata-inc/dolt/go/libraries/doltcore/doltdb"
 	"github.com/liquidata-inc/dolt/go/libraries/doltcore/env"
 	"github.com/liquidata-inc/dolt/go/libraries/utils/argparser"
+	"github.com/liquidata-inc/dolt/go/libraries/utils/filesys"
 )
 
 var tblMvShortDesc = "Renames a table"
