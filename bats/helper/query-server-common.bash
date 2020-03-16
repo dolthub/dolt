f77a5d5965cfc8313778dbab39e96d123e6a844b
Brian Hendriks <brian@liquidata.co>
2020-03-15 17:30:01 -0700

    sql-server testing

---
+++ bats/helper/query-server-common.bash
@@ -0,0 +1,61 @@
+
+wait_for_connection() {
+    python -c "
+import os
+import sys
+
+args = sys.argv[sys.argv.index('--') + 1:]
+working_dir, port_str, timeout_ms = args
+os.chdir(working_dir)
+
+from pytest import wait_for_connection
+wait_for_connection(port=int(port_str), timeout_ms=int(timeout_ms))
+" -- $PYTEST_DIR $SQL_PORT $1
+}
+
+# server_query connects to a running mysql server, executes a query and compares the results against what is expected.
+# In the event that the results do not match expectations, the python process will exit with an exit code of 1
+#  * param1 is the query_str
+#  * param2 is a csv representing the expected result set.  If a query is not expected to have a result set "" should
+#      be passed.
+server_query() {
+    python -c "
+import os
+import sys
+
+args = sys.argv[sys.argv.index('--') + 1:]
+print(args)
+working_dir, port_str, query_str, query_results = args
+os.chdir(working_dir)
+
+from pytest import DoltConnection, csv_to_row_maps
+
+expected_rows = csv_to_row_maps(query_results)
+
+dc = DoltConnection(port=int(port_str))
+dc.connect()
+
+print('executing:', query_str)
+actual_rows = dc.query(query_str)
+
+print('expected:', expected_rows, '\n  actual:', actual_rows)
+
+if expected_rows != actual_rows:
+    print('expected:', expected_rows, '\n  actual:', actual_rows)
+    sys.exit(1)
+" -- $PYTEST_DIR $SQL_PORT "$1" "$2"
+}
+
+# update_query runs an update query and should be called with 2 parameters
+#   * param1 is the query string
+#   * param2 is the expected number of rows affected
+update_query() {
+    server_query "$1" "matched,updated\n$2,$2"
+}
+
+# insert_query runs an insert query and should be called with 2 parameters
+#   * param1 is the query string
+#   * param2 is the expected number of rows inserted
+insert_query() {
+    server_query "$1" "updated\n$2"
+}
---
+++ bats/sql-server.bats
@@ -0,0 +1,32 @@
+#!/usr/bin/env bats
+load $BATS_TEST_DIRNAME/helper/common.bash
+load $BATS_TEST_DIRNAME/helper/query-server-common.bash
+
+setup() {
+    setup_common
+    export PYTEST_DIR=`batshelper`
+    export SQL_PORT=$(($$ % (65536-1024) + 1024))
+    dolt sql-server --port=$SQL_PORT &
+    wait_for_connection 5000
+}
+
+teardown() {
+    pkill dolt
+    teardown_common
+}
+
+@test "run sql server and verify connection" {
+    server_query "SHOW tables" 'Tables'
+    server_query "CREATE TABLE one_pk (
+                    pk BIGINT NOT NULL COMMENT 'tag:0',
+                    c1 BIGINT COMMENT 'tag:1',
+                    c2 BIGINT COMMENT 'tag:2',
+                    PRIMARY KEY (pk)
+                  )" ""
+     insert_query "INSERT INTO one_pk (pk) values (0)" 1
+     server_query "SELECT * from one_pk" "pk,c1,c2\n0,None,None"
+     insert_query "INSERT INTO one_pk (pk,c1) values (1,1)" 1
+     insert_query "INSERT INTO one_pk (pk,c1,c2) values (2,2,2),(3,3,3)" 2
+     server_query "SELECT * from one_pk" "pk,c1,c2\n0,None,None\n1,1,None\n2,2,2\n3,3,3"
+     update_query "UPDATE one_pk SET c2=c1 WHERE c2 is NULL and c1 IS NOT NULL" 1
+}
