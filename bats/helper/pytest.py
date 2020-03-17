d963b0abc6e64060d572334ac4209ffae936a1b7
Brian Hendriks <brian@liquidata.co>
2020-03-17 14:06:35 -0700

    dolt sql-server bats test support (#469)
    * renamed files to be consistent across .bats files
    * dolt sql-server bats testing
    
    Co-authored-by: Aaron Son <aaron@liquidata.co>
    Co-authored-by: Daylon Wilkins <Hydrocharged@users.noreply.github.com>

---
+++ bats/helper/pytest.py
@@ -0,0 +1,99 @@
+import csv
+import sys
+import time
+import mysql.connector
+
+from io import StringIO
+from multiprocessing import Process
+
+
+def _connect(user, host, port, database):
+    return mysql.connector.connect(user=user, host=host, port=port, database=database)
+
+
+def _print_err_and_exit(e):
+    print(e, file=sys.stderr)
+    sys.exit(1)
+
+
+def csv_to_row_maps(csv_str):
+    csv_str = csv_str.replace('\\n', '\n')
+    rd = csv.DictReader(StringIO(csv_str))
+    rows = []
+    for row in rd:
+        rows.append(row)
+
+    return rows
+
+
+class DoltConnection(object):
+    def __init__(self, user='root', host='127.0.0.1', port=3306, database='dolt'):
+        self.user = user
+        self.host = host
+        self.port = port
+        self.database = database
+        self.cnx = None
+
+    def connect(self):
+        try:
+            self.cnx = _connect(self.user, self.host, self.port, self.database)
+        except BaseException as e:
+            _print_err_and_exit(e)
+
+    def close(self):
+        self.cnx.close()
+
+    def query(self, query_str):
+        try:
+            cursor = self.cnx.cursor()
+            cursor.execute(query_str)
+
+            if cursor.description is None:
+                return []
+
+            raw = cursor.fetchall()
+
+            row_maps = []
+            for curr in raw:
+                r = {}
+                for i, k in enumerate(cursor.column_names):
+                    r[k] = str(curr[i])
+                row_maps.append(r)
+
+            return row_maps
+
+        except BaseException as e:
+            _print_err_and_exit(e)
+
+
+class InfiniteRetryConnection(DoltConnection):
+    def connect(self):
+        while True:
+            try:
+                self.cnx = _connect(user=self.user, host=self.host, port=self.port, database=self.database)
+
+                try:
+                    self.cnx.close()
+                except BaseException:
+                    pass
+
+                return
+
+            except BaseException:
+                pass
+
+
+def wait_for_connection(user='root', host='127.0.0.1', port=3306, database='dolt', timeout_ms=5000):
+    timeoutf = timeout_ms / 1000.0
+    exit_zero_on_connect = InfiniteRetryConnection(user=user, host=host, port=port, database=database)
+
+    cnx_proc = Process(target=exit_zero_on_connect.connect)
+    cnx_proc.start()
+    cnx_proc.join(timeout=timeoutf)
+
+    if cnx_proc.exitcode is None:
+        cnx_proc.terminate()
+        _print_err_and_exit(Exception("Failed to establish connection in time."))
+    elif cnx_proc.exitcode != 0:
+        _print_err_and_exit(Exception("Connection process exited with exit code %d." % cnx_proc.exitcode))
+
---
+++ bats/helper/query-server-common.bash
@@ -0,0 +1,90 @@
+SERVER_REQS_INSTALLED="FALSE"
+SERVER_PID=""
+
+set_server_reqs_installed() {
+    SERVER_REQS_INSTALLED=$(python3 -c "
+requirements_installed = True
+try:
+    import mysql.connector
+except:
+    requirements_installed = False
+
+print(str(requirements_installed).upper())
+")
+}
+
+wait_for_connection() {
+    PYTEST_DIR="$BATS_TEST_DIRNAME/helper"
+    python3 -c "
+import os
+import sys
+
+args = sys.argv[sys.argv.index('--') + 1:]
+working_dir, port_str, timeout_ms = args
+os.chdir(working_dir)
+
+from pytest import wait_for_connection
+wait_for_connection(port=int(port_str), timeout_ms=int(timeout_ms))
+" -- $PYTEST_DIR $1 $2
+}
+
+start_sql_server() {
+    let PORT="$$ % (65536-1024) + 1024"
+    dolt sql-server --port=$PORT &
+    SERVER_PID=$!
+    wait_for_connection $PORT 5000
+}
+
+stop_sql_server() {
+    let PORT="$$ % (65536-1024) + 1024"
+    kill $SERVER_PID
+}
+
+# server_query connects to a running mysql server, executes a query and compares the results against what is expected.
+# In the event that the results do not match expectations, the python process will exit with an exit code of 1
+#  * param1 is the query_str
+#  * param2 is a csv representing the expected result set.  If a query is not expected to have a result set "" should
+#      be passed.
+server_query() {
+    let PORT="$$ % (65536-1024) + 1024"
+    PYTEST_DIR="$BATS_TEST_DIRNAME/helper"
+    python3 -c "
+import os
+import sys
+
+args = sys.argv[sys.argv.index('--') + 1:]
+print(args)
+working_dir, port_str, query_str, query_results = args
+os.chdir(working_dir)
+
+from pytest import DoltConnection, csv_to_row_maps
+
+expected_rows = csv_to_row_maps(query_results)
+
+dc = DoltConnection(port=int(port_str))
+dc.connect()
+
+print('executing:', query_str)
+actual_rows = dc.query(query_str)
+
+print('expected:', expected_rows, '\n  actual:', actual_rows)
+
+if expected_rows != actual_rows:
+    print('expected:', expected_rows, '\n  actual:', actual_rows)
+    sys.exit(1)
+" -- $PYTEST_DIR $PORT "$1" "$2"
+}
+
+# update_query runs an update query and should be called with 2 parameters
+#   * param1 is the query string
+#   * param2 is the expected number of rows affected
+update_query() {
+    server_query "$1" "matched,updated\n$2,$2"
+}
+
+# insert_query runs an insert query and should be called with 2 parameters
+#   * param1 is the query string
+#   * param2 is the expected number of rows inserted
+insert_query() {
+    server_query "$1" "updated\n$2"
+}
