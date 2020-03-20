81ed62c78ea1d4716bcd220ae63f430d6cace43e
Andy Arthur <andy@liquidata.co>
2020-03-20 11:03:26 -0700

    executing tests in isolation

--- bats/compatibility/runner.sh
+++ bats/compatibility/runner.sh
@@ -11,8 +11,20 @@ function setup_dir() {
   if [ -d "$1" ]; then rm -r "$1"; fi
   mkdir "$1"
   pushd "$1" > /dev/null || exit
-  "$top_dir/setup_repo.sh" > setup_repo.log
+
   cp -r "$top_dir"/bats/* .
+
+  mkdir "repo"
+  pushd "repo" > /dev/null || exit
+  "$top_dir/setup_repo.sh" > setup_repo.log
+  popd > /dev/null || exit
+
+  num_tests=$(($(grep -c '@test' *.bats)))
+  for ((i=1;i<=$num_tests;i++))
+  do
+    cp -r repo/ "test$i"
+  done
+
   popd > /dev/null || exit
 }
 
@@ -28,7 +40,7 @@ function run_bats_tests() {
 # ensure that we have a clean working change set before we begin
 if [[ $(git diff --stat) != '' ]]; then
   echo "cannot run compatibility tests with git working changes"
-  exit
+  exit 1
 fi
 
 # copy all the test files to take them out of source control
@@ -44,24 +56,27 @@ top_dir=$(pwd)
 starting_branch=$(git rev-parse --abbrev-ref HEAD)
 dolt_dir="../../../go/cmd/dolt/"
 
-# setup a repository with dolt built
-# from the current branch
+# for each legacy version, setup a repository
+# using dolt built from the current branch
 build_dolt "$starting_branch"
-setup_dir "head"
+while IFS= read -r ver
+do
+  setup_dir "$ver-forward_compat"
+done < <(grep -v '^ *#' < dolt_versions.txt)
 
 status=0
 
 while IFS= read -r ver
 do
   build_dolt "$ver"
-  setup_dir "$ver"
+  setup_dir "$ver-backward_compat"
 
   # run compatibility.bats to ensure dolt @ $ver can
   # read a repo created with dolt @ HEAD
   echo
-  echo "testing dolt @ $(git describe --tags) against repo in head/"
-  run_bats_tests head
-  #status=$((status+$?))
+  echo "testing dolt @ $(git describe --tags) against repo in $ver-forward_compat/"
+  run_bats_tests "$ver-forward_compat"
+  status=$((status+$?))
   echo
 done < <(grep -v '^ *#' < dolt_versions.txt)
 
@@ -72,8 +87,8 @@ build_dolt "$starting_branch"
 while IFS= read -r ver
 do
   echo
-  echo "testing dolt @ $(git rev-parse --abbrev-ref HEAD) against repo in $ver/"
-  run_bats_tests "$ver"
+  echo "testing dolt @ $(git rev-parse --abbrev-ref HEAD) against repo in $ver-backward_compat/"
+  run_bats_tests "$ver-backward_compat"
   status=$((status+$?))
   echo
 done < <(grep -v '^ *#' < dolt_versions.txt)
