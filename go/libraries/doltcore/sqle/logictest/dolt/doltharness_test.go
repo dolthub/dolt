a94c883d9c2aad533c6ff9cd79e58fd7f1ffb93b
Dustin Brown <dustin@liquidata.co>
2020-04-08 14:59:15 -0700

    /go/libraries/doltcore/sqle/logictest/dolt/doltharness_test.go: Refactor test structure

--- go/libraries/doltcore/sqle/logictest/dolt/doltharness_test.go
+++ go/libraries/doltcore/sqle/logictest/dolt/doltharness_test.go
@@ -23,53 +23,98 @@ func createTestEnvWithFS(fs filesys.Filesys, workingDir string) *env.DoltEnv {
 	return dEnv
 }
 
+type statementTest struct {
+	statement string
+	expErr error
+}
+
+type queryTest struct {
+	query string
+	expErr error
+	expSchema string
+	expResults []string
+}
+
 func TestDoltHarness(t *testing.T) {
-	t.Run("should execute simple query against dolt", func(t *testing.T) {
+	home := "/home"
+	wd := filepath.Join(home, "/harnesstester")
+
+	statementTests := []statementTest{
+		{
+			statement: "CREATE TABLE t1(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER);",
+			expErr: nil,
+		},
+		{
+			statement: "INSERT INTO t1(e,c,b,d,a) VALUES(103,102,100,101,104);",
+			expErr: nil,
+		},
+		{
+			statement: "INSERT INTO t1(a,c,d,e,b) VALUES(107,106,108,109,105);",
+			expErr: nil,
+		},
+	}
+
+	queryTests := []queryTest{
+		{
+			query: "SELECT a,c,e FROM t1;",
+			expErr: nil,
+			expSchema: "III",
+			expResults: []string{"104", "102", "103", "107", "106", "109"},
+		},
+		{
+			query: "SELECT b,d FROM t1;",
+			expErr: nil,
+			expSchema: "II",
+			expResults: []string{"100", "101", "105", "108"},
+		},
+		{
+			query: "SELECT * FROM t1 WHERE d < 107;",
+			expErr: nil,
+			expSchema: "IIIII",
+			expResults: []string{"104", "100", "102", "101", "103"},
+		},
+		{
+			query: "SELECT * FROM t1 WHERE d > 102;",
+			expErr: nil,
+			expSchema: "IIIII",
+			expResults: []string{"107", "105", "106", "108", "109"},
+		},
+	}
+
+	t.Run("should execute simple sql statements against Dolt", func(t *testing.T) {
 		h := &DoltHarness{}
-		home := "/home"
-		wd := filepath.Join(home, "/harnesstester")
 		fs := filesys.NewInMemFS([]string{}, nil, home)
 		dEnv := createTestEnvWithFS(fs, wd)
 
 		err := innerInit(h, dEnv)
 		assert.Equal(t, nil, err)
 
-		ct := "CREATE TABLE t1(a INTEGER, b INTEGER, c INTEGER, d INTEGER, e INTEGER);"
-		i1 := "INSERT INTO t1(e,c,b,d,a) VALUES(103,102,100,101,104);"
-		i2 := "INSERT INTO t1(a,c,d,e,b) VALUES(107,106,108,109,105);"
+		for _, test := range statementTests {
+			err = executeStatement(h, test.statement)
+			assert.Equal(t, test.expErr, err)
+		}
+	})
 
-		q1 := "SELECT a,c,e FROM t1;"
-		q2 := "SELECT b,d FROM t1;"
-		q3 := "SELECT * FROM t1 WHERE d < 107;"
-		q4 := "SELECT * FROM t1 WHERE d > 102;"
+	t.Run("should execute simple sql queries against Dolt", func(t *testing.T) {
+		h := &DoltHarness{}
+		fs := filesys.NewInMemFS([]string{}, nil, home)
+		dEnv := createTestEnvWithFS(fs, wd)
 
-		err = executeStatement(h, ct)
+		err := innerInit(h, dEnv)
 		assert.Equal(t, nil, err)
 
-		err = executeStatement(h, i1)
-		assert.Equal(t, nil, err)
+		// setup repo with statements
+		for _, test := range statementTests {
+			err = executeStatement(h, test.statement)
+			assert.Equal(t, test.expErr, err)
+		}
 
-		err = executeStatement(h, i2)
-		assert.Equal(t, nil, err)
-
-		schema, results, err := executeQuery(h, q1)
-		assert.Equal(t, nil, err)
-		assert.Equal(t, schema, "III")
-		assert.Equal(t, []string{"104", "102", "103", "107", "106", "109"}, results)
-
-		schema, results, err = executeQuery(h, q2)
-		assert.Equal(t, nil, err)
-		assert.Equal(t, schema, "II")
-		assert.Equal(t, []string{"100", "101", "105", "108"}, results)
-
-		schema, results, err = executeQuery(h, q3)
-		assert.Equal(t, nil, err)
-		assert.Equal(t, "IIIII", schema)
-		assert.Equal(t, []string{"104", "100", "102", "101", "103"}, results)
-
-		schema, results, err = executeQuery(h, q4)
-		assert.Equal(t, nil, err)
-		assert.Equal(t, "IIIII", schema)
-		assert.Equal(t, []string{"107", "105", "106", "108", "109"}, results)
+		// test queries
+		for _, test := range queryTests {
+			schema, results, err := executeQuery(h, test.query)
+			assert.Equal(t, test.expErr, err)
+			assert.Equal(t, test.expSchema, schema)
+			assert.Equal(t, test.expResults, results)
+		}
 	})
 }
