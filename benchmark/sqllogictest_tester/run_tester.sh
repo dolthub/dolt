704d18a6527c2d627887bad4ab7b8e711fa73316
Dustin Brown <dustin@liquidata.co>
2020-04-22 06:38:25 -0700

    /benchmark/sqllogictest_tester/run_tester.sh: Refactoring and testing

--- benchmark/sqllogictest_tester/run_tester.sh
+++ benchmark/sqllogictest_tester/run_tester.sh
@@ -11,8 +11,9 @@ base_dir=$(cd ../../ && pwd)
 dsp_dir=$(pwd)
 log_dir="$dsp_dir"/tempLogs
 sqllogictest_checkout="$base_dir/sqllogictest"
-logictest="$base_dir/go/libraries/doltcore/sqle/logictest"
+logictest="$base_dir/tempDolt/go/libraries/doltcore/sqle/logictest"
 logictest_main="$logictest"/main
+schema_dir="$base_dir/go/libraries/doltcore/sqle/logictest"
 old_path=$PATH
 
 
@@ -53,29 +54,9 @@ function setup_testing_dir() {
     for fd in "${test_list[@]}"
     do
          dir=$(dirname "$fd")
-
-         echo "This is the dirname:"
-         echo "$dir"
-
          mkdir -p "$TMP_TESTING_DIR"/"$dir"
-
-         echo "created dir:"
-         echo "$TMP_TESTING_DIR"/"$dir"
-
          cp -r "$sqllogictest_checkout"/test/"$fd" "$TMP_TESTING_DIR"/"$fd"
-
-         echo "copied file:"
-         ls "$TMP_TESTING_DIR"/"$dir"
     done
-
-    echo "Files/Directories that will be tested:"
-    find "$TMP_TESTING_DIR"
-}
-
-function setup_query_db() {
-    rm -f query_db
-    touch query_db
-    sqlite3 query_db < "$logictest"/regressions.sql
 }
 
 function with_dolt_commit() {
@@ -83,14 +64,29 @@ function with_dolt_commit() {
     (
       cd "$base_dir"/tempDolt/go
       git checkout master
-      git checkout -b "temp-$commit_hash" "$commit_hash"
+
+      exists=$(git branch --list "temp-$commit_hash"| sed '/^\s*$/d' | wc -l)
+
+      if [ "$exists" -eq 0 ]; then
+        git checkout -b "temp-$commit_hash" "$commit_hash";
+      else
+        git checkout "temp-$commit_hash";
+      fi
+
       git log -n 1
+
       if ! [ -x "$base_dir"/.ci_bin/"$commit_hash"/dolt ]; then
           if ! [ -d "$base_dir"/.ci_bin ]; then
             mkdir -p "$base_dir"/.ci_bin/"$commit_hash"
           fi
+
+          echo "This is where i am building dolt from the commit hash from:"
+          pwd
+
+          git status
           go get -mod=readonly ./...
           go build -mod=readonly -o "$base_dir"/.ci_bin/"$commit_hash"/dolt ./cmd/dolt/.
+          find "$base_dir"/.ci_bin/
       fi
     )
 
@@ -103,16 +99,16 @@ function with_dolt_commit() {
 function with_dolt_checkout() {
     (
       cd "$base_dir"/go
-      if ! [ -x "$base_dir"/.ci_bin/dolt ]; then
+      if ! [ -x "$base_dir"/.ci_bin/checkout/dolt ]; then
           if ! [ -d "$base_dir"/.ci_bin ]; then
-            mkdir -p "$base_dir"/.ci_bin
+            mkdir -p "$base_dir"/.ci_bin/checkout
           fi
           go get -mod=readonly ./...
-          go build -mod=readonly -o "$base_dir"/.ci_bin/dolt ./cmd/dolt/.
+          go build -mod=readonly -o "$base_dir"/.ci_bin/checkout/dolt ./cmd/dolt/.
       fi
     )
     echo "Finished installing dolt from checkout:"
-    export PATH="$base_dir/.ci_bin":$old_path
+    export PATH="$base_dir/.ci_bin/checkout":$old_path
     dolt version
     echo Using current path: "$PATH"
 }
@@ -127,6 +123,9 @@ function import_parsed() {
     dolt sql -r csv -q "\
     select * from nightly_dolt_results;"\
     > "$TMP_CSV_DIR"/"$commit_hash"_results.csv
+
+    cat "$TMP_CSV_DIR"/"$commit_hash"_results.csv
+
     dolt add nightly_dolt_results
     dolt commit -m "add results for dolt at commit $commit_hash"
     ls "$TMP_CSV_DIR"
@@ -136,15 +135,12 @@ function import_parsed() {
 function import_and_query_db() {
     rm -f query_db
     touch query_db
-    sqlite3 query_db < "$logictest"/regressions.sql
+    sqlite3 query_db < "$schema_dir"/regressions.sql
 
     local commit_hash="$1"
-#    local db_copy="$dsp_dir/query_db_$commit_hash"
     local release_csv="$TMP_CSV_DIR/release_results.csv"
     local commiter_csv="$TMP_CSV_DIR/${commit_hash}_results.csv"
 
-#    echo "$commiter_csv"
-#    cp "$dsp_dir"/query_db "$db_copy"
     sqlite3 query_db <<SQL
 .mode csv
 .import $commiter_csv nightly_dolt_results
@@ -154,8 +150,11 @@ SQL
   echo "sqlite tables:"
   sqlite3 query_db '.tables'
 
-  echo "sqlite all test_files:"
+  echo "sqlite all test files in release table:"
   sqlite3 query_db 'select test_file from releases_dolt_results group by test_file'
+
+  echo "sqlite all test files in committer (nightly) table:"
+  sqlite3 query_db 'select test_file from nightly_dolt_results group by test_file'
 
   result_query_output=`sqlite3 query_db 'select * from release_committer_result_change'`
 
@@ -173,21 +172,30 @@ function run_once() {
     local results="$log_dir/results-$commit_hash".log
     local parsed="$log_dir/parsed-$commit_hash".json
 
-    with_dolt_commit "$commit_hash"
+    (with_dolt_commit "$commit_hash"; dolt version; rm -rf .dolt; dolt init)
 
-    rm -rf .dolt
-    dolt init
+    (with_dolt_commit "$commit_hash"; pwd; echo "Running tests and creating $results"; go run . run "$TMP_TESTING_DIR" > "$results"; cat "$results")
 
-    pwd
-    echo "Running tests and logging raw results"
-    go run . run "$TMP_TESTING_DIR" > "$results"
+    (with_dolt_commit "$commit_hash"; echo "Parsing $results and generating $parsed"; go run . parse "$commit_hash" "$results" > "$parsed"; cat "$parsed")
 
-    echo "Parsing $results and generating $parsed"
-    go run . parse "$commit_hash" "$results" > "$parsed"
+#    with_dolt_commit "$commit_hash"
+#    rm -rf .dolt
+#    dolt init
+#
+#    pwd
+#    echo "Running tests and creating $results"
+#    go run . run "$TMP_TESTING_DIR" > "$results"
+#
+#    cat "$results"
+#
+#    echo "Parsing $results and generating $parsed"
+#    go run . parse "$commit_hash" "$results" > "$parsed"
+#
+#    cat "$parsed"
 
-    (with_dolt_checkout; cd "$dsp_dir"/dolt-sql-performance; import_parsed "$parsed" "$commit_hash")
+    (with_dolt_checkout; dolt version; cd "$dsp_dir"/dolt-sql-performance; import_parsed "$parsed" "$commit_hash")
 
-    import_and_query_db "$commit_hash"
+    (import_and_query_db "$commit_hash")
 }
 
 function run() {
@@ -199,47 +207,8 @@ function run() {
     do
         run_once "$c"
     done
+
     rm -rf .dolt
-}
-
-function import_nightly() {
-    dolt checkout nightly
-    seq 1 $TEST_N_TIMES | while read test_num; do
-        import_one_nightly "$test_num"
-    done
-    dolt sql -r csv -q "\
-select version, test_file, line_num, avg(duration) as mean_duration, result from dolt_history_nightly_dolt_results where version=\"${DOLT_VERSION}\" group by test_file, line_num;\
-" > nightly_mean.csv
-    dolt table import -u nightly_dolt_mean_results nightly_mean.csv
-    dolt add nightly_dolt_mean_results
-    dolt commit -m "update dolt sql performance mean results ($DOLT_VERSION)"
-    dolt push origin nightly
-
-    dolt checkout regressions
-    dolt merge nightly
-    dolt add .
-    dolt commit -m "merge nightly"
-    dolt push origin regressions
-
-    dolt checkout releases
-    dolt sql -r csv -q "\
-select * from releases_dolt_mean_results;\
-" > releases_mean.csv
-    rm -f regressions_db
-    touch regressions_db
-    sqlite3 regressions_db < ../"$logictest"/regressions.sql
-    cp ../"$logictest"/import.sql .
-    sqlite3 regressions_db < import.sql
-    echo "Checking for test regressions..."
-
-    duration_query_output=`sqlite3 regressions_db 'select * from releases_nightly_duration_change'`
-    result_query_output=`sqlite3 regressions_db 'select * from releases_nightly_result_change'`
-
-    duration_regressions=`echo $duration_query_output | sed '/^\s*$/d' | wc -l | tr -d '[:space:]'`
-    result_regressions=`echo $result_query_output | sed '/^\s*$/d' | wc -l | tr -d '[:space:]'`
-
-    if [ "$duration_regressions" != 0 ]; then echo "Duration regression found, $duration_regressions != 0" && echo $duration_query_output && exit 1; else echo "No duration regressions found"; fi
-    if [ "$result_regressions" != 0 ]; then echo "Result regression found, $result_regressions != 0" && echo $result_query_output && exit 1; else echo "No result regressions found"; fi
 }
 
 append() {
@@ -292,69 +261,13 @@ function fetch_repo() {
     dolt fetch origin
 }
 
-#rm -rf dolt-sql-performance
-#(with_dolt_checkout; dolt clone Liquidata/dolt-sql-performance)
-
 (with_dolt_checkout; setup)
 
 rm -rf dolt-sql-performance && mkdir dolt-sql-performance
 (with_dolt_checkout; cd dolt-sql-performance; fetch_repo)
 
-(with_dolt_checkout; setup_testing_dir; setup_query_db)
+(with_dolt_checkout; setup_testing_dir)
 
 (with_dolt_checkout; cd dolt-sql-performance; create_releases_csv)
 
 (cd "$logictest_main"; run)
-
-# then I want to cd into that dsp
-# checkout regresssions
-# run a query that gets the results for releases_dolt_results
-# but limited to only the test files that are specified in the TEST_LIST
-# get those results as csv
-
-# create a temporary test dir
-# copy all files/dirs from TEST_LIST into this test dir
-# setup a sqlite3 db with schema for releases and nightly -- setup_query_db
-
-# run a loop for each commit in COMMIT_LIST
-# use with_dolt_commit(COMMIT) which will checkout dolt repo at that commit and install it
-# then if LOG_RESULTS_ONLY is true, gen results but do not write to a file, so raw results
-
-# are output to Jenkins logs
-# otherwise, gen results to a results file
-# using dolt binary from master,
-# parse the results file
-# import the parsed results into nightly_dolt_results
-# get that data as a csv
-
-# cp the empty sqlite3 db
-# import release csv and commit(nightly) csv into sqlite3
-# run query to see if regressions has occured between release and commit
-# if it has not,
-# continue loop
-# if it has, output results of query to Jenkins log, fail the job
-
-
-
-
-# rm -rf dolt-sql-performance
-# (with_dolt_checkout; dolt clone Liquidata/dolt-sql-performance)
-
-# (with_dolt_checkout; cd "$logictest_main"; setup)
-
-#if [[ "$FAIL_ON_EXISTING_VERSION" == true ]]; then
-#  (with_dolt_checkout; cd dolt-sql-performance; check_version_exists)
-#fi
-
-#if [[ "$JOB_TYPE" == "release" ]]; then
-#   (with_dolt_release; cd "$logictest_main"; run)
-#else
-#   (with_dolt_checkout; cd "$logictest_main"; run)
-#fi
-
-#if [[ "$JOB_TYPE" == "nightly" ]]; then
-#  (with_dolt_checkout; cd dolt-sql-performance; import_nightly);
-#elif [ "$JOB_TYPE" == "release" ]; then
-#  (with_dolt_checkout; cd dolt-sql-performance; import_releases)
-#else fail Unknown JOB_TYPE specified;
-#fi
