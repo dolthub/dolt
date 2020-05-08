8f6352ec1e72c2bb20c5044af1e8543ebae000f7
Brian Hendriks <brian@liquidata.co>
2020-05-07 18:19:31 -0700

    fix

--- go/cmd/dolt/system_checks_linux.go
+++ go/cmd/dolt/system_checks_linux.go
@@ -63,7 +63,7 @@ func warnIfTmpDirMoveFails() {
 		_ = os.Remove(name)
 
 		cli.PrintErrln(color.YellowString("WARNING"))
-		cli.PrintErrln(color.YellowString("Failed to move the temporary file %s to the current directory."))
+		cli.PrintErrln(color.YellowString("Failed to move the temporary file %s to the current directory.", name))
 		cli.PrintErrln(color.YellowString("This is likely due to your temp directory not being located on the same volume as the current directory."))
 		cli.PrintErrln(color.YellowString("While we tackle this issue you can change your TMPDIR to be a directory on the current volume using the env var TMPDIR"))
 		cli.PrintErrln(color.YellowString("Visit https://github.com/liquidata-inc/dolt/issues/253 to find the state of this issue."))
