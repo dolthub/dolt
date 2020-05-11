e4dc4875cba8356e1592419bc153af8b98e53dc4
Dustin Brown <dustin@liquidata.co>
2020-05-11 11:57:03 -0700

    /benchmark/sqllogictest_tester/run_tester.sh: Iterating on wrong test_file fix

--- benchmark/sqllogictest_tester/run_tester.sh
+++ benchmark/sqllogictest_tester/run_tester.sh
@@ -53,6 +53,13 @@ function setup_testing_dir() {
          mkdir -p "$TMP_TESTING_DIR"/"$dir"
          cp -r "$sqllogictest_checkout"/test/"$fd" "$TMP_TESTING_DIR"/"$fd"
     done
+
+    # Keep only the tests relevant to this run
+    # tests must be kept in the `test` dir of sqllogictest
+    # so all test_file paths match
+    rm -rf "$sqllogictest_checkout"/test/
+    mkdir "$sqllogictest_checkout"/test/
+    cp -r "$TMP_TESTING_DIR"/* "$sqllogictest_checkout"/test/
 }
 
 function checkout_branch_if_exists() {
@@ -192,7 +199,7 @@ function run_once() {
       echo "Running tests and creating $results"
       echo "Using the following files/dirs:"
       find ../../../../../../../tempTesting
-      go run . run ../../../../../../../tempTesting > "$results"
+      go run . run "$sqllogictest_checkout"/test > "$results"
 #      go run . run "$TMP_TESTING_DIR" > "$results"
 
       echo "Parsing $results and generating $parsed"
