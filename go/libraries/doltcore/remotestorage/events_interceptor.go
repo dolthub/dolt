183478c3e21e5af2aa2b9e3a053dafce050cdc6f
Aaron Son <aaron@liquidata.co>
2020-05-18 18:37:19 -0700

    repo_fmt.

--- go/libraries/doltcore/remotestorage/events_interceptor.go
+++ go/libraries/doltcore/remotestorage/events_interceptor.go
@@ -15,16 +15,16 @@
 package remotestorage
 
 import (
-        "context"
+	"context"
 
-        "google.golang.org/grpc"
+	"google.golang.org/grpc"
 
 	eventsapi "github.com/liquidata-inc/dolt/go/gen/proto/dolt/services/eventsapi/v1alpha1"
 	"github.com/liquidata-inc/dolt/go/libraries/events"
 )
 
 func EventsUnaryClientInterceptor(collector *events.Collector) grpc.UnaryClientInterceptor {
-	return func (ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 		errCount := int32(0)
 		if event, ok := remoteAPIMethodsToEvents[method]; ok {
 			evt := events.NewEvent(event)
@@ -37,7 +37,7 @@ func EventsUnaryClientInterceptor(collector *events.Collector) grpc.UnaryClientI
 				collector.CloseEventAndAdd(evt)
 			}()
 		}
-        	err := invoker(ctx, method, req, reply, cc, opts...)
+		err := invoker(ctx, method, req, reply, cc, opts...)
 		if err != nil {
 			errCount++
 		}
@@ -46,13 +46,13 @@ func EventsUnaryClientInterceptor(collector *events.Collector) grpc.UnaryClientI
 }
 
 var remoteAPIMethodsToEvents map[string]eventsapi.ClientEventType = map[string]eventsapi.ClientEventType{
-	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/GetRepoMetadata": eventsapi.ClientEventType_REMOTEAPI_GET_REPO_METADATA,
-	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/HasChunks": eventsapi.ClientEventType_REMOTEAPI_HAS_CHUNKS,
+	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/GetRepoMetadata":      eventsapi.ClientEventType_REMOTEAPI_GET_REPO_METADATA,
+	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/HasChunks":            eventsapi.ClientEventType_REMOTEAPI_HAS_CHUNKS,
 	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/GetDownloadLocations": eventsapi.ClientEventType_REMOTEAPI_GET_DOWNLOAD_LOCATIONS,
-	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/GetUploadLocations": eventsapi.ClientEventType_REMOTEAPI_GET_UPLOAD_LOCATIONS,
-	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/Rebase": eventsapi.ClientEventType_REMOTEAPI_REBASE,
-	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/Root": eventsapi.ClientEventType_REMOTEAPI_ROOT,
-	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/Commit": eventsapi.ClientEventType_REMOTEAPI_COMMIT,
-	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/ListTableFiles": eventsapi.ClientEventType_REMOTEAPI_LIST_TABLE_FILES,
-//	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/AddTableFiles": eventsapi.ClientEventType_REMOTEAPI_ADD_TABLE_FILES,
+	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/GetUploadLocations":   eventsapi.ClientEventType_REMOTEAPI_GET_UPLOAD_LOCATIONS,
+	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/Rebase":               eventsapi.ClientEventType_REMOTEAPI_REBASE,
+	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/Root":                 eventsapi.ClientEventType_REMOTEAPI_ROOT,
+	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/Commit":               eventsapi.ClientEventType_REMOTEAPI_COMMIT,
+	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/ListTableFiles":       eventsapi.ClientEventType_REMOTEAPI_LIST_TABLE_FILES,
+	//	"/dolt.services.remotesapi.v1alpha1.ChunkStoreService/AddTableFiles": eventsapi.ClientEventType_REMOTEAPI_ADD_TABLE_FILES,
 }
