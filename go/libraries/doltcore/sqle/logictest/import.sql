ec9c291881052b1f83e7539fc6c4b780fa4281c6
Dustin Brown <dustin@liquidata.co>
2020-05-29 20:06:05 -0700

    /go/libraries/doltcore/sqle/logictest/import.sql: fix typo

--- go/libraries/doltcore/sqle/logictest/import.sql
+++ go/libraries/doltcore/sqle/logictest/import.sql
@@ -1,3 +1,3 @@
 .mode csv
 .import headerless_nightly_mean.csv nightly_dolt_mean_results
-.import heaserless_releases_mean.csv releases_dolt_mean_results
+.import headerless_releases_mean.csv releases_dolt_mean_results
