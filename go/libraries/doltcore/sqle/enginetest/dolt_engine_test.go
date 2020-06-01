fdf9b1ba6518397732d0cd5bf8d245179140b7b4
Zach Musgrave <zach@liquidata.co>
2020-06-01 14:40:44 -0700

    Working dolt harness. Most tests in suite passing.
    Signed-off-by: Zach Musgrave <zach@liquidata.co>

---
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -0,0 +1,142 @@
+// Copyright 2020 Liquidata, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package enginetest
+
+import (
+	"github.com/liquidata-inc/go-mysql-server/enginetest"
+	"testing"
+)
+
+func TestQueries(t *testing.T) {
+	enginetest.TestQueries(t, newDoltHarness(t))
+}
+
+// Tests of choosing the correct execution plan independent of result correctness. Mostly useful for confirming that
+// the right indexes are being used for joining tables.
+func TestQueryPlans(t *testing.T) {
+	enginetest.TestQueryPlans(t, newDoltHarness(t))
+}
+
+func TestQueryErrors(t *testing.T) {
+	enginetest.TestQueryErrors(t, newDoltHarness(t))
+}
+
+func TestInfoSchema(t *testing.T) {
+	enginetest.TestInfoSchema(t, newDoltHarness(t))
+}
+
+func TestColumnAliases(t *testing.T) {
+	enginetest.TestColumnAliases(t, newDoltHarness(t))
+}
+
+func TestOrderByGroupBy(t *testing.T) {
+	enginetest.TestOrderByGroupBy(t, newDoltHarness(t))
+}
+
+func TestAmbiguousColumnResolution(t *testing.T) {
+	enginetest.TestAmbiguousColumnResolution(t, newDoltHarness(t))
+}
+
+func TestInsertInto(t *testing.T) {
+	enginetest.TestInsertInto(t, newDoltHarness(t))
+}
+
+func TestInsertIntoErrors(t *testing.T) {
+	enginetest.TestInsertIntoErrors(t, newDoltHarness(t))
+}
+
+func TestReplaceInto(t *testing.T) {
+	enginetest.TestReplaceInto(t, newDoltHarness(t))
+}
+
+func TestReplaceIntoErrors(t *testing.T) {
+	enginetest.TestReplaceIntoErrors(t, newDoltHarness(t))
+}
+
+func TestUpdate(t *testing.T) {
+	enginetest.TestUpdate(t, newDoltHarness(t))
+}
+
+func TestUpdateErrors(t *testing.T) {
+	enginetest.TestUpdateErrors(t, newDoltHarness(t))
+}
+
+func TestDeleteFrom(t *testing.T) {
+	enginetest.TestDelete(t, newDoltHarness(t))
+}
+
+func TestDeleteFromErrors(t *testing.T) {
+	enginetest.TestDeleteErrors(t, newDoltHarness(t))
+}
+
+func TestCreateTable(t *testing.T) {
+	enginetest.TestCreateTable(t, newDoltHarness(t))
+}
+
+func TestDropTable(t *testing.T) {
+	enginetest.TestDropTable(t, newDoltHarness(t))
+}
+
+func TestRenameTable(t *testing.T) {
+	enginetest.TestRenameTable(t, newDoltHarness(t))
+}
+
+func TestRenameColumn(t *testing.T) {
+	enginetest.TestRenameColumn(t, newDoltHarness(t))
+}
+
+func TestAddColumn(t *testing.T) {
+	enginetest.TestAddColumn(t, newDoltHarness(t))
+}
+
+func TestModifyColumn(t *testing.T) {
+	enginetest.TestModifyColumn(t, newDoltHarness(t))
+}
+
+func TestDropColumn(t *testing.T) {
+	enginetest.TestDropColumn(t, newDoltHarness(t))
+}
+
+func TestExplode(t *testing.T) {
+	enginetest.TestExplode(t, newDoltHarness(t))
+}
+
+func TestReadOnly(t *testing.T) {
+	enginetest.TestReadOnly(t, newDoltHarness(t))
+}
+
+func TestViews(t *testing.T) {
+	enginetest.TestViews(t, newDoltHarness(t))
+}
+
+func TestVersionedViews(t *testing.T) {
+	enginetest.TestVersionedViews(t, newDoltHarness(t))
+}
+
+func TestNaturalJoin(t *testing.T) {
+	enginetest.TestNaturalJoin(t, newDoltHarness(t))
+}
+
+func TestNaturalJoinEqual(t *testing.T) {
+	enginetest.TestNaturalJoinEqual(t, newDoltHarness(t))
+}
+
+func TestNaturalJoinDisjoint(t *testing.T) {
+	enginetest.TestNaturalJoinDisjoint(t, newDoltHarness(t))
+}
+
+func TestInnerNestedInNaturalJoins(t *testing.T) {
+	enginetest.TestInnerNestedInNaturalJoins(t, newDoltHarness(t))
+}
---
+++ go/libraries/doltcore/sqle/enginetest/dolt_harness.go
@@ -0,0 +1,84 @@
+// Copyright 2020 Liquidata, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package enginetest
+
+import (
+	"context"
+	"github.com/liquidata-inc/dolt/go/libraries/doltcore/dtestutils"
+	"github.com/liquidata-inc/dolt/go/libraries/doltcore/env"
+	"github.com/liquidata-inc/dolt/go/libraries/doltcore/sqle"
+	"github.com/liquidata-inc/go-mysql-server/enginetest"
+	"github.com/liquidata-inc/go-mysql-server/sql"
+	"github.com/stretchr/testify/require"
+	"testing"
+
+	. "github.com/liquidata-inc/dolt/go/libraries/doltcore/sql/sqltestutil"
+)
+
+type doltHarness struct {
+	t       *testing.T
+	session *sqle.DoltSession
+	mrEnv   env.MultiRepoEnv
+}
+
+var _ enginetest.Harness = (*doltHarness)(nil)
+
+func newDoltHarness(t *testing.T) *doltHarness {
+	return &doltHarness{
+		t:       t,
+		session: sqle.DefaultDoltSession(),
+		mrEnv:   make(env.MultiRepoEnv),
+	}
+}
+
+func (d *doltHarness) Parallelism() int {
+	return 1
+}
+
+func (d *doltHarness) NewContext() *sql.Context {
+	return sql.NewContext(
+		context.Background(),
+		sql.WithSession(d.session),
+		sql.WithViewRegistry(sql.NewViewRegistry()),
+	)
+}
+
+func (d *doltHarness) NewDatabase(name string) sql.Database {
+	dEnv := dtestutils.CreateTestEnv()
+	CreateEmptyTestDatabase(dEnv, d.t)
+	root, err := dEnv.WorkingRoot(enginetest.NewContext(d))
+	require.NoError(d.t, err)
+
+	d.mrEnv.AddEnv(name, dEnv)
+	db := sqle.NewDatabase(name, dEnv.DoltDB, dEnv.RepoState, dEnv.RepoStateWriter())
+	err = db.SetRoot(enginetest.NewContext(d).WithCurrentDB(db.Name()), root)
+	require.NoError(d.t, err)
+	return db
+}
+
+func (d *doltHarness) NewTable(db sql.Database, name string, schema sql.Schema) (sql.Table, error) {
+	doltDatabase := db.(sqle.Database)
+	err := doltDatabase.CreateTable(enginetest.NewContext(d).WithCurrentDB(db.Name()), name, schema)
+	if err != nil {
+		return nil, err
+	}
+
+	table, ok, err := doltDatabase.GetTableInsensitive(enginetest.NewContext(d).WithCurrentDB(db.Name()), name)
+
+	require.NoError(d.t, err)
+	require.True(d.t, ok, "table %s not found after creation", name)
+	return table, nil
+}
+
