7f87808987dfbbaca9c87d7f18d8a3c11050ffeb
Aaron Son <aaron@liquidata.co>
2020-07-07 14:55:51 -0700

    go/libraries/doltcore/env: repo_state.go: Remove MergeState.Head field, which is unused.

--- go/libraries/doltcore/env/repo_state.go
+++ go/libraries/doltcore/env/repo_state.go
@@ -44,7 +44,6 @@ type BranchConfig struct {
 }
 
 type MergeState struct {
-	Head            ref.MarshalableRef `json:"head"`
 	Commit          string             `json:"commit"`
 	PreMergeWorking string             `json:"working_pre_merge"`
 }
@@ -145,7 +144,7 @@ func (rs *RepoState) CWBHeadSpec() *doltdb.CommitSpec {
 }
 
 func (rs *RepoState) StartMerge(dref ref.DoltRef, commit string, fs filesys.Filesys) error {
-	rs.Merge = &MergeState{ref.MarshalableRef{Ref: dref}, commit, rs.Working}
+	rs.Merge = &MergeState{commit, rs.Working}
 	return rs.Save(fs)
 }
 
