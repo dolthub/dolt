ba0a65f7f8caefcdf957e83f50192457e49ff928
Andy Arthur <andy@liquidata.co>
2020-07-14 13:29:12 -0700

    lookup commit hashes instead of pinning them

--- go/libraries/doltcore/sqle/integration_test/history_table_test.go
+++ go/libraries/doltcore/sqle/integration_test/history_table_test.go
@@ -16,28 +16,25 @@ package integration_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
-	"time"
 
-	"github.com/liquidata-inc/go-mysql-server/sql"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/liquidata-inc/dolt/go/cmd/dolt/cli"
 	"github.com/liquidata-inc/dolt/go/cmd/dolt/commands"
-	"github.com/liquidata-inc/dolt/go/libraries/doltcore/doltdb"
 	"github.com/liquidata-inc/dolt/go/libraries/doltcore/dtestutils"
+	"github.com/liquidata-inc/dolt/go/libraries/doltcore/env"
 	"github.com/liquidata-inc/dolt/go/libraries/doltcore/sqle"
+	"github.com/liquidata-inc/go-mysql-server/sql"
 )
 
 func TestHistoryTable(t *testing.T) {
-	// pin the commit time to get const hashes
-	doltdb.CommitNowFunc = constTimeFunc
-	doltdb.CommitLoc = time.UTC
-
-	for _, test := range historyTableTests {
+	dEnv := setupHistoryTests(t)
+	for _, test := range historyTableTests() {
 		t.Run(test.name, func(t *testing.T) {
-			testHistoryTable(t, test)
+			testHistoryTable(t, test, dEnv)
 		})
 	}
 }
@@ -73,134 +70,159 @@ var setupCommon = []testCommand{
 	{commands.SqlCmd{}, []string{"-q", "update test set c0 = c0+10 where c0 % 2 = 0"}},
 	{commands.AddCmd{}, []string{"."}},
 	{commands.CommitCmd{}, []string{"-m", "fourth"}},
+	{commands.LogCmd{}, []string{}},
 }
 
-var historyTableTests = []historyTableTest{
-	{
-		name:  "select pk, c0 from dolt_history_test",
-		query: "select pk, c0 from dolt_history_test",
-		rows: []sql.Row{
-			{int32(0), int32(10)},
-			{int32(1), int32(1)},
-			{int32(2), int32(12)},
-			{int32(3), int32(3)},
-			{int32(0), int32(0)},
-			{int32(1), int32(1)},
-			{int32(2), int32(2)},
-			{int32(3), int32(3)},
-			{int32(0), int32(0)},
-			{int32(1), int32(1)},
+func historyTableTests() []historyTableTest {
+	return []historyTableTest{
+		{
+			name:  "select pk, c0 from dolt_history_test",
+			query: "select pk, c0 from dolt_history_test",
+			rows: []sql.Row{
+				{int32(0), int32(10)},
+				{int32(1), int32(1)},
+				{int32(2), int32(12)},
+				{int32(3), int32(3)},
+				{int32(0), int32(0)},
+				{int32(1), int32(1)},
+				{int32(2), int32(2)},
+				{int32(3), int32(3)},
+				{int32(0), int32(0)},
+				{int32(1), int32(1)},
+			},
 		},
-	},
-	{
-		name:  "select commit_hash from dolt_history_test",
-		query: "select commit_hash from dolt_history_test",
-		rows: []sql.Row{
-			{"4l64009toat97c0eama1j7hbst4br2l5"},
-			{"4l64009toat97c0eama1j7hbst4br2l5"},
-			{"4l64009toat97c0eama1j7hbst4br2l5"},
-			{"4l64009toat97c0eama1j7hbst4br2l5"},
-			{"u1to822j6l5nf1nr0jjoum45nm0mo495"},
-			{"u1to822j6l5nf1nr0jjoum45nm0mo495"},
-			{"u1to822j6l5nf1nr0jjoum45nm0mo495"},
-			{"u1to822j6l5nf1nr0jjoum45nm0mo495"},
-			{"tq3f54aftrgie4h5v9746sjftlkstrgg"},
-			{"tq3f54aftrgie4h5v9746sjftlkstrgg"},
+		{
+			name:  "select commit_hash from dolt_history_test",
+			query: "select commit_hash from dolt_history_test",
+			rows: []sql.Row{
+				{HEAD},
+				{HEAD},
+				{HEAD},
+				{HEAD},
+				{HEAD_1},
+				{HEAD_1},
+				{HEAD_1},
+				{HEAD_1},
+				{HEAD_2},
+				{HEAD_2},
+			},
 		},
-	},
-	{
-		name:  "filter for a specific commit hash",
-		query: "select * from dolt_history_test where commit_hash = 'u1to822j6l5nf1nr0jjoum45nm0mo495';",
-		rows: []sql.Row{
-			{int32(0), int32(0), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(2), int32(2), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(3), int32(3), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
+		{
+			name:  "filter for a specific commit hash",
+			query: fmt.Sprintf("select pk, c0, commit_hash from dolt_history_test where commit_hash = '%s';", HEAD_1),
+			rows: []sql.Row{
+				{int32(0), int32(0), HEAD_1},
+				{int32(1), int32(1), HEAD_1},
+				{int32(2), int32(2), HEAD_1},
+				{int32(3), int32(3), HEAD_1},
+			},
 		},
-	},
-	{
-		name:  "filter out a specific commit hash",
-		query: "select * from dolt_history_test where commit_hash != 'u1to822j6l5nf1nr0jjoum45nm0mo495';",
-		rows: []sql.Row{
-			{int32(0), int32(10), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(2), int32(12), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(3), int32(3), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(0), int32(0), "tq3f54aftrgie4h5v9746sjftlkstrgg", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "tq3f54aftrgie4h5v9746sjftlkstrgg", "billy bob", constTimeFunc()},
+		{
+			name:  "filter out a specific commit hash",
+			query: fmt.Sprintf("select pk, c0, commit_hash from dolt_history_test where commit_hash != '%s';", HEAD_1),
+			rows: []sql.Row{
+				{int32(0), int32(10), HEAD},
+				{int32(1), int32(1), HEAD},
+				{int32(2), int32(12), HEAD},
+				{int32(3), int32(3), HEAD},
+				{int32(0), int32(0), HEAD_2},
+				{int32(1), int32(1), HEAD_2},
+			},
 		},
-	},
-	{
-		name: "compound or filter on commit hash",
-		query: "select * from dolt_history_test where " +
-			"commit_hash = 'u1to822j6l5nf1nr0jjoum45nm0mo495' or " +
-			"commit_hash = 'tq3f54aftrgie4h5v9746sjftlkstrgg';",
-		rows: []sql.Row{
-			{int32(0), int32(0), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(2), int32(2), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(3), int32(3), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(0), int32(0), "tq3f54aftrgie4h5v9746sjftlkstrgg", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "tq3f54aftrgie4h5v9746sjftlkstrgg", "billy bob", constTimeFunc()},
+		{
+			name: "compound or filter on commit hash",
+			query: fmt.Sprintf("select pk, c0, commit_hash from dolt_history_test " +
+				"where commit_hash = '%s' or commit_hash = '%s';", HEAD_1, HEAD_2),
+			rows: []sql.Row{
+				{int32(0), int32(0), HEAD_1},
+				{int32(1), int32(1), HEAD_1},
+				{int32(2), int32(2), HEAD_1},
+				{int32(3), int32(3), HEAD_1},
+				{int32(0), int32(0), HEAD_2},
+				{int32(1), int32(1), HEAD_2},
+			},
 		},
-	},
-	{
-		name: "commit hash in value set",
-		query: "select * from dolt_history_test where commit_hash in " +
-			"('u1to822j6l5nf1nr0jjoum45nm0mo495', " +
-			" 'tq3f54aftrgie4h5v9746sjftlkstrgg');",
-		rows: []sql.Row{
-			{int32(0), int32(0), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(2), int32(2), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(3), int32(3), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(0), int32(0), "tq3f54aftrgie4h5v9746sjftlkstrgg", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "tq3f54aftrgie4h5v9746sjftlkstrgg", "billy bob", constTimeFunc()},
+		{
+			name: "commit hash in value set",
+			query: fmt.Sprintf("select pk, c0, commit_hash from dolt_history_test " +
+				"where commit_hash in ('%s', '%s');", HEAD_1, HEAD_2),
+			rows: []sql.Row{
+				{int32(0), int32(0), HEAD_1},
+				{int32(1), int32(1), HEAD_1},
+				{int32(2), int32(2), HEAD_1},
+				{int32(3), int32(3), HEAD_1},
+				{int32(0), int32(0), HEAD_2},
+				{int32(1), int32(1), HEAD_2},
+			},
 		},
-	},
-	{
-		name: "commit hash not in value set",
-		query: "select * from dolt_history_test where commit_hash not in " +
-			"('u1to822j6l5nf1nr0jjoum45nm0mo495', " +
-			" 'tq3f54aftrgie4h5v9746sjftlkstrgg');",
-		rows: []sql.Row{
-			{int32(0), int32(10), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(2), int32(12), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(3), int32(3), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
+		{
+			name: "commit hash not in value set",
+			query: fmt.Sprintf("select pk, c0, commit_hash from dolt_history_test " +
+				"where commit_hash not in ('%s','%s');", HEAD_1, HEAD_2),
+			rows: []sql.Row{
+				{int32(0), int32(10), HEAD},
+				{int32(1), int32(1), HEAD},
+				{int32(2), int32(12), HEAD},
+				{int32(3), int32(3), HEAD},
+			},
 		},
-	},
-	{
-		name:  "commit is not null",
-		query: "select * from dolt_history_test where commit_hash is not null;",
-		rows: []sql.Row{
-			{int32(0), int32(10), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(2), int32(12), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(3), int32(3), "4l64009toat97c0eama1j7hbst4br2l5", "billy bob", constTimeFunc()},
-			{int32(0), int32(0), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(2), int32(2), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(3), int32(3), "u1to822j6l5nf1nr0jjoum45nm0mo495", "billy bob", constTimeFunc()},
-			{int32(0), int32(0), "tq3f54aftrgie4h5v9746sjftlkstrgg", "billy bob", constTimeFunc()},
-			{int32(1), int32(1), "tq3f54aftrgie4h5v9746sjftlkstrgg", "billy bob", constTimeFunc()},
+		{
+			name:  "commit is not null",
+			query: fmt.Sprintf("select pk, c0, commit_hash from dolt_history_test where commit_hash is not null;"),
+			rows: []sql.Row{
+				{int32(0), int32(10), HEAD},
+				{int32(1), int32(1), HEAD},
+				{int32(2), int32(12), HEAD},
+				{int32(3), int32(3), HEAD},
+				{int32(0), int32(0), HEAD_1},
+				{int32(1), int32(1), HEAD_1},
+				{int32(2), int32(2), HEAD_1},
+				{int32(3), int32(3), HEAD_1},
+				{int32(0), int32(0), HEAD_2},
+				{int32(1), int32(1), HEAD_2},
+			},
 		},
-	},
-	{
-		name:  "commit is null",
-		query: "select * from dolt_history_test where commit_hash is null;",
-		rows:  []sql.Row{},
-	},
+		{
+			name:  "commit is null",
+			query: "select * from dolt_history_test where commit_hash is null;",
+			rows:  []sql.Row{},
+		},
+	}
 }
 
-func testHistoryTable(t *testing.T, test historyTableTest) {
+var HEAD   = ""  // HEAD
+var HEAD_1 = ""  // HEAD~1
+var HEAD_2 = ""  // HEAD~2
+var HEAD_3 = ""  // HEAD~3
+var INIT   = ""  // HEAD~4
+
+func setupHistoryTests(t *testing.T) *env.DoltEnv {
 	ctx := context.Background()
 	dEnv := dtestutils.CreateTestEnv()
-
 	for _, c := range setupCommon {
 		exitCode := c.cmd.Exec(ctx, c.cmd.Name(), c.args, dEnv)
 		require.Equal(t, 0, exitCode)
 	}
+
+	root, err := dEnv.WorkingRoot(ctx)
+	require.NoError(t, err)
+
+	// get commit hashes from the log table
+	q := "select commit_hash, date from dolt_log order by date desc;"
+	rows, err := sqle.ExecuteSelect(dEnv, dEnv.DoltDB, root, q)
+	require.NoError(t, err)
+	require.Equal(t, 5, len(rows))
+	HEAD   = rows[0][0].(string)
+	HEAD_1 = rows[1][0].(string)
+	HEAD_2 = rows[2][0].(string)
+	HEAD_3 = rows[3][0].(string)
+	INIT   = rows[4][0].(string)
+
+	return dEnv
+}
+
+func testHistoryTable(t *testing.T, test historyTableTest, dEnv *env.DoltEnv) {
+	ctx := context.Background()
 	for _, c := range test.setup {
 		exitCode := c.cmd.Exec(ctx, c.cmd.Name(), c.args, dEnv)
 		require.Equal(t, 0, exitCode)
@@ -216,8 +238,4 @@ func testHistoryTable(t *testing.T, test historyTableTest) {
 	for i := range test.rows {
 		assert.Equal(t, test.rows[i], actRows[i])
 	}
-}
-
-func constTimeFunc() time.Time {
-	return time.Unix(0, 0).In(time.UTC)
 }
