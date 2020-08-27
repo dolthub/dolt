12f2857623d8ac2d36636307b450633b3dcb9db0
Dustin Brown <dustin@liquidata.co>
2020-08-27 16:19:44 -0700

    /{.github/, MySQLDockerfile}: Add private actions for mysql client integration tests

--- mysql-client-tests-entrypoint.sh
+++ mysql-client-tests-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+echo "updating dolt config for tests:"
 dolt config --global --add metrics.disabled true
 dolt config --global --add metrics.host localhost
 dolt config --global --add user.name mysql-test-runner
 dolt config --global --add user.email mysql-test-runner@liquidata.co
 
+echo "Running mysql-client-tests:"
 bats /mysql-client-tests/mysql-client-tests.bats
