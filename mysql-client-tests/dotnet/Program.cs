0ef69dababedcfe23562ebe94fe32dbc30595b4a
Dustin Brown <dustin@liquidata.co>
2020-08-31 17:05:11 -0700

    /mysql-client-tests/dotnet: Iterating on dotnet mysql client test

---
+++ mysql-client-tests/dotnet/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+public class Tutorial1
+{
+    public static int Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            System.Console.WriteLine("Must supply $USER $PORT $REPO arguments.");
+            return 1;
+        }
+
+        var user = args[0];
+        var port = args[1];
+        var db = args[2];
+
+        //Console.WriteLine(user);
+        //Console.WriteLine(port);
+        //Console.WriteLine(db);
+
+        string connStr = $"server=127.0.0.1;user={user};database={db};port={port};";
+        Console.WriteLine(connStr);
+        MySqlConnection conn = new MySqlConnection(connStr);
+        try
+        {
+            Console.WriteLine("Connecting to MySQL...");
+            conn.Open();
+            // Perform database operations
+
+            SetupTest(conn);
+            ExecuteTest(conn);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        conn.Close();
+        Console.WriteLine("Done.");
+        return 0;
+    }
+
+    public static void SetupTest(MySqlConnection conn)
+    {
+        using var cmd = new MySqlCommand();
+        cmd.Connection = conn;
+
+        cmd.CommandText = "DROP TABLE IF EXISTS test";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = @"CREATE TABLE test (pk int, value int, primary key(pk))";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = "INSERT INTO test(pk, value) VALUES(0,0)";
+        cmd.ExecuteNonQuery();
+    }
+
+    public static void ExecuteTest(MySqlConnection conn)
+    {
+        string sql = "SELECT * FROM test";
+        using var cmd = new MySqlCommand(sql, conn);
+
+        using MySqlDataReader rdr = cmd.ExecuteReader();
+
+        while (rdr.Read())
+        {
+            // make sure results are correct
+            Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+        }
+        rdr.Close();
+    }
+}
