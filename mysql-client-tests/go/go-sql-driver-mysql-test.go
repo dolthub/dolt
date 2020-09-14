0ff2efe5c6ea688b3efa8ca44d500c1ae3a2bfad
Timothy Sehn <tim@liquidata.co>
2020-09-14 16:52:55 -0700

    Working go code. Gotta get Docker working.

--- mysql-client-tests/go/go-sql-driver-mysql-test.go
+++ mysql-client-tests/go/go-sql-driver-mysql-test.go
@@ -6,6 +6,13 @@ import "fmt"
 import "database/sql"
 import _ "github.com/go-sql-driver/mysql"
 
+var queries [5]string = [5]string{
+    "create table test (pk int, value int, primary key(pk))",
+    "describe test",
+    "select * from test",
+    "insert into test (pk, value) values (0,0)",
+    "select * from test"}
+
 func main() {
     var user = os.Args[1]
     var port = os.Args[2]
@@ -28,5 +35,18 @@ func main() {
         panic(err)
     }
 
-    os.Exit(1)
+    for _, query := range queries {
+        rows, err := database.Query(query)
+        if err != nil {
+	    fmt.Println("QUERY: " + query)
+	    panic(err)
+        }
+	
+	// Ignoring result as only way to get results is rows.Next()
+	// Requires custom typoing of the results.
+	
+	rows.Close()
+    }
+
+    os.Exit(0)
 }
