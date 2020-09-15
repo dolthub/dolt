fd90fec266a22338a6f4c234025c47332aa59795
Timothy Sehn <tim@liquidata.co>
2020-09-15 16:33:49 -0700

    Got C++ test working locally per Aaron's example

--- mysql-client-tests/cpp/mysql-connector-cpp-test.cpp
+++ mysql-client-tests/cpp/mysql-connector-cpp-test.cpp
@@ -1,30 +1,17 @@
-#include <stdio.h>
-#include <string>
+#include <iostream>
 
-#include <mysqlx/xdevapi.h>
+#include "mysql_driver.h"
 
-#define QUERIES_SIZE 5
-
-using namespace ::mysqlx;
-
-const char *queries[QUERIES_SIZE] =
-  {
-   "create table test (pk int, value int, primary key(pk))",
-   "describe test",
-   "select * from test",
-   "insert into test (pk, value) values (0,0)", 
-   "select * from test"
-  };
-
-int main(int argc, char **argv) { 
-
+int main(int argc, char **argv) {
   std::string user = argv[1];
   std::string port = argv[2];
   std::string db   = argv[3];
-
-  std::string url = "mysqlx://" + user + "@127.0.0.1:" + port;
-
-  Session sess(url);
   
-  return 0;
+  sql::mysql::MySQL_Driver *driver;
+  sql::Connection *con;
+  
+  driver = sql::mysql::get_mysql_driver_instance();
+  con = driver->connect("tcp://127.0.0.1:" + port, user, "");
+  
+  delete con;
 }
