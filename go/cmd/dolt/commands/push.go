6089d7e15d5fe4b02a4dc13630289baee7f937b0
Taylor Bantle <taylor@liquidata.co>
2020-11-30 13:23:56 -0800

    Use FromError to match PermissionDenied error on push

--- go/cmd/dolt/commands/push.go
+++ go/cmd/dolt/commands/push.go
@@ -21,6 +21,8 @@ import (
 	"time"
 
 	"github.com/dustin/go-humanize"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/dolthub/dolt/go/cmd/dolt/cli"
 	"github.com/dolthub/dolt/go/cmd/dolt/errhand"
@@ -392,8 +394,8 @@ func pushToRemoteBranch(ctx context.Context, dEnv *env.DoltEnv, mode ref.RefUpda
 				cli.Println("hint: 'dolt pull ...') before pushing again.")
 				return errhand.BuildDError("").Build()
 			} else {
-				errString := err.Error()
-				if errString == "rpc error: code = PermissionDenied desc = permission denied" {
+				status, ok := status.FromError(err)
+				if ok && status.Code() == codes.PermissionDenied {
 					cli.Println("hint: have you logged into DoltHub using 'dolt login'?")
 					cli.Println("hint: check that user.email in 'dolt config --list' has write perms to DoltHub repo")
 				}
