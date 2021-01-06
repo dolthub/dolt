ad365d1dd025e6574ac2935a13673e83543c8319
Dustin Brown <dustin@dolthub.com>
2021-01-05 19:16:52 -0800

    /.github/scripts/performance-benchmarking/get-job-json.sh: fix query, allow real commits

--- .github/scripts/performance-benchmarking/get-job-json.sh
+++ .github/scripts/performance-benchmarking/get-job-json.sh
@@ -8,10 +8,24 @@ if [ "$#" -ne 5 ]; then
 fi
 
 jobname="$1"
-fromVersion="0.22.5"
-toVersion="0.22.6"
+fromVersion="$2"
+toVersion="$3"
 timeprefix="$4"
 actorprefix="$5"
+
+average_time_percent_change="select
+f.test_name as test_name,
+f.run_id as run_id,
+f.latency_sum_ms as from_latency_sum_ms,
+f.sql_transactions_total as from_sql_transactions_total,
+(f.latency_sum_ms / (cast(f.sql_transactions_total as decimal) + .000001)) as from_average_time,
+t.latency_sum_ms as to_latency_sum_ms,
+t.sql_transactions_total as to_sql_transactions_total,
+(t.latency_sum_ms / (cast(t.sql_transactions_total as decimal) + .000001)) as to_average_time,
+(100 * (1.0 - ((t.latency_sum_ms / (cast(t.sql_transactions_total as decimal) + .000001)) / (f.latency_sum_ms / (cast(f.sql_transactions_total as decimal) + .000001))))) as from_to_average_time_percent_change
+from from_results as f
+join to_results as t
+on f.test_name = t.test_name and f.run_id = t.run_id;"
 
 echo '
 {
@@ -22,7 +36,7 @@ echo '
     "namespace": "performance-benchmarking"
   },
   "spec": {
-    "backoffLimit": 2,
+    "backoffLimit": 1,
     "template": {
       "spec": {
         "serviceAccountName": "performance-benchmarking",
@@ -31,14 +45,14 @@ echo '
             "name": "performance-benchmarking",
             "image": "407903926827.dkr.ecr.us-west-2.amazonaws.com/liquidata/performance-benchmarking:latest",
             "args": [
+              "--schema=/schema.sql",
               "--from-version='$fromVersion'",
               "--to-version='$toVersion'",
               "--bucket=performance-benchmarking-github-actions-results",
               "--region=us-west-2",
               "--results-dir='$timeprefix'",
               "--results-prefix='$actorprefix'",
-              "select * from from_results;",
-              "select * from to_results;"
+              "'$average_time_percent_change'"
             ]
           }
         ],
