55cf2a8b600ea1c858c8aecf92ad610133e1dfdc
Zach Musgrave <zach@dolthub.com>
2021-01-12 10:49:18 -0800

    Added pagination and auth to api calls
    Signed-off-by: Zach Musgrave <zach@dolthub.com>

--- gen_release_notes.pl
+++ gen_release_notes.pl
@@ -8,17 +8,23 @@ use open ":std", ":encoding(UTF-8)";
 use JSON::Parse 'json_file_to_perl';
 use Data::Dumper;
 
+use Getopt::Long;
+
+my $token = "";
+GetOptions ("token=s" => \$token) or die "Error in command line args";
+
 # no arg for changes since last release
 my $releaseTag = shift @ARGV;
 
 my $tmpDir = "/var/tmp";
-my $releasesFile = "/$tmpDir/releases-$$.json";
+my $releasesFile = "$tmpDir/releases-$$.json";
 my $doltPullsFile = "$tmpDir/dolt-prs-$$.json";
 my $doltIssuesFile = "$tmpDir/dolt-issues-$$.json";
 my $gmsPullsFile = "$tmpDir/gms-prs-$$.json";
 
 my $doltReleasesUrl = 'https://api.github.com/repos/dolthub/dolt/releases';
 my $curlReleases = "curl -H 'Accept: application/vnd.github.v3+json' '$doltReleasesUrl' > $releasesFile";
+print STDERR "$curlReleases\n";
 system($curlReleases) and die $!;
 
 my $releasesJson = json_file_to_perl($releasesFile);
@@ -37,49 +43,16 @@ foreach my $release (@$releasesJson) {
 
 die "Couldn't find release" unless $toTime;
 
-print "from $fromTime to $toTime\n";
+print STDERR "Looking for PRs and issues from $fromTime to $toTime\n";
 
 my $doltPullRequestsUrl = 'https://api.github.com/repos/dolthub/dolt/pulls?state=closed&per_page=100';
-my $curlDoltPulls = "curl -H 'Accept: application/vnd.github.v3+json' '$doltPullRequestsUrl' > $doltPullsFile";
-system($curlDoltPulls) and die $!;
+my $mergedDoltPrs = getPRs($doltPullRequestsUrl, $fromTime, $toTime);
 
-my $doltPullsJson = json_file_to_perl($doltPullsFile);
-my @mergedDoltPrs;
-foreach my $pull (@$doltPullsJson) {
-    next unless $pull->{merged_at};
-    last if $pull->{merged_at} lt $fromTime;
-    my %pr = (
-        'url' => $pull->{html_url},
-        'number' => $pull->{number},
-        'title' => $pull->{title},
-        'body' => $pull->{body},
-    );
-
-    push (@mergedDoltPrs, \%pr) if $pull->{merged_at} gt $toTime;
-}
-
-my $doltIssuesUrl = "https://api.github.com/repos/dolthub/dolt/issues?state=closed&since=$fromTime&perPage=100";
-my $curlDoltIssues = "curl -H 'Accept: application/vnd.github.v3+json' '$doltIssuesUrl' > $doltIssuesFile";
-system($curlDoltIssues) and die $!;
-
-my $doltIssuesJson = json_file_to_perl($doltIssuesFile);
-my @closedIssues;
-foreach my $issue (@$doltIssuesJson) {
-    next unless $issue->{closed_at};
-    last if $issue->{closed_at} lt $fromTime;
-    next if $issue->{html_url} =~ m|/pull/|; # the issues API also returns PR results
-    my %i = (
-        'url' => $issue->{html_url},
-        'number' => $issue->{number},
-        'title' => $issue->{title},
-        'body' => $issue->{body},
-    );
-
-    push (@closedIssues, \%i) if $issue->{closed_at} gt $toTime; 
-}
+my $doltIssuesUrl = "https://api.github.com/repos/dolthub/dolt/issues?state=closed&since=$fromTime&per_page=100";
+my $closedIssues = getIssues($doltIssuesUrl, $fromTime, $toTime);
 
 print "# Merged PRs:\n\n";
-foreach my $pr (@mergedDoltPrs) {
+foreach my $pr (@$mergedDoltPrs) {
     if ($pr->{body}) {
         print "* [$pr->{number}]($pr->{url}): $pr->{title} ($pr->{body})\n";        
     } else {
@@ -88,6 +61,93 @@ foreach my $pr (@mergedDoltPrs) {
 }
 
 print "\n\n# Closed Issues\n\n";
-foreach my $pr (@closedIssues) {
+foreach my $pr (@$closedIssues) {
     print "* [$pr->{number}]($pr->{url}): $pr->{title}\n";
+}
+
+sub curlCmd {
+    my $url = shift;
+    my $outFile = shift;
+    my $token = shift;
+    
+    my $baseCmd = "curl -H 'Accept: application/vnd.github.v3+json'";
+    $baseCmd .= " -H 'Authorization: token $token'" if $token;
+    $baseCmd .= " '$url' > $outFile";
+
+    return $baseCmd;
+}
+
+sub getPRs {
+    my $baseUrl = shift;
+    my $fromTime = shift;
+    my $toTime = shift;
+    
+    my $page = 1;
+    my $more = 0;
+    
+    my @mergedDoltPrs;
+    do {
+        my $pullsUrl = "$baseUrl&page=$page";
+        my $curlDoltPulls = curlCmd($pullsUrl, $doltPullsFile, $token);
+        print STDERR "$curlDoltPulls\n";
+        system($curlDoltPulls) and die $!;
+
+        $more = 0;
+        my $doltPullsJson = json_file_to_perl($doltPullsFile);
+        foreach my $pull (@$doltPullsJson) {
+            $more = 1;
+            next unless $pull->{merged_at};
+            return \@mergedDoltPrs if $pull->{merged_at} lt $fromTime;
+            my %pr = (
+                'url' => $pull->{html_url},
+                'number' => $pull->{number},
+                'title' => $pull->{title},
+                'body' => $pull->{body},
+                );
+
+            push (@mergedDoltPrs, \%pr) if $pull->{merged_at} lt $toTime;
+        }
+
+        $page++;
+    } while $more;
+    
+    return \@mergedDoltPrs;
+}
+
+sub getIssues {
+    my $baseUrl = shift;
+    my $fromTime = shift;
+    my $toTime = shift;
+    
+    my $page = 1;
+    my $more = 0;
+
+    my @closedIssues;
+    do {
+        my $issuesUrl = "$baseUrl&page=$page";
+        my $curlDoltIssues = curlCmd($issuesUrl, $doltIssuesFile, $token);
+        print STDERR "$curlDoltIssues\n";
+        system($curlDoltIssues) and die $!;
+
+        $more = 0;
+        my $doltIssuesJson = json_file_to_perl($doltIssuesFile);
+        foreach my $issue (@$doltIssuesJson) {
+            $more = 1;
+            next unless $issue->{closed_at};
+            return \@closedIssues if $issue->{closed_at} lt $fromTime;
+            next if $issue->{html_url} =~ m|/pull/|; # the issues API also returns PR results
+            my %i = (
+                'url' => $issue->{html_url},
+                'number' => $issue->{number},
+                'title' => $issue->{title},
+                'body' => $issue->{body},
+                );
+            
+            push (@closedIssues, \%i) if $issue->{closed_at} lt $toTime; 
+        }
+
+        $page++;
+    } while $more;
+    
+    return \@closedIssues;
 }
