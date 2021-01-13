300d9aae05aed9e1495e063c073a80a770a617f4
Dustin Brown <dustin@dolthub.com>
2021-01-13 14:45:19 -0800

    /.github/{scripts,workflows}: update benchmark queries, change trigger

--- .github/scripts/performance-benchmarking/get-job-json.sh
+++ .github/scripts/performance-benchmarking/get-job-json.sh
@@ -13,6 +13,10 @@ toVersion="$3"
 timeprefix="$4"
 actorprefix="$5"
 
+average_time_change_query="select f.test_name as test_name, ABS(ROUND(100 * (1.0 - ((AVG(t.latency_sum_ms) / (AVG(cast(t.sql_transactions_total as decimal)) + .000001)) / (AVG(f.latency_sum_ms) / (AVG(cast(f.sql_transactions_total as decimal)) + .000001)))))) as average_time_percent_change, case when (100 * (1.0 - ((AVG(t.latency_sum_ms) / (AVG(cast(t.sql_transactions_total as decimal)) + .000001)) / (AVG(f.latency_sum_ms) / (AVG(cast(f.sql_transactions_total as decimal)) + .000001))))) < 0 then true else false end as is_faster from from_results as f join to_results as t on f.test_name = t.test_name group by f.test_name;"
+
+transactions_change_query="select f.test_name as test_name, ROUND(100 * ((AVG(cast(t.sql_transactions_total as decimal)) - AVG(cast(f.sql_transactions_total as decimal))) / (AVG(cast(f.sql_transactions_total as decimal)) + .000001))) as sql_transactions_percent_change from from_results as f join to_results as t on f.test_name = t.test_name group by f.test_name;"
+
 echo '
 {
   "apiVersion": "batch/v1",
@@ -38,7 +42,8 @@ echo '
               "--region=us-west-2",
               "--results-dir='$timeprefix'",
               "--results-prefix='$actorprefix'",
-              "select f.test_name as test_name, f.server_version as from_version, AVG(f.latency_sum_ms) as from_latency_sum_ms, AVG(f.sql_transactions_total) as from_sql_transactions_total, (AVG(f.latency_sum_ms) / (AVG(cast(f.sql_transactions_total as decimal)) + .000001)) as from_average_time, t.server_version as to_version, AVG(t.latency_sum_ms) as to_latency_sum_ms, AVG(t.sql_transactions_total) as to_sql_transactions_total, (AVG(t.latency_sum_ms) / (AVG(cast(t.sql_transactions_total as decimal)) + .000001)) as to_average_time, (100 * (1.0 - ((AVG(t.latency_sum_ms) / (AVG(cast(t.sql_transactions_total as decimal)) + .000001)) / (AVG(f.latency_sum_ms) / (AVG(cast(f.sql_transactions_total as decimal)) + .000001))))) as from_to_average_time_percent_change from from_results as f join to_results as t on f.test_name = t.test_name group by f.test_name;"
+              "'"$average_time_change_query"'",
+              "'"$transactions_change_query"'"
             ]
           }
         ],
