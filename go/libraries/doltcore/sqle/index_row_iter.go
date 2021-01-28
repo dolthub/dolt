768906afae76c1742be999079fc741369e3e4063
Brian Hendriks <brian@liquidata.co>
2021-01-27 17:16:04 -0800

    remove layered parallelism.

--- go/libraries/doltcore/sqle/index_row_iter.go
+++ go/libraries/doltcore/sqle/index_row_iter.go
@@ -15,27 +15,23 @@
 package sqle
 
 import (
-	"context"
+	"errors"
 	"io"
-	"runtime"
 
 	"github.com/dolthub/go-mysql-server/sql"
 
 	"github.com/dolthub/dolt/go/libraries/doltcore/schema"
-	"github.com/dolthub/dolt/go/libraries/utils/async"
 	"github.com/dolthub/dolt/go/libraries/utils/set"
 	"github.com/dolthub/dolt/go/store/types"
 )
 
 type indexLookupRowIterAdapter struct {
+	ctx     *sql.Context
+	nbf     *types.NomsBinFormat
 	idx     DoltIndex
 	keyIter nomsKeyIter
 	pkTags  map[uint64]int
 	conv    *KVToSqlRowConverter
-	ctx     *sql.Context
-	rowChan chan sql.Row
-	err     error
-	buffer  []sql.Row
 }
 
 type keyPos struct {
@@ -54,81 +50,59 @@ func NewIndexLookupRowIterAdapter(ctx *sql.Context, idx DoltIndex, keyIter nomsK
 	conv := NewKVToSqlRowConverterForCols(idx.IndexRowData().Format(), cols)
 
 	iter := &indexLookupRowIterAdapter{
+		ctx:     ctx,
+		nbf:     idx.IndexRowData().Format(),
 		idx:     idx,
 		keyIter: keyIter,
 		conv:    conv,
 		pkTags:  pkTags,
-		ctx:     ctx,
-		rowChan: make(chan sql.Row, runtime.NumCPU()*10),
-		buffer:  make([]sql.Row, runtime.NumCPU()*5),
 	}
-	go iter.queueRows()
+
 	return iter
 }
 
 // Next returns the next row from the iterator.
 func (i *indexLookupRowIterAdapter) Next() (sql.Row, error) {
-	r, ok := <-i.rowChan
-	if !ok { // Only closes when we are finished iterating over the keys or an error has occurred.
-		if i.err != nil {
-			return nil, i.err
-		}
-		return nil, io.EOF
+	indexKey, err := i.keyIter.ReadKey(i.ctx)
+
+	if err != nil {
+		return nil, err
 	}
-	return r, nil
+
+	tableKey, err := i.indexKeyToTableKey(i.nbf, indexKey)
+
+	if err != nil {
+		return nil ,err
+	}
+
+	tableData := i.idx.TableData()
+	pkTupleVal, err := i.indexKeyToTableKey(tableData.Format(), tableKey)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fieldsVal, ok, err := tableData.MaybeGetTuple(i.ctx, pkTupleVal)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !ok {
+		return nil, errors.New("missing indexed row in table row data")
+	}
+
+	sqlRow, err := i.conv.ConvertKVTuplesToSqlRow(pkTupleVal, fieldsVal)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return sqlRow, nil
 }
 
 func (*indexLookupRowIterAdapter) Close() error {
 	return nil
-}
-
-// queueRows reads each key from the key iterator and runs a goroutine for each logical processor to process the keys.
-func (i *indexLookupRowIterAdapter) queueRows() {
-	defer close(i.rowChan)
-	exec := async.NewActionExecutor(i.ctx, i.processKey, uint32(runtime.NumCPU()), 0)
-
-	var err error
-	for {
-		shouldBreak := false
-		pos := 0
-		for ; pos < len(i.buffer); pos++ {
-			var indexKey types.Tuple
-			indexKey, err = i.keyIter.ReadKey(i.ctx)
-			if err != nil {
-				break
-			}
-			exec.Execute(keyPos{
-				key:      indexKey,
-				position: pos,
-			})
-		}
-		if err != nil {
-			if err == io.EOF {
-				shouldBreak = true
-			} else {
-				i.err = err
-				return
-			}
-		}
-		i.err = exec.WaitForEmpty()
-		if err != nil {
-			if err == io.EOF {
-				shouldBreak = true
-			} else {
-				i.err = err
-				return
-			}
-		}
-		for idx, r := range i.buffer {
-			if idx == pos {
-				break
-			}
-			i.rowChan <- r
-		}
-		if shouldBreak {
-			break
-		}
-	}
 }
 
 func (i *indexLookupRowIterAdapter) indexKeyToTableKey(nbf *types.NomsBinFormat, indexKey types.Tuple) (types.Tuple, error) {
@@ -171,35 +145,6 @@ func (i *indexLookupRowIterAdapter) indexKeyToTableKey(nbf *types.NomsBinFormat,
 	}
 
 	return types.NewTuple(nbf, resVals...)
-}
-
-// processKey is called within queueRows and processes each key, sending the resulting row to the row channel.
-func (i *indexLookupRowIterAdapter) processKey(_ context.Context, valInt interface{}) error {
-	val := valInt.(keyPos)
-
-	tableData := i.idx.TableData()
-	pkTupleVal, err := i.indexKeyToTableKey(tableData.Format(), val.key)
-
-	if err != nil {
-		return err
-	}
-
-	fieldsVal, ok, err := tableData.MaybeGetTuple(i.ctx, pkTupleVal)
-	if err != nil {
-		return err
-	}
-
-	if !ok {
-		return nil
-	}
-
-	sqlRow, err := i.conv.ConvertKVTuplesToSqlRow(pkTupleVal, fieldsVal)
-	if err != nil {
-		return err
-	}
-
-	i.buffer[val.position] = sqlRow
-	return nil
 }
 
 type coveringIndexRowIterAdapter struct {
