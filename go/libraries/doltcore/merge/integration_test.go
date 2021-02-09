a838286b67bc87112398772d9a9692e6956fe22f
Andy Arthur <andy@dolthub.com>
2021-02-09 10:07:42 -0800

    added integration tests for conflicts and resolution

--- go/libraries/doltcore/merge/integration_test.go
+++ go/libraries/doltcore/merge/integration_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	cmd "github.com/dolthub/dolt/go/cmd/dolt/commands"
+	"github.com/dolthub/dolt/go/cmd/dolt/commands/cnfcmds"
 	dtu "github.com/dolthub/dolt/go/libraries/doltcore/dtestutils"
 	"github.com/dolthub/dolt/go/libraries/doltcore/sqle"
 )
@@ -102,6 +103,119 @@ func TestMerge(t *testing.T) {
 				{"x"},
 				{"y"},
 				{"z"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctx := context.Background()
+			dEnv := dtu.CreateTestEnv()
+
+			for _, tc := range setupCommon {
+				tc.exec(t, ctx, dEnv)
+			}
+			for _, tc := range test.setup {
+				tc.exec(t, ctx, dEnv)
+			}
+
+			root, err := dEnv.WorkingRoot(ctx)
+			require.NoError(t, err)
+			actRows, err := sqle.ExecuteSelect(dEnv, dEnv.DoltDB, root, test.query)
+			require.NoError(t, err)
+
+			require.Equal(t, len(test.expected), len(actRows))
+			for i := range test.expected {
+				assert.Equal(t, test.expected[i], actRows[i])
+			}
+		})
+	}
+}
+
+func TestMergeConflicts(t *testing.T) {
+
+	setupCommon := []testCommand{
+		{cmd.SqlCmd{}, args{"-q", "CREATE TABLE test (pk int PRIMARY KEY, c0 int);"}},
+		{cmd.CommitCmd{}, args{"-am", "created table test"}},
+	}
+
+	tests := []struct {
+		name  string
+		setup []testCommand
+
+		query    string
+		expected []sql.Row
+	}{
+		{
+			name: "conflict on merge",
+			setup: []testCommand{
+				{cmd.CheckoutCmd{}, args{"-b", "other"}},
+				{cmd.SqlCmd{}, args{"-q", "INSERT INTO test VALUES (1,1),(2,2);"}},
+				{cmd.CommitCmd{}, args{"-am", "added rows on other"}},
+				{cmd.CheckoutCmd{}, args{"master"}},
+				{cmd.SqlCmd{}, args{"-q", "INSERT INTO test VALUES (1,11),(2,22);"}},
+				{cmd.CommitCmd{}, args{"-am", "added the same rows on master"}},
+				{cmd.MergeCmd{}, args{"other"}},
+			},
+			query: "SELECT * FROM dolt_conflicts",
+			expected: []sql.Row{
+				{"test", uint64(2)},
+			},
+		},
+		{
+			name: "conflict on merge, resolve with ours",
+			setup: []testCommand{
+				{cmd.CheckoutCmd{}, args{"-b", "other"}},
+				{cmd.SqlCmd{}, args{"-q", "INSERT INTO test VALUES (1,1),(2,2);"}},
+				{cmd.CommitCmd{}, args{"-am", "added rows on other"}},
+				{cmd.CheckoutCmd{}, args{"master"}},
+				{cmd.SqlCmd{}, args{"-q", "INSERT INTO test VALUES (1,11),(2,22);"}},
+				{cmd.CommitCmd{}, args{"-am", "added the same rows on master"}},
+				{cmd.MergeCmd{}, args{"other"}},
+				{cnfcmds.ResolveCmd{}, args{"--ours", "test"}},
+			},
+			query: "SELECT * FROM test",
+			expected: []sql.Row{
+				{int32(1), int32(11)},
+				{int32(2), int32(22)},
+			},
+		},
+		{
+			name: "conflict on merge, no table in ancestor",
+			setup: []testCommand{
+				{cmd.CheckoutCmd{}, args{"-b", "other"}},
+				{cmd.SqlCmd{}, args{"-q", "CREATE TABLE quiz (pk int PRIMARY KEY, c0 int);"}},
+				{cmd.SqlCmd{}, args{"-q", "INSERT INTO quiz VALUES (1,1),(2,2);"}},
+				{cmd.CommitCmd{}, args{"-am", "added rows on other"}},
+				{cmd.CheckoutCmd{}, args{"master"}},
+				{cmd.SqlCmd{}, args{"-q", "CREATE TABLE quiz (pk int PRIMARY KEY, c0 int);"}},
+				{cmd.SqlCmd{}, args{"-q", "INSERT INTO quiz VALUES (1,11),(2,22);"}},
+				{cmd.CommitCmd{}, args{"-am", "added the same rows on master"}},
+				{cmd.MergeCmd{}, args{"other"}},
+			},
+			query: "SELECT * FROM dolt_conflicts",
+			expected: []sql.Row{
+				{"quiz", uint64(2)},
+			},
+		},
+		{
+			name: "conflict on merge, no table in ancestor, resolve with theirs",
+			setup: []testCommand{
+				{cmd.CheckoutCmd{}, args{"-b", "other"}},
+				{cmd.SqlCmd{}, args{"-q", "CREATE TABLE quiz (pk int PRIMARY KEY, c0 int);"}},
+				{cmd.SqlCmd{}, args{"-q", "INSERT INTO quiz VALUES (1,1),(2,2);"}},
+				{cmd.CommitCmd{}, args{"-am", "added rows on other"}},
+				{cmd.CheckoutCmd{}, args{"master"}},
+				{cmd.SqlCmd{}, args{"-q", "CREATE TABLE quiz (pk int PRIMARY KEY, c0 int);"}},
+				{cmd.SqlCmd{}, args{"-q", "INSERT INTO quiz VALUES (1,11),(2,22);"}},
+				{cmd.CommitCmd{}, args{"-am", "added the same rows on master"}},
+				{cmd.MergeCmd{}, args{"other"}},
+				{cnfcmds.ResolveCmd{}, args{"--theirs", "quiz"}},
+			},
+			query: "SELECT * FROM quiz",
+			expected: []sql.Row{
+				{int32(1), int32(1)},
+				{int32(2), int32(2)},
 			},
 		},
 	}
