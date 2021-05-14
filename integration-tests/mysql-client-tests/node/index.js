2fe9436ad516a5234e61fa032555cf6433f2ab01
Andy Arthur <andy@dolthub.com>
2021-05-14 13:57:33 -0700

    update expected server status flags for node/

--- integration-tests/mysql-client-tests/node/index.js
+++ integration-tests/mysql-client-tests/node/index.js
@@ -68,7 +68,7 @@ async function main() {
 	    fieldCount: 0,
 	    affectedRows: 1,
 	    insertId: 0,
-	    serverStatus: 0,
+	    serverStatus: 2,
 	    warningCount: 0,
 	    message: '',
 	    protocol41: true,
