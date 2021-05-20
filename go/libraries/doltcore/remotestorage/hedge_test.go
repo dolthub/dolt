108130249a6172214317e7f526216ff1b190a794
Aaron Son <aaron@dolthub.com>
2021-05-20 15:10:03 -0700

    go/libraries/doltcore/remotestorage: Limit concurrent hedges of a single request to one by default.

--- go/libraries/doltcore/remotestorage/hedge_test.go
+++ go/libraries/doltcore/remotestorage/hedge_test.go
@@ -23,6 +23,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func init() {
+	MaxHedgesPerRequest = 1024
+}
+
 func TestPercentileStrategy(t *testing.T) {
 	s := NewPercentileStrategy(0, 1*time.Hour, 95.0)
 	for i := 0; i < 90; i++ {
@@ -88,8 +92,6 @@ func TestHedgerObeysMaxHedges(t *testing.T) {
 	try := func(max int) {
 		h := NewHedger(int64(max), NewMinStrategy(1*time.Millisecond, nil))
 		cnt := int32(0)
-		ch := make(chan int, 1)
-		ch <- 1
 		i, err := h.Do(context.Background(), Work{
 			Work: func(ctx context.Context) (interface{}, error) {
 				cur := atomic.AddInt32(&cnt, 1)
@@ -109,6 +111,49 @@ func TestHedgerObeysMaxHedges(t *testing.T) {
 	try(1)
 	try(2)
 	try(3)
+}
+
+func TestMaxHedgesPerRequestObeyed(t *testing.T) {
+	before := MaxHedgesPerRequest
+	defer func() {
+		MaxHedgesPerRequest = before
+	}()
+
+	MaxHedgesPerRequest = 0
+	h := NewHedger(int64(32), NewMinStrategy(1*time.Millisecond, nil))
+	cnt := int32(0)
+	i, err := h.Do(context.Background(), Work{
+		Work: func(ctx context.Context) (interface{}, error) {
+			cur := atomic.AddInt32(&cnt, 1)
+			if cur == int32(1) {
+				time.Sleep(100 * time.Millisecond)
+				return 1, nil
+			} else {
+				return 2, nil
+			}
+		},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 1, i.(int))
+
+	MaxHedgesPerRequest = 1
+	cnt = int32(0)
+	i, err = h.Do(context.Background(), Work{
+		Work: func(ctx context.Context) (interface{}, error) {
+			cur := atomic.AddInt32(&cnt, 1)
+			if cur == int32(1) {
+				time.Sleep(2 * time.Second)
+				return 1, nil
+			} else if cur == int32(2) {
+				time.Sleep(100 * time.Millisecond)
+				return 2, nil
+			} else {
+				return 3, nil
+			}
+		},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 2, i.(int))
 }
 
 func TestHedgerContextCancelObeyed(t *testing.T) {
