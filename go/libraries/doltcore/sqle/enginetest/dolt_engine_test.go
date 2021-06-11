7459dab905f3549158a8443cee58777df81e1e14
Andy Arthur <andy@dolthub.com>
2021-06-11 14:43:30 -0700

    added broken queries

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -41,19 +41,25 @@ func TestSingleQuery(t *testing.T) {
 
 	var test enginetest.QueryTest
 	test = enginetest.QueryTest{
-		Query: "SELECT t1.c1,t2.c2 FROM one_pk t1, two_pk t2 WHERE pk1=1 AND pk2=1 ORDER BY 1,2",
-		Expected: []sql.Row{
-			{0, 31},
-			{10, 31},
-			{20, 31},
-			{30, 31},
-		},
+		Query: `SELECT 
+					myTable.i, 
+					(SELECT 
+						dolt_commit_diff_mytable.diff_type 
+					FROM 
+						dolt_commit_diff_mytable
+					WHERE (
+						dolt_commit_diff_mytable.from_commit = 'abc' AND 
+						dolt_commit_diff_mytable.to_commit = 'abc' AND
+						dolt_commit_diff_mytable.to_i = myTable.i  -- extra filter clause
+					)) AS diff_type 
+				FROM myTable`,
+		Expected: []sql.Row{},
 	}
 
 	harness := newDoltHarness(t)
 	engine := enginetest.NewEngine(t, harness)
-	engine.Analyzer.Debug = true
-	engine.Analyzer.Verbose = true
+	//engine.Analyzer.Debug = true
+	//engine.Analyzer.Verbose = true
 
 	enginetest.TestQuery(t, harness, engine, test.Query, test.Expected, test.ExpectedColumns, test.Bindings)
 }
---
+++ go/libraries/doltcore/sqle/enginetest/dolt_system_table_queries.go
@@ -0,0 +1,40 @@
+package enginetest
+
+import (
+	"github.com/dolthub/go-mysql-server/enginetest"
+	"github.com/dolthub/go-mysql-server/sql/analyzer"
+)
+
+var BrokenQueries = []enginetest.QueryErrorTest{
+	{
+		Query: `SELECT 
+					myTable.i, 
+					(SELECT 
+						U0.diff_type 
+					FROM 
+						dolt_commit_diff_mytable U0 
+					WHERE (
+						U0.from_commit = 'abc' AND 
+						U0.to_commit = 'abc'
+					)) AS diff_type 
+				FROM myTable`,
+		ExpectedErr: analyzer.ErrFieldMissing,
+	},
+	{
+		// extra filter clause breaks filter pushdown
+		// `dolt_commit_diff_*` relies on filter pushdown to function
+		Query: `SELECT 
+					myTable.i, 
+					(SELECT 
+						dolt_commit_diff_mytable.diff_type 
+					FROM 
+						dolt_commit_diff_mytable
+					WHERE (
+						dolt_commit_diff_mytable.from_commit = 'abc' AND 
+						dolt_commit_diff_mytable.to_commit = 'abc' AND
+						dolt_commit_diff_mytable.to_i = myTable.i  -- extra filter clause
+					)) AS diff_type 
+				FROM myTable`,
+		ExpectedErr: analyzer.ErrFieldMissing,
+	},
+}
