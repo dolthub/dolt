8254d6c70facbcd50af79f456beb58045bd5f431
Andy Arthur <andy@dolthub.com>
2021-07-12 13:24:51 -0700

    use branch refs to calculate diff table super schema, not all refs

--- go/cmd/dolt/dolt.go
+++ go/cmd/dolt/dolt.go
@@ -234,7 +234,7 @@ func runMain() int {
 	restoreIO := cli.InitIO()
 	defer restoreIO()
 
-	warnIfMaxFilesTooLow()
+	//warnIfMaxFilesTooLow()
 
 	ctx := context.Background()
 	dEnv := env.Load(ctx, env.GetCurrentUserHomeDir, filesys.LocalFS, doltdb.LocalDirDoltDB, Version)
@@ -283,7 +283,7 @@ func runMain() int {
 		return 1
 	}
 
-	err = reconfigIfTempFileMoveFails(dEnv)
+	//err = reconfigIfTempFileMoveFails(dEnv)
 
 	if err != nil {
 		cli.PrintErrln(color.RedString("Failed to setup the temporary directory. %v`", err))
