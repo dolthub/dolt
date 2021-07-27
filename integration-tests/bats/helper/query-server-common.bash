fa32453e31ce1e6b136de49a796b35134cdca06b
Zach Musgrave <zach@dolthub.com>
2021-07-27 13:11:14 -0700

    Added a repo name param to all server tests

--- integration-tests/bats/helper/query-server-common.bash
+++ integration-tests/bats/helper/query-server-common.bash
@@ -14,7 +14,7 @@ else:
     working_dir, database, port_str, auto_commit, query_strs = args
 
 print('Query Strings: ' + query_strs)
-print('Workind Dir: ' + working_dir)
+print('Working Dir: ' + working_dir)
 print('Database: ' + database)
 print('Port: ' + port_str)
 print('Autocommit: ' + auto_commit)
@@ -130,40 +130,44 @@ stop_sql_server() {
 
 # server_query connects to a running mysql server, executes a query and compares the results against what is expected.
 # In the event that the results do not match expectations, the python process will exit with an exit code of 1
-#  * param1 is 1 for autocommit = true, 0 for autocommit = false
-#  * param2 is the query_str
-#  * param3 is a csv representing the expected result set.  If a query is not expected to have a result set "" should
+#  * param1 is the database name for the connection string
+#  * param2 is 1 for autocommit = true, 0 for autocommit = false
+#  * param3 is the query_str
+#  * param4 is a csv representing the expected result set.  If a query is not expected to have a result set "" should
 #      be passed.
 server_query() {
     let PORT="$$ % (65536-1024) + 1024"
     PYTEST_DIR="$BATS_TEST_DIRNAME/helper"
     echo Executing server_query
-    python3 -c "$PYTHON_QUERY_SCRIPT" -- "$PYTEST_DIR" "$DEFAULT_DB" "$PORT" "$1" "$2" "$3"
+    python3 -c "$PYTHON_QUERY_SCRIPT" -- "$PYTEST_DIR" "$1" "$PORT" "$2" "$3" "$4"
 }
 
 # server_query connects to a running mysql server, executes a query and compares the results against what is expected.
 # In the event that the results do not match expectations, the python process will exit with an exit code of 1
-#  * param1 is 1 for autocommit = true, 0 for autocommit = false
-#  * param2 is the query_str
+#  * param1 is the database name for the connection string
+#  * param2 is 1 for autocommit = true, 0 for autocommit = false
+#  * param3 is the query_str
 multi_query() {
     let PORT="$$ % (65536-1024) + 1024"
     PYTEST_DIR="$BATS_TEST_DIRNAME/helper"
     echo Executing multi_query
-    python3 -c "$PYTHON_QUERY_SCRIPT" -- "$PYTEST_DIR" "$DEFAULT_DB" "$PORT" "$1" "$2"
+    python3 -c "$PYTHON_QUERY_SCRIPT" -- "$PYTEST_DIR" "$1" "$PORT" "$2" "$3"
 }
 
-# update_query runs an update query and should be called with 2 parameters
-#   * param1 is 1 for autocommit = true, 0 for autocommit = false
-#   * param2 is the query string
+# update_query runs an update query and should be called with 3 parameters
+#   * param1 is the database name for the connection string
+#   * param2 is 1 for autocommit = true, 0 for autocommit = false
+#   * param3 is the query string
 update_query() {
-    server_query $1 "$2" ""
+    server_query "$1" "$2" "$3" ""
 }
 
-# insert_query runs an insert query and should be called with 2 parameters
-#   * param1 is 1 for autocommit = true, 0 for autocommit = false
-#   * param2 is the query string
+# insert_query runs an insert query and should be called with 3 parameters
+#   * param1 is the database name for the connection string
+#   * param2 is 1 for autocommit = true, 0 for autocommit = false
+#   * param3 is the query string
 insert_query() {
-    server_query $1 "$2" ""
+    server_query "$1" "$2" "$3" ""
 }
 
 # unselected_server_query connects to a running mysql server, but not to a particular database, executes a query and
