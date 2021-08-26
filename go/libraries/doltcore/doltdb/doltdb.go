20fa409023f5569dfea60adb76eb4e9a7957a19a
Dustin Brown <dustin@dolthub.com>
2021-08-26 12:23:18 -0700

    /go/{libraries,store}: remove manifest version getter

--- go/libraries/doltcore/doltdb/doltdb.go
+++ go/libraries/doltcore/doltdb/doltdb.go
@@ -1220,7 +1220,3 @@ func (ddb *DoltDB) PullChunks(ctx context.Context, tempDir string, srcDB *DoltDB
 func (ddb *DoltDB) Clone(ctx context.Context, destDB *DoltDB, eventCh chan<- datas.TableFileEvent) error {
 	return datas.Clone(ctx, ddb.db, destDB.db, eventCh)
 }
-
-func (ddb *DoltDB) GetStorageVersion(ctx context.Context) (string, error) {
-	return datas.GetManifestStorageVersion(ctx, ddb.db)
-}
