58f22d395d5c9990c4d7ddaa2fa444dd4a3db308
Brian Hendriks <brian@dolthub.com>
2021-08-29 09:28:40 -0700

    fixes + import perf

--- go/libraries/doltcore/table/editor/bulk_import_tea.go
+++ go/libraries/doltcore/table/editor/bulk_import_tea.go
@@ -16,8 +16,7 @@ package editor
 
 import (
 	"context"
-
-	"github.com/dolthub/dolt/go/libraries/utils/set"
+	"github.com/dolthub/dolt/go/libraries/doltcore/schema"
 
 	"github.com/dolthub/dolt/go/store/hash"
 	"github.com/dolthub/dolt/go/store/types"
@@ -37,10 +36,6 @@ type BulkImportTEA struct {
 	opCount int64
 	adds    map[hash.Hash]bool
 	deletes map[hash.Hash]bool
-}
-
-func (tea *BulkImportTEA) OpCount() int64 {
-	return tea.opCount
 }
 
 func (tea *BulkImportTEA) Delete(keyHash hash.Hash, key types.Tuple) error {
@@ -112,6 +107,92 @@ func (tea *BulkImportTEA) MaterializeEdits(ctx context.Context, nbf *types.NomsB
 	return currMap, nil
 }
 
+var _ IndexEditAccumulator = (*BulkImportIEA)(nil)
+
+type BulkImportIEA struct {
+	teaf       DbEaFactory
+	emptyTuple types.Tuple
+
+	ea      types.EditAccumulator
+	rowData types.Map
+
+	// opCount contains the number of edits that would be applied in materializing the edits
+	opCount int64
+	adds    map[hash.Hash]bool
+	deletes map[hash.Hash]bool
+}
+
+func (tea *BulkImportIEA) Delete(ctx context.Context, keyHash, partialKeyHash hash.Hash, key, partialKey, value types.Tuple) error {
+	tea.opCount++
+	tea.ea.AddEdit(key, nil)
+
+	tea.deletes[keyHash] = true
+	delete(tea.adds, keyHash)
+	return nil
+}
+
+func (tea *BulkImportIEA) Insert(ctx context.Context, keyHash, partialKeyHash hash.Hash, key, partialKey types.Tuple, val types.Tuple) error {
+	tea.opCount++
+	tea.ea.AddEdit(key, val)
+
+	tea.adds[keyHash] = true
+	delete(tea.deletes, keyHash)
+	return nil
+}
+
+func (tea *BulkImportIEA) Has(ctx context.Context, keyHash hash.Hash, key types.Tuple) (bool, error) {
+	if tea.deletes[keyHash] {
+		return false, nil
+	}
+
+	if tea.adds[keyHash] {
+		return true, nil
+	}
+
+	ok, err := tea.rowData.Has(ctx, key)
+
+	if err != nil {
+		return false, err
+	} else if !ok {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (tea *BulkImportIEA) HasPartial(ctx context.Context, idxSch schema.Schema, partialKeyHash hash.Hash, partialKey types.Tuple) ([]hashedTuple, error) {
+	panic("implement me")
+}
+
+func (tea *BulkImportIEA) Commit(ctx context.Context, nbf *types.NomsBinFormat) error {
+	panic("Not Supported")
+}
+
+func (tea *BulkImportIEA) Rollback(ctx context.Context) error {
+	panic("Not Supported")
+}
+
+func (tea *BulkImportIEA) MaterializeEdits(ctx context.Context, nbf *types.NomsBinFormat) (m types.Map, err error) {
+	ea := tea.ea
+	defer ea.Close(ctx)
+
+	itr, err := ea.FinishedEditing()
+	if err != nil {
+		return types.EmptyMap, err
+	}
+
+	currMap := tea.rowData
+	for !itr.ReachedEOF() {
+		currMap, _, err = types.ApplyNEdits(ctx, itr, currMap, 256*1024)
+		if err != nil {
+			return types.EmptyMap, err
+		}
+	}
+
+	*tea = *(tea.teaf.NewIndexEA(ctx, currMap).(*BulkImportIEA))
+	return currMap, nil
+}
+
 var _ DbEaFactory = (*BulkImportTEAFactory)(nil)
 
 type BulkImportTEAFactory struct {
@@ -147,17 +228,19 @@ func (b *BulkImportTEAFactory) NewTableEA(ctx context.Context, rowData types.Map
 }
 
 func (b *BulkImportTEAFactory) NewIndexEA(ctx context.Context, rowData types.Map) IndexEditAccumulator {
-	return &indexEditAccumulatorImpl{
-		nbf:                rowData.Format(),
-		rowData:            rowData,
-		committed:          newInMemIndexEdits(),
-		uncommitted:        newInMemIndexEdits(),
-		uncommittedFlushed: newInMemIndexEdits(),
-		commitEA:           edits.NewAsyncSortedEditsWithDefaults(rowData.Format()),
-		commitEAId:         0,
-		accumulatorIdx:     1,
-		flusher:            edits.NewDiskEditFlusher(ctx, b.directory, rowData.Format(), b.vrw),
-		committedEaIds:     set.NewUint64Set(nil),
-		uncommittedEaIds:   set.NewUint64Set(nil),
+	const flushInterval = 256 * 1024
+
+	createMapEA := func() types.EditAccumulator {
+		return types.CreateEditAccForMapEdits(b.nbf)
+	}
+
+	ea := edits.NewDiskBackedEditAcc(ctx, b.nbf, b.vrw, flushInterval, b.directory, createMapEA)
+	return &BulkImportIEA{
+		teaf:       b,
+		rowData:    rowData,
+		ea:         ea,
+		adds:       make(map[hash.Hash]bool),
+		deletes:    make(map[hash.Hash]bool),
+		emptyTuple: types.EmptyTuple(b.nbf),
 	}
 }
