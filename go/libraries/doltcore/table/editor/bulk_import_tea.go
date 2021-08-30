0cb64621eafaeb3c194face6a957ea8252515694
Brian Hendriks <brian@dolthub.com>
2021-08-30 11:29:31 -0700

    HasPartial in Bult Import Editor

--- go/libraries/doltcore/table/editor/bulk_import_tea.go
+++ go/libraries/doltcore/table/editor/bulk_import_tea.go
@@ -16,7 +16,12 @@ package editor
 
 import (
 	"context"
+	"io"
+
+	"github.com/dolthub/dolt/go/libraries/doltcore/row"
 	"github.com/dolthub/dolt/go/libraries/doltcore/schema"
+	"github.com/dolthub/dolt/go/libraries/doltcore/table"
+	"github.com/dolthub/dolt/go/libraries/doltcore/table/typed/noms"
 
 	"github.com/dolthub/dolt/go/store/hash"
 	"github.com/dolthub/dolt/go/store/types"
@@ -117,39 +122,49 @@ type BulkImportIEA struct {
 	rowData types.Map
 
 	// opCount contains the number of edits that would be applied in materializing the edits
-	opCount int64
-	adds    map[hash.Hash]bool
-	deletes map[hash.Hash]bool
+	opCount     int64
+	adds        map[hash.Hash]bool
+	deletes     map[hash.Hash]bool
+	partialAdds map[hash.Hash]map[hash.Hash]types.Tuple
 }
 
-func (tea *BulkImportIEA) Delete(ctx context.Context, keyHash, partialKeyHash hash.Hash, key, partialKey, value types.Tuple) error {
-	tea.opCount++
-	tea.ea.AddEdit(key, nil)
+func (iea *BulkImportIEA) Delete(ctx context.Context, keyHash, partialKeyHash hash.Hash, key, partialKey, value types.Tuple) error {
+	iea.opCount++
+	iea.ea.AddEdit(key, nil)
 
-	tea.deletes[keyHash] = true
-	delete(tea.adds, keyHash)
+	iea.deletes[keyHash] = true
+	delete(iea.adds, keyHash)
+	delete(iea.partialAdds[partialKeyHash], keyHash)
+
 	return nil
 }
 
-func (tea *BulkImportIEA) Insert(ctx context.Context, keyHash, partialKeyHash hash.Hash, key, partialKey types.Tuple, val types.Tuple) error {
-	tea.opCount++
-	tea.ea.AddEdit(key, val)
+func (iea *BulkImportIEA) Insert(ctx context.Context, keyHash, partialKeyHash hash.Hash, key, partialKey types.Tuple, val types.Tuple) error {
+	iea.opCount++
+	iea.ea.AddEdit(key, val)
 
-	tea.adds[keyHash] = true
-	delete(tea.deletes, keyHash)
+	iea.adds[keyHash] = true
+	delete(iea.deletes, keyHash)
+
+	if matchingMap, ok := iea.partialAdds[partialKeyHash]; ok {
+		matchingMap[keyHash] = key
+	} else {
+		iea.partialAdds[partialKeyHash] = map[hash.Hash]types.Tuple{keyHash: key}
+	}
+
 	return nil
 }
 
-func (tea *BulkImportIEA) Has(ctx context.Context, keyHash hash.Hash, key types.Tuple) (bool, error) {
-	if tea.deletes[keyHash] {
+func (iea *BulkImportIEA) Has(ctx context.Context, keyHash hash.Hash, key types.Tuple) (bool, error) {
+	if iea.deletes[keyHash] {
 		return false, nil
 	}
 
-	if tea.adds[keyHash] {
+	if iea.adds[keyHash] {
 		return true, nil
 	}
 
-	ok, err := tea.rowData.Has(ctx, key)
+	ok, err := iea.rowData.Has(ctx, key)
 
 	if err != nil {
 		return false, err
@@ -160,20 +175,66 @@ func (tea *BulkImportIEA) Has(ctx context.Context, keyHash hash.Hash, key types.
 	return true, nil
 }
 
-func (tea *BulkImportIEA) HasPartial(ctx context.Context, idxSch schema.Schema, partialKeyHash hash.Hash, partialKey types.Tuple) ([]hashedTuple, error) {
-	panic("implement me")
+func (iea *BulkImportIEA) HasPartial(ctx context.Context, idxSch schema.Schema, partialKeyHash hash.Hash, partialKey types.Tuple) ([]hashedTuple, error) {
+	if hasNulls, err := partialKey.Contains(types.NullValue); err != nil {
+		return nil, err
+	} else if hasNulls { // rows with NULL are considered distinct, and therefore we do not match on them
+		return nil, nil
+	}
+
+	var err error
+	var matches []hashedTuple
+	var mapIter table.TableReadCloser = noms.NewNomsRangeReader(idxSch, iea.rowData, []*noms.ReadRange{
+		{Start: partialKey, Inclusive: true, Reverse: false, Check: func(tuple types.Tuple) (bool, error) {
+			return tuple.StartsWith(partialKey), nil
+		}}})
+	defer mapIter.Close(ctx)
+	var r row.Row
+	for r, err = mapIter.ReadRow(ctx); err == nil; r, err = mapIter.ReadRow(ctx) {
+		tplKeyVal, err := r.NomsMapKey(idxSch).Value(ctx)
+		if err != nil {
+			return nil, err
+		}
+		key := tplKeyVal.(types.Tuple)
+		tplValVal, err := r.NomsMapValue(idxSch).Value(ctx)
+		if err != nil {
+			return nil, err
+		}
+		val := tplValVal.(types.Tuple)
+		keyHash, err := key.Hash(key.Format())
+		if err != nil {
+			return nil, err
+		}
+		matches = append(matches, hashedTuple{key, val, keyHash})
+	}
+
+	if err != io.EOF {
+		return nil, err
+	}
+
+	for i := len(matches) - 1; i >= 0; i-- {
+		// If we've removed a key that's present here, remove it from the slice
+		if _, ok := iea.deletes[matches[i].hash]; ok {
+			matches[i] = matches[len(matches)-1]
+			matches = matches[:len(matches)-1]
+		}
+	}
+	for addedHash, addedTpl := range iea.partialAdds[partialKeyHash] {
+		matches = append(matches, hashedTuple{addedTpl, types.EmptyTuple(addedTpl.Format()), addedHash})
+	}
+	return matches, nil
 }
 
-func (tea *BulkImportIEA) Commit(ctx context.Context, nbf *types.NomsBinFormat) error {
+func (iea *BulkImportIEA) Commit(ctx context.Context, nbf *types.NomsBinFormat) error {
 	panic("Not Supported")
 }
 
-func (tea *BulkImportIEA) Rollback(ctx context.Context) error {
+func (iea *BulkImportIEA) Rollback(ctx context.Context) error {
 	panic("Not Supported")
 }
 
-func (tea *BulkImportIEA) MaterializeEdits(ctx context.Context, nbf *types.NomsBinFormat) (m types.Map, err error) {
-	ea := tea.ea
+func (iea *BulkImportIEA) MaterializeEdits(ctx context.Context, nbf *types.NomsBinFormat) (m types.Map, err error) {
+	ea := iea.ea
 	defer ea.Close(ctx)
 
 	itr, err := ea.FinishedEditing()
@@ -181,7 +242,7 @@ func (tea *BulkImportIEA) MaterializeEdits(ctx context.Context, nbf *types.NomsB
 		return types.EmptyMap, err
 	}
 
-	currMap := tea.rowData
+	currMap := iea.rowData
 	for !itr.ReachedEOF() {
 		currMap, _, err = types.ApplyNEdits(ctx, itr, currMap, 256*1024)
 		if err != nil {
@@ -189,7 +250,7 @@ func (tea *BulkImportIEA) MaterializeEdits(ctx context.Context, nbf *types.NomsB
 		}
 	}
 
-	*tea = *(tea.teaf.NewIndexEA(ctx, currMap).(*BulkImportIEA))
+	*iea = *(iea.teaf.NewIndexEA(ctx, currMap).(*BulkImportIEA))
 	return currMap, nil
 }
 
@@ -236,11 +297,12 @@ func (b *BulkImportTEAFactory) NewIndexEA(ctx context.Context, rowData types.Map
 
 	ea := edits.NewDiskBackedEditAcc(ctx, b.nbf, b.vrw, flushInterval, b.directory, createMapEA)
 	return &BulkImportIEA{
-		teaf:       b,
-		rowData:    rowData,
-		ea:         ea,
-		adds:       make(map[hash.Hash]bool),
-		deletes:    make(map[hash.Hash]bool),
-		emptyTuple: types.EmptyTuple(b.nbf),
+		teaf:        b,
+		rowData:     rowData,
+		ea:          ea,
+		adds:        make(map[hash.Hash]bool),
+		deletes:     make(map[hash.Hash]bool),
+		partialAdds: make(map[hash.Hash]map[hash.Hash]types.Tuple),
+		emptyTuple:  types.EmptyTuple(b.nbf),
 	}
 }
