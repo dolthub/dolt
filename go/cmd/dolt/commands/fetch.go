cbf973d129e88927fa61d421ab965f48c66c300c
Vinai Rachakonda <vinai@dolthub.com>
2021-08-31 13:45:00 -0700

    Addresses different types of panics with remotes/fetches Issue: #2048 (#2067)
    This pr addresses three issues with fetching remotes.
    
    - A panic with fetching a remote in the form "remotes/.." that is invalid
    - A panic when an added remote that is then fetched is invalid
    - A lack of error when fetching an invalid remote

--- go/cmd/dolt/commands/fetch.go
+++ go/cmd/dolt/commands/fetch.go
@@ -193,10 +193,13 @@ func fetchRefSpecs(ctx context.Context, mode ref.UpdateMode, dEnv *env.DoltEnv, 
 			return errhand.BuildDError("error: failed to read from ").AddCause(err).Build()
 		}
 
+		rsSeen := false
+
 		for _, branchRef := range branchRefs {
 			remoteTrackRef := rs.DestRef(branchRef)
 
 			if remoteTrackRef != nil {
+				rsSeen = true
 				srcDBCommit, verr := fetchRemoteBranch(ctx, dEnv, rem, srcDB, dEnv.DoltDB, branchRef, remoteTrackRef)
 
 				if verr != nil {
@@ -225,6 +228,14 @@ func fetchRefSpecs(ctx context.Context, mode ref.UpdateMode, dEnv *env.DoltEnv, 
 					}
 				}
 			}
+		}
+
+		if !rsSeen {
+			if rb, ok := rs.(ref.BranchToTrackingBranchRefSpec); ok {
+				return errhand.BuildDError("error: %s does not appear to be a dolt database. could not read from the remote database. please make sure you have the correct access rights and the database exists", rb.GetRemRefToLocal()).Build()
+			}
+
+			return errhand.BuildDError("error: remote does not appear to be a dolt database. could not read from the remote database. lease make sure you have the correct access rights and the database exists").Build()
 		}
 	}
 
