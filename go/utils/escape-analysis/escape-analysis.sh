b415f30a596f062887c428e585e8276b74f609b3
Andy Arthur <andy@dolthub.com>
2021-09-06 08:30:18 -0700

    added filter, sort, and uniq

--- go/utils/escape-analysis/escape-analysis.sh
+++ go/utils/escape-analysis/escape-analysis.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-go build -gcflags="-m -N -l" "$1" 2> >(grep -i "/$2")
+go build -gcflags="-m -N -l" "$1" 2>&1 >/dev/null \
+  | grep -i "/$2" \
+  | sed '/does not escape/d' \
+  | sort -s \
+  | uniq
