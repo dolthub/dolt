20419853d4cf3bff4fcf5cd62ab54f835b08bb17
Andy Arthur <andy@dolthub.com>
2021-09-06 12:16:19 -0700

    added setup info

--- go/utils/escape-analysis/escape-analysis.sh
+++ go/utils/escape-analysis/escape-analysis.sh
@@ -6,6 +6,11 @@ LOGLEVEL=1
 #   -m turns on logging with level=$LOGLEVEL
 #   -N disables optimizations
 #   -l disables inlining
+#
+#   To configure this script as a Goland annotation generator,
+#     add a File Watcher (Preferences > Tools > File Watchers).
+#     See "goland-file-watcher-config.png" for config info.
+#
 go build -gcflags="-m=$LOGLEVEL -N -l" "$1" 2>&1 >/dev/null \
   | sed '/does not escape/d' \
   | sort | uniq
