04da15733ab6ac58e4be398a399b2e49b8ac03fa
Max Hoffman <max@dolthub.com>
2021-09-17 19:32:48 -0700

    delete unnecessary code

--- go/libraries/doltcore/doltdb/commit_hooks.go
+++ go/libraries/doltcore/doltdb/commit_hooks.go
@@ -8,28 +8,6 @@ import (
 	"sync"
 )
 
-//const BackupRemoteKey = "DOLT_BACKUP_REMOTE"
-//var postCommitHooks []datas.UpdateHook
-//
-//func init() {
-//	backupUrl := os.Getenv(BackupRemoteKey)
-//	if backupUrl != "" {
-//		// parse remote
-//		ctx := context.Background()
-//		r, srcDb, err := env.CreateRemote(ctx, "backup", backupUrl, nil, nil)
-//		if err != nil {
-//			return
-//		}
-//		// build destDB
-//
-//		postCommitHooks = append(postCommitHooks, func(ctx context.Context, ds datas.Dataset) error {
-//			headRef, _, _ := ds.MaybeHeadRef()
-//			//id := ds.ID()
-//			//return backup.Backup(ctx, srcDb, "temp", r, headRef, nil, nil)
-//		})
-//	}
-//}
-
 func Replicate(ctx context.Context, srcDB datas.Database, destDB *DoltDB, tempTableDir string, ds datas.Dataset) error {
 	stRef, ok, err := ds.MaybeHeadRef()
 	t, _ := stRef.TargetType()
