292fc7caa83c33e7bf72476a3e6e1ea53e9d8a48
Brian Hendriks <brian@dolthub.com>
2021-09-22 12:35:17 -0700

    formatting

--- go/libraries/doltcore/sqle/tables.go
+++ go/libraries/doltcore/sqle/tables.go
@@ -47,7 +47,7 @@ const (
 	partitionMultiplier = 2.0
 )
 
-var MaxRowsPerPartition uint64 = 32*1024
+var MaxRowsPerPartition uint64 = 32 * 1024
 var MinRowsPerPartition uint64 = 1024
 
 func init() {
@@ -361,8 +361,8 @@ func (t *DoltTable) Partitions(ctx *sql.Context) (sql.PartitionIter, error) {
 
 	itemsPerPartition := MaxRowsPerPartition
 	numPartitions := (numElements / itemsPerPartition) + 1
-	if numPartitions < uint64(partitionMultiplier * runtime.NumCPU()) {
-		itemsPerPartition = numElements / uint64(partitionMultiplier * runtime.NumCPU())
+	if numPartitions < uint64(partitionMultiplier*runtime.NumCPU()) {
+		itemsPerPartition = numElements / uint64(partitionMultiplier*runtime.NumCPU())
 		numPartitions = (numElements / itemsPerPartition) + 1
 	}
 
