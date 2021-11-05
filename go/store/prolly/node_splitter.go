efb64f796c53feabb957bd0645c522aace8f32ec
Andy Arthur <andy@dolthub.com>
2021-11-05 12:07:32 -0700

    refactored node splitter, added doc comments

--- go/store/prolly/node_splitter.go
+++ go/store/prolly/node_splitter.go
@@ -15,21 +15,19 @@
 package prolly
 
 import (
-	"sync"
-
 	"github.com/kch42/buzhash"
 )
 
 const (
-	defaultChunkPattern = uint32(1<<12 - 1) // Avg Chunk Size of 4k
-
 	// The window size to use for computing the rolling hash. This is way more than necessary assuming random data
 	// (two bytes would be sufficient with a target chunk size of 4k). The benefit of a larger window is it allows
 	// for better distribution on input with lower entropy. At a target chunk size of 4k, any given byte changing
 	// has roughly a 1.5% chance of affecting an existing boundary, which seems like an acceptable trade-off. The
 	// choice of a prime number provides better distribution for repeating input.
-	chunkWindow  = uint32(67)
-	maxChunkSize = 1 << 24
+	chunkWindow = uint32(67)
+
+	minChunkSize = 1 << 9
+	maxChunkSize = 1 << 14
 )
 
 // nodeSplitter decides where sequences should be split into chunks.
@@ -48,93 +46,101 @@ type nodeSplitter interface {
 	Reset()
 }
 
-type rollingHasher struct {
-	bz              *buzhash.BuzHash
-	offset          uint32
-	salt            byte
-	pattern         uint32
-	window          uint32
+func newDefaultNodeSplitter(salt byte) nodeSplitter {
+	return newSmoothRollingHasher(salt)
+}
+
+// dynamicNodeSplitter is a nodeSplitter designed to constrain the chunk size
+// distribution by reducing the liklihood of forming very large or very small chunks.
+// As the size of the current chunk grows, dynamicNodeSplitter changes the target
+// pattern to make it easier to match. The result is a chunk size distribution
+// that is closer to a binomial distribution, rather than geometric.
+type dynamicNodeSplitter struct {
+	bz     *buzhash.BuzHash
+	offset uint32
+	window uint32
+	salt   byte
+
+	patterns        []dynamicPattern
 	crossedBoundary bool
 }
 
-var _ nodeSplitter = &rollingHasher{}
+var _ nodeSplitter = &dynamicNodeSplitter{}
 
-// Only set by tests
-var (
-	chunkPattern  = defaultChunkPattern
-	chunkConfigMu = &sync.Mutex{}
-)
+type dynamicPattern [2]uint32
 
-func chunkingConfig() (pattern, window uint32) {
-	chunkConfigMu.Lock()
-	defer chunkConfigMu.Unlock()
-	return chunkPattern, chunkWindow
+func (pr dynamicPattern) rangeEnd() uint32 {
+	return pr[0]
 }
 
-func smallTestChunks() {
-	chunkConfigMu.Lock()
-	defer chunkConfigMu.Unlock()
-	chunkPattern = uint32(1<<8 - 1) // Avg Chunk Size of 256 bytes
+func (pr dynamicPattern) pattern() uint32 {
+	return pr[1]
 }
 
-func normalProductionChunks() {
-	chunkConfigMu.Lock()
-	defer chunkConfigMu.Unlock()
-	chunkPattern = defaultChunkPattern
+var smoothPatterns = []dynamicPattern{
+	// min size 512
+	{1024, 1<<16 - 1},
+	{2048, 1<<14 - 1},
+	{4096, 1<<12 - 1},
+	{8192, 1<<10 - 1},
+	{maxChunkSize, 1<<8 - 1},
+	// max size 16K
 }
 
-// TestWithSmallChunks allows testing with small chunks outside of pkg types.
-func TestWithSmallChunks(cb func()) {
-	smallTestChunks()
-	defer normalProductionChunks()
-	cb()
-}
-
-func newDefaultNodeSplitter(salt byte) nodeSplitter {
-	return newRollingHasher(salt)
-	//return newSmoothRollingHasher(salt)
-}
-
-func newRollingHasher(salt byte) *rollingHasher {
-	pattern, window := chunkingConfig()
-
-	rv := &rollingHasher{
-		bz:      buzhash.NewBuzHash(window),
-		pattern: pattern,
-		window:  window,
-		salt:    salt,
+func newSmoothRollingHasher(salt byte) *dynamicNodeSplitter {
+	return &dynamicNodeSplitter{
+		bz:       buzhash.NewBuzHash(chunkWindow),
+		window:   chunkWindow,
+		salt:     salt,
+		patterns: smoothPatterns,
 	}
-
-	return rv
 }
 
-func (rv *rollingHasher) Append(items ...nodeItem) (err error) {
+// Append implements NodeSplitter
+func (sns *dynamicNodeSplitter) Append(items ...nodeItem) (err error) {
 	for _, it := range items {
 		for _, byt := range it {
-			_ = rv.hashByte(byt)
+			_ = sns.hashByte(byt)
 		}
 	}
 	return nil
 }
 
-func (rv *rollingHasher) hashByte(b byte) bool {
-	rv.offset++
-	if !rv.crossedBoundary {
-		rv.bz.HashByte(b ^ rv.salt)
-		rv.crossedBoundary = (rv.bz.Sum32()&rv.pattern == rv.pattern)
-		if rv.offset > maxChunkSize {
-			rv.crossedBoundary = true
+func (sns *dynamicNodeSplitter) hashByte(b byte) bool {
+	sns.offset++
+
+	if !sns.crossedBoundary {
+		sns.bz.HashByte(b ^ sns.salt)
+
+		if sns.offset < minChunkSize {
+			return sns.crossedBoundary
+		}
+		if sns.offset > maxChunkSize {
+			sns.crossedBoundary = true
+			return sns.crossedBoundary
+		}
+
+		hash := sns.bz.Sum32()
+		for _, rp := range sns.patterns {
+			if sns.offset < rp.rangeEnd() {
+				pat := rp.pattern()
+				sns.crossedBoundary = hash&pat == pat
+				return sns.crossedBoundary
+			}
 		}
 	}
-	return rv.crossedBoundary
+
+	return sns.crossedBoundary
 }
 
-func (rv *rollingHasher) CrossedBoundary() bool {
-	return rv.crossedBoundary
+// CrossedBoundary implements NodeSplitter
+func (sns *dynamicNodeSplitter) CrossedBoundary() bool {
+	return sns.crossedBoundary
 }
 
-func (rv *rollingHasher) Reset() {
-	rv.crossedBoundary = false
-	rv.offset = 0
-	rv.bz = buzhash.NewBuzHash(rv.window)
+// Reset implements NodeSplitter
+func (sns *dynamicNodeSplitter) Reset() {
+	sns.crossedBoundary = false
+	sns.offset = 0
+	sns.bz = buzhash.NewBuzHash(sns.window)
 }
