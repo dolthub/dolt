a2a8bdf8315f924de443cdc3c7dd2b9675c41f8e
JCOR11599 <JCOR11599@users.noreply.github.com>
2021-12-27 22:20:22 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/store/types/polygon.go
+++ go/store/types/polygon.go
@@ -23,8 +23,9 @@ package types
 
 import (
 	"context"
-	"github.com/dolthub/dolt/go/store/hash"
 	"strconv"
+
+	"github.com/dolthub/dolt/go/store/hash"
 )
 
 // Polygon is a Noms Value wrapper around a string.
