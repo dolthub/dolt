6a39c9ff161e84534a82bf1f984a21babb1d381b
Vinai Rachakonda <rachakondavinai@gmail.com>
2022-01-06 16:40:19 -0800

    add quotes around table name

--- go/libraries/doltcore/mvdata/engine_table_reader.go
+++ go/libraries/doltcore/mvdata/engine_table_reader.go
@@ -88,7 +88,7 @@ func NewSqlEngineReader(ctx context.Context, dEnv *env.DoltEnv, tableName string
 
 // Used by Dolthub API
 func NewSqlEngineTableReaderWithEngine(sqlCtx *sql.Context, se *sqle.Engine, db dsqle.Database, root *doltdb.RootValue, tableName string) (*sqlEngineTableReader, error) {
-	sch, iter, err := se.Query(sqlCtx, fmt.Sprintf("SELECT * FROM %s", tableName))
+	sch, iter, err := se.Query(sqlCtx, fmt.Sprintf("SELECT * FROM `%s`", tableName))
 	if err != nil {
 		return nil, err
 	}
