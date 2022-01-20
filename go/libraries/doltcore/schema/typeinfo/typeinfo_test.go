2b1f6b5fc4f1303e8672578f0b094cc2f847b335
James Cor <james@dolthub.com>
2022-01-20 14:15:13 -0800

    adding typeinfo tests

--- go/libraries/doltcore/schema/typeinfo/typeinfo_test.go
+++ go/libraries/doltcore/schema/typeinfo/typeinfo_test.go
@@ -353,6 +353,9 @@ func generateTypeInfoArrays(t *testing.T) ([][]TypeInfo, [][]types.Value) {
 			{DefaultInlineBlobType},
 			{Int8Type, Int16Type, Int24Type, Int32Type, Int64Type},
 			{JSONType},
+			{LinestringType},
+			{PointType},
+			{PolygonType},
 			generateSetTypes(t, 16),
 			{TimeType},
 			{Uint8Type, Uint16Type, Uint24Type, Uint32Type, Uint64Type},
@@ -385,6 +388,9 @@ func generateTypeInfoArrays(t *testing.T) ([][]TypeInfo, [][]types.Value) {
 			{types.Int(20), types.Int(215), types.Int(237493), types.Int(2035753568), types.Int(2384384576063)},                            //Int
 			{json.MustTypesJSON(`null`), json.MustTypesJSON(`[]`), json.MustTypesJSON(`"lorem ipsum"`), json.MustTypesJSON(`2.71`),
 				json.MustTypesJSON(`false`), json.MustTypesJSON(`{"a": 1, "b": []}`)}, //JSON
+			{types.Linestring{SRID: 0, Points: []types.Point{{SRID: 0, X: 1, Y: 2},{SRID: 0, X: 3, Y: 4}}}}, // Linestring
+			{types.Point{SRID: 0, X: 1, Y: 2}}, // Point
+			{types.Polygon{SRID: 0, Lines: []types.Linestring{{SRID: 0, Points: []types.Point{{SRID: 0, X: 0, Y: 0},{SRID: 0, X: 0, Y: 1}, {SRID: 0, X: 1, Y: 1}, {SRID: 0, X: 0, Y: 0}}}}}}, // Polygon
 			{types.Uint(1), types.Uint(5), types.Uint(64), types.Uint(42), types.Uint(192)},                                                                                                //Set
 			{types.Int(0), types.Int(1000000 /*"00:00:01"*/), types.Int(113000000 /*"00:01:53"*/), types.Int(247019000000 /*"68:36:59"*/), types.Int(458830485214 /*"127:27:10.485214"*/)}, //Time
 			{types.Uint(20), types.Uint(275), types.Uint(328395), types.Uint(630257298), types.Uint(93897259874)},                                                                          //Uint
