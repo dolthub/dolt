0431cafdd2cdcea550dfffbf3e9d3e64c666e0c6
Andy Arthur <andy@dolthub.com>
2022-01-20 10:21:16 -0800

    swap default format

--- go/store/constants/version.go
+++ go/store/constants/version.go
@@ -41,4 +41,4 @@ const Format718String = "7.18"
 const FormatLD1String = "__LD_1__"
 const FormatDolt1String = "__DOLT_1__"
 
-var FormatDefaultString = FormatLD1String
+var FormatDefaultString = FormatDolt1String
