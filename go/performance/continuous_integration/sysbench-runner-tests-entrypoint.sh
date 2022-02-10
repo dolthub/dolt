303ac04e30b47807549ce5bfe2fcea83efa2402e
Vinai Rachakonda <rachakondavinai@gmail.com>
2022-02-09 17:18:44 -0800

    temp commit for testing

--- go/performance/continuous_integration/sysbench-runner-tests-entrypoint.sh
+++ go/performance/continuous_integration/sysbench-runner-tests-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 dolt version
 # Github Actions ignores the WORKDIR?
-cd ./go/performance/utils/sysbench_runner/cmd
-DEBUG=1 go run . --config=/config.json
+#cd ./go/performance/utils/sysbench_runner/cmd
+#DEBUG=1 go run . --config=/config.json
+
+cd ./go/performance/utils/tpcc_runner/cmd
+DEBUG=1 go run . --config=/tpcc-config.json
