216b3ae9dbc407c160528834544820107510554e
Vinai Rachakonda <rachakondavinai@gmail.com>
2022-02-10 13:02:57 -0800

    remove uneeded directory

--- go/performance/continuous_integration/tpcc/tpcc-runner-tests-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-dolt version
-ls -a .
-# Github Actions ignores the WORKDIR?
-dolt config --global --add user.email "you@example.com"
-dolt config --global --add user.name "Your Name"
-#cd ./go/performance/utils/tpcc_runner/cmd
-DEBUG=1 go run . --config=/tpcc-config.json
