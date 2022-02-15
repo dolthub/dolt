6a1c332a199cca3ff7d4fdbce4c169ba06000f24
Dhruv Sringari <dhruv@dolthub.com>
2022-02-15 10:02:48 -0800

    fix tests

--- go/store/nbs/table_persister_test.go
+++ go/store/nbs/table_persister_test.go
@@ -45,7 +45,7 @@ func TestPlanCompaction(t *testing.T) {
 		}
 		data, name, err := buildTable(content)
 		require.NoError(t, err)
-		ti, err := parseTableIndex(data)
+		ti, err := parseTableIndexByCopy(data)
 		require.NoError(t, err)
 		tr, err := newTableReader(ti, tableReaderAtFromBytes(data), fileBlockSize)
 		require.NoError(t, err)
