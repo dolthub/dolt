865d1af61a80bb19401a9586b608e08f9dead7f5
Andy Arthur <andy@dolthub.com>
2022-02-16 09:47:56 -0800

    added bash script to run tppc locally

--- go/performance/utils/tpcc_runner/local_tpcc.sh
+++ go/performance/utils/tpcc_runner/local_tpcc.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
 
+set -eo pipefail
+
 [ ! -d "./sysbench-tpcc" ] && \
 	echo "run 'git clone git@github.com:Percona-Lab/sysbench-tpcc.git' to gather tpcc scripts" && \
 	exit 1
+
+# parse options
+# superuser.com/questions/186272/
+while test $# -gt 0
+do
+    case "$1" in
+
+        # benchmark with new NomsBinFmt
+        --new-nbf) export DOLT_FORMAT_FEATURE_FLAG=true
+            ;;
+
+    esac
+    shift
+done
+
+DOLTPATH=`which dolt`
+
+cat <<JSON > tpcc-config.json
+{
+  "Servers": [
+    {
+      "Host": "127.0.0.1",
+      "Port": 3307,
+      "Server": "dolt",
+      "Version": "HEAD",
+      "ResultsFormat": "csv",
+      "ServerExec": "$DOLTPATH"
+    }
+  ],
+  "ScriptDir":"./sysbench-tpcc",
+  "ScaleFactors": [1]
+}
+JSON
+
+go run cmd/main.go --config=tpcc-config.json
+
+# cleanup our env var
+unset DOLT_FORMAT_FEATURE_FLAG
 
 echo "success"
