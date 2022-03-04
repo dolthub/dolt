0950ec814369fd04f6f1936943f27a0fe6619d48
Andy Arthur <andy@dolthub.com>
2022-03-04 15:29:44 -0800

    smoke test working

--- integration-tests/transactions/concurrent_tx_test.go
+++ integration-tests/transactions/concurrent_tx_test.go
@@ -15,10 +15,24 @@
 package transactions
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/require"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gocraft/dbr/v2"
 )
+
+var defaultConfig = ServerConfig{
+	database: "mysql",
+	host:     "127.0.0.1",
+	port:     3316,
+	user:     "root",
+	password: "toor",
+}
 
 func TestConcurrentTransactions(t *testing.T) {
 	for _, test := range txTests {
@@ -29,13 +43,128 @@ func TestConcurrentTransactions(t *testing.T) {
 }
 
 type ConcurrentTxTest struct {
-	name string
+	name    string
+	queries []concurrentQuery
 }
 
+type concurrentQuery struct {
+	conn     string
+	write    string
+	query    selector
+	expected []testRow
+}
+
+type selector func(s *dbr.Session) *dbr.SelectStmt
+
+type testRow struct {
+	Pk, C0 int
+}
+
+const (
+	one = "one"
+	two = "two"
+)
+
 var txTests = []ConcurrentTxTest{
-	{name: "todo"},
+	{
+		name: "smoke test",
+		queries: []concurrentQuery{
+			{
+				conn: one,
+				query: func(s *dbr.Session) *dbr.SelectStmt {
+					return s.Select("*").From("data")
+				},
+				expected: []testRow{
+					{1, 1},
+					{2, 2},
+					{3, 3},
+				},
+			},
+		},
+	},
+}
+
+func setupCommon(sess *dbr.Session) (err error) {
+	queries := []string{
+		"CREATE DATABASE IF NOT EXISTS tx;",
+		"USE tx;",
+		"CREATE TABLE data (pk int primary key, c0 int);",
+		"INSERT INTO data VALUES (1,1),(2,2),(3,3);",
+	}
+
+	for _, q := range queries {
+		if _, err = sess.Exec(q); err != nil {
+			return
+		}
+	}
+	return
 }
 
 func testConcurrentTx(t *testing.T, test ConcurrentTxTest) {
-	assert.True(t, true)
+	conns, err := createNamedConnections(defaultConfig, one, two)
+	require.NoError(t, err)
+	defer func() { require.NoError(t, closeNamedConnections(conns)) }()
+
+	err = setupCommon(conns[one])
+	defer func() { require.NoError(t, teardownCommon(conns[one])) }()
+
+	for _, q := range test.queries {
+		conn := conns[q.conn]
+		if q.write != "" {
+			_, err = conn.Query(q.write)
+			require.NoError(t, err)
+		}
+
+		var actual []testRow
+		_, err = q.query(conn).Load(&actual)
+		require.NoError(t, err)
+		assert.Equal(t, q.expected, actual)
+	}
+}
+
+func teardownCommon(sess *dbr.Session) (err error) {
+	_, err = sess.Exec("DROP DATABASE tx;")
+	return
+}
+
+type ServerConfig struct {
+	database string
+	host     string
+	port     int
+	user     string
+	password string
+}
+
+type namedConnections map[string]*dbr.Session
+
+// ConnectionString returns a Data Source Name (DSN) to be used by go clients for connecting to a running server.
+func ConnectionString(config ServerConfig) string {
+	return fmt.Sprintf("%v:%v@tcp(%v:%v)/%s",
+		config.user,
+		config.password,
+		config.host,
+		config.port,
+		config.database,
+	)
+}
+
+func createNamedConnections(config ServerConfig, names ...string) (nc namedConnections, err error) {
+	nc = make(namedConnections, len(names))
+	for _, name := range names {
+		var c *dbr.Connection
+		if c, err = dbr.Open("mysql", ConnectionString(config), nil); err != nil {
+			return nil, err
+		}
+		nc[name] = c.NewSession(nil)
+	}
+	return
+}
+
+func closeNamedConnections(nc namedConnections) (err error) {
+	for _, conn := range nc {
+		if err = conn.Close(); err != nil {
+			return
+		}
+	}
+	return
 }
--- integration-tests/transactions/go.mod
+++ integration-tests/transactions/go.mod
@@ -6,6 +6,7 @@ require github.com/go-sql-driver/mysql v1.6.0
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/gocraft/dbr/v2 v2.7.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
