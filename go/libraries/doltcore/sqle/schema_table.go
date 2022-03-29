78603d3007293f8dc24880b29489bf380f0b10c8
James Cor <james@dolthub.com>
2022-03-29 14:47:04 -0700

    please work

--- go/libraries/doltcore/sqle/schema_table.go
+++ go/libraries/doltcore/sqle/schema_table.go
@@ -169,7 +169,7 @@ func migrateOldSchemasTableToNew(
 	[]sql.Row,
 	error,
 ) {
-	// Copy all of the old data over and add an index column
+	// Copy all of the old data over and add an index column and a extra column
 	var rowsToAdd []sql.Row
 	table, err := schemasTable.doltTable(ctx)
 	if err != nil {
@@ -190,8 +190,8 @@ func migrateOldSchemasTableToNew(
 		if err != nil {
 			return err
 		}
-		// prepend the new id to each row
-		sqlRow = append(sqlRow, id)
+		// prepend the new id and nil to each row
+		sqlRow = append(sqlRow, id, nil)
 		rowsToAdd = append(rowsToAdd, sqlRow)
 		id++
 		return nil
