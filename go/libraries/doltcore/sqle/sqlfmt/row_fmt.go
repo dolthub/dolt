85b4b81417bb9eaca560d935c86c8ca2e3cae21e
JCOR11599 <JCOR11599@users.noreply.github.com>
2022-04-15 18:43:49 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/sqlfmt/row_fmt.go
+++ go/libraries/doltcore/sqle/sqlfmt/row_fmt.go
@@ -18,11 +18,11 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"github.com/dolthub/vitess/go/vt/sqlparser"
 	"strings"
 
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/vitess/go/sqltypes"
+	"github.com/dolthub/vitess/go/vt/sqlparser"
 
 	"github.com/dolthub/dolt/go/libraries/doltcore/row"
 	"github.com/dolthub/dolt/go/libraries/doltcore/schema"
