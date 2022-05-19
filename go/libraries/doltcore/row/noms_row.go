e552a592fc848b9c3eea8269dd508200fca8a817
jennifersp <jennifersp@users.noreply.github.com>
2022-05-19 20:30:21 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/row/noms_row.go
+++ go/libraries/doltcore/row/noms_row.go
@@ -17,9 +17,11 @@ package row
 import (
 	"errors"
 	"fmt"
+
+	"github.com/dolthub/go-mysql-server/sql"
+
 	"github.com/dolthub/dolt/go/libraries/doltcore/schema"
 	"github.com/dolthub/dolt/go/store/types"
-	"github.com/dolthub/go-mysql-server/sql"
 )
 
 type nomsRow struct {
