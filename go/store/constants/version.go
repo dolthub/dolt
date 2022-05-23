86176b220ef7b172b4b379b62f2f4616dfe27d63
Zach Musgrave <zach@dolthub.com>
2022-05-23 14:08:25 -0700

    Restoring ld1

--- go/store/constants/version.go
+++ go/store/constants/version.go
@@ -50,5 +50,5 @@ const FormatDolt1String = "__DOLT_1__"
 // data maps.
 const FormatDoltDevString = "__DOLT_DEV__"
 
-// var FormatDefaultString = FormatLD1String
-var FormatDefaultString = FormatDolt1String
+var FormatDefaultString = FormatLD1String
+// var FormatDefaultString = FormatDolt1String
