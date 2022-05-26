cfd74ab17dc24406bc25dee30e05bd31cbe07089
Andy Arthur <andy@dolthub.com>
2022-05-26 11:56:55 -0700

    unskip typestable enginetests

--- go/libraries/doltcore/sqle/enginetest/dolt_harness.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_harness.go
@@ -103,7 +103,6 @@ var defaultSkippedQueries = []string{
 	"show variables",             // we set extra variables
 	"show create table fk_tbl",   // we create an extra key for the FK that vanilla gms does not
 	"show indexes from",          // we create / expose extra indexes (for foreign keys)
-	"typestable",                 // Bit type isn't working?
 	"show global variables like", // we set extra variables
 }
 
