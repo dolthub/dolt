5803564b6d793fc2a19cf9526ae6d10ad2ef130f
Andy Arthur <andy@dolthub.com>
2022-06-15 10:44:44 -0700

    revert constraint violation skips

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -682,7 +682,9 @@ func TestDoltMerge(t *testing.T) {
 
 // eventually this will be part of TestDoltMerge
 func TestDoltMergeArtifacts(t *testing.T) {
-	t.Skip()
+	if !types.IsFormat_DOLT_1(types.Format_Default) {
+		t.Skip()
+	}
 	for _, script := range MergeViolationsAndConflictsMergeScripts {
 		enginetest.TestScript(t, newDoltHarness(t), script)
 	}
@@ -691,7 +693,9 @@ func TestDoltMergeArtifacts(t *testing.T) {
 // these tests are temporary while there is a difference between the old format
 // and new format merge behaviors.
 func TestDoltMergeAbortOnConflictsAppendViolations(t *testing.T) {
-	t.Skip()
+	if types.IsFormat_DOLT_1(types.Format_Default) {
+		t.Skip()
+	}
 	for _, script := range AppendViolationsAbortOnConflictsMergeScripts {
 		enginetest.TestScript(t, newDoltHarness(t), script)
 	}
