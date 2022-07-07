9162d24a32dfe031fc497829991fad3aea2aa8bc
jennifersp <44716627+jennifersp@users.noreply.github.com>
2022-07-06 17:10:51 -0700

    fix for datetime and time types for exporting parquet file (#3743)

--- go/libraries/doltcore/table/typed/parquet/reader.go
+++ go/libraries/doltcore/table/typed/parquet/reader.go
@@ -28,6 +28,7 @@ import (
 
 	"github.com/dolthub/dolt/go/libraries/doltcore/row"
 	"github.com/dolthub/dolt/go/libraries/doltcore/schema"
+	"github.com/dolthub/dolt/go/libraries/doltcore/schema/typeinfo"
 	"github.com/dolthub/dolt/go/store/types"
 )
 
@@ -102,11 +103,11 @@ func (pr *ParquetReader) ReadSqlRow(ctx context.Context) (sql.Row, error) {
 	allCols.Iter(func(tag uint64, col schema.Column) (stop bool, err error) {
 		val := pr.fileData[col.Name][pr.rowReadCounter]
 		if val != nil {
-			sqlType := col.TypeInfo.ToSqlType()
-			if _, ok := sqlType.(sql.DatetimeType); ok {
-				val = time.Unix(val.(int64), 0)
-			} else if _, ok := sqlType.(sql.TimeType); ok {
-				val = sql.Timespan(val.(int64))
+			switch col.TypeInfo.GetTypeIdentifier() {
+			case typeinfo.DatetimeTypeIdentifier:
+				val = time.UnixMicro(val.(int64))
+			case typeinfo.TimeTypeIdentifier:
+				val = sql.Timespan(time.Duration(val.(int64)).Microseconds())
 			}
 		}
 
