e31d52f7437ac0d45d7c8b6171d7e984da9c7b6a
Jason Fulghum <jason@dolthub.com>
2022-07-20 16:04:06 -0700

    Tweaking argument descriptions.

--- go/cmd/dolt/cli/arg_parser_helpers.go
+++ go/cmd/dolt/cli/arg_parser_helpers.go
@@ -151,8 +151,8 @@ func CreateAddArgParser() *argparser.ArgParser {
 
 func CreateCloneArgParser() *argparser.ArgParser {
 	ap := argparser.NewArgParser()
-	ap.SupportsString(RemoteParam, "", "name", "Name of the remote to be added. Default will be 'origin'.")
-	ap.SupportsString(BranchParam, "b", "branch", "The branch to be cloned.  If not specified all branches will be cloned.")
+	ap.SupportsString(RemoteParam, "", "name", "Name of the remote to be added to the cloned database. The default is 'origin'.")
+	ap.SupportsString(BranchParam, "b", "branch", "The branch to be cloned. If not specified all branches will be cloned.")
 	ap.SupportsString(dbfactory.AWSRegionParam, "", "region", "")
 	ap.SupportsValidatedString(dbfactory.AWSCredsTypeParam, "", "creds-type", "", argparser.ValidatorFromStrList(dbfactory.AWSCredsTypeParam, dbfactory.AWSCredTypes))
 	ap.SupportsString(dbfactory.AWSCredsFileParam, "", "file", "AWS credentials file.")
