494f356d45155a7ad150a887251b1f43b9257f35
jennifersp <jennifersp@users.noreply.github.com>
2022-07-25 18:33:14 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/enginetest/dolt_server_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_server_test.go
@@ -17,7 +17,6 @@ package enginetest
 import (
 	"context"
 	gosql "database/sql"
-	"github.com/gocraft/dbr/v2"
 	"math/rand"
 	"strings"
 	"testing"
@@ -25,6 +24,7 @@ import (
 
 	"github.com/dolthub/go-mysql-server/enginetest/queries"
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/gocraft/dbr/v2"
 	"github.com/stretchr/testify/require"
 
 	"github.com/dolthub/dolt/go/cmd/dolt/commands/sqlserver"
