2b78e64fc5704031bc010caa3a5fbf1834af8549
Max Hoffman <max@dolthub.com>
2022-07-28 13:19:16 -0700

    edit prep workflow script

--- go/utils/repofmt/check_prepared_tests.sh
+++ go/utils/repofmt/check_prepared_tests.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#set -eo pipefail
+set -eo pipefail
 
 script_dir=$(dirname "$0")
 cd $script_dir/../..
 
-all_cnt=$(grep "func Test.*" libraries/doltcore/sqle/enginetest/dolt_engine_test.go | wc -l |  tr -dc '0-9')
-prep_cnt=$(grep "func Test.*Prepared" libraries/doltcore/sqle/enginetest/dolt_engine_test.go | wc -l | tr -dc '0-9')
-skip_cnt=$(grep "SkipPreparedsCount" libraries/doltcore/sqle/enginetest/dolt_engine_test.go | awk '{print $4}' | tr -dc '0-9')
+enginetest_path=libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+all_cnt=$(grep "func Test.*" "$enginetest_path" | wc -l |  tr -dc '0-9')
+prep_cnt=$(grep "func Test.*Prepared" "$enginetest_path" | wc -l | tr -dc '0-9')
+skip_cnt=$(grep "SkipPreparedsCount" "$enginetest_path" | awk '{print $4}' | tr -dc '0-9')
 
 expected="$((all_cnt-skip_cnt))"
 if [[ "$expected" != "$prep_cnt" ]]; then
