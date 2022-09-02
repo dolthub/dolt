c94b71120e0ee1c8ad8d7872fe83e9f852aed99e
Max Hoffman <max@dolthub.com>
2022-09-02 11:27:18 -0700

    PartitionRows lookupBuilder access race

--- go/libraries/doltcore/sqle/indexed_dolt_table.go
+++ go/libraries/doltcore/sqle/indexed_dolt_table.go
@@ -16,6 +16,7 @@ package sqle
 
 import (
 	"github.com/dolthub/go-mysql-server/sql"
+	"sync"
 
 	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/index"
 	"github.com/dolthub/dolt/go/store/types"
@@ -29,6 +30,7 @@ type IndexedDoltTable struct {
 	idx          index.DoltIndex
 	lb           index.LookupBuilder
 	isDoltFormat bool
+	mu           *sync.Mutex
 }
 
 func NewIndexedDoltTable(t *DoltTable, idx index.DoltIndex) *IndexedDoltTable {
@@ -36,6 +38,7 @@ func NewIndexedDoltTable(t *DoltTable, idx index.DoltIndex) *IndexedDoltTable {
 		table:        t,
 		idx:          idx,
 		isDoltFormat: types.IsFormat_DOLT(t.Format()),
+		mu:           &sync.Mutex{},
 	}
 }
 
@@ -70,6 +73,8 @@ func (idt *IndexedDoltTable) Partitions(ctx *sql.Context) (sql.PartitionIter, er
 }
 
 func (idt *IndexedDoltTable) PartitionRows(ctx *sql.Context, part sql.Partition) (sql.RowIter, error) {
+	idt.mu.Lock()
+	defer idt.mu.Unlock()
 	key, canCache, err := idt.table.DataCacheKey(ctx)
 	if err != nil {
 		return nil, err
@@ -82,6 +87,8 @@ func (idt *IndexedDoltTable) PartitionRows(ctx *sql.Context, part sql.Partition)
 }
 
 func (idt *IndexedDoltTable) PartitionRows2(ctx *sql.Context, part sql.Partition) (sql.RowIter, error) {
+	idt.mu.Lock()
+	defer idt.mu.Unlock()
 	key, canCache, err := idt.table.DataCacheKey(ctx)
 	if err != nil {
 		return nil, err
@@ -109,6 +116,7 @@ func NewWritableIndexedDoltTable(t *WritableDoltTable, idx index.DoltIndex) *Wri
 		WritableDoltTable: t,
 		idx:               idx,
 		isDoltFormat:      types.IsFormat_DOLT(idx.Format()),
+		mu:                &sync.Mutex{},
 	}
 }
 
@@ -117,6 +125,7 @@ type WritableIndexedDoltTable struct {
 	idx          index.DoltIndex
 	isDoltFormat bool
 	lb           index.LookupBuilder
+	mu           *sync.Mutex
 }
 
 var _ sql.Table2 = (*WritableIndexedDoltTable)(nil)
@@ -130,6 +139,8 @@ func (t *WritableIndexedDoltTable) Partitions(ctx *sql.Context) (sql.PartitionIt
 }
 
 func (t *WritableIndexedDoltTable) PartitionRows(ctx *sql.Context, part sql.Partition) (sql.RowIter, error) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
 	key, canCache, err := t.DataCacheKey(ctx)
 	if err != nil {
 		return nil, err
