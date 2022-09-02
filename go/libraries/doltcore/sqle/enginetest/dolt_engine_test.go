8e071cfb84b54b5d400a78c737b76e4c7f7c9f33
JCOR11599 <JCOR11599@users.noreply.github.com>
2022-09-02 21:17:15 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -25,7 +25,6 @@ import (
 	"github.com/dolthub/go-mysql-server/enginetest/scriptgen/setup"
 	"github.com/dolthub/go-mysql-server/server"
 	"github.com/dolthub/go-mysql-server/sql"
-	"github.com/dolthub/go-mysql-server/sql/expression"
 	"github.com/dolthub/go-mysql-server/sql/mysql_db"
 	"github.com/dolthub/go-mysql-server/sql/plan"
 	"github.com/dolthub/vitess/go/mysql"
