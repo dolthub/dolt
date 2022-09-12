a5cd11d9481258fcf886dea9b5fd1e90810c8eca
Zach Musgrave <zach@dolthub.com>
2022-09-12 15:26:22 -0700

    killed fwt package

--- go/libraries/doltcore/table/untyped/tabular/formatter_test.go
+++ go/libraries/doltcore/table/untyped/tabular/formatter_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Dolthub, Inc.
+// Copyright 2022 Dolthub, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package fwt
+package tabular
