337aa8bde651e4e5340c3f0897b7bfa2ef40b7ce
Zach Musgrave <zach@dolthub.com>
2022-09-12 15:27:54 -0700

    More killing

--- go/libraries/doltcore/table/pipeline/row.go
+++ go/libraries/doltcore/table/pipeline/row.go
@@ -14,11 +14,6 @@
 
 package pipeline
 
-import (
-	"github.com/dolthub/dolt/go/libraries/doltcore/row"
-	"github.com/dolthub/dolt/go/libraries/doltcore/rowconv"
-)
-
 // ReadableMap is an interface that provides read only access to map properties
 type ReadableMap interface {
 	// Get retrieves an element from the map, and a bool which says if there was a property that exists with that
@@ -59,47 +54,4 @@ func (ip ImmutableProperties) Set(updates map[string]interface{}) ImmutablePrope
 	}
 
 	return ImmutableProperties{allProps}
-}
-
-// RowWithProps is a struct that couples a row being processed by a pipeline with properties.  These properties work as
-// a means of passing data about a row between stages in a pipeline.
-type RowWithProps struct {
-	Row   row.Row
-	Props ImmutableProperties
-}
-
-// NewRowWithProps creates a RowWith props from a row and a map of properties
-func NewRowWithProps(r row.Row, props map[string]interface{}) RowWithProps {
-	return RowWithProps{r, ImmutableProperties{props}}
-}
-
-// GetRowConvTranformFunc can be used to wrap a RowConverter and use that RowConverter in a pipeline.
-func GetRowConvTransformFunc(rc *rowconv.RowConverter) func(row.Row, ReadableMap) ([]*TransformedRowResult, string) {
-	if rc.IdentityConverter {
-		return func(inRow row.Row, props ReadableMap) (outRows []*TransformedRowResult, badRowDetails string) {
-			return []*TransformedRowResult{{RowData: inRow, PropertyUpdates: nil}}, ""
-		}
-	} else {
-		return func(inRow row.Row, props ReadableMap) (outRows []*TransformedRowResult, badRowDetails string) {
-			outRow, err := rc.Convert(inRow)
-
-			if err != nil {
-				return nil, err.Error()
-			}
-
-			if isv, err := row.IsValid(outRow, rc.DestSch); err != nil {
-				return nil, err.Error()
-			} else if !isv {
-				col, err := row.GetInvalidCol(outRow, rc.DestSch)
-
-				if err != nil {
-					return nil, "invalid column"
-				} else {
-					return nil, "invalid column: " + col.Name
-				}
-			}
-
-			return []*TransformedRowResult{{RowData: outRow, PropertyUpdates: nil}}, ""
-		}
-	}
 }
