ff04472567a61b34d3b37453e2fc5a9e2634c032
xujihui1985 <xujihui1985@gmail.com>
2022-09-22 09:27:45 +0800

    fix: copyright and failed unittest

--- go/libraries/doltcore/dbfactory/oss.go
+++ go/libraries/doltcore/dbfactory/oss.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package dbfactory
 
 import (
@@ -5,7 +19,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -161,7 +174,7 @@ func (opt ossCredential) getAccessKeySecret() (string, error) {
 }
 
 func readOSSCredentialsFromFile(credFile string) (ossCredentials, error) {
-	data, err := ioutil.ReadFile(credFile)
+	data, err := os.ReadFile(credFile)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read oss cred file %s, err: %s", credFile, err)
 	}
