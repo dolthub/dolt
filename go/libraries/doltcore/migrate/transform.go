a787f5baff10ab886ea0c7ccfb8d54e38a678d1d
Dhruv Sringari <dhruv@dolthub.com>
2022-10-26 13:07:40 -0700

    validate ws migration, skip table validation if table did not change (#4630)

--- go/libraries/doltcore/migrate/transform.go
+++ go/libraries/doltcore/migrate/transform.go
@@ -74,6 +74,16 @@ func migrateWorkingSet(ctx context.Context, brRef ref.BranchRef, wsRef ref.Worki
 		return err
 	}
 
+	err = validateRootValue(ctx, oldHeadRoot, oldWs.WorkingRoot(), wr)
+	if err != nil {
+		return err
+	}
+
+	err = validateRootValue(ctx, oldHeadRoot, oldWs.StagedRoot(), sr)
+	if err != nil {
+		return err
+	}
+
 	newWs := doltdb.EmptyWorkingSet(wsRef).WithWorkingRoot(wr).WithStagedRoot(sr)
 
 	return new.UpdateWorkingSet(ctx, wsRef, newWs, hash.Hash{}, oldWs.Meta())
@@ -179,7 +189,7 @@ func migrateCommit(ctx context.Context, oldCm *doltdb.Commit, new *doltdb.DoltDB
 
 	// validate root after we flush the ChunkStore to facilitate
 	// investigating failed migrations
-	if err = validateRootValue(ctx, oldRoot, mRoot); err != nil {
+	if err = validateRootValue(ctx, oldParentRoot, oldRoot, mRoot); err != nil {
 		return err
 	}
 
