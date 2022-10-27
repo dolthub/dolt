ad966508a3484c7b44329f3d8a6cb7235c24efa8
James Cor <james@dolthub.com>
2022-10-27 10:49:37 -0700

    some feedback

--- go/libraries/doltcore/schema/index.go
+++ go/libraries/doltcore/schema/index.go
@@ -69,16 +69,16 @@ type indexImpl struct {
 }
 
 func NewIndex(name string, tags, allTags []uint64, indexColl IndexCollection, props IndexProperties) Index {
-	var idxColl *indexCollectionImpl
+	var indexCollImpl *indexCollectionImpl
 	if indexColl != nil {
-		idxColl = indexColl.(*indexCollectionImpl)
+		indexCollImpl = indexColl.(*indexCollectionImpl)
 	}
 
 	return &indexImpl{
 		name:          name,
 		tags:          tags,
 		allTags:       allTags,
-		indexColl:     idxColl,
+		indexColl:     indexCollImpl,
 		isUnique:      props.IsUnique,
 		isUserDefined: props.IsUserDefined,
 		comment:       props.Comment,
