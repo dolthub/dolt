e078cf09a3722ce5a617fd3fb748b6edd5425c0d
Dhruv Sringari <dhruv@dolthub.com>
2022-11-30 16:57:40 -0800

    fix old format tests

--- go/libraries/doltcore/sqle/tables.go
+++ go/libraries/doltcore/sqle/tables.go
@@ -766,14 +766,19 @@ func (t *WritableDoltTable) truncate(
 
 func copyConstraintViolationsAndConflicts(ctx context.Context, from, to *doltdb.Table) (*doltdb.Table, error) {
 	if !types.IsFormat_DOLT(to.Format()) {
-		confSch, conf, err := from.GetConflicts(ctx)
-		if err != nil {
+		if has, err := from.HasConflicts(ctx); err != nil {
 			return nil, err
+		} else if has {
+			confSch, conf, err := from.GetConflicts(ctx)
+			if err != nil {
+				return nil, err
+			}
+			to, err = to.SetConflicts(ctx, confSch, conf)
+			if err != nil {
+				return nil, err
+			}
 		}
-		to, err = to.SetConflicts(ctx, confSch, conf)
-		if err != nil {
-			return nil, err
-		}
+
 		viols, err := from.GetConstraintViolations(ctx)
 		if err != nil {
 			return nil, err
