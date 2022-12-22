9179cfdfa6bcafeac29f2d9ef1d91a057bde98a7
Andy Arthur <andy@dolthub.com>
2022-12-21 17:06:57 -0800

    turn on chunk journal feature-flag

--- go/store/nbs/chunk_journal.go
+++ go/store/nbs/chunk_journal.go
@@ -30,7 +30,7 @@ import (
 	"github.com/dolthub/dolt/go/store/hash"
 )
 
-var ChunkJournalFeatureFlag = false
+var ChunkJournalFeatureFlag = true
 
 func init() {
 	if os.Getenv("DOLT_ENABLE_CHUNK_JOURNAL") != "" {
