6fbe5b71cc969ed41cfd286f15d1870a9853e3c8
Andy Arthur <andy@dolthub.com>
2023-01-13 15:43:53 -0800

    go/store/prolly/tree: fix recursive tree walks in ChunkStore sanity check

--- go/store/prolly/tree/node_store.go
+++ go/store/prolly/tree/node_store.go
@@ -18,6 +18,8 @@ import (
 	"context"
 	"sync"
 
+	"github.com/dolthub/dolt/go/store/prolly/message"
+
 	"github.com/dolthub/dolt/go/store/chunks"
 	"github.com/dolthub/dolt/go/store/hash"
 	"github.com/dolthub/dolt/go/store/pool"
@@ -147,16 +149,13 @@ func (ns nodeStore) Write(ctx context.Context, nd Node) (hash.Hash, error) {
 	c := chunks.NewChunk(nd.bytes())
 	assertTrue(c.Size() > 0, "cannot write empty chunk to ChunkStore")
 
-	getAddrs := func(ctx context.Context, c chunks.Chunk) (hash.HashSet, error) {
-		valRefs := make(hash.HashSet)
-		err := WalkAddresses(ctx, nd, ns, func(ctx context.Context, addr hash.Hash) error {
-			valRefs.Insert(addr)
+	getAddrs := func(ctx context.Context, ch chunks.Chunk) (addrs hash.HashSet, err error) {
+		addrs = hash.NewHashSet()
+		err = message.WalkAddresses(ctx, ch.Data(), func(ctx context.Context, a hash.Hash) error {
+			addrs.Insert(a)
 			return nil
 		})
-		if err != nil {
-			return nil, err
-		}
-		return valRefs, nil
+		return
 	}
 
 	if err := ns.store.Put(ctx, c, getAddrs); err != nil {
