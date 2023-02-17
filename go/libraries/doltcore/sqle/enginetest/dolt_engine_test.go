8b104c534296bccfcb76a67dd70e92b89a017ff3
James Cor <james@dolthub.com>
2023-02-17 14:49:44 -0800

    unscoped diff table prepared fixes

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -26,7 +26,6 @@ import (
 	"github.com/dolthub/go-mysql-server/enginetest/scriptgen/setup"
 	"github.com/dolthub/go-mysql-server/server"
 	"github.com/dolthub/go-mysql-server/sql"
-	"github.com/dolthub/go-mysql-server/sql/expression"
 	"github.com/dolthub/go-mysql-server/sql/mysql_db"
 	"github.com/dolthub/go-mysql-server/sql/plan"
 	gmstypes "github.com/dolthub/go-mysql-server/sql/types"
@@ -190,50 +189,83 @@ func TestSingleQueryPrepared(t *testing.T) {
 }
 
 func TestSingleScriptPrepared(t *testing.T) {
-	t.Skip()
-	s := []setup.SetupScript{
-		{
-			"create table test (pk int primary key, c1 int)",
-			"call dolt_add('.')",
-			"insert into test values (0,0), (1,1);",
-			"set @Commit1 = dolt_commit('-am', 'creating table');",
-			"call dolt_branch('-c', 'main', 'newb')",
-			"alter table test add column c2 int;",
-			"set @Commit2 = dolt_commit('-am', 'alter table');",
-		},
-	}
-	tt := queries.QueryTest{
-		Query: "select * from test as of 'HEAD~2' where pk=?",
-		Bindings: map[string]sql.Expression{
-			"v1": expression.NewLiteral(0, gmstypes.Int8),
-		},
-		Expected: []sql.Row{{0, 0}},
-	}
+	var script = queries.ScriptTest{
+		Name:        "DELETE ME",
+		SetUpScript: []string{
+			"create table regularTable (a int primary key, b int, c int);",
+			"create table droppedTable (a int primary key, b int, c int);",
+			"create table renamedEmptyTable (a int primary key, b int, c int);",
+			"call dolt_add('.');",
+			"insert into regularTable values (1, 2, 3), (2, 3, 4);",
+			"insert into droppedTable values (1, 2, 3), (2, 3, 4);",
+			"set @Commit1 = '';",
+			"CALL DOLT_COMMIT_HASH_OUT(@Commit1, '-am', 'Creating tables x and y');",
 
+			// changeSet: STAGED; data change: false; schema change: true
+			"create table addedTable (a int primary key, b int, c int);",
+			"call DOLT_ADD('addedTable');",
+			// changeSet: STAGED; data change: true; schema change: true
+			"drop table droppedTable;",
+			"call DOLT_ADD('droppedTable');",
+			// changeSet: WORKING; data change: false; schema change: true
+			"rename table renamedEmptyTable to newRenamedEmptyTable",
+			// changeSet: WORKING; data change: true; schema change: false
+			"insert into regularTable values (3, 4, 5);",
+		},
+		Assertions: []queries.ScriptTestAssertion{
+			{
+				Query:    "SELECT commit_hash, committer FROM DOLT_DIFF WHERE commit_hash <> @Commit1 AND committer = 'billy bob' AND commit_hash NOT IN ('WORKING','STAGED');",
+				Expected: []sql.Row{},
+			},
+		},
+	}
 	harness := newDoltHarness(t)
-	harness.Setup(setup.MydbData, s)
+	enginetest.TestScriptPrepared(t, harness, script)
 
-	e, err := harness.NewEngine(t)
-	defer e.Close()
-	require.NoError(t, err)
-	ctx := harness.NewContext()
-
-	//e.Analyzer.Debug = true
-	//e.Analyzer.Verbose = true
-
-	// full impl
-	pre1, sch1, rows1 := enginetest.MustQueryWithPreBindings(ctx, e, tt.Query, tt.Bindings)
-	fmt.Println(pre1, sch1, rows1)
-
-	// inline bindings
-	sch2, rows2 := enginetest.MustQueryWithBindings(ctx, e, tt.Query, tt.Bindings)
-	fmt.Println(sch2, rows2)
-
-	// no bindings
-	//sch3, rows3 := enginetest.MustQuery(ctx, e, rawQuery)
-	//fmt.Println(sch3, rows3)
-
-	enginetest.TestQueryWithContext(t, ctx, e, harness, tt.Query, tt.Expected, tt.ExpectedColumns, tt.Bindings)
+	//t.Skip()
+	//s := []setup.SetupScript{
+	//	{
+	//		"create table test (pk int primary key, c1 int)",
+	//		"call dolt_add('.')",
+	//		"insert into test values (0,0), (1,1);",
+	//		"set @Commit1 = dolt_commit('-am', 'creating table');",
+	//		"call dolt_branch('-c', 'main', 'newb')",
+	//		"alter table test add column c2 int;",
+	//		"set @Commit2 = dolt_commit('-am', 'alter table');",
+	//	},
+	//}
+	//tt := queries.QueryTest{
+	//	Query: "select * from test as of 'HEAD~2' where pk=?",
+	//	Bindings: map[string]sql.Expression{
+	//		"v1": expression.NewLiteral(0, gmstypes.Int8),
+	//	},
+	//	Expected: []sql.Row{{0, 0}},
+	//}
+	//
+	//harness := newDoltHarness(t)
+	//harness.Setup(setup.MydbData, s)
+	//
+	//e, err := harness.NewEngine(t)
+	//defer e.Close()
+	//require.NoError(t, err)
+	//ctx := harness.NewContext()
+	//
+	////e.Analyzer.Debug = true
+	////e.Analyzer.Verbose = true
+	//
+	//// full impl
+	//pre1, sch1, rows1 := enginetest.MustQueryWithPreBindings(ctx, e, tt.Query, tt.Bindings)
+	//fmt.Println(pre1, sch1, rows1)
+	//
+	//// inline bindings
+	//sch2, rows2 := enginetest.MustQueryWithBindings(ctx, e, tt.Query, tt.Bindings)
+	//fmt.Println(sch2, rows2)
+	//
+	//// no bindings
+	////sch3, rows3 := enginetest.MustQuery(ctx, e, rawQuery)
+	////fmt.Println(sch3, rows3)
+	//
+	//enginetest.TestQueryWithContext(t, ctx, e, harness, tt.Query, tt.Expected, tt.ExpectedColumns, tt.Bindings)
 }
 
 func TestVersionedQueries(t *testing.T) {
