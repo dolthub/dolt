f015743f17df6e7f505194be0bb1a28a29659d45
Maximilian Hoffman <max@dolthub.com>
2023-02-22 10:54:29 -0800

    [no-release-notes] better systab CI tests (#5395)
    * add tests for different systab joins
    
    * add tests for different systab joins
    
    * delete merge tests
    
    * [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh
    
    * bump GMS
    
    * bump
    
    * [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh
    
    ---------
    
    Co-authored-by: max-hoffman <max-hoffman@users.noreply.github.com>

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -26,6 +26,7 @@ import (
 	"github.com/dolthub/go-mysql-server/enginetest/scriptgen/setup"
 	"github.com/dolthub/go-mysql-server/server"
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/analyzer"
 	"github.com/dolthub/go-mysql-server/sql/mysql_db"
 	"github.com/dolthub/go-mysql-server/sql/plan"
 	gmstypes "github.com/dolthub/go-mysql-server/sql/types"
@@ -1351,6 +1352,13 @@ func mustNewEngine(t *testing.T, h enginetest.Harness) *gms.Engine {
 	return e
 }
 
+var biasedCosters = []analyzer.Coster{
+	analyzer.NewInnerBiasedCoster(),
+	analyzer.NewLookupBiasedCoster(),
+	analyzer.NewHashBiasedCoster(),
+	analyzer.NewMergeBiasedCoster(),
+}
+
 func TestSystemTableIndexes(t *testing.T) {
 	if !types.IsFormat_DOLT(types.Format_Default) {
 		t.Skip("only new format support system table indexing")
@@ -1361,23 +1369,27 @@ func TestSystemTableIndexes(t *testing.T) {
 		harness.SkipSetupCommit()
 		e := mustNewEngine(t, harness)
 		defer e.Close()
+		e.Analyzer.Coster = analyzer.NewMergeBiasedCoster()
 
 		ctx := enginetest.NewContext(harness)
 		for _, q := range stt.setup {
 			enginetest.RunQuery(t, e, harness, q)
 		}
 
-		for _, tt := range stt.queries {
-			t.Run(fmt.Sprintf("%s: %s", stt.name, tt.query), func(t *testing.T) {
-				if tt.skip {
-					t.Skip()
-				}
+		for i, c := range []string{"inner", "lookup", "hash", "merge"} {
+			e.Analyzer.Coster = biasedCosters[i]
+			for _, tt := range stt.queries {
+				t.Run(fmt.Sprintf("%s(%s): %s", stt.name, c, tt.query), func(t *testing.T) {
+					if tt.skip {
+						t.Skip()
+					}
 
-				ctx = ctx.WithQuery(tt.query)
-				if tt.exp != nil {
-					enginetest.TestQueryWithContext(t, ctx, e, harness, tt.query, tt.exp, nil, nil)
-				}
-			})
+					ctx = ctx.WithQuery(tt.query)
+					if tt.exp != nil {
+						enginetest.TestQueryWithContext(t, ctx, e, harness, tt.query, tt.exp, nil, nil)
+					}
+				})
+			}
 		}
 	}
 }
