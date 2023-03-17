622b65264d1a21522ce3c85ef43fe40c0da82e9e
Aaron Son <aaron@dolthub.com>
2023-03-17 12:27:17 -0700

    integration-tests/go-sql-server-driver: Add a concurrent gc test where we create dolt commits.

--- integration-tests/go-sql-server-driver/concurrent_gc_test.go
+++ integration-tests/go-sql-server-driver/concurrent_gc_test.go
@@ -16,114 +16,105 @@ package main
 
 import (
 	"context"
-	"testing"
-	"strings"
-	"time"
-	"fmt"
+	"database/sql"
 	sqldriver "database/sql/driver"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
 
+	"github.com/stretchr/testify/require"
 	"golang.org/x/sync/errgroup"
-        "github.com/stretchr/testify/require"
 
 	driver "github.com/dolthub/dolt/go/libraries/doltcore/dtestutils/sql_server_driver"
 )
 
 func TestConcurrentGC(t *testing.T) {
-	NumThreads := 8
-	Duration := 30 * time.Second
-
-        u, err := driver.NewDoltUser()
-        require.NoError(t, err)
-        t.Cleanup(func() {
-                u.Cleanup()
-        })
-
-        rs, err := u.MakeRepoStore()
-        require.NoError(t, err)
-
-        repo, err := rs.MakeRepo("concurrent_gc_test")
-        require.NoError(t, err)
-
-	server := MakeServer(t, repo, &driver.Server{})
-	server.DBName = "concurrent_gc_test"
-
-	db, err := server.DB(driver.Connection{User: "root"})
-	require.NoError(t, err)
-	defer db.Close()
-
-	func() {
-		conn, err := db.Conn(context.Background())
-		require.NoError(t, err)
-		defer conn.Close()
-
-		// We're going to bootstrap the database with a table which has id, val, id == [0,7*1024], val == 0.
-		_, err = conn.ExecContext(context.Background(), "create table vals (id int primary key, val int)")
-		require.NoError(t, err)
-		vals := []string{}
-		for i := 0; i <= 7 * 1024; i++ {
-			vals = append(vals, fmt.Sprintf("(%d,0)", i))
-		}
-		_, err = conn.ExecContext(context.Background(), "insert into vals values " + strings.Join(vals, ","))
-		require.NoError(t, err)
-	}()
-
-	start := time.Now()
-
-	var eg errgroup.Group
-
-	// We're going to spawn 8 threads, each running mutations on their own part of the table...
-	for i := 0; i < NumThreads; i++ {
-		i := i * 1024
-		eg.Go(func() error {
-			for j := 0; time.Since(start) < Duration; j++ {
-				func() {
-					conn, err := db.Conn(context.Background())
-					if err != nil {
-						t.Logf("err in Conn: %v", err)
-						return
-					}
-					defer conn.Close()
-					_, err = conn.ExecContext(context.Background(), "update vals set val = val+1 where id = ?", i)
-					if err != nil {
-						t.Logf("err in Exec: %v", err)
-					}
-				}()
-			}
-			return nil
-		})
-	}
-
-	// We spawn a thread which calls dolt_gc() periodically
-	eg.Go(func() error {
-		for time.Since(start) < Duration {
-			func() {
-				conn, err := db.Conn(context.Background())
-				if err != nil {
-					t.Logf("err in Conn for dolt_gc: %v", err)
-					return
-				}
-				b := time.Now()
-				_, err = conn.ExecContext(context.Background(), "call dolt_gc()")
-				if err != nil {
-					t.Logf("err in Exec dolt_gc: %v", err)
-				} else {
-					t.Logf("successful dolt_gc took %v", time.Since(b))
-				}
-				// After calling dolt_gc, the connection is bad. Remove it from the connection pool.
-				conn.Raw(func(_ any) error {
-					return sqldriver.ErrBadConn
-				})
-			}()
-			time.Sleep(100 * time.Millisecond)
-		}
-		return nil
+	var gct gcTest
+	gct.numThreads = 8
+	gct.duration = 10 * time.Second
+	t.Run("NoCommits", func(t *testing.T) {
+		gct.run(t)
 	})
+	gct.commit = true
+	t.Run("Commits", func(t *testing.T) {
+		gct.run(t)
+	})
+}
 
-	eg.Wait()
+type gcTest struct {
+	numThreads int
+	duration   time.Duration
+	commit     bool
+}
 
-	conn, err := db.Conn(context.Background())
+func (gct gcTest) createDB(t *testing.T, ctx context.Context, db *sql.DB) {
+	conn, err := db.Conn(ctx)
 	require.NoError(t, err)
-	rows, err := conn.QueryContext(context.Background(), "select val from vals where id in (0, 1024, 2048, 3072, 4096, 5120, 6144, 7168)")
+	defer conn.Close()
+
+	// We're going to bootstrap the database with a table which has id, val, id == [0,7*1024], val == 0.
+	_, err = conn.ExecContext(ctx, "create table vals (id int primary key, val int)")
+	require.NoError(t, err)
+	vals := []string{}
+	for i := 0; i <= (gct.numThreads-1)*1024; i++ {
+		vals = append(vals, fmt.Sprintf("(%d,0)", i))
+	}
+	_, err = conn.ExecContext(ctx, "insert into vals values "+strings.Join(vals, ","))
+	require.NoError(t, err)
+	_, err = conn.ExecContext(ctx, "call dolt_commit('-Am', 'create vals table')")
+	require.NoError(t, err)
+}
+
+func (gct gcTest) doUpdate(t *testing.T, ctx context.Context, db *sql.DB, i int) {
+	conn, err := db.Conn(ctx)
+	if err != nil {
+		t.Logf("err in Conn: %v", err)
+		return
+	}
+	defer conn.Close()
+	_, err = conn.ExecContext(ctx, "update vals set val = val+1 where id = ?", i)
+	if err != nil {
+		t.Logf("err in Exec update: %v", err)
+	}
+	if gct.commit {
+		_, err = conn.ExecContext(ctx, fmt.Sprintf("call dolt_commit('-am', 'increment vals id = %d')", i))
+		if err != nil {
+			t.Logf("err in Exec call dolt_commit: %v", err)
+		}
+	}
+}
+
+func (gct gcTest) doGC(t *testing.T, ctx context.Context, db *sql.DB) {
+	conn, err := db.Conn(ctx)
+	if err != nil {
+		t.Logf("err in Conn for dolt_gc: %v", err)
+		return
+	}
+	b := time.Now()
+	_, err = conn.ExecContext(ctx, "call dolt_gc()")
+	if err != nil {
+		t.Logf("err in Exec dolt_gc: %v", err)
+	} else {
+		t.Logf("successful dolt_gc took %v", time.Since(b))
+	}
+	// After calling dolt_gc, the connection is bad. Remove it from the connection pool.
+	conn.Raw(func(_ any) error {
+		return sqldriver.ErrBadConn
+	})
+}
+
+func (gct gcTest) finalize(t *testing.T, ctx context.Context, db *sql.DB) {
+	conn, err := db.Conn(ctx)
+	require.NoError(t, err)
+	var ids []any
+	var qmarks []string
+	for i := 0; i < gct.numThreads*1024; i += 1024 {
+		ids = append(ids, i)
+		qmarks = append(qmarks, "?")
+	}
+	rows, err := conn.QueryContext(ctx, fmt.Sprintf("select val from vals where id in (%s)", strings.Join(qmarks, ",")), ids...)
+	require.NoError(t, err)
 	i := 0
 	cnt := 0
 	for rows.Next() {
@@ -132,9 +123,69 @@ func TestConcurrentGC(t *testing.T) {
 		require.NoError(t, rows.Scan(&val))
 		cnt += val
 	}
-	require.Equal(t, 8, i)
+	require.Equal(t, len(ids), i)
 	t.Logf("successfully updated val %d times", cnt)
 	require.NoError(t, rows.Close())
 	require.NoError(t, rows.Err())
+
+	rows, err = conn.QueryContext(ctx, "select count(*) from dolt_log")
+	require.NoError(t, err)
+	require.True(t, rows.Next())
+	require.NoError(t, rows.Scan(&cnt))
+	t.Logf("database has %d commit(s)", cnt)
+	require.False(t, rows.Next())
+	require.NoError(t, rows.Close())
+	require.NoError(t, rows.Err())
 	require.NoError(t, conn.Close())
+}
+
+func (gct gcTest) run(t *testing.T) {
+	u, err := driver.NewDoltUser()
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		u.Cleanup()
+	})
+
+	rs, err := u.MakeRepoStore()
+	require.NoError(t, err)
+
+	repo, err := rs.MakeRepo("concurrent_gc_test")
+	require.NoError(t, err)
+
+	server := MakeServer(t, repo, &driver.Server{})
+	server.DBName = "concurrent_gc_test"
+
+	db, err := server.DB(driver.Connection{User: "root"})
+	require.NoError(t, err)
+	defer db.Close()
+
+	gct.createDB(t, context.Background(), db)
+
+	start := time.Now()
+
+	var eg errgroup.Group
+
+	// We're going to spawn 8 threads, each running mutations on their own part of the table...
+	for i := 0; i < gct.numThreads; i++ {
+		i := i * 1024
+		eg.Go(func() error {
+			for j := 0; time.Since(start) < gct.duration; j++ {
+				gct.doUpdate(t, context.Background(), db, i)
+			}
+			return nil
+		})
+	}
+
+	// We spawn a thread which calls dolt_gc() periodically
+	eg.Go(func() error {
+		for time.Since(start) < gct.duration {
+			gct.doGC(t, context.Background(), db)
+			time.Sleep(100 * time.Millisecond)
+		}
+		return nil
+	})
+
+	eg.Wait()
+
+	gct.finalize(t, context.Background(), db)
 }
