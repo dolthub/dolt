0bdcf865bde06c72d01bb006584a03a271d0bfea
Jason Fulghum <jason@dolthub.com>
2023-03-23 12:28:11 -0700

    whitespace

--- go/libraries/doltcore/merge/merge.go
+++ go/libraries/doltcore/merge/merge.go
@@ -159,7 +159,6 @@ func MergeRoots(
 			if err != nil {
 				return nil, nil, err
 			}
-
 		} else {
 			// This is a deleted table that the merge root still has
 			if stats.Operation != TableRemoved {
