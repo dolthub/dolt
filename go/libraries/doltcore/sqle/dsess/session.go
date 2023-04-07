4bc64f35e01ab32f0e6de51574c739708fbad401
zachmu <zachmu@users.noreply.github.com>
2023-04-07 22:38:31 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/dsess/session.go
+++ go/libraries/doltcore/sqle/dsess/session.go
@@ -579,12 +579,12 @@ func (d *DoltSession) NewPendingCommit(ctx *sql.Context, dbName string, roots do
 		}
 		roots.Head = newRoots.Head
 		roots.Staged = newRoots.Staged
-		
+
 		err = d.SetWorkingSet(ctx, dbName, sessionState.WorkingSet.WithStagedRoot(newRoots.Staged))
 		if err != nil {
 			return nil, err
 		}
-		
+
 		roots.Head = newRoots.Head
 	}
 
