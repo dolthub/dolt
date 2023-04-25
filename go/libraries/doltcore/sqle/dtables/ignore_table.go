6c29430fbafb94d0c5bba8132c81756145d31919
Nick Tobey <nicktobey@nicktobey.me>
2023-04-25 12:26:00 -0700

    When creating the backing table, avoid calling DoltSession.SetRoot.

--- go/libraries/doltcore/sqle/dtables/ignore_table.go
+++ go/libraries/doltcore/sqle/dtables/ignore_table.go
@@ -241,6 +241,15 @@ func (iw *ignoreWriter) StatementBegin(ctx *sql.Context) {
 			return
 		}
 
+		// We use WriteSession.SetWorkingSet instead of DoltSession.SetRoot because we want to avoid modifying the root
+		// until the end of the transaction, but we still want the WriteSession to be able to find the newly
+		// created table.
+		err = dbState.WriteSession.SetWorkingSet(ctx, dbState.WorkingSet.WithWorkingRoot(newRootValue))
+		if err != nil {
+			iw.errDuringStatementBegin = err
+			return
+		}
+
 		dSess.SetRoot(ctx, dbName, newRootValue)
 	}
 
