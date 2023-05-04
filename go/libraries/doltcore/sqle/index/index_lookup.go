5397b495ad2611174f52763e342ce335a2734f3d
James Cor <james@dolthub.com>
2023-05-04 15:46:16 -0700

    delete reversible for old format

--- go/libraries/doltcore/sqle/index/index_lookup.go
+++ go/libraries/doltcore/sqle/index/index_lookup.go
@@ -469,7 +469,6 @@ type nomsLookupBuilder struct {
 // NewRowIter implements IndexLookup
 func (lb *nomsLookupBuilder) NewRowIter(ctx *sql.Context, part sql.Partition) (sql.RowIter, error) {
 	p := part.(rangePartition)
-	p.nomsRange.Reverse = p.isReverse
 	ranges := []*noms.ReadRange{p.nomsRange}
 	return RowIterForNomsRanges(ctx, lb.idx, ranges, lb.projections, lb.s)
 }
