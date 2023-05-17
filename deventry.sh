2ad7e46c65c18201a8f8dc6cc25c543617f06553
Aaron Son <aaron@dolthub.com>
2023-05-17 10:28:04 -0700

    deventry.sh: Add a small entrypoint shell script for setting up a dev env in a docker container.
    We can iterate from here.

---
+++ deventry.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# A quick entrypoint for working in a pristine, ephemeral docker container with Ubuntu.
+#
+# Suggested utilization:
+#
+# docker run --rm -ti -v `pwd`:/src ubuntu /src/deventry.sh
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+
+apt-get install -y curl 
+curl -o /go1.20.4.linux-amd64.tar.gz -L https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf /go1.20.4.linux-amd64.tar.gz
+rm -f /go1.20.4.linux-amd64.tar.gz
+
+apt-get install -y git python3 python3.10-venv
+
+git clone https://github.com/bats-core/bats-core.git /bats-core
+cd /bats-core
+./install.sh /usr/local
+
+python3 -m venv /bats_venv
+echo "source /bats_venv/bin/activate" >> /etc/bash.bashrc
+source /bats_venv/bin/activate
+pip3 install mysql-connector-python pandas pyarrow
+
+unset DEBIAN_FRONTEND
+cd /src
+export PATH=/root/go/bin:/usr/local/go/bin:"$PATH"
+exec /bin/bash -l
