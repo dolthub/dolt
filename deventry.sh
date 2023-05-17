22a7ee67a2fe087b6f6a413a913213ba6ceb5722
Aaron Son <aaron@dolthub.com>
2023-05-17 12:23:50 -0700

    deventry.sh: Move version numbers of somem dependencies to shell parameters.

--- deventry.sh
+++ deventry.sh
@@ -2,27 +2,30 @@
 
 # A quick entrypoint for working in a pristine, ephemeral docker container with Ubuntu.
 #
-# Suggested utilization:
+# Suggested usage:
 #
 # docker run --rm -ti -v `pwd`:/src ubuntu /src/deventry.sh
 
 set -e
+
+BATS_VERSION=1.9.0
+GO_VERSION=1.20.4
 
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 
 apt-get install -y curl 
-curl -o /go1.20.4.linux-amd64.tar.gz -L https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf /go1.20.4.linux-amd64.tar.gz
-rm -f /go1.20.4.linux-amd64.tar.gz
+curl -o /go"$GO_VERSION".linux-amd64.tar.gz -L https://go.dev/dl/go"$GO_VERSION".linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf /go"$GO_VERSION".linux-amd64.tar.gz
+rm -f /go"$GO_VERSION".linux-amd64.tar.gz
 
 apt-get install -y git python3 python3.10-venv
 
-curl -o /bats-v1.9.0.tar.gz -L https://github.com/bats-core/bats-core/archive/refs/tags/v1.9.0.tar.gz
+curl -o /bats-v"$BATS_VERSION".tar.gz -L https://github.com/bats-core/bats-core/archive/refs/tags/v"$BATS_VERSION".tar.gz
 cd /
-tar zxf /bats-v1.9.0.tar.gz
-cd /bats-core-1.9.0
+tar zxf /bats-v"$BATS_VERSION".tar.gz
+cd /bats-core-"$BATS_VERSION"
 ./install.sh /usr/local
 cd /
 
