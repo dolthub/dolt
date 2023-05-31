d5f95648951e020b9efc9c06c5fdfe1f1bd5b956
Nick Tobey <nick@dolthub.com>
2023-05-31 14:13:35 -0700

    Allow specifying user in start_sql_server() bats helper function.

--- integration-tests/bats/helper/local-remote.bash
+++ integration-tests/bats/helper/local-remote.bash
@@ -1,22 +1,21 @@
-SKIP_SERVER_TESTS=(
-
-)
+load helper/query-server-common
 
 setup_remote_server() {
-  script_name = $(basename $BASH_SOURCE)
-  if [ "$START_SQL_SERVER" = $true ];
+  script_name=$(basename $BASH_SOURCE)
+  if [ "$START_SQL_SERVER" = "true" ];
   then
-    if [ echo "$SKIP_SERVER_TESTS" | grep "$script_name" ];
+    if [[ "$SKIP_SERVER_TESTS" =~ "$script_name" ]];
     then
       skip
     else
+      USER=root
       start_sql_server
     fi
   fi
 }
 
 teardown_remote_server() {
-  if [ "$START_SQL_SERVER" = $true ];
+  if [ "$START_SQL_SERVER" = "true" ];
   then
     stop_sql_server
   fi
