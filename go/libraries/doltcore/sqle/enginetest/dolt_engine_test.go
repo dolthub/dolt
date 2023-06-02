783ba4a1f8ec56a2d29f61d797c9daae2fcb1e9f
Zach Musgrave <zach@dolthub.com>
2023-06-02 13:43:11 -0700

    Update skipped counter

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -49,7 +49,7 @@ var skipPrepared bool
 // SkipPreparedsCount is used by the "ci-check-repo CI workflow
 // as a reminder to consider prepareds when adding a new
 // enginetest suite.
-const SkipPreparedsCount = 85
+const SkipPreparedsCount = 87
 
 const skipPreparedFlag = "DOLT_SKIP_PREPARED_ENGINETESTS"
 
