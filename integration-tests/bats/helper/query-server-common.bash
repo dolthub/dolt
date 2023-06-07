f7e76ee79f585db2290367050121d7ea7537d8fb
Jason Fulghum <jason@dolthub.com>
2023-06-07 14:38:29 -0700

    Quoting $DEFAULT_DB so empty values don't mess up arg parsing

--- integration-tests/bats/helper/query-server-common.bash
+++ integration-tests/bats/helper/query-server-common.bash
@@ -17,7 +17,7 @@ wait_for_connection() {
   # exit code, so we need to temporarily enable 'set +e', but be sure to turn 'set -e' back on before we exit.
   set +e
   while [ $SECONDS -lt $end_time ]; do
-    dolt sql-client -u $user --host localhost --port $port --use-db $DEFAULT_DB --timeout 1 -q "SELECT 1;"
+    dolt sql-client -u $user --host localhost --port $port --use-db "$DEFAULT_DB" --timeout 1 -q "SELECT 1;"
     if [ $? -eq 0 ]; then
       echo "Connected successfully!"
       set -e
