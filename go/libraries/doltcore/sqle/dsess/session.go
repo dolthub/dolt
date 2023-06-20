7a952d9c4e8fdbb7c4d7b21a69b9d4bb5b34b082
zachmu <zachmu@users.noreply.github.com>
2023-06-20 22:23:53 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/dsess/session.go
+++ go/libraries/doltcore/sqle/dsess/session.go
@@ -183,7 +183,7 @@ func (d *DoltSession) lookupDbState(ctx *sql.Context, dbName string) (*branchSta
 	return dbState.heads[strings.ToLower(database.Revision())], true, nil
 }
 
-// RevisionDbName returns the name of the revision db for the base name and revision string given 
+// RevisionDbName returns the name of the revision db for the base name and revision string given
 func RevisionDbName(baseName string, rev string) string {
 	return baseName + DbRevisionDelimiter + rev
 }
