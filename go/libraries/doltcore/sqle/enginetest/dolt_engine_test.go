4097d402e7d2349e9261ec33ca781829a0638f4d
Pavel Safronov <pavel@dolthub.com>
2023-06-21 10:05:25 -0700

    Update SkipPreparedsCount so we can have TestDescribeTableAsOf + TestDescribeTableAsOfPrepared tests

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -49,7 +49,7 @@ var skipPrepared bool
 // SkipPreparedsCount is used by the "ci-check-repo CI workflow
 // as a reminder to consider prepareds when adding a new
 // enginetest suite.
-const SkipPreparedsCount = 83
+const SkipPreparedsCount = 82
 
 const skipPreparedFlag = "DOLT_SKIP_PREPARED_ENGINETESTS"
 
