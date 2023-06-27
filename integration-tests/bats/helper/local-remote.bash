6a9af52264f5df942ba253e9f695719279e31cc8
Pavel Safronov <pavel@dolthub.com>
2023-06-27 15:31:58 -0700

    Temporarily skip conflicts-resolve.bats while `dolt checkout` is not yet migrated.

--- integration-tests/bats/helper/local-remote.bash
+++ integration-tests/bats/helper/local-remote.bash
@@ -67,6 +67,7 @@ SKIP_SERVER_TESTS=$(cat <<-EOM
 ~tableplus.bats~
 ~multidb.bats~
 ~sql-conflicts-resolve.bats~
+~conflicts-resolve.bats~
 ~export-tables.bats~
 ~filter-branch.bats~
 ~arg-parsing.bats~
