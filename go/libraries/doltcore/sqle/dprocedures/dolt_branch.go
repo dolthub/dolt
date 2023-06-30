887f5922c64d06de38d971294abf956f15100e30
Nick Tobey <nick@dolthub.com>
2023-06-30 15:35:47 -0700

    When committing a transaction after creating a branch, start a new transaction.

--- go/libraries/doltcore/sqle/dprocedures/dolt_branch.go
+++ go/libraries/doltcore/sqle/dprocedures/dolt_branch.go
@@ -90,9 +90,17 @@ func doDoltBranch(ctx *sql.Context, args []string) (int, error) {
 }
 
 func commitTransaction(ctx *sql.Context, dSess *dsess.DoltSession, rsc *doltdb.ReplicationStatusController) error {
-	err := dSess.CommitTransaction(ctx, ctx.GetTransaction())
-	if err != nil {
-		return err
+	currentTx := ctx.GetTransaction()
+	if currentTx != nil {
+		err := dSess.CommitTransaction(ctx, currentTx)
+		if err != nil {
+			return err
+		}
+		newTx, err := dSess.StartTransaction(ctx, sql.ReadWrite)
+		if err != nil {
+			return err
+		}
+		ctx.SetTransaction(newTx)
 	}
 
 	if rsc != nil {
