60055b0e010068edb2cd480ca99912e00fdc920d
Nick Tobey <nick@dolthub.com>
2023-07-05 12:37:39 -0700

    Ensure the `dolt_checkout` works in detached head state.

--- go/libraries/doltcore/sqle/dprocedures/dolt_checkout.go
+++ go/libraries/doltcore/sqle/dprocedures/dolt_checkout.go
@@ -51,6 +51,34 @@ func generateSuccessMessage(newBranch, upstream string) string {
 	return result
 }
 
+// willModifyDb determines whether or not this operation is a no-op and can return early with a helpful message.
+func willModifyDb(dSess *dsess.DoltSession, data env.DbData, dbName, branchName string, updateHead bool) (bool, error) {
+	headRef, err := data.Rsr.CWBHeadRef()
+	// If we're in a detached head state, allow checking out a new branch.
+	if err == doltdb.ErrOperationNotSupportedInDetachedHead {
+		return true, nil
+	}
+	if err != nil {
+		return false, err
+	}
+
+	// If the operation won't modify either the active session or the default session, return early.
+	isModification := headRef.GetPath() != branchName
+	if updateHead {
+		fs, err := dSess.Provider().FileSystemForDatabase(dbName)
+		if err != nil {
+			return false, err
+		}
+		repoState, err := env.LoadRepoState(fs)
+		if err != nil {
+			return false, err
+		}
+		defaultBranch := repoState.CWBHeadRef().GetPath()
+		isModification = isModification || (defaultBranch != branchName)
+	}
+	return isModification, nil
+}
+
 func doDoltCheckout(ctx *sql.Context, args []string) (int, string, error) {
 	currentDbName := ctx.GetCurrentDatabase()
 	if len(currentDbName) == 0 {
@@ -94,24 +122,9 @@ func doDoltCheckout(ctx *sql.Context, args []string) (int, string, error) {
 		return 1, "", ErrEmptyBranchName
 	}
 
-	headRef, err := dbData.Rsr.CWBHeadRef()
+	isModification, err := willModifyDb(dSess, dbData, currentDbName, branchName, updateHead)
 	if err != nil {
 		return 1, "", err
-	}
-
-	// If the operation won't modify either the active session or the default session, return early.
-	isModification := headRef.GetPath() != branchName
-	if updateHead {
-		fs, err := dSess.Provider().FileSystemForDatabase(currentDbName)
-		if err != nil {
-			return 1, "", err
-		}
-		repoState, err := env.LoadRepoState(fs)
-		if err != nil {
-			return 1, "", err
-		}
-		defaultBranch := repoState.CWBHeadRef().GetPath()
-		isModification = isModification || (defaultBranch != branchName)
 	}
 	if !isModification {
 		return 0, fmt.Sprintf("Already on branch '%s'\n", branchName), nil
@@ -161,6 +174,10 @@ func doDoltCheckout(ctx *sql.Context, args []string) (int, string, error) {
 
 	// Check if the user executed `dolt checkout .`
 	if apr.NArg() == 1 && apr.Arg(0) == "." {
+		headRef, err := dbData.Rsr.CWBHeadRef()
+		if err != nil {
+			return 1, "", err
+		}
 
 		ws, err := dSess.WorkingSet(ctx, currentDbName)
 		if err != nil {
