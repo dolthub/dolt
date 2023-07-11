e7633d4fe4f0d950433dd6e0caa64af2cec3b90a
Nick Tobey <nick@dolthub.com>
2023-07-10 21:39:02 -0700

    Remove `dEnv == nil` checks. It should always be set.

--- go/cmd/dolt/commands/checkout.go
+++ go/cmd/dolt/commands/checkout.go
@@ -124,7 +124,7 @@ func (cmd CheckoutCmd) Exec(ctx context.Context, commandStr string, args []strin
 	if err != nil {
 		// In fringe cases the server can't start because the default branch doesn't exist, `dolt checkout <existing branch>`
 		// offers an escape hatch.
-		if dEnv != nil && !branchOrTrack && strings.Contains(err.Error(), "cannot resolve default branch head for database") {
+		if !branchOrTrack && strings.Contains(err.Error(), "cannot resolve default branch head for database") {
 			err := saveHeadBranch(dEnv.FS, branchName)
 			if err != nil {
 				cli.PrintErr(err)
@@ -153,7 +153,7 @@ func (cmd CheckoutCmd) Exec(ctx context.Context, commandStr string, args []strin
 		cli.Println(message)
 	}
 
-	if dEnv != nil && strings.Contains(message, "Switched to branch") {
+	if strings.Contains(message, "Switched to branch") {
 		err := saveHeadBranch(dEnv.FS, branchName)
 		if err != nil {
 			cli.PrintErr(err)
