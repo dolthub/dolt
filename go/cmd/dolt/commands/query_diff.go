c69e5e704a656e7b0811a91b338bb4170893892e
James Cor <james@dolthub.com>
2023-08-14 16:23:52 -0700

    simple same table diff working

--- go/cmd/dolt/commands/query_diff.go
+++ go/cmd/dolt/commands/query_diff.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"github.com/dolthub/dolt/go/cmd/dolt/cli"
 	"github.com/dolthub/dolt/go/cmd/dolt/errhand"
+	"github.com/dolthub/dolt/go/libraries/doltcore/diff"
 	"github.com/dolthub/dolt/go/libraries/doltcore/env"
 	"github.com/dolthub/dolt/go/libraries/doltcore/table/untyped/tabular"
 	"github.com/dolthub/dolt/go/libraries/utils/argparser"
@@ -119,41 +120,85 @@ func (q QueryDiff) Exec(ctx context.Context, commandStr string, args []string, d
 			}
 		}
 
+		dw, err := newDiffWriter(TabularDiffOutput)
+		if err != nil {
+			return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err), usage)
+		}
+		err = dw.BeginTable(query1, query2, false, false)
+		if err != nil {
+			return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err), usage)
+		}
+		wr, err := dw.RowWriter(nil, nil, diff.TableDeltaSummary{}, schema1)
+		if err != nil {
+			return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err), usage)
+		}
+
 		// TODO: assume both are sorted according to their primary keys
-		var results []sql.Row
 		row1, err1 := rowIter1.Next(sqlCtx)
 		row2, err2 := rowIter2.Next(sqlCtx)
+		removedChange := make([]diff.ChangeType, len(schema1))
+		for i := range removedChange {
+			removedChange[i] = diff.Removed
+		}
+		addedChange := make([]diff.ChangeType, len(schema1))
+		for i := range addedChange {
+			addedChange[i] = diff.Added
+		}
 		for err1 == nil && err2 == nil {
-			cmp, diff := q.compareRows(pkOrds, row1, row2)
+			cmp, d := q.compareRows(pkOrds, row1, row2)
 			switch cmp {
 			case -1: // deleted
-				results = append(results, row1)
+				if err = wr.WriteRow(ctx, row1, diff.Removed, removedChange); err != nil {
+					return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err), usage)
+				}
 				row1, err1 = rowIter1.Next(sqlCtx)
 			case 1: // added
-				results = append(results, row2)
+				if err = wr.WriteRow(ctx, row2, diff.Added, addedChange); err != nil {
+					return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err), usage)
+				}
 				row2, err2 = rowIter2.Next(sqlCtx)
 			default: // modified or no change
-				if diff {
-					results = append(results, row1)
-					results = append(results, row2)
+				if d {
+					if err = wr.WriteCombinedRow(ctx, row1, row2, diff.ModeContext); err != nil {
+						return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err), usage)
+					}
 				}
 				row1, err1 = rowIter1.Next(sqlCtx)
 				row2, err2 = rowIter2.Next(sqlCtx)
 			}
 		}
 		// Append any remaining rows
-		var rowIter sql.RowIter
+		//var rowIter sql.RowIter
 		if err1 == io.EOF && err2 == io.EOF {
-			rowIter1.Close(sqlCtx)
-			rowIter2.Close(sqlCtx)
+			//rowIter1.Close(sqlCtx)
+			//rowIter2.Close(sqlCtx)
 		} else if err1 == io.EOF {
-			results = append(results, row2)
-			rowIter = rowIter2
+			if err = wr.WriteRow(ctx, row2, diff.Added, addedChange); err != nil {
+				return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err), usage)
+			}
+			for {
+				row2, err2 = rowIter2.Next(sqlCtx)
+				if err2 == io.EOF {
+					break
+				}
+				if err = wr.WriteRow(ctx, row2, diff.Added, addedChange); err != nil {
+					return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err), usage)
+				}
+			}
 			rowIter1.Close(sqlCtx)
 		} else if err2 == io.EOF  {
-			results = append(results, row1)
-			rowIter = rowIter1
-			rowIter2.Close(sqlCtx)
+			if err = wr.WriteRow(ctx, row1, diff.Removed, removedChange); err != nil {
+				return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err), usage)
+			}
+			for {
+				row1, err1 = rowIter1.Next(sqlCtx)
+				if err1 == io.EOF {
+					break
+				}
+				if err = wr.WriteRow(ctx, row1, diff.Removed, removedChange); err != nil {
+					return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err), usage)
+				}
+			}
 		} else {
 			if err1 != nil {
 				return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err1), usage)
@@ -161,22 +206,9 @@ func (q QueryDiff) Exec(ctx context.Context, commandStr string, args []string, d
 				return HandleVErrAndExitCode(errhand.VerboseErrorFromError(err2), usage)
 			}
 		}
-		if rowIter != nil {
-			for {
-				row, err := rowIter.Next(sqlCtx)
-				if err == io.EOF {
-					break
-				}
-				results = append(results, row)
-			}
-		}
-
-		cliWR := iohelp.NopWrCloser(cli.OutStream)
-		wr := tabular.NewFixedWidthTableWriter(append(schema1), cliWR, 100)
+		rowIter1.Close(sqlCtx)
+		rowIter2.Close(sqlCtx)
 		defer wr.Close(ctx)
-		for _, row := range results {
-			wr.WriteSqlRow(ctx, row)
-		}
 	} else {
 		cliWR := iohelp.NopWrCloser(cli.OutStream)
 		wr := tabular.NewFixedWidthTableWriter(append(schema1, schema2...), cliWR, 100)
