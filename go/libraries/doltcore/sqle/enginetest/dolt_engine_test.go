e2b0f2baa62c931a6d1c05bfb97e3969f73ac9cd
jennifersp <jennifer@dolthub.com>
2023-11-01 16:00:15 -0700

    fix

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -552,7 +552,7 @@ func TestDeleteFrom(t *testing.T) {
 func TestDeleteFromErrors(t *testing.T) {
 	h := newDoltHarness(t)
 	defer h.Close()
-	enginetest.TestDeleteErrors(t, h)
+	enginetest.TestDeleteFromErrors(t, h)
 }
 
 func TestSpatialDelete(t *testing.T) {
