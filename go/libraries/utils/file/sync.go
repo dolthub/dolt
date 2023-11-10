1dc224e325f76b154912f7ab7096e210a807d5ce
Aaron Son <aaron@dolthub.com>
2023-11-10 08:07:00 -0800

    [no-release-notes] go/libraries/utils/file: Small refactor so attempts to do atomic file writes use a common helper method.

--- go/libraries/utils/file/sync.go
+++ go/libraries/utils/file/sync.go
@@ -15,7 +15,9 @@
 package file
 
 import (
+	"io"
 	"os"
+	"path/filepath"
 	"runtime"
 )
 
@@ -30,4 +32,56 @@ func SyncDirectoryHandle(dir string) (err error) {
 	}
 	defer d.Close()
 	return d.Sync()
+}
+
+// WriteFileAtomically will attempt to write the contents of |rd| to a file named
+// |name|, atomically. It uses POSIX filesystem semantics to attempt to
+// accomplish this. It writes the entire contents of |rd| to a temporary file,
+// fsync's the file, renames the file to its desired name, and then fsyncs the
+// directory handle for the directory where these operations took place.
+//
+// Note: On non-Unix platforms, os.Rename is not an atomic operation.
+func WriteFileAtomically(name string, rd io.Reader, mode os.FileMode) error {
+	name, err := filepath.Abs(name)
+	if err != nil {
+		return err
+	}
+	dir := filepath.Dir(name)
+	f, err := os.CreateTemp(dir, filepath.Base(name)+"-*")
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(f, rd)
+	if err != nil {
+		f.Close()
+		os.Remove(f.Name())
+		return err
+	}
+
+	err = f.Sync()
+	if err != nil {
+		f.Close()
+		os.Remove(f.Name())
+		return err
+	}
+
+	err = f.Close()
+	if err != nil {
+		os.Remove(f.Name())
+		return err
+	}
+
+	err = os.Chmod(f.Name(), mode)
+	if err != nil {
+		os.Remove(f.Name())
+		return err
+	}
+
+	err = os.Rename(f.Name(), name)
+	if err != nil {
+		os.Remove(f.Name())
+		return err
+	}
+
+	return SyncDirectoryHandle(filepath.Dir(f.Name()))
 }
