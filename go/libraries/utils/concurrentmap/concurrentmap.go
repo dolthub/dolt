9358062a67b90d3cb7031a4285564d255628f9b2
Alex Giurgiu <alex@giurgiu.io>
2023-11-13 21:42:28 +0200

    method docs

--- go/libraries/utils/concurrentmap/concurrentmap.go
+++ go/libraries/utils/concurrentmap/concurrentmap.go
@@ -28,6 +28,7 @@ type Map[K comparable, V any] struct {
 	m  map[K]V
 }
 
+// Get returns the value for the given key. If the key does not exist, the zero value for the value type will be returned.
 func (cm *Map[K, V]) Get(key K) (V, bool) {
 	cm.mu.RLock()
 	defer cm.mu.RUnlock()
@@ -38,24 +39,28 @@ func (cm *Map[K, V]) Get(key K) (V, bool) {
 	return zero, false
 }
 
+// Set sets the value for the given key. If the key already exists, it will be overwritten.
 func (cm *Map[K, V]) Set(key K, value V) {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
 	cm.m[key] = value
 }
 
+// Delete removes the key from the map if it exists. If the key does not exist, this is a no-op.
 func (cm *Map[K, V]) Delete(key K) {
 	cm.mu.Lock()
 	defer cm.mu.Unlock()
 	delete(cm.m, key)
 }
 
+// Len returns the number of items in the map at the time of the call.
 func (cm *Map[K, V]) Len() int {
 	cm.mu.RLock()
 	defer cm.mu.RUnlock()
 	return len(cm.m)
 }
 
+// DeepCopy returns a deep copy of the concurrent map.
 func (cm *Map[K, V]) DeepCopy() *Map[K, V] {
 	cm.mu.RLock()
 	defer cm.mu.RUnlock()
@@ -66,6 +71,7 @@ func (cm *Map[K, V]) DeepCopy() *Map[K, V] {
 	return &Map[K, V]{m: newMap}
 }
 
+// Iter iterates over the map, calling the provided function for each key/value pair. If the function returns false, the iteration stops.
 func (cm *Map[K, V]) Iter(f func(key K, value V) bool) {
 	cm.mu.RLock()
 	defer cm.mu.RUnlock()
@@ -76,6 +82,7 @@ func (cm *Map[K, V]) Iter(f func(key K, value V) bool) {
 	}
 }
 
+// Snapshot returns a copy of the internal map at the time of the call. Returns a native map, not a concurrent one.
 func (cm *Map[K, V]) Snapshot() map[K]V {
 	cm.mu.RLock()
 	defer cm.mu.RUnlock()
