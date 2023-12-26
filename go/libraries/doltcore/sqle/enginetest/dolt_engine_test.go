22fd71232c6c5f73f7b2cf75eb19091ce84f7102
James Cor <james@dolthub.com>
2023-12-26 10:26:59 -0800

    tmp test

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -116,51 +116,84 @@ func TestSingleQuery(t *testing.T) {
 
 // Convenience test for debugging a single query. Unskip and set to the desired query.
 func TestSingleScript(t *testing.T) {
-	t.Skip()
+	//t.Skip()
 
 	var scripts = []queries.ScriptTest{
 		{
-			Name: "physical columns added after virtual one",
+			Name: "resolve foreign key on indexed update",
 			SetUpScript: []string{
-				"create table t (pk int primary key, col1 int as (pk + 1));",
-				"insert into t (pk) values (1), (3)",
-				"alter table t add index idx1 (col1, pk);",
-				"alter table t add index idx2 (col1);",
-				"alter table t add column col2 int;",
-				"alter table t add column col3 int;",
-				"insert into t (pk, col2, col3) values (2, 4, 5);",
+				"set foreign_key_checks=0;",
+				"create table parent (i int primary key);",
+				"create table child (i int primary key, foreign key (i) references parent(i));",
+				"set foreign_key_checks=1;",
 			},
 			Assertions: []queries.ScriptTestAssertion{
 				{
-					Query: "select * from t where col1 = 2",
+					Query: "update child set i = 1 where i = 1;",
 					Expected: []sql.Row{
-						{1, 2, nil, nil},
+						{gmstypes.OkResult{RowsAffected: 0, Info: plan.UpdateInfo{Matched: 0, Updated: 0}}},
 					},
 				},
 			},
 		},
 	}
 
-	tcc := &testCommitClock{}
-	cleanup := installTestCommitClock(tcc)
-	defer cleanup()
-
 	for _, script := range scripts {
-		sql.RunWithNowFunc(tcc.Now, func() error {
-			harness := newDoltHarness(t)
-			harness.Setup(setup.MydbData)
-
-			engine, err := harness.NewEngine(t)
-			if err != nil {
-				panic(err)
-			}
-			// engine.EngineAnalyzer().Debug = true
-			// engine.EngineAnalyzer().Verbose = true
-
-			enginetest.TestScriptWithEngine(t, engine, harness, script)
-			return nil
-		})
+		harness := newDoltHarness(t)
+		harness.Setup(setup.MydbData)
+		engine, err := harness.NewEngine(t)
+		if err != nil {
+			panic(err)
+		}
+		// engine.EngineAnalyzer().Debug = true
+		// engine.EngineAnalyzer().Verbose = true
+		enginetest.TestScriptWithEngine(t, engine, harness, script)
 	}
+	//t.Skip()
+	//
+	//var scripts = []queries.ScriptTest{
+	//	{
+	//		Name: "physical columns added after virtual one",
+	//		SetUpScript: []string{
+	//			"create table t (pk int primary key, col1 int as (pk + 1));",
+	//			"insert into t (pk) values (1), (3)",
+	//			"alter table t add index idx1 (col1, pk);",
+	//			"alter table t add index idx2 (col1);",
+	//			"alter table t add column col2 int;",
+	//			"alter table t add column col3 int;",
+	//			"insert into t (pk, col2, col3) values (2, 4, 5);",
+	//		},
+	//		Assertions: []queries.ScriptTestAssertion{
+	//			{
+	//				Query: "select * from t where col1 = 2",
+	//				Expected: []sql.Row{
+	//					{1, 2, nil, nil},
+	//				},
+	//			},
+	//		},
+	//	},
+	//}
+	//
+	//tcc := &testCommitClock{}
+	//cleanup := installTestCommitClock(tcc)
+	//defer cleanup()
+	//
+	//for _, script := range scripts {
+	//	sql.RunWithNowFunc(tcc.Now, func() error {
+	//		harness := newDoltHarness(t)
+	//		harness.Setup(setup.MydbData)
+	//
+	//		engine, err := harness.NewEngine(t)
+	//		if err != nil {
+	//			panic(err)
+	//		}
+	//		// engine.EngineAnalyzer().Debug = true
+	//		// engine.EngineAnalyzer().Verbose = true
+	//
+	//		enginetest.TestScriptWithEngine(t, engine, harness, script)
+	//		return nil
+	//	})
+	//}
 }
 
 func newUpdateResult(matched, updated int) gmstypes.OkResult {
