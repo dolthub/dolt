cdbd35cb5e5f1342ef67589a7e963d445a244d4f
max-hoffman <max-hoffman@users.noreply.github.com>
2024-01-23 23:21:51 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/stats/refresh.go
+++ go/libraries/doltcore/sqle/stats/refresh.go
@@ -19,12 +19,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/dolthub/go-mysql-server/sql/stats"
 	"io"
 	"strings"
 	"time"
 
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/stats"
 
 	"github.com/dolthub/dolt/go/libraries/doltcore/doltdb"
 	"github.com/dolthub/dolt/go/libraries/doltcore/doltdb/durable"
