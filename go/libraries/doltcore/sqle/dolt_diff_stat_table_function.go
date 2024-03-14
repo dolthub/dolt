3b533192567c8904d84f479947fe79a3462ffe77
max-hoffman <max-hoffman@users.noreply.github.com>
2024-03-14 18:10:42 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/dolt_diff_stat_table_function.go
+++ go/libraries/doltcore/sqle/dolt_diff_stat_table_function.go
@@ -17,12 +17,12 @@ package sqle
 import (
 	"errors"
 	"fmt"
-	"github.com/dolthub/go-mysql-server/sql/expression"
 	"io"
 	"math"
 	"strings"
 
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/expression"
 	"github.com/dolthub/go-mysql-server/sql/types"
 	"golang.org/x/sync/errgroup"
 
