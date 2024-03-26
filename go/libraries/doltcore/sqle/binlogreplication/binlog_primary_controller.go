e10fded7f74a85ac4d839a606e65c3f518da2061
Jason Fulghum <jason@dolthub.com>
2024-03-26 11:04:42 -0700

    First pass at hardcoded binlog event stream prototype

--- go/libraries/doltcore/sqle/binlogreplication/binlog_primary_controller.go
+++ go/libraries/doltcore/sqle/binlogreplication/binlog_primary_controller.go
@@ -16,6 +16,8 @@ package binlogreplication
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/binlogreplication"
 	"github.com/dolthub/vitess/go/mysql"
@@ -43,8 +45,9 @@ type doltBinlogPrimaryController struct {
 
 var _ binlogreplication.BinlogPrimaryController = (*doltBinlogPrimaryController)(nil)
 
+// RegisterReplica implements the BinlogPrimaryController interface.
 func (d doltBinlogPrimaryController) RegisterReplica(ctx *sql.Context, c *mysql.Conn, replicaHost string, replicaPort uint16) error {
-	// TODO: Do we actually need the connection here?
+	// TODO: Do we actually need the connection here? Doesn't seem like it...
 	// TODO: Obviously need locking on the datastructure, but just getting something stubbed out
 	d.registeredReplicas = append(d.registeredReplicas, &registeredReplica{
 		connectionId: c.ConnectionID,
@@ -55,22 +58,257 @@ func (d doltBinlogPrimaryController) RegisterReplica(ctx *sql.Context, c *mysql.
 	return nil
 }
 
+// BinlogDumpGtid implements the BinlogPrimaryController interface.
 func (d doltBinlogPrimaryController) BinlogDumpGtid(ctx *sql.Context, c *mysql.Conn, gtidSet mysql.GTIDSet) error {
-	// TODO: Update docs: When this method is received, we need to start streaming binlog events over the specified connection.
-
-	// TODO: How do you stop a binlog dump? Connection just closes from the client side?
+	if err := sendTestBinlogEvents(ctx, c); err != nil {
+		return err
+	}
 
 	return nil
 }
 
+// ListReplicas implements the BinlogPrimaryController interface.
 func (d doltBinlogPrimaryController) ListReplicas(ctx *sql.Context) error {
 	return fmt.Errorf("DOLT: ListReplicas not implemented yet")
 }
 
+// ListBinaryLogs implements the BinlogPrimaryController interface.
 func (d doltBinlogPrimaryController) ListBinaryLogs(ctx *sql.Context) error {
 	return fmt.Errorf("DOLT: ListBinaryLogs not implemented yet")
 }
 
+// GetBinaryLogStatus implements the BinlogPrimaryController interface.
 func (d doltBinlogPrimaryController) GetBinaryLogStatus(ctx *sql.Context) error {
 	return fmt.Errorf("DOLT: GetBinaryLogStatus not implemented yet")
+}
+
+func getServerUuid(ctx *sql.Context) (string, error) {
+	variable, err := ctx.GetSessionVariable(ctx, "server_uuid")
+	if err != nil {
+		return "", err
+	}
+
+	if s, ok := variable.(string); ok {
+		if len(s) == 0 {
+			return "", fmt.Errorf("@@server_uuid is empty – must be set to a valid UUID")
+		}
+		return s, nil
+	}
+
+	return "", fmt.Errorf("@@server_uuid is not a string – must be set to a valid UUID")
+}
+
+func sendTestBinlogEvents(ctx *sql.Context, conn *mysql.Conn) error {
+	binlogFormat := mysql.NewMySQL56BinlogFormat()
+	// TODO: We should be able to turn checksums back on
+	binlogFormat.ChecksumAlgorithm = mysql.BinlogChecksumAlgOff
+	binlogStream := &mysql.BinlogStream{
+		ServerID:    42, // TODO: Hardcoded – read from @@server_id instead
+		LogPosition: 0,
+		Timestamp:   uint32(time.Now().Unix()),
+	}
+
+	// Rotate
+	err := sendRotateEvent(conn, binlogFormat, binlogStream)
+	if err != nil {
+		return err
+	}
+
+	// Format Description
+	err = sendFormatDescription(conn, binlogFormat, binlogStream)
+	if err != nil {
+		return err
+	}
+
+	err = conn.FlushBuffer()
+	if err != nil {
+		return err
+	}
+
+	// TODO: Send Previous GTIDs event? Is this always needed?
+	//binlogEvent = mysql.NewPreviousGTIDsEvent(binlogFormat, binlogStream)
+	//err = conn.WriteBinlogEvent(binlogEvent, false)
+	//if err != nil {
+	//	return err
+	//}
+
+	// GTID: sequence 1
+	err = sendGtidEvent(ctx, conn, binlogFormat, binlogStream, 1)
+	if err != nil {
+		return err
+	}
+
+	// Query: CREATE TABLE...
+	err = sendQueryEvent(conn, binlogFormat, binlogStream,
+		"CREATE TABLE t (pk int primary key, c1 varchar(100));")
+	if err != nil {
+		return err
+	}
+
+	err = conn.FlushBuffer()
+	if err != nil {
+		return err
+	}
+
+	// GTID: sequence 2
+	err = sendGtidEvent(ctx, conn, binlogFormat, binlogStream, 2)
+	if err != nil {
+		return err
+	}
+
+	// TableMap: db01.t
+	// TODO: Right now, hardcoded to a single table: db01.t, with the schema (int, varchar)
+	tableId := uint64(49)
+	err = sendTableMapEvent(conn, binlogFormat, binlogStream, tableId)
+	if err != nil {
+		return err
+	}
+
+	// WriteRows
+	rows, err := createInsertRows()
+	if err != nil {
+		return err
+	}
+	err = sendWriteRowsEvent(conn, binlogFormat, binlogStream, tableId, rows)
+	if err != nil {
+		return err
+	}
+
+	// Send the XID event to commit the transaction
+	err = sendXidEvent(conn, binlogFormat, binlogStream)
+	if err != nil {
+		return err
+	}
+
+	err = conn.FlushBuffer()
+	if err != nil {
+		return err
+	}
+
+	// Sleep for 10 minutes to avoid immediately closing the connection
+	for i := 0; i < 20; i++ {
+		time.Sleep(30 * time.Second)
+
+		err = sendHeartbeat(conn, binlogFormat, binlogStream)
+		if err != nil {
+			return err
+		}
+
+		err = conn.FlushBuffer()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func sendRotateEvent(conn *mysql.Conn, binlogFormat mysql.BinlogFormat, binlogStream *mysql.BinlogStream) error {
+	// TODO: position and filename should be tracked in a struct somewhere
+	binlogEvent := mysql.NewRotateEvent(binlogFormat, binlogStream, 0, "binlog.000028")
+	return conn.WriteBinlogEvent(binlogEvent, false)
+}
+
+func sendFormatDescription(conn *mysql.Conn, binlogFormat mysql.BinlogFormat, binlogStream *mysql.BinlogStream) error {
+	binlogEvent := mysql.NewFormatDescriptionEvent(binlogFormat, binlogStream)
+	return conn.WriteBinlogEvent(binlogEvent, false)
+}
+
+func sendHeartbeat(conn *mysql.Conn, binlogFormat mysql.BinlogFormat, binlogStream *mysql.BinlogStream) error {
+	binlogEvent := mysql.NewHeartbeatEvent(binlogFormat, binlogStream)
+	return conn.WriteBinlogEvent(binlogEvent, false)
+}
+
+func sendQueryEvent(conn *mysql.Conn, binlogFormat mysql.BinlogFormat, binlogStream *mysql.BinlogStream, query string) error {
+	binlogEvent := mysql.NewQueryEvent(binlogFormat, binlogStream, mysql.Query{
+		Database: "db01",
+		Charset:  nil,
+		SQL:      query,
+		Options:  0,
+		SqlMode:  0,
+	})
+	return conn.WriteBinlogEvent(binlogEvent, false)
+}
+
+func sendGtidEvent(ctx *sql.Context, conn *mysql.Conn, binlogFormat mysql.BinlogFormat, binlogStream *mysql.BinlogStream, sequenceNumber int64) error {
+	serverUuid, err := getServerUuid(ctx)
+	if err != nil {
+		return err
+	}
+
+	sid, err := mysql.ParseSID(serverUuid)
+	if err != nil {
+		return err
+	}
+	gtid := mysql.Mysql56GTID{Server: sid, Sequence: sequenceNumber}
+	binlogEvent := mysql.NewMySQLGTIDEvent(binlogFormat, binlogStream, gtid, false)
+	return conn.WriteBinlogEvent(binlogEvent, false)
+}
+
+func sendTableMapEvent(conn *mysql.Conn, binlogFormat mysql.BinlogFormat, binlogStream *mysql.BinlogStream, tableId uint64) error {
+	tableMap := &mysql.TableMap{
+		// TODO: What do these flags mean?
+		Flags:    0x8090,
+		Database: "db01",
+		Name:     "t",
+		Types: []byte{
+			mysql.TypeLong,
+			mysql.TypeVarchar,
+		},
+		CanBeNull: mysql.NewServerBitmap(2),
+		// https://mariadb.com/kb/en/table_map_event/#optional-metadata-block
+		Metadata: []uint16{
+			0,
+			400, // varchar size 4*100=400
+		},
+	}
+	tableMap.CanBeNull.Set(1, true)
+	binlogEvent := mysql.NewTableMapEvent(binlogFormat, binlogStream, tableId, tableMap)
+	return conn.WriteBinlogEvent(binlogEvent, false)
+}
+
+func sendXidEvent(conn *mysql.Conn, binlogFormat mysql.BinlogFormat, binlogStream *mysql.BinlogStream) error {
+	binlogEvent := mysql.NewXIDEvent(binlogFormat, binlogStream)
+	return conn.WriteBinlogEvent(binlogEvent, false)
+}
+
+func createInsertRows() (*mysql.Rows, error) {
+	rows := mysql.Rows{
+		Flags: 0x1234, // TODO: ???
+		//IdentifyColumns: mysql.NewServerBitmap(2),
+		DataColumns: mysql.NewServerBitmap(2),
+		Rows: []mysql.Row{
+			{
+				// NOTE: We don't send identify information for inserts
+				//NullIdentifyColumns: mysql.NewServerBitmap(2),
+				//Identify: []byte{
+				//	0x10, 0x20, 0x30, 0x40, // long
+				//	0x03, 0x00, // len('abc')
+				//	'a', 'b', 'c', // 'abc'
+				//},
+
+				NullColumns: mysql.NewServerBitmap(2),
+				Data: []byte{
+					// | 1076895760 | abcd       |
+					//   270544960
+					//   1076895760 <-- little endian
+					0x10, 0x20, 0x30, 0x40, // long
+					0x04, 0x00, // len('abcd')
+					'a', 'b', 'c', 'd', // 'abcd'
+				},
+			},
+		},
+	}
+	// All rows are included, none are NULL.
+	//rows.IdentifyColumns.Set(0, true)
+	//rows.IdentifyColumns.Set(1, true)
+	rows.DataColumns.Set(0, true)
+	rows.DataColumns.Set(1, true)
+
+	return &rows, nil
+}
+
+func sendWriteRowsEvent(conn *mysql.Conn, binlogFormat mysql.BinlogFormat, binlogStream *mysql.BinlogStream, tableId uint64, rows *mysql.Rows) error {
+	binlogEvent := mysql.NewWriteRowsEvent(binlogFormat, binlogStream, tableId, *rows)
+	return conn.WriteBinlogEvent(binlogEvent, false)
 }
--- go/libraries/doltcore/sqle/binlogreplication/binlog_primary_test.go
+++ go/libraries/doltcore/sqle/binlogreplication/binlog_primary_test.go
@@ -15,11 +15,15 @@
 package binlogreplication
 
 import (
-	"github.com/stretchr/testify/require"
+	"fmt"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
+// TestBinlogPrimary runs a simple sanity check that a MySQL replica can connect to a Dolt primary and receive
+// binlog events.
 func TestBinlogPrimary(t *testing.T) {
 	defer teardown(t)
 	startSqlServers(t)
@@ -30,49 +34,52 @@ func TestBinlogPrimary(t *testing.T) {
 	primaryDatabase = replicaDatabase
 	replicaDatabase = tempDatabase
 
-	// TODO: On MySQL Replica, need to run:
-	//       set GLOBAL GTID_MODE="OFF_PERMISSIVE";
-	//       set GLOBAL GTID_MODE="ON_PERMISSIVE";
-	//       set GLOBAL ENFORCE_GTID_CONSISTENCY="ON";
-	//       set GLOBAL GTID_MODE="ON";
+	// Clear out any existing GTID record on the replica
+	replicaDatabase.MustExec("reset binary logs and gtids;")
+
+	// On the Primary, we need to set @@server_uuid to a valid UUID:
+	primaryDatabase.MustExec("set GLOBAL server_uuid='3ab04dd4-8c9e-471e-a223-9712a3b7c37e';")
+
+	// On the Primary, turn on GTID mode
+	// NOTE: Dolt doesn't currently require moving through the GTID_MODE states like this, but
+	//       MySQL does, so we do it here anyway.
+	primaryDatabase.MustExec("set GLOBAL GTID_MODE='OFF_PERMISSIVE';")
+	primaryDatabase.MustExec("set GLOBAL GTID_MODE='ON_PERMISSIVE';")
+	primaryDatabase.MustExec("set GLOBAL ENFORCE_GTID_CONSISTENCY='ON';")
+	primaryDatabase.MustExec("set GLOBAL GTID_MODE='ON';")
+
+	// On the Primary, make sure we have a non-zero SERVER_ID set
+	primaryDatabase.MustExec("set GLOBAL SERVER_ID=42;")
 
 	// Create the replication user on the Dolt primary server
 	// TODO: this should be done on both as part of the shared setup code
 	primaryDatabase.MustExec("CREATE USER 'replicator'@'%' IDENTIFIED BY 'Zqr8_blrGm1!';")
 	primaryDatabase.MustExec("GRANT REPLICATION SLAVE ON *.* TO 'replicator'@'%';")
 
-	// change replication source to SOURCE_HOST='localhost', SOURCE_USER='root', SOURCE_PASSWORD='', SOURCE_PORT=11229, SOURCE_AUTO_POSITION=1;
-
-	// TODO: Not receiving heartbeats will cause the replica to disconnect eventually, so we specify
-	//       SOURCE_HEARTBEAT_PERIOD=0 to disable them temporarily.
-	// Or... change replication source to SOURCE_HOST='localhost', SOURCE_USER='root', SOURCE_PASSWORD='', SOURCE_PORT=11229, SOURCE_AUTO_POSITION=1, SOURCE_HEARTBEAT_PERIOD=0;
-
-	// For now, we pre-create the table on the replica, so that it can receive an update
-	// TODO: we could just add this as a Query event to the hardcoded events though.
-	createTableStatement := "create table t (pk int primary key, c1 varchar(100))"
-	replicaDatabase.MustExec(createTableStatement)
-
 	startReplication(t, doltPort)
-
-	// TODO: For now, we manually create the database and the table on the primary
-	//       since we don't support replicating DDL statements yet.
-	primaryDatabase.MustExec("create database db01")
-	primaryDatabase.MustExec("use db01")
-	createTableStatement := "create table t (pk int primary key, c1 varchar(100))"
-	primaryDatabase.MustExec(createTableStatement)
-	replicaDatabase.MustExec(createTableStatement)
-
-	// Insert rows into the primary database and assert they get replicated
-	primaryDatabase.MustExec("insert into t values (1, 'one')")
-
 	// NOTE: waitForReplicaToCatchUp won't work until we implement GTID support
+	//       Here we just pause to let the hardcoded binlog events be delivered
 	time.Sleep(500 * time.Millisecond)
 
-	rows, err := replicaDatabase.Queryx("select * from db01.t;")
+	// Sanity check on SHOW REPLICA STATUS
+	rows, err := replicaDatabase.Queryx("show replica status;")
 	require.NoError(t, err)
 	allRows := readAllRows(t, rows)
 	require.Equal(t, 1, len(allRows))
 	require.NoError(t, rows.Close())
-	require.Equal(t, 1, allRows[0]["pk"])
-	require.Equal(t, "one", allRows[0]["c1"])
+	fmt.Printf("SHOW REPLICA STATUS: %v\n", allRows)
+	require.Equal(t, "3ab04dd4-8c9e-471e-a223-9712a3b7c37e:1-2", allRows[0]["Executed_Gtid_Set"])
+	require.Equal(t, "", allRows[0]["Last_IO_Error"])
+	require.Equal(t, "", allRows[0]["Last_SQL_Error"])
+	require.Equal(t, "Yes", allRows[0]["Replica_IO_Running"])
+	require.Equal(t, "Yes", allRows[0]["Replica_SQL_Running"])
+
+	// Test that the table was created and one row inserted
+	rows, err = replicaDatabase.Queryx("select * from db01.t;")
+	require.NoError(t, err)
+	allRows = readAllRows(t, rows)
+	require.Equal(t, 1, len(allRows))
+	require.NoError(t, rows.Close())
+	require.Equal(t, "1076895760", allRows[0]["pk"])
+	require.Equal(t, "abcd", allRows[0]["c1"])
 }
