c05dfe7e809b87bc1eb9a099ed55bd9e157b192c
James Cor <james@dolthub.com>
2024-04-08 11:42:44 -0700

    revert test

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -141,32 +141,50 @@ func TestSchemaOverrides(t *testing.T) {
 
 // Convenience test for debugging a single query. Unskip and set to the desired query.
 func TestSingleScript(t *testing.T) {
+	t.Skip()
+
 	var scripts = []queries.ScriptTest{
 		{
-			Name: "delete me",
+			Name: "physical columns added after virtual one",
 			SetUpScript: []string{
-				"create table t (i int, index(i));",
-				"alter table t add index (i)",
+				"create table t (pk int primary key, col1 int as (pk + 1));",
+				"insert into t (pk) values (1), (3)",
+				"alter table t add index idx1 (col1, pk);",
+				"alter table t add index idx2 (col1);",
+				"alter table t add column col2 int;",
+				"alter table t add column col3 int;",
+				"insert into t (pk, col2, col3) values (2, 4, 5);",
 			},
 			Assertions: []queries.ScriptTestAssertion{
 				{
-					Query:    "show create table t",
-					Expected: []sql.Row{},
+					Query: "select * from t where col1 = 2",
+					Expected: []sql.Row{
+						{1, 2, nil, nil},
+					},
 				},
 			},
 		},
 	}
 
-	for _, script := range scripts {
-		harness := newDoltHarness(t)
-		engine, err := harness.NewEngine(t)
-		if err != nil {
-			panic(err)
-		}
-		// engine.EngineAnalyzer().Debug = true
-		// engine.EngineAnalyzer().Verbose = true
+	tcc := &testCommitClock{}
+	cleanup := installTestCommitClock(tcc)
+	defer cleanup()
 
-		enginetest.TestScriptWithEngine(t, engine, harness, script)
+	for _, script := range scripts {
+		sql.RunWithNowFunc(tcc.Now, func() error {
+			harness := newDoltHarness(t)
+			harness.Setup(setup.MydbData)
+
+			engine, err := harness.NewEngine(t)
+			if err != nil {
+				panic(err)
+			}
+			// engine.EngineAnalyzer().Debug = true
+			// engine.EngineAnalyzer().Verbose = true
+
+			enginetest.TestScriptWithEngine(t, engine, harness, script)
+			return nil
+		})
 	}
 }
 
