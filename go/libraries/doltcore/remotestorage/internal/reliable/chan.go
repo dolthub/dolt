480722e6687d4e2793bc346b9265bca389a08506
Aaron Son <aaron@dolthub.com>
2024-05-06 12:14:29 -0700

    go/libraries/doltcore/remotestorage: internal/reliable/chan.go: Add some comments regarding the implementation and usage.

--- go/libraries/doltcore/remotestorage/internal/reliable/chan.go
+++ go/libraries/doltcore/remotestorage/internal/reliable/chan.go
@@ -36,16 +36,44 @@ import (
 //
 // Close should always be called on an reliable.Chan to ensure resource cleanup.
 type Chan[T any] struct {
+	// All unack'd |T|s are stored in |buff|. As they get Ackd, they get poped from here.
 	buff *circular.Buff[T]
 
-	input  chan T
+	// We return new |T|s from here and they go into |buff| to be delivered
+	// to |output|, possibly multiple times if |Reset| is called before
+	// they are Ackd. This is the |src| channel.
+	input chan T
+
+	// The current destination where we try to deliver new |T|s that have
+	// not been delivered since the last |Reset| call. This is a new
+	// channel every time |Reset| is called.
 	output chan T
 
-	done     chan struct{}
-	ack      chan struct{}
-	reset    chan struct{}
-	closed   chan struct{}
-	front    chan frontReq[T]
+	// Request channel to ask |thread| to shutdown.
+	done chan struct{}
+
+	// Request channel used to ack the Front |T|. After this succeeds, the
+	// Ackd message will never be delivered on |output| again, even after a
+	// |Reset|.  The Ackd message should have already been delivered on
+	// |output| for this window of |Reset|. It usually indicates an error
+	// in logic to call this concurrently with |Reset| or with a receive
+	// from a channel returned by |Recv|.
+	ack chan struct{}
+
+	// Request channel to reset the current channel. After this succeeds,
+	// all unAckd |T|s in |buff| will be delivered to the new |output|
+	// channel.
+	reset chan struct{}
+	// Indicates |thread| is shutdown. All requests made to |thread| also
+	// read from this so they can return successfully if |thread| is
+	// already shutdown or shutsdown concurrently.
+
+	closed chan struct{}
+	// Used by clients to request to peek at the Front of the current
+	// |buff|.
+	front chan frontReq[T]
+
+	// Used by clients to read the current value of |c.output|.
 	outputCh chan chan T
 }
 
@@ -72,6 +100,8 @@ func NewChan[T any](src chan T) *Chan[T] {
 	return ret
 }
 
+// Returns the channel to read from in order to read |T|s that have been
+// delivered to |src|.
 func (c *Chan[T]) Recv() <-chan T {
 	select {
 	case o := <-c.outputCh:
@@ -81,6 +111,9 @@ func (c *Chan[T]) Recv() <-chan T {
 	}
 }
 
+// Peek at the earliest unAckd element in |buff|. For reliable, in-order RPC
+// machinery, this will be the Request that corresponds to the most recently
+// successfully received Response.
 func (c *Chan[T]) Front() (T, bool) {
 	var res T
 	resCh := make(chan T)
@@ -92,6 +125,9 @@ func (c *Chan[T]) Front() (T, bool) {
 	}
 }
 
+// Acknowledge the earlier buffered |T| in |buff|, indicating that a response
+// was successfully received and processed for it and it will never need to be
+// redriven.
 func (c *Chan[T]) Ack() {
 	select {
 	case c.ack <- struct{}{}:
@@ -99,6 +135,9 @@ func (c *Chan[T]) Ack() {
 	}
 }
 
+// Reset the channel so that it will redrive all unacknowledged |T|s into the
+// channel returned from |Recv| before it sends any new |T|s that it reads from
+// |src|.
 func (c *Chan[T]) Reset() {
 	select {
 	case c.reset <- struct{}{}:
@@ -106,18 +145,32 @@ func (c *Chan[T]) Reset() {
 	}
 }
 
+// Shutdown the Chan and cleanup any resources associated with it.
 func (c *Chan[T]) Close() {
 	close(c.done)
 	<-c.closed
 }
 
+// Chan works on the basis of a request/response actor that run in its own
+// goroutine. All the client methods send and receive from channels,
+// interacting with this actor thread.
 func (c *Chan[T]) thread() {
 	input := c.input
+	// The current index in |buff| which we are trying to send to |output|.
+	// When we reset, this goes back to 0 and we start working through
+	// |buff| again.
 	outI := 0
 	for {
+		// |input| gets nil'd out when we see |c.input| close.
+		// If |c.input| is closed and our |buff| is empty (all buffered
+		// messages have been Ackd), then we are done and we shut down
+		// here.
 		if input == nil && c.buff.Len() == 0 {
 			return
 		}
+
+		// We only read a new element from |input| when we don't have
+		// anything buffered we're trying to send to |output|.
 
 		thisInput := input
 		thisOutput := c.output
@@ -139,20 +192,34 @@ func (c *Chan[T]) thread() {
 		case t, ok := <-thisInput:
 			if !ok {
 				input = nil
+				// We can close |c.output| here because, since
+				// we are in this branch, we know we had
+				// nothing left to try to send to |output|. If
+				// |Reset| gets called, we will get a new
+				// output channel, and the buffer will be sent
+				// to it. We will make another read from
+				// |c.input|, and we will see the closed
+				// channel again, and we will close |c.output|
+				// again.
 				close(c.output)
 			} else {
+				// Add the newly read |T| to our buffer.
 				c.buff.Push(t)
 			}
 		case thisOutput <- toOut:
+			// We successfully delivered the current element in |buff|.
+			// The index in |buff| which we will try to deliver next is |outI + 1|.
 			outI += 1
 		case <-c.reset:
 			input = c.input
 			c.output = make(chan T)
 			outI = 0
 		case <-c.ack:
+			// Remove the front element from |buff| and decrement |outI|, since |buff| is one element shorter now.
 			c.buff.Pop()
 			outI -= 1
 		case req := <-c.front:
+			// Peek at the front of the buffer. Return the element on |resCh|.
 			req.resCh <- c.buff.Front()
 		}
 	}
