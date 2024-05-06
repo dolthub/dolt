48414a3ddcb7ed6d3676d14161c56539b773e280
fulghum <fulghum@users.noreply.github.com>
2024-05-06 18:58:38 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/binlogreplication/binlog_type_serialization.go
+++ go/libraries/doltcore/sqle/binlogreplication/binlog_type_serialization.go
@@ -21,13 +21,14 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/dolthub/dolt/go/store/hash"
-	"github.com/dolthub/dolt/go/store/prolly/tree"
-	"github.com/dolthub/dolt/go/store/val"
 	"github.com/dolthub/go-mysql-server/sql"
 	gmstypes "github.com/dolthub/go-mysql-server/sql/types"
 	"github.com/dolthub/vitess/go/mysql"
 	"github.com/dolthub/vitess/go/vt/proto/query"
+
+	"github.com/dolthub/dolt/go/store/hash"
+	"github.com/dolthub/dolt/go/store/prolly/tree"
+	"github.com/dolthub/dolt/go/store/val"
 )
 
 // typeSerializer defines the serialization interface for serializing a value in Dolt's
