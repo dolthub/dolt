fd7c27241e2fed9315cdd806976fab5e05bd7beb
jycor <jycor@users.noreply.github.com>
2024-05-17 19:23:18 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/cmd/dolt/commands/cnfcmds/cat.go
+++ go/cmd/dolt/commands/cnfcmds/cat.go
@@ -30,10 +30,10 @@ import (
 	"github.com/dolthub/dolt/go/cmd/dolt/errhand"
 	eventsapi "github.com/dolthub/dolt/go/gen/proto/dolt/services/eventsapi/v1alpha1"
 	"github.com/dolthub/dolt/go/libraries/doltcore/env"
-			"github.com/dolthub/dolt/go/libraries/doltcore/table/untyped/tabular"
+	"github.com/dolthub/dolt/go/libraries/doltcore/table/untyped/tabular"
 	"github.com/dolthub/dolt/go/libraries/utils/argparser"
 	"github.com/dolthub/dolt/go/libraries/utils/iohelp"
-	)
+)
 
 type mergeStatus struct {
 	isMerging      bool
