73506d20f6107eb022b1ebb42a092a4125233176
Nick Tobey <nick@dolthub.com>
2024-06-03 11:36:14 -0700

    Add tests for indexed JSON_INSERT implementation.

---
+++ go/store/prolly/tree/json_indexed_document_test.go
@@ -0,0 +1,30 @@
+package tree
+
+import (
+	"context"
+	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/expression/function/json"
+	"github.com/dolthub/go-mysql-server/sql/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+// NewIndexedJsonDocumentFromBytes creates an IndexedJsonDocument from a byte sequence, which has already been validated and normalized
+func newIndexedJsonDocumentFromValue(t *testing.T, ctx context.Context, ns NodeStore, v interface{}) IndexedJsonDocument {
+	doc, _, err := types.JSON.Convert(v)
+	require.NoError(t, err)
+	root, err := SerializeJsonToAddr(ctx, ns, doc.(sql.JSONWrapper))
+	require.NoError(t, err)
+	return NewIndexedJsonDocument(root, ns)
+}
+
+func TestIndexedJsonDocument_Insert(t *testing.T) {
+	ctx := context.Background()
+	ns := NewTestNodeStore()
+	convertStringToIndexedJsonDocument := func(t *testing.T, s string) interface{} {
+		return newIndexedJsonDocumentFromValue(t, ctx, ns, s)
+	}
+
+	testCases := json.JsonInsertTestCases(t, convertStringToIndexedJsonDocument)
+	json.RunJsonTests(t, testCases)
+}
