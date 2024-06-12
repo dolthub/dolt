d4a876ad249fe60c8c5fc26b7c17b154ba888215
Nick Tobey <nick@dolthub.com>
2024-06-11 19:11:03 -0700

    Add tests for JSON_VALUE, JSON_EXTRACT, and JSON_CONTAINS_PATH for IndexedJsonDocument.

--- go/store/prolly/tree/json_indexed_document_test.go
+++ go/store/prolly/tree/json_indexed_document_test.go
@@ -21,10 +21,42 @@ func newIndexedJsonDocumentFromValue(t *testing.T, ctx context.Context, ns NodeS
 func TestIndexedJsonDocument_Insert(t *testing.T) {
 	ctx := context.Background()
 	ns := NewTestNodeStore()
-	convertStringToIndexedJsonDocument := func(t *testing.T, s string) interface{} {
+	convertToIndexedJsonDocument := func(t *testing.T, s interface{}) interface{} {
 		return newIndexedJsonDocumentFromValue(t, ctx, ns, s)
 	}
 
-	testCases := jsontests.JsonInsertTestCases(t, convertStringToIndexedJsonDocument)
-	json.RunJsonTests(t, testCases)
+	testCases := jsontests.JsonInsertTestCases(t, convertToIndexedJsonDocument)
+	jsontests.RunJsonTests(t, testCases)
+}
+
+func TestIndexedJsonDocument_Extract(t *testing.T) {
+	ctx := context.Background()
+	ns := NewTestNodeStore()
+	convertToIndexedJsonDocument := func(t *testing.T, s interface{}) interface{} {
+		return newIndexedJsonDocumentFromValue(t, ctx, ns, s)
+	}
+
+	testCases := jsontests.JsonExtractTestCases(t, convertToIndexedJsonDocument)
+	jsontests.RunJsonTests(t, testCases)
+}
+
+func TestIndexedJsonDocument_Value(t *testing.T) {
+	ctx := context.Background()
+	ns := NewTestNodeStore()
+	convertToIndexedJsonDocument := func(t *testing.T, s interface{}) interface{} {
+		return newIndexedJsonDocumentFromValue(t, ctx, ns, s)
+	}
+
+	jsontests.RunJsonValueTests(t, convertToIndexedJsonDocument)
+}
+
+func TestIndexedJsonDocument_ContainsPath(t *testing.T) {
+	ctx := context.Background()
+	ns := NewTestNodeStore()
+	convertToIndexedJsonDocument := func(t *testing.T, s interface{}) interface{} {
+		return newIndexedJsonDocumentFromValue(t, ctx, ns, s)
+	}
+
+	testCases := jsontests.JsonContainsPathTestCases(t, convertToIndexedJsonDocument)
+	jsontests.RunJsonTests(t, testCases)
 }
