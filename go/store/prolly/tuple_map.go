db954ca7baeabe99b9635f490410439a4d781edd
Max Hoffman <maximilian.wolfgang1@gmail.com>
2024-06-27 13:17:12 -0700

    enable key range iter

--- go/store/prolly/tuple_map.go
+++ go/store/prolly/tuple_map.go
@@ -307,10 +307,12 @@ func (m Map) HasPrefix(ctx context.Context, preKey val.Tuple, preDesc val.TupleD
 }
 
 // IterRange returns a mutableMapIter that iterates over a Range.
-func (m Map) IterRange(ctx context.Context, rng Range) (MapIter, error) {
-	iter, err := treeIterFromRange(ctx, m.tuples.Root, m.tuples.NodeStore, rng)
-	if err != nil {
-		return nil, err
+func (m Map) IterRange(ctx context.Context, rng Range) (iter MapIter, err error) {
+	stop, ok := rng.KeyRangeLookup(m.Pool())
+	if ok {
+		iter, err = m.IterKeyRange(ctx, rng.Tup, stop)
+	} else {
+		iter, err = treeIterFromRange(ctx, m.tuples.Root, m.tuples.NodeStore, rng)
 	}
 	return filteredIter{iter: iter, rng: rng}, nil
 }
