d413b043c56a5baed9b9f279ba81792ea706dd13
Neil Macneale IV <neil@dolthub.com>
2024-08-07 08:28:10 -0700

    First pass on dolt_workspace_ read operations implemented and tested

--- go/libraries/doltcore/doltdb/system_table.go
+++ go/libraries/doltcore/doltdb/system_table.go
@@ -191,6 +191,7 @@ var generatedSystemTablePrefixes = []string{
 	DoltHistoryTablePrefix,
 	DoltConfTablePrefix,
 	DoltConstViolTablePrefix,
+	DoltWorkspaceTablePrefix,
 }
 
 const (
@@ -260,6 +261,8 @@ const (
 	DoltConfTablePrefix = "dolt_conflicts_"
 	// DoltConstViolTablePrefix is the prefix assigned to all the generated constraint violation tables
 	DoltConstViolTablePrefix = "dolt_constraint_violations_"
+	// DoltWorkspaceTablePrefix is the prefix assigned to all the generated workspace tables
+	DoltWorkspaceTablePrefix = "dolt_workspace_"
 )
 
 const (
---
+++ go/libraries/doltcore/sqle/enginetest/dolt_queries_workspace.go
@@ -0,0 +1,181 @@
+// Copyright 2024 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package enginetest
+
+import (
+	"github.com/dolthub/go-mysql-server/enginetest/queries"
+	"github.com/dolthub/go-mysql-server/sql"
+)
+
+var DoltWorkspaceScriptTests = []queries.ScriptTest{
+	{
+		Name: "dolt_workspace_* multiple edits of a single row",
+		SetUpScript: []string{
+			"create table tbl (pk int primary key, val int);",
+			"call dolt_commit('-Am', 'creating table t');",
+
+			"insert into tbl values (42,42);",
+			"insert into tbl values (43,43);",
+			"call dolt_commit('-am', 'inserting rows 3 rows at HEAD');",
+
+			"update tbl set val=51 where pk=42;",
+			"call dolt_add('tbl');",
+		},
+
+		Assertions: []queries.ScriptTestAssertion{
+			{
+				Query: "select * from dolt_workspace_tbl",
+				Expected: []sql.Row{
+					{0, true, "modified", 42, 51, 42, 42},
+				},
+			},
+			{
+				Query: "update tbl set val= 108 where pk = 42;",
+			},
+			{
+				Query: "select * from dolt_workspace_tbl",
+				Expected: []sql.Row{
+					{0, true, "modified", 42, 51, 42, 42},
+					{1, false, "modified", 42, 108, 42, 51},
+				},
+			},
+			{
+				Query: "call dolt_add('tbl');",
+			},
+			{
+				Query: "select * from dolt_workspace_tbl",
+				Expected: []sql.Row{
+					{0, true, "modified", 42, 108, 42, 42},
+				},
+			},
+		},
+	},
+	{
+		Name: "dolt_workspace_* single unstaged row",
+		SetUpScript: []string{
+			"create table tbl (pk int primary key, val int);",
+			"call dolt_commit('-Am', 'creating table t');",
+
+			"insert into tbl values (42,42);",
+			"insert into tbl values (43,43);",
+			"call dolt_commit('-am', 'inserting rows 3 rows at HEAD');",
+
+			"update tbl set val=51 where pk=42;",
+		},
+
+		Assertions: []queries.ScriptTestAssertion{
+			{
+				Query: "select * from dolt_workspace_tbl",
+				Expected: []sql.Row{
+					{0, false, "modified", 42, 51, 42, 42},
+				},
+			},
+		},
+	},
+	{
+		Name: "dolt_workspace_* inserted row",
+		SetUpScript: []string{
+			"create table tbl (pk int primary key, val int);",
+			"call dolt_commit('-Am', 'creating table t');",
+
+			"insert into tbl values (42,42);",
+			"insert into tbl values (43,43);",
+			"call dolt_commit('-am', 'inserting rows 3 rows at HEAD');",
+			"insert into tbl values (44,44);",
+		},
+
+		Assertions: []queries.ScriptTestAssertion{
+			{
+				Query: "select * from dolt_workspace_tbl",
+				Expected: []sql.Row{
+					{0, false, "added", 44, 44, nil, nil},
+				},
+			},
+			{
+				Query: "call dolt_add('tbl');",
+			},
+			{
+				Query: "select * from dolt_workspace_tbl",
+				Expected: []sql.Row{
+					{0, true, "added", 44, 44, nil, nil},
+				},
+			},
+			{
+				Query: "update tbl set val = 108 where pk = 44;",
+			},
+			{
+				Query: "select * from dolt_workspace_tbl",
+				Expected: []sql.Row{
+					{0, true, "added", 44, 44, nil, nil},
+					{1, false, "modified", 44, 108, 44, 44},
+				},
+			},
+		},
+	},
+
+	{
+		Name: "dolt_workspace_* deleted row",
+		SetUpScript: []string{
+			"create table tbl (pk int primary key, val int);",
+			"call dolt_commit('-Am', 'creating table t');",
+
+			"insert into tbl values (42,42);",
+			"insert into tbl values (43,43);",
+			"call dolt_commit('-am', 'inserting rows 3 rows at HEAD');",
+			"delete from tbl where pk = 42;",
+		},
+
+		Assertions: []queries.ScriptTestAssertion{
+			{
+				Query: "select * from dolt_workspace_tbl",
+				Expected: []sql.Row{
+					{0, false, "removed", nil, nil, 42, 42},
+				},
+			},
+			{
+				Query: "call dolt_add('tbl');",
+			},
+			{
+				Query: "select * from dolt_workspace_tbl",
+				Expected: []sql.Row{
+					{0, true, "removed", nil, nil, 42, 42},
+				},
+			},
+		},
+	},
+
+	{
+		Name: "dolt_workspace_* clean workspace",
+		SetUpScript: []string{
+			"create table tbl (pk int primary key, val int);",
+			"call dolt_commit('-Am', 'creating table t');",
+
+			"insert into tbl values (42,42);",
+			"insert into tbl values (43,43);",
+			"call dolt_commit('-am', 'inserting rows 3 rows at HEAD');",
+		},
+
+		Assertions: []queries.ScriptTestAssertion{
+			{
+				Query:    "select * from dolt_workspace_tbl",
+				Expected: []sql.Row{},
+			},
+			{
+				Query:    "select * from dolt_workspace_unknowntable",
+				Expected: []sql.Row{},
+			},
+		},
+	},
+}
