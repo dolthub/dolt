645a4796bf42616a6dea83c303852288698b9a4c
Zach Musgrave <zach@dolthub.com>
2024-09-05 17:38:03 -0700

    Added schema deltas to dolt_status

--- go/libraries/doltcore/diff/database_schema_deltas.go
+++ go/libraries/doltcore/diff/database_schema_deltas.go
@@ -4,13 +4,89 @@ import (
 	"context"
 
 	"github.com/dolthub/dolt/go/libraries/doltcore/doltdb"
+	"github.com/dolthub/dolt/go/libraries/utils/set"
 )
 
+// DatabaseSchemaDelta represents a change in the set of database schemas between two roots
 type DatabaseSchemaDelta struct {
 	FromName string
 	ToName   string
 }
 
+func (d DatabaseSchemaDelta) IsAdd() bool {
+	return d.FromName == "" && d.ToName != ""
+}
+
+func (d DatabaseSchemaDelta) IsDrop() bool {
+	return d.FromName != "" && d.ToName == ""
+}
+
+func (d DatabaseSchemaDelta) CurName() string {
+	if d.ToName != "" {
+		return d.ToName
+	}
+	return d.FromName
+}
+
+// GetDatabaseSchemaDeltas returns a list of DatabaseSchemaDelta objects representing the changes in database schemas
 func GetDatabaseSchemaDeltas(ctx context.Context, fromRoot, toRoot doltdb.RootValue) ([]DatabaseSchemaDelta, error) {
-	return nil, nil
+	fromNames, err := getDatabaseSchemaNames(ctx, fromRoot)
+	if err != nil {
+		return nil, err
+	}
+
+	toNames, err := getDatabaseSchemaNames(ctx, toRoot)
+	if err != nil {
+		return nil, err
+	}
+
+	// short circuit for common case where there are no schemas (dolt)
+	if fromNames.Size() == 0 && toNames.Size() == 0 {
+		return nil, nil
+	}
+
+	// generate a diff for each schema name that's present in one root but not the other
+	var deltas []DatabaseSchemaDelta
+	fromNames.Iterate(func(name string) (stop bool) {
+		if !toNames.Contains(name) {
+			deltas = append(deltas, DatabaseSchemaDelta{FromName: name})
+		}
+		return false
+	})
+
+	toNames.Iterate(func(name string) (stop bool) {
+		if !fromNames.Contains(name) {
+			deltas = append(deltas, DatabaseSchemaDelta{ToName: name})
+		}
+		return false
+	})
+
+	return deltas, nil
+}
+
+// GetStagedUnstagedDatabaseSchemaDeltas represents staged and unstaged changes as DatabaseSchemaDelta slices.
+func GetStagedUnstagedDatabaseSchemaDeltas(ctx context.Context, roots doltdb.Roots) (staged, unstaged []DatabaseSchemaDelta, err error) {
+	staged, err = GetDatabaseSchemaDeltas(ctx, roots.Head, roots.Staged)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	unstaged, err = GetDatabaseSchemaDeltas(ctx, roots.Staged, roots.Working)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return staged, unstaged, nil
+}
+
+func getDatabaseSchemaNames(ctx context.Context, dest doltdb.RootValue) (*set.StrSet, error) {
+	dbSchemaNames := set.NewEmptyStrSet()
+	dbSchemas, err := dest.GetDatabaseSchemas(ctx)
+	if err != nil {
+		return nil, err
+	}
+	for _, dbSchema := range dbSchemas {
+		dbSchemaNames.Add(dbSchema.Name)
+	}
+	return dbSchemaNames, nil
 }
