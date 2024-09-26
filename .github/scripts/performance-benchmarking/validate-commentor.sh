347639b4773d037250d2a16c41020c2f57139df8
James Cor <james@dolthub.com>
2024-09-26 15:19:56 -0700

    benchmark

--- .github/scripts/performance-benchmarking/validate-commentor.sh
+++ .github/scripts/performance-benchmarking/validate-commentor.sh
@@ -7,7 +7,7 @@ if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
-validcommentors="coffeegoddd andy-wm-arthur bheni Hydrocharged reltuk tbantle22 timsehn zachmu max-hoffman nicktobey fulghum macneale4"
+validcommentors="coffeegoddd andy-wm-arthur bheni Hydrocharged reltuk tbantle22 timsehn zachmu max-hoffman nicktobey fulghum macneale4 jycor"
 
 contains() {
     [[ $1 =~ (^|[[:space:]])$2($|[[:space:]]) ]] && echo "valid=true" >> $GITHUB_OUTPUT || exit 0
