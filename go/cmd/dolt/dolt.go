81fdf65cbd6b86cabf03ee0df03591cfc0a4ecb2
coffeegoddd <dustin@dolthub.com>
2024-10-18 14:14:26 -0700

    /go/{cmd,libraries}: wip, trying to get tables created

--- go/cmd/dolt/dolt.go
+++ go/cmd/dolt/dolt.go
@@ -20,6 +20,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"github.com/dolthub/dolt/go/cmd/dolt/commands/ci"
 	"math/rand"
 	"net/http"
 	_ "net/http/pprof"
@@ -126,6 +127,7 @@ var doltSubCommands = []cli.Command{
 	commands.ReflogCmd{},
 	commands.RebaseCmd{},
 	commands.ArchiveCmd{},
+	ci.Commands,
 }
 
 var commandsWithoutCliCtx = []cli.Command{
@@ -166,6 +168,7 @@ var commandsWithoutGlobalArgSupport = []cli.Command{
 	sqlserver.SqlServerCmd{VersionStr: doltversion.Version},
 	commands.VersionCmd{VersionStr: doltversion.Version},
 	commands.ConfigCmd{},
+	ci.Commands,
 }
 
 // commands that do not need write access for the current directory
