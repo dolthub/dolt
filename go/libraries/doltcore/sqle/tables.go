192f5aeb11d8bea3eabd1c4815194719aa4b2cbf
coffeegoddd <dustin@dolthub.com>
2024-10-22 11:51:23 -0700

    /go/libraries/doltcore/{schema,sqle}: finally!!

--- go/libraries/doltcore/sqle/tables.go
+++ go/libraries/doltcore/sqle/tables.go
@@ -3089,7 +3089,7 @@ func (t *WritableDoltTable) updateFromRoot(ctx *sql.Context, root doltdb.RootVal
 		return fmt.Errorf("table `%s` cannot find itself", t.tableName)
 	}
 	var updatedTable *AlterableDoltTable
-	if doltdb.HasDoltPrefix(t.tableName) && !doltdb.IsReadOnlySystemTable(t.tableName) {
+	if doltdb.HasDoltPrefix(t.tableName) && !doltdb.IsReadOnlySystemTable(t.tableName) && !doltdb.IsDoltCITable(t.tableName) {
 		updatedTable = &AlterableDoltTable{*updatedTableSql.(*WritableDoltTable)}
 	} else {
 		updatedTable = updatedTableSql.(*AlterableDoltTable)
