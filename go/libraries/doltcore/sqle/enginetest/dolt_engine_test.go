970943210e0e407417e0179e183e7f370cd78bd3
coffeegoddd <dustin@dolthub.com>
2024-10-23 11:28:18 -0700

    /go/libraries/doltcore/{doltcore,sqle}: fix get database

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_test.go
@@ -2052,8 +2052,3 @@ func TestDoltWorkspace(t *testing.T) {
 	harness := newDoltEnginetestHarness(t)
 	RunDoltWorkspaceTests(t, harness)
 }
-
-func TestDoltCIConfig(t *testing.T) {
-	h := newDoltEnginetestHarness(t)
-	RunDoltCIConfigTests(t, h)
-}
