c695fd07b58ce0f643ddf28eeb5f5f8de2f0e336
Max Hoffman <maximilian.wolfgang1@gmail.com>
2024-12-12 21:37:33 -0800

    fix file ident

--- go/gen/fb/serial/fileidentifiers.go
+++ go/gen/fb/serial/fileidentifiers.go
@@ -41,7 +41,7 @@ const StashListFileID = "SLST"
 const StashFileID = "STSH"
 const StatisticFileID = "STAT"
 const DoltgresRootValueFileID = "DGRV"
-const TupleFileID = "KVAL"
+const TupleFileID = "TUPL"
 
 const MessageTypesKind int = 27
 
--- go/serial/fileidentifiers.go
+++ go/serial/fileidentifiers.go
@@ -41,7 +41,7 @@ const StashListFileID = "SLST"
 const StashFileID = "STSH"
 const StatisticFileID = "STAT"
 const DoltgresRootValueFileID = "DGRV"
-const TupleFileId = "TVAL"
+const TupleFileId = "TUPL"
 
 const MessageTypesKind int = 27
 
