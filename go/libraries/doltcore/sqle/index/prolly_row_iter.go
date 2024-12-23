084062ee6a3e75ff1c35607b8dcb671c7f0a37c7
Max Hoffman <maximilian.wolfgang1@gmail.com>
2024-12-23 13:49:41 -0800

    Skip convT for tuple field access

--- go/libraries/doltcore/sqle/index/prolly_row_iter.go
+++ go/libraries/doltcore/sqle/index/prolly_row_iter.go
@@ -176,14 +176,14 @@ func (it prollyRowIter) Next(ctx *sql.Context) (sql.Row, error) {
 	row := make(sql.Row, it.rowLen)
 	for i, idx := range it.keyProj {
 		outputIdx := it.ordProj[i]
-		row[outputIdx], err = tree.GetField(ctx, it.keyDesc, idx, key, it.ns)
+		err = tree.GetFieldIntoRow(ctx, it.keyDesc, idx, outputIdx, key, row, it.ns)
 		if err != nil {
 			return nil, err
 		}
 	}
 	for i, idx := range it.valProj {
 		outputIdx := it.ordProj[len(it.keyProj)+i]
-		row[outputIdx], err = tree.GetField(ctx, it.valDesc, idx, value, it.ns)
+		err = tree.GetFieldIntoRow(ctx, it.valDesc, idx, outputIdx, value, row, it.ns)
 		if err != nil {
 			return nil, err
 		}
