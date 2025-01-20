ba2b90c36dfd161ad3c88d55f5963e39fa131f80
milogreg <oldesoot@gmail.com>
2025-01-20 14:37:03 -0800

    Make dolt_help table tests less fragile against changes in synopsis, long description, and arguments

--- go/libraries/doltcore/sqle/enginetest/dolt_queries_help.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_queries_help.go
@@ -15,8 +15,6 @@
 package enginetest
 
 import (
-	"encoding/json"
-
 	"github.com/dolthub/go-mysql-server/enginetest/queries"
 	"github.com/dolthub/go-mysql-server/sql"
 )
@@ -91,28 +89,21 @@ var DoltHelpScripts = []queries.ScriptTest{
 		SetUpScript: []string{},
 		Assertions: []queries.ScriptTestAssertion{
 			{
-				Query: "select synopsis from dolt_help where name='dolt_conflicts_resolve'",
+				Query: "select count(*) from dolt_help where name='dolt_conflicts_resolve' and synopsis like '%dolt conflicts resolve%table%'",
 				Expected: []sql.Row{
-					{"dolt conflicts resolve --ours|--theirs <table>..."},
+					{1},
 				},
 			},
 			{
-				Query: "select synopsis from dolt_help where name='dolt_merge'",
+				Query: "select count(*) from dolt_help where name='dolt_merge' and synopsis like '%dolt merge%branch%'",
 				Expected: []sql.Row{
-					{`dolt merge [--squash] <branch>
-dolt merge --no-ff [-m message] <branch>
-dolt merge --abort`,
-					},
+					{1},
 				},
 			},
 			{
-				Query: "select synopsis from dolt_help where name='dolt_branch'",
+				Query: "select count(*) from dolt_help where name='dolt_branch' and synopsis like '%dolt branch%--list%'",
 				Expected: []sql.Row{
-					{`dolt branch [--list] [-v] [-a] [-r]
-dolt branch [-f] <branchname> [<start-point>]
-dolt branch -m [-f] [<oldbranch>] <newbranch>
-dolt branch -c [-f] [<oldbranch>] <newbranch>
-dolt branch -d [-f] [-r] <branchname>...`},
+					{1},
 				},
 			},
 		},
@@ -147,34 +138,21 @@ dolt branch -d [-f] [-r] <branchname>...`},
 		SetUpScript: []string{},
 		Assertions: []queries.ScriptTestAssertion{
 			{
-				Query: "select long_description from dolt_help where name='dolt_add'",
+				Query: "select count(*) from dolt_help where name='dolt_add' and long_description like '%updates the list of tables%'",
 				Expected: []sql.Row{
-					{`
-This command updates the list of tables using the current content found in the working root, to prepare the content staged for the next commit. It adds the current content of existing tables as a whole or remove tables that do not exist in the working root anymore.
-
-This command can be performed multiple times before a commit. It only adds the content of the specified table(s) at the time the add command is run; if you want subsequent changes included in the next commit, then you must run dolt add again to add the new content to the index.
-
-The dolt status command can be used to obtain a summary of which tables have changes that are staged for the next commit.`},
+					{1},
 				},
 			},
 			{
-				Query: "select long_description from dolt_help where name='dolt_cherry_pick'",
+				Query: "select count(*) from dolt_help where name='dolt_cherry_pick' and long_description like '%Applies the changes from an existing commit%'",
 				Expected: []sql.Row{
-					{`
-Applies the changes from an existing commit and creates a new commit from the current HEAD. This requires your working tree to be clean (no modifications from the HEAD commit).
-
-Cherry-picking merge commits or commits with table drops/renames is not currently supported. 
-
-If any data conflicts, schema conflicts, or constraint violations are detected during cherry-picking, you can use Dolt's conflict resolution features to resolve them. For more information on resolving conflicts, see: https://docs.dolthub.com/concepts/dolt/git/conflicts.
-`},
+					{1},
 				},
 			},
 			{
-				Query: "select long_description from dolt_help where name='dolt_revert'",
+				Query: "select count(*) from dolt_help where name='dolt_revert' and long_description like '%Removes the changes made in a commit%'",
 				Expected: []sql.Row{
-					{`Removes the changes made in a commit (or series of commits) from the working set, and then automatically commits the result. This is done by way of a three-way merge. Given a specific commit (e.g. <b>HEAD~1</b>), this is similar to applying the patch from <b>HEAD~1..HEAD~2</b>, giving us a patch of what to remove to effectively remove the influence of the specified commit. If multiple commits are specified, then this process is repeated for each commit in the order specified. This requires a clean working set.
-
-Any conflicts or constraint violations caused by the merge cause the command to fail.`},
+					{1},
 				},
 			},
 		},
@@ -185,47 +163,29 @@ Any conflicts or constraint violations caused by the merge cause the command to 
 		SetUpScript: []string{},
 		Assertions: []queries.ScriptTestAssertion{
 			{
-				Query: "select arguments from dolt_help where name='dolt_commit'",
+				Query: "select count(*) from dolt_help where name='dolt_commit' and arguments like '%--amend%Amend previous commit%'",
 				Expected: []sql.Row{
-					{string(jsonMarshalOrPanic(map[string]string{
-						"--amend":       "Amend previous commit",
-						"-A, --ALL":     "Adds all tables and databases (including new tables) in the working set to the staged set.",
-						"-a, --all":     "Adds all existing, changed tables (but not new tables) in the working set to the staged set.",
-						"-f, --force":   "Ignores any foreign key warnings and proceeds with the commit.",
-						"--skip-empty":  "Only create a commit if there are staged changes. If no changes are staged, the call to commit is a no-op. Cannot be used with --allow-empty.",
-						"--allow-empty": "Allow recording a commit that has the exact same data as its sole parent. This is usually a mistake, so it is disabled by default. This option bypasses that safety. Cannot be used with --skip-empty.", "--date=<date>": "Specify the date used in the commit. If not specified the current system time is used.", "--author=<author>": "Specify an explicit author using the standard A U Thor <author@example.com> format.", "-m <msg>, --message=<msg>": "Use the given <msg> as the commit message.", "-S <key-id>, --gpg-sign=<key-id>": "Sign the commit using GPG. If no key-id is provided the key-id is taken from 'user.signingkey' the in the configuration",
-					}))},
+					{1},
 				},
 			},
 			{
-				Query: "select arguments from dolt_help where name='dolt_rebase'",
+				Query: "select count(*) from dolt_help where name='dolt_commit' and arguments like '%--skip-empty%Only create a commit if there are staged changes%'",
 				Expected: []sql.Row{
-					{string(jsonMarshalOrPanic(map[string]string{
-						"--abort":           "Abort an interactive rebase and return the working set to the pre-rebase state",
-						"--continue":        "Continue an interactive rebase after adjusting the rebase plan",
-						"--empty=<empty>":   "How to handle commits that are not empty to start, but which become empty after rebasing. Valid values are: drop (default) or keep",
-						"-i, --interactive": "Start an interactive rebase",
-					}))},
+					{1},
 				},
 			},
 			{
-				Query: "select arguments from dolt_help where name='dolt_revert'",
+				Query: "select count(*) from dolt_help where name='dolt_rebase' and arguments like '%--continue%Continue an interactive rebase%'",
 				Expected: []sql.Row{
-					{string(jsonMarshalOrPanic(map[string]string{
-						"--author=<author>": "Specify an explicit author using the standard A U Thor <author@example.com> format.",
-						"<revision>":        "The commit revisions. If multiple revisions are given, they're applied in the order given.",
-					}))},
+					{1},
+				},
+			},
+			{
+				Query: "select count(*) from dolt_help where name='dolt_revert' and arguments like '%--author%Specify an explicit author%'",
+				Expected: []sql.Row{
+					{1},
 				},
 			},
 		},
 	},
-}
-
-func jsonMarshalOrPanic(v any) []byte {
-	argsJson, err := json.Marshal(v)
-	if err != nil {
-		panic(err)
-	}
-
-	return argsJson
 }
