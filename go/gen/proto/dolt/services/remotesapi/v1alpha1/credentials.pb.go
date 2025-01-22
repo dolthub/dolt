4656df6aee25b8da900875d50d5ed3143a89a7f3
coffeegoddd <coffeegoddd@users.noreply.github.com>
2025-01-22 01:20:00 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/gen/proto/dolt/services/remotesapi/v1alpha1/credentials.pb.go
+++ go/gen/proto/dolt/services/remotesapi/v1alpha1/credentials.pb.go
@@ -21,10 +21,11 @@
 package remotesapi
 
 import (
-	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
-	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
 	sync "sync"
+
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 )
 
 const (
--- go/gen/proto/dolt/services/replicationapi/v1alpha1/replication.pb.go
+++ go/gen/proto/dolt/services/replicationapi/v1alpha1/replication.pb.go
@@ -21,10 +21,11 @@
 package replicationapi
 
 import (
-	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
-	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
 	sync "sync"
+
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 )
 
 const (
