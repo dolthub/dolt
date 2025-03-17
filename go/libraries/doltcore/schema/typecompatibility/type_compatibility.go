18edfefce1987b69c50c5904ae4b1ff50ee9ec51
Jason Fulghum <jason@dolthub.com>
2025-03-17 12:20:03 -0700

    Bug fix for TypeCompatibilityChecker to ignore ExtenedTypes

--- go/libraries/doltcore/schema/typecompatibility/type_compatibility.go
+++ go/libraries/doltcore/schema/typecompatibility/type_compatibility.go
@@ -119,6 +119,14 @@ func (d doltTypeCompatibilityChecker) IsTypeChangeCompatible(from, to typeinfo.T
 		return res
 	}
 
+	// The TypeCompatibility checkers don't support ExtendedTypes added by integrators, so if we see
+	// one, return early and report the types are not compatible.
+	_, fromExtendedType := fromSqlType.(types.ExtendedType)
+	_, toExtendedType := toSqlType.(types.ExtendedType)
+	if fromExtendedType || toExtendedType {
+		return res
+	}
+
 	for _, checker := range d.checkers {
 		if checker.canHandle(fromSqlType, toSqlType) {
 			subcheckerResult := checker.isCompatible(fromSqlType, toSqlType)
