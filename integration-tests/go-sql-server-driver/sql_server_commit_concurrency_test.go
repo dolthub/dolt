dc16683143f04d2af76fba68f0e04ad24c422aeb
Neil Macneale IV <neil@dolthub.com>
2025-04-07 11:36:33 -0700

    A more non-deterministic test for --amend

--- integration-tests/go-sql-server-driver/sql_server_commit_concurrency_test.go
+++ integration-tests/go-sql-server-driver/sql_server_commit_concurrency_test.go
@@ -16,7 +16,12 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -26,6 +31,7 @@ import (
 func TestCommitConcurrency(t *testing.T) {
 	t.Parallel()
 	t.Run("SQL transaction with amend commit", testSQLTransactionWithAmendCommit)
+	t.Run("SQL racing amend", testSQLRacingAmend)
 }
 
 // testSQLTransactionWithAmendCommit verifies that two transactions started at the same state will not both be able
@@ -116,4 +122,135 @@ CREATE TABLE test_table (
 	require.NoError(t, err)
 	require.Equal(t, "tx2 amended commit", commitMessage)
 
+}
+
+func testSQLRacingAmend(t *testing.T) {
+	ctx := context.Background()
+	u, err := driver.NewDoltUser()
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		u.Cleanup()
+	})
+
+	rs, err := u.MakeRepoStore()
+	require.NoError(t, err)
+	repo, err := rs.MakeRepo("racing_amend_test")
+	require.NoError(t, err)
+
+	srvSettings := &driver.Server{
+		Args:        []string{"--port", `{{get_port "server"}}`},
+		DynamicPort: "server",
+	}
+	var ports DynamicPorts
+	ports.global = &GlobalPorts
+	ports.t = t
+	server := MakeServer(t, repo, srvSettings, &ports)
+	server.DBName = "racing_amend_test"
+
+	db, err := server.DB(driver.Connection{User: "root"})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		db.Close()
+	})
+
+	_, err = db.ExecContext(ctx, `
+	CREATE TABLE test_table (
+	  id INT AUTO_INCREMENT PRIMARY KEY,
+	  value VARCHAR(20)
+	);`)
+	require.NoError(t, err)
+
+	_, err = db.ExecContext(ctx, "INSERT INTO test_table VALUES (1, 'initial')")
+	require.NoError(t, err)
+
+	_, err = db.ExecContext(ctx, "CALL DOLT_COMMIT('-A','-m', 'initial commit')")
+	require.NoError(t, err)
+
+	type txIdFunc struct {
+		txNum  int
+		txFunc func() error
+	}
+
+	var transactions []txIdFunc
+	for txNum := 1; txNum <= 200; txNum++ {
+		txCtx, cancel := context.WithTimeout(ctx, 15*time.Second) // Should never hit, but just in case
+		tx, err := db.BeginTx(txCtx, nil)
+		require.NoError(t, err)
+		f := func() error {
+			defer cancel()
+			// update required to get a transaction conflict.
+			_, e2 := tx.ExecContext(txCtx, "UPDATE test_table SET value = ? WHERE id = 1", fmt.Sprintf("tx%d value", txNum))
+			require.NoError(t, e2)
+			_, e2 = tx.ExecContext(txCtx, "INSERT INTO test_table (value) VALUES (?)", fmt.Sprintf("tx%d new row", txNum))
+			require.NoError(t, e2)
+
+			// We want other transactions to have the chance to mess with the db. We'll verify that what's committed is what we expect.
+			time.Sleep(time.Duration(rand.Intn(1000)+500) * time.Millisecond)
+
+			// This will commit the transaction, or error.
+			_, e2 = tx.ExecContext(txCtx, "CALL DOLT_COMMIT('--amend','-a', '-m', ?)", fmt.Sprintf("tx%d amend", txNum))
+			if e2 != nil {
+				tx.Rollback()
+			}
+			return e2
+		}
+		transactions = append(transactions, txIdFunc{txNum: txNum, txFunc: f})
+	}
+
+	rand.Shuffle(len(transactions), func(i, j int) {
+		transactions[i], transactions[j] = transactions[j], transactions[i]
+	})
+
+	var atomicInt atomic.Int32
+	atomicInt.Store(-1)
+
+	var wg sync.WaitGroup
+	for _, txn := range transactions {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			// radomly sleep .5 - 1.5 seconds
+			time.Sleep(time.Duration(rand.Intn(1000)+500) * time.Millisecond)
+			err := txn.txFunc()
+
+			if err == nil {
+				// If there are multiple updates, something went wrong.
+				require.True(t, atomicInt.CompareAndSwap(-1, int32(txn.txNum)))
+			}
+			// Errors are expected.
+		}()
+	}
+	wg.Wait()
+
+	winner := atomicInt.Load()
+	require.NotEqual(t, -1, winner)
+
+	// Verify there are only 2 rows in the table
+	rows, err := db.QueryContext(ctx, "SELECT COUNT(*) FROM test_table")
+	require.NoError(t, err)
+	defer rows.Close()
+	rows.Next()
+	var count int
+	err = rows.Scan(&count)
+	require.NoError(t, err)
+	require.Equal(t, 2, count)
+
+	// Verify the final state
+	row := db.QueryRowContext(ctx, "SELECT value FROM test_table WHERE id = 1")
+	var value string
+	err = row.Scan(&value)
+	require.NoError(t, err)
+	require.Equal(t, fmt.Sprintf("tx%d value", winner), value)
+
+	row = db.QueryRowContext(ctx, "SELECT value FROM test_table WHERE id != 1")
+	err = row.Scan(&value)
+	require.NoError(t, err)
+	require.Equal(t, fmt.Sprintf("tx%d new row", winner), value)
+
+	// Verify the commit message
+	row = db.QueryRowContext(ctx, "SELECT message FROM dolt_log ORDER BY date DESC LIMIT 1")
+	var commitMessage string
+	err = row.Scan(&commitMessage)
+	require.NoError(t, err)
+	require.Equal(t, fmt.Sprintf("tx%d amend", winner), commitMessage)
 }
