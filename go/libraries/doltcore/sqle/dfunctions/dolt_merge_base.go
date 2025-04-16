cac827c17e65fab50059ce09dfdc01e335175091
jycor <jycor@users.noreply.github.com>
2025-04-16 23:53:02 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/dfunctions/dolt_merge_base.go
+++ go/libraries/doltcore/sqle/dfunctions/dolt_merge_base.go
@@ -17,7 +17,6 @@ package dfunctions
 import (
 	"errors"
 	"fmt"
-	"github.com/dolthub/dolt/go/libraries/doltcore/ref"
 
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/expression"
@@ -25,6 +24,7 @@ import (
 
 	"github.com/dolthub/dolt/go/libraries/doltcore/doltdb"
 	"github.com/dolthub/dolt/go/libraries/doltcore/merge"
+	"github.com/dolthub/dolt/go/libraries/doltcore/ref"
 	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/dsess"
 )
 
