65dc5866edcf18a93ff49f1bd10499342cba95ee
Neil Macneale IV <neil@dolthub.com>
2025-05-06 11:18:05 -0700

    conflict resolution test.

--- go/libraries/doltcore/sqle/enginetest/dolt_queries_long_lived_branches.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_queries_long_lived_branches.go
@@ -19,26 +19,28 @@ import (
 
 	"github.com/dolthub/go-mysql-server/enginetest/queries"
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/dolthub/go-mysql-server/sql/plan"
+	"github.com/dolthub/go-mysql-server/sql/types"
 )
 
-// Starting state for all test in this file. We create three empty tables A,B,C which have each have a single in pk column
+// Starting state for all test in this file. We create three empty tables A,B,C which have each have a pk column and a value column (both ints),
 // Three branches will be created: brA, brB, brC.
 // brA will insert 1,2,3 into A.       It will be one commit ahead of main.
 // brB will insert 10,20,30 into B.    It will be one commit ahead of main.
 // brC will insert 100,200,300 into C. It will be one commit ahead of brB
 var createTablesAndBranches = []string{
-	"CREATE TABLE A (pk INT PRIMARY KEY);",
-	"CREATE TABLE B (pk INT PRIMARY KEY);",
-	"CREATE TABLE C (pk INT PRIMARY KEY);",
+	"CREATE TABLE A (pk INT PRIMARY KEY,val INT);",
+	"CREATE TABLE B (pk INT PRIMARY KEY,val INT);",
+	"CREATE TABLE C (pk INT PRIMARY KEY,val INT);",
 	"CALL DOLT_COMMIT('-Am', 'empty tables');",
 	"CALL DOLT_CHECKOUT('-b', 'brA', 'main');",
-	"INSERT INTO A VALUES (1),(2),(3);",
+	"INSERT INTO A VALUES (1,1),(2,2),(3,3);",
 	"CALL DOLT_COMMIT('-am', 'insert into A while on brA');",
 	"CALL DOLT_CHECKOUT('-b', 'brB', 'main');",
-	"INSERT INTO B VALUES (10),(20),(30);",
+	"INSERT INTO B VALUES (10,10),(20,20),(30,30);",
 	"CALL DOLT_COMMIT('-am', 'insert into B while on brB');",
 	"CALL DOLT_CHECKOUT('-b', 'brC', 'brB');", // Branch from brB, not main.
-	"INSERT INTO C VALUES (100),(200),(300);",
+	"INSERT INTO C VALUES (100,100),(200,200),(300,300);",
 	"CALL DOLT_COMMIT('-am', 'insert into C while on brC');",
 }
 
@@ -48,7 +50,7 @@ var createTablesAndBranches = []string{
 // 3) record commit as @BR_B_CHANGES_A
 var brBChangesTableA = []string{
 	"CALL DOLT_CHECKOUT('brB');",
-	"INSERT INTO A VALUES (42),(53),(64);",
+	"INSERT INTO A VALUES (42,42),(53,53),(64,64);",
 	"CALL DOLT_COMMIT('-am', 'insert into A while on brB');",
 	captureHEADAs("BR_B_CHANGES_A"),
 }
@@ -74,11 +76,11 @@ var DoltLongLivedBranchTests = []queries.ScriptTest{
 		SetUpScript: chain(
 			createTablesAndBranches,
 			"CALL DOLT_CHECKOUT('brC');",
-			"INSERT INTO A VALUES (42),(53),(64);",
+			"INSERT INTO A VALUES (42,24),(53,35),(64,46);",
 			"CALL DOLT_COMMIT('-am', 'insert into A while on brC. Revert Me');",
 			captureHEADAs("revert_me"),
 			"CALL DOLT_CHECKOUT('brB');",
-			"INSERT INTO B VALUES (40),(50),(60);",
+			"INSERT INTO B VALUES (40,4),(50,5),(60,6);",
 			"CALL DOLT_COMMIT('-am', 'insert into B while on brB');",
 			"CALL DOLT_MERGE('brC');",
 			"CALL DOLT_REVERT(@revert_me);", // Revert that changes on table A. They should not show up on subsequent merges.
@@ -89,7 +91,7 @@ var DoltLongLivedBranchTests = []queries.ScriptTest{
 			"CALL DOLT_MERGE('brB');",
 			captureHEADAs("A_MERGE_1"),
 			"CALL DOLT_CHECKOUT('brC');",
-			"INSERT INTO C VALUES (400),(500),(600);",
+			"INSERT INTO C VALUES (400,400),(500,500),(600,600);",
 			"CALL DOLT_COMMIT('-am', 'insert into C while on branch brC');",
 			"CALL DOLT_CHECKOUT('brB');",
 			"CALL DOLT_MERGE('brC');",
@@ -107,19 +109,19 @@ var DoltLongLivedBranchTests = []queries.ScriptTest{
 				Expected: []sql.Row{},
 			},
 			{
-				Query: "SELECT * FROM A AS OF @A_MERGE_1;",
+				Query: "SELECT pk FROM A AS OF @A_MERGE_1;",
 				Expected: []sql.Row{
 					{1}, {2}, {3},
 				},
 			},
 			{
-				Query: "SELECT * FROM A AS OF @A_MERGE_2;",
+				Query: "SELECT pk FROM A AS OF @A_MERGE_2;",
 				Expected: []sql.Row{
 					{1}, {2}, {3},
 				},
 			},
 			{
-				Query: "SELECT * FROM A AS OF 'brC';",
+				Query: "SELECT pk FROM A AS OF 'brC';",
 				Expected: []sql.Row{
 					{42}, {53}, {64},
 				},
@@ -133,13 +135,13 @@ var DoltLongLivedBranchTests = []queries.ScriptTest{
 			brBChangesTableA,
 			"CALL DOLT_CHECKOUT('brA');",
 			captureHEADAs("A_initial"),
-			"INSERT INTO A VALUES (1111);",
+			"INSERT INTO A VALUES (1111,1111);",
 			"CALL DOLT_COMMIT('-am', 'insert into A while on brA');",
 			"CALL DOLT_MERGE('--no-ff', @BR_B_CHANGES_A);",
 			"CALL DOLT_REVERT(@BR_B_CHANGES_A);",
 			captureHEADAs("reverted_br_b_changes_a"),
 			"CALL DOLT_CHECKOUT('brB');",
-			"INSERT INTO B VALUES (2222);",
+			"INSERT INTO B VALUES (2222,2222);",
 			"CALL DOLT_COMMIT('-am', 'insert into B while on brB');",
 			"CALL DOLT_MERGE('--no-ff', @A_initial);",
 			captureHEADAs("B_MERGE_ME"),
@@ -151,14 +153,85 @@ var DoltLongLivedBranchTests = []queries.ScriptTest{
 			{
 				Query: "SELECT * FROM A AS OF 'brA';",
 				Expected: []sql.Row{
-					{1}, {2}, {3}, {1111},
+					{1, 1}, {2, 2}, {3, 3}, {1111, 1111},
 				},
 			},
 			{
 				// Verify that values inserted into A while on brB don't come back after the cross merge.
 				Query: "SELECT * FROM A AS OF 'brB';",
 				Expected: []sql.Row{
-					{1}, {2}, {3}, {1111},
+					{1, 1}, {2, 2}, {3, 3}, {1111, 1111},
+				},
+			},
+		},
+	},
+	{
+		Name: "Test re-resolve with multiple branches and conflicts",
+		SetUpScript: []string{
+			"CREATE TABLE test (pk INT PRIMARY KEY, val INT);",
+			"INSERT INTO test VALUES (1, 1), (2, 2), (3, 3);",
+			"CALL DOLT_COMMIT('-Am', 'Seed commit');",
+			"CALL DOLT_BRANCH('branchA');",
+			"CALL DOLT_BRANCH('branchB');",
+			"CALL DOLT_BRANCH('branchC');",
+			"CALL DOLT_CHECKOUT('branchA');",
+			"INSERT INTO test VALUES (4, 4);",
+			"CALL DOLT_COMMIT('-Am', 'Insert (4,4) on branchA');",
+			"CALL DOLT_CHECKOUT('branchB');",
+			"INSERT INTO test VALUES (5, 5);",
+			"CALL DOLT_COMMIT('-Am', 'Insert (5,5) on branchB');",
+			"CALL DOLT_CHECKOUT('branchC');",
+			"INSERT INTO test VALUES (6, 6);",
+			"CALL DOLT_COMMIT('-Am', 'Insert (6,6) on branchC');",
+			"CALL DOLT_CHECKOUT('branchA');",
+			"UPDATE test SET val = 10 WHERE pk = 1;",
+			"CALL DOLT_COMMIT('-Am', 'Update (1,10) on branchA');",
+			"CALL DOLT_CHECKOUT('branchB');",
+			"UPDATE test SET val = 20 WHERE pk = 1;",
+			"CALL DOLT_COMMIT('-Am', 'Update (1,20) on branchB');",
+			"CALL DOLT_CHECKOUT('branchC');",
+			"CALL DOLT_MERGE('branchB');",
+			"CALL DOLT_CHECKOUT('branchB');",
+			"SET @@autocommit = 0;",
+		},
+		Assertions: []queries.ScriptTestAssertion{
+			{
+				Query:    "CALL DOLT_MERGE('branchA');",
+				Expected: []sql.Row{{"", 0, 1, "conflicts found"}},
+			},
+			{
+				Query:    "SELECT base_pk, our_val, their_val FROM dolt_conflicts_test;",
+				Expected: []sql.Row{{1, 20, 10}},
+			},
+			{
+				Query:    "UPDATE dolt_conflicts_test SET our_val = 30;",
+				Expected: []sql.Row{{types.OkResult{RowsAffected: 1, InsertID: 0, Info: plan.UpdateInfo{Matched: 1, Updated: 1, Warnings: 0}}}},
+			},
+			{
+				Query:    "DELETE FROM dolt_conflicts_test;",
+				Expected: []sql.Row{{types.OkResult{RowsAffected: 1}}},
+			},
+			{
+				Query:            "CALL DOLT_COMMIT('-a', '-m', 'Resolve conflict by setting value to 30');",
+				SkipResultsCheck: true,
+			},
+			{
+				Query:            "CALL DOLT_CHECKOUT('branchC');",
+				SkipResultsCheck: true,
+			},
+			{
+				Query:            "CALL DOLT_MERGE('branchB');",
+				SkipResultsCheck: true,
+			},
+			{
+				Query: "SELECT * FROM test ORDER BY pk;",
+				Expected: []sql.Row{
+					{1, 30},
+					{2, 2},
+					{3, 3},
+					{4, 4},
+					{5, 5},
+					{6, 6},
 				},
 			},
 		},
