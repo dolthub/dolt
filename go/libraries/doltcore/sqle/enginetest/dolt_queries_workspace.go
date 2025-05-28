0c82594857396a2b7faee0affbc7f1d8161dba37
Tim Sehn <tim@dolthub.com>
2025-05-28 10:20:57 -0700

    Fix dolt_workspace go tests

--- go/libraries/doltcore/sqle/enginetest/dolt_queries_workspace.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_queries_workspace.go
@@ -65,7 +65,7 @@ var DoltWorkspaceScriptTests = []queries.ScriptTest{
 				Expected: []sql.Row{
 					{"id", "bigint unsigned", "NO", "PRI", nil, ""},
 					{"staged", "tinyint(1)", "NO", "", nil, ""},
-					{"diff_type", "varchar(200)", "NO", "", nil, ""},
+					{"diff_type", "varchar(1023)", "NO", "", nil, ""},
 					{"to_pk", "int", "YES", "", nil, ""},
 					{"to_val", "int", "YES", "", nil, ""},
 					{"from_pk", "int", "YES", "", nil, ""},
