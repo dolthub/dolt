6ee8893b71fc0cfb0d98180c3e457838053d3299
Neil Macneale IV <neil@dolthub.com>
2025-05-30 10:12:13 -0700

    Replace custom cache with L2Q cache

--- go/store/prolly/tree/node_cache.go
+++ go/store/prolly/tree/node_cache.go
@@ -15,10 +15,8 @@
 package tree
 
 import (
-	"fmt"
-	"sync"
-
 	"github.com/dolthub/dolt/go/store/hash"
+	lru "github.com/hashicorp/golang-lru/v2"
 )
 
 const (
@@ -54,145 +52,29 @@ func (c nodeCache) purge() {
 	}
 }
 
-type centry struct {
-	a    hash.Hash
-	n    Node
-	i    int
-	prev *centry
-	next *centry
-}
-
 type stripe struct {
-	mu     *sync.Mutex
-	chunks map[hash.Hash]*centry
-	head   *centry
-	sz     int
-	maxSz  int
-	rev    int
+	cache *lru.TwoQueueCache[hash.Hash, Node]
 }
 
 func newStripe(maxSize int) *stripe {
-	return &stripe{
-		&sync.Mutex{},
-		make(map[hash.Hash]*centry),
-		nil,
-		0,
-		maxSize,
-		0,
+	c, err := lru.New2Q[hash.Hash, Node](maxSize)
+	if err != nil {
+		panic(err) // A good reason to die. Not enough memory to allocate cache.
 	}
-}
 
-func removeFromList(e *centry) {
-	e.prev.next = e.next
-	e.next.prev = e.prev
-	e.prev = e
-	e.next = e
+	return &stripe{cache: c}
 }
 
 func (s *stripe) purge() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.chunks = make(map[hash.Hash]*centry)
-	s.head = nil
-	s.sz = 0
-	s.rev = 0
-}
-
-func (s *stripe) moveToFront(e *centry) {
-	e.i = s.rev
-	s.rev++
-	if s.head == e {
-		return
-	}
-	if s.head != nil {
-		removeFromList(e)
-		e.next = s.head
-		e.prev = s.head.prev
-		s.head.prev = e
-		e.prev.next = e
-	}
-	s.head = e
+	s.cache.Purge()
 }
 
 func (s *stripe) get(h hash.Hash) (Node, bool) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	if e, ok := s.chunks[h]; ok {
-		s.moveToFront(e)
-		return e.n, true
-	} else {
-		return Node{}, false
-	}
+	return s.cache.Peek(h)
+
+	// return s.cache.Get(h)
 }
 
 func (s *stripe) insert(addr hash.Hash, node Node) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	if e, ok := s.chunks[addr]; !ok {
-		e = &centry{addr, node, 0, nil, nil}
-		e.next = e
-		e.prev = e
-		s.moveToFront(e)
-		s.chunks[addr] = e
-		s.sz += node.Size()
-		s.shrinkToMaxSz()
-	} else {
-		s.moveToFront(e)
-	}
-}
-
-func (s *stripe) shrinkToMaxSz() {
-	for s.sz > s.maxSz {
-		if s.head != nil {
-			t := s.head.prev
-			removeFromList(t)
-			if t == s.head {
-				s.head = nil
-			}
-			delete(s.chunks, t.a)
-			s.sz -= t.n.Size()
-		} else {
-			panic("cache is empty but cache Size is > than max Size")
-		}
-	}
-}
-
-func (s *stripe) sanityCheck() {
-	if s.head != nil {
-		p := s.head.next
-		i := 1
-		sz := s.head.n.Size()
-		lasti := s.head.i
-		for p != s.head {
-			i++
-			sz += p.n.Size()
-			if p.i >= lasti {
-				panic("encountered lru list entry with higher rev later in the list.")
-			}
-			p = p.next
-		}
-		if i != len(s.chunks) {
-			panic(fmt.Sprintf("cache lru list has different Size than cache.chunks. %d vs %d", i, len(s.chunks)))
-		}
-		if sz != s.sz {
-			panic("entries reachable from lru list have different Size than cache.sz.")
-		}
-		j := 1
-		p = s.head.prev
-		for p != s.head {
-			j++
-			p = p.prev
-		}
-		if j != i {
-			panic("length of list backwards is not equal to length of list forward")
-		}
-	} else {
-		if len(s.chunks) != 0 {
-			panic("lru list is empty but s.chunks is not")
-		}
-		if s.sz != 0 {
-			panic("lru list is empty but s.sz is not 0")
-		}
-	}
+	s.cache.Add(addr, node)
 }
