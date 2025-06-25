ac820ba945b3631afbe01f51732cf011b23b0e66
NathanGabrielson <NathanGabrielson@users.noreply.github.com>
2025-06-25 23:21:15 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/sqle/dprocedures/dolt_rm.go
+++ go/libraries/doltcore/sqle/dprocedures/dolt_rm.go
@@ -16,13 +16,15 @@ package dprocedures
 
 import (
 	"fmt"
+
+	"github.com/dolthub/go-mysql-server/sql"
+
 	"github.com/dolthub/dolt/go/cmd/dolt/cli"
 	"github.com/dolthub/dolt/go/libraries/doltcore/branch_control"
 	"github.com/dolthub/dolt/go/libraries/doltcore/doltdb"
 	"github.com/dolthub/dolt/go/libraries/doltcore/env/actions"
 	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/dsess"
 	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/resolve"
-	"github.com/dolthub/go-mysql-server/sql"
 )
 
 // doltRm is the stored procedure for the NOT YET IMPLEMENTED cli command dolt rm
