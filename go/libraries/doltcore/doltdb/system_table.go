08ed9a68d175d4f763c89dd2782b8eb8080f1de9
elianddb <elian@dolthub.com>
2025-08-15 19:15:44 +0000

    rm extra registration, and add compact override

--- go/libraries/doltcore/doltdb/system_table.go
+++ go/libraries/doltcore/doltdb/system_table.go
@@ -385,6 +385,8 @@ var GetTestsTableName = func() string {
 const (
 	// LogTableName is the log system table name
 	LogTableName = "dolt_log"
+    // LogTableNameCompact is the compact log system table name
+    LogTableNameCompact = "dolt_log_compact"
 
 	// DiffTableName is the name of the table with a map of commits to tables changed
 	DiffTableName = "dolt_diff"
@@ -447,6 +449,7 @@ const (
 // in the `dolt.` schema, e.g. `dolt.branches`
 var DoltGeneratedTableNames = []string{
 	LogTableName,
+    LogTableNameCompact,
 	DiffTableName,
 	ColumnDiffTableName,
 	TableOfTablesInConflictName,
