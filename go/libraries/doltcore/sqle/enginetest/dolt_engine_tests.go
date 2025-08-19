a9378d3c9b6021bd3375b0f634f0f5cb240c812f
Nathan Gabrielson <nathan@dolthub.com>
2025-08-19 13:59:45 -0700

    WIP with feedback

--- go/libraries/doltcore/sqle/enginetest/dolt_engine_tests.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_engine_tests.go
@@ -2070,11 +2070,19 @@ func RunDoltQueryCatalogTests(t *testing.T, harness DoltEnginetestHarness) {
 func RunDoltTestsTableTests(t *testing.T, harness DoltEnginetestHarness) {
 	dtables.DoltCommand = doltcmd.DoltCommand
 
-	for _, scripte := range DoltTestTableScripts {
-		t.Run(scripte.Name, func(t *testing.T) {
+	for _, script := range DoltTestTableScripts {
+		t.Run(script.Name, func(t *testing.T) {
 			harness = harness.NewHarness(t)
 			defer harness.Close()
-			enginetest.TestScript(t, harness, scripte)
+			enginetest.TestScript(t, harness, script)
+		})
+	}
+
+	for _, script := range DoltTestRunFunctionScripts {
+		t.Run(script.Name, func(t *testing.T) {
+			harness = harness.NewHarness(t)
+			defer harness.Close()
+			enginetest.TestScript(t, harness, script)
 		})
 	}
 }
