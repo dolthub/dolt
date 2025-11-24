0493a7a1ef9839ba508835629c87e7f4676d3f12
elianddb <elian@dolthub.com>
2025-11-24 15:39:13 -0800

    fix getAbsFileRemoteUrl sideaffect of creating dirs if they do not exist

--- go/libraries/doltcore/env/remotes.go
+++ go/libraries/doltcore/env/remotes.go
@@ -692,15 +692,7 @@ func getAbsFileRemoteUrl(u *url.URL, fs filesys2.Filesys) (string, error) {
 	}
 
 	exists, isDir := fs.Exists(urlStr)
-
-	if !exists {
-		// TODO: very odd that GetAbsRemoteUrl will create a directory if it doesn't exist.
-		//  This concern should be separated
-		err = fs.MkDirs(urlStr)
-		if err != nil {
-			return "", fmt.Errorf("failed to create directory '%s': %w", urlStr, err)
-		}
-	} else if !isDir {
+	if exists && !isDir {
 		return "", filesys2.ErrIsFile
 	}
 
