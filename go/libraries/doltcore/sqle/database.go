9a1492854e89d9535d43ad8c538536178a647790
elianddb <elian@dolthub.com>
2026-01-07 11:07:43 -0800

    fix show tables to show singular name when local table name conflicts with non-local table

--- go/libraries/doltcore/sqle/database.go
+++ go/libraries/doltcore/sqle/database.go
@@ -1690,6 +1690,7 @@ func (db Database) GetAllTableNames(ctx *sql.Context, showSystemTables bool) ([]
 func (db Database) getAllTableNames(ctx *sql.Context, root doltdb.RootValue, includeGeneratedSystemTables bool, includeRootObjects bool, includeNonlocalTables readNonlocalTablesFlag) ([]string, error) {
 	var err error
 	var result []string
+	localNameSet := map[string]any{}
 	// If we are in a schema-enabled session and the schema name is not set, we need to union all table names in all
 	// schemas in the search_path
 	if resolve.UseSearchPath && db.schemaName == "" {
@@ -1704,6 +1705,9 @@ func (db Database) getAllTableNames(ctx *sql.Context, root doltdb.RootValue, inc
 		result, err = root.GetTableNames(ctx, db.schemaName, includeRootObjects)
 		if err != nil {
 			return nil, err
+		}
+		for _, nameStr := range result {
+			localNameSet[nameStr] = nil
 		}
 	}
 
@@ -1722,7 +1726,12 @@ func (db Database) getAllTableNames(ctx *sql.Context, root doltdb.RootValue, inc
 		if err != nil {
 			return nil, err
 		}
-		result = append(result, nonlocalTableNames...)
+		for _, nonLocalTableName := range nonlocalTableNames {
+			_, ok := localNameSet[nonLocalTableName]
+			if !ok {
+				result = append(result, nonLocalTableName)
+			}
+		}
 	}
 
 	return result, nil
