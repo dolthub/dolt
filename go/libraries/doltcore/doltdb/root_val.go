294a863b1b1e0a4c18e416eadf4ded28534dbebd
zachmu <zachmu@users.noreply.github.com>
2026-01-07 00:35:25 +0000

    [ga-format-pr] Run go/utils/repofmt/format_repo.sh and go/Godeps/update.sh

--- go/libraries/doltcore/doltdb/root_val.go
+++ go/libraries/doltcore/doltdb/root_val.go
@@ -386,12 +386,12 @@ func GenerateTagsForNewColColl(ctx context.Context, root RootValue, tableName st
 // GenerateTagsForNewColumns deterministically generates a slice of new tags that are unique within the history of this root. The names and NomsKinds of
 // the new columns are used to see the tag generator.
 func GenerateTagsForNewColumns(
-		ctx context.Context,
-		root RootValue,
-		tableName TableName,
-		newColNames []string,
-		newColKinds []types.NomsKind,
-		headRoot RootValue,
+	ctx context.Context,
+	root RootValue,
+	tableName TableName,
+	newColNames []string,
+	newColKinds []types.NomsKind,
+	headRoot RootValue,
 ) ([]uint64, error) {
 	if len(newColNames) != len(newColKinds) {
 		return nil, fmt.Errorf("error generating tags, newColNames and newColKinds must be of equal length")
@@ -413,7 +413,7 @@ func GenerateTagsForNewColumns(
 			// Only re-use tags if the noms kind didn't change
 			// TODO: revisit this when new storage format is further along
 			if strings.EqualFold(newColNames[i], col.Name) &&
-					newColKinds[i] == col.TypeInfo.NomsKind() {
+				newColKinds[i] == col.TypeInfo.NomsKind() {
 				newTags[i] = &col.Tag
 				break
 			}
@@ -446,11 +446,11 @@ func GenerateTagsForNewColumns(
 }
 
 func GetExistingColumns(
-		ctx context.Context,
-		root, headRoot RootValue,
-		tableName TableName,
-		newColNames []string,
-		newColKinds []types.NomsKind,
+	ctx context.Context,
+	root, headRoot RootValue,
+	tableName TableName,
+	newColNames []string,
+	newColKinds []types.NomsKind,
 ) ([]schema.Column, error) {
 
 	var existingCols []schema.Column
