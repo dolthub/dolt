418a46b9031266444e0b8438278999fee0f0c263
coffeegoddd☕️✨ <dustin@dolthub.com>
2026-02-03 14:28:26 -0800

    /go/store/blobstore/internal/git: wip, add read api

---
+++ go/store/blobstore/internal/git/errors.go
@@ -0,0 +1,61 @@
+// Copyright 2026 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package git
+
+import "fmt"
+
+// RefNotFoundError indicates that a ref (e.g. refs/dolt/data) could not be resolved.
+type RefNotFoundError struct {
+	Ref string
+}
+
+func (e RefNotFoundError) Error() string {
+	return fmt.Sprintf("git ref not found: %s", e.Ref)
+}
+
+// PathNotFoundError indicates that a tree path could not be resolved within a commit.
+type PathNotFoundError struct {
+	Commit string
+	Path   string
+}
+
+func (e PathNotFoundError) Error() string {
+	return fmt.Sprintf("git path not found: %s:%s", e.Commit, e.Path)
+}
+
+// NotBlobError indicates that a resolved path did not refer to a blob object.
+type NotBlobError struct {
+	Commit string
+	Path   string
+	Type   string
+}
+
+func (e NotBlobError) Error() string {
+	if e.Type == "" {
+		return fmt.Sprintf("git path is not a blob: %s:%s", e.Commit, e.Path)
+	}
+	return fmt.Sprintf("git path is not a blob (%s): %s:%s", e.Type, e.Commit, e.Path)
+}
+
+func IsRefNotFound(err error) bool {
+	_, ok := err.(RefNotFoundError)
+	return ok
+}
+
+func IsPathNotFound(err error) bool {
+	_, ok := err.(PathNotFoundError)
+	return ok
+}
+
