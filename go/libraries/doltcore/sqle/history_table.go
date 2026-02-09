a14c81217e2810fbb3cf5c0159a8f020bf7c18c4
angelamayxie <angela@dolthub.com>
2026-02-09 13:42:20 -0800

    undo changes to history table

--- go/libraries/doltcore/sqle/history_table.go
+++ go/libraries/doltcore/sqle/history_table.go
@@ -363,6 +363,10 @@ func (ht *HistoryTable) Projections() []string {
 }
 
 func (ht *HistoryTable) ProjectedTags() []uint64 {
+	if ht.projectedCols != nil {
+		return ht.projectedCols
+	}
+	
 	// Otherwise (no projection), return the tags for the underlying table with the extra meta tags appended
 	return append(ht.doltTable.ProjectedTags(), schema.HistoryCommitHashTag, schema.HistoryCommitterTag, schema.HistoryCommitDateTag)
 }
