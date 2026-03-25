dc56b198ad1d004075406304f7200354a070ee24
Zach Musgrave <zach@dolthub.com>
2026-03-25 15:38:54 -0700

    bidirectional tests

--- integration-tests/compatibility/runner.sh
+++ integration-tests/compatibility/runner.sh
@@ -69,6 +69,24 @@ function test_backward_compatibility() {
   DOLT_LEGACY_BIN="$(pwd)/$bin/dolt" DEFAULT_BRANCH="$DEFAULT_BRANCH" REPO_DIR="$(pwd)/repos/$ver" DOLT_VERSION="$ver" bats --print-output-on-failure ./test_files/bats
 }
 
+function test_bidirectional_compatibility() {
+  ver=$1
+  bin=`download_release "$ver"`
+
+  DOLT_NEW=`which dolt`
+
+  mkdir forward && cd forward
+  echo "Run the bidirectional tests with current Dolt version and older Dolt version $ver"
+  DOLT_LEGACY_BIN="$(pwd)/$bin/dolt" DOLT_NEW_BIN="$DOLT_NEW" bats --print-output-on-failure ./test_files/bats/bidirectional
+  cd ..
+
+  # same thing, but in the oppposite direction
+  mkdir backward && cd backward
+  echo "Run the bidirectional tests with older Dolt version $ver and current Dolt version"
+  DOLT_LEGACY_BIN="$DOLT_NEW" DOLT_NEW_BIN="$(pwd)/$bin/dolt" bats --print-output-on-failure ./test_files/bats/bidirectional
+  cd ..
+}
+
 function list_forward_compatible_versions() {
   grep -v '^ *#' < test_files/forward_compatible_versions.txt
 }
@@ -131,19 +149,27 @@ _main() {
   trap cleanup "EXIT"
 
   # test backward compatibility
-  list_backward_compatible_versions | while IFS= read -r ver; do
-    test_backward_compatibility "$ver"
-  done
+  # list_backward_compatible_versions | while IFS= read -r ver; do
+  #   test_backward_compatibility "$ver"
+  # done
 
   # setup repo for current dolt version
   setup_repo HEAD
 
   # test forward compatibility
+  # if [ -s "test_files/forward_compatible_versions.txt" ]; then
+  #     list_forward_compatible_versions | while IFS= read -r ver; do
+  #       test_forward_compatibility "$ver"
+  #     done
+  # fi
+
+  # test bidirectional compatibility
   if [ -s "test_files/forward_compatible_versions.txt" ]; then
       list_forward_compatible_versions | while IFS= read -r ver; do
-        test_forward_compatibility "$ver"
+        test_bidirectional_compatibility "$ver"
       done
   fi
+
 
   # sanity check: run tests against current version
   echo "Run the bats tests using current Dolt version hitting repositories from the current Dolt version"
