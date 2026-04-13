90521e8fbb9b7a7835151b3787d6fcda107e34fc
Nick Tobey <nick@dolthub.com>
2026-04-13 14:10:07 -0700

    Update incremental GC test to run dolt fsck.

--- integration-tests/go-sql-server-driver/gc_incremental_test.go
+++ integration-tests/go-sql-server-driver/gc_incremental_test.go
@@ -29,7 +29,7 @@ import (
 )
 
 func TestGCIncremental(t *testing.T) {
-	//t.Parallel()
+	t.Parallel()
 
 	archiveLevels := []chunks.GCArchiveLevel{chunks.NoArchive, chunks.SimpleArchive}
 
@@ -39,7 +39,7 @@ func TestGCIncremental(t *testing.T) {
 
 	// medium_file_size is chosen so that multiple chunks are put into each table file, and ensures that we correctly handle
 	// an in-progress table file at the end of the mark-and-sweep.
-	mediumFileSize := 10_000
+	mediumFileSize := 5_000
 
 	// large_file_size is arbitrarily large and tests the case where all leaf chunks fit in a single table file.
 	largeFileSize := 1_000_0000
@@ -47,98 +47,95 @@ func TestGCIncremental(t *testing.T) {
 	fileSizes := []int{smallFileSize, mediumFileSize, largeFileSize}
 
 	for _, archiveLevel := range archiveLevels {
-		for _, fileSize := range fileSizes {
-			t.Run(fmt.Sprintf("archiveLevel=%d,fileSize=%d", archiveLevel, fileSize), func(t *testing.T) {
-				//t.Parallel()
-				var ports DynamicResources
-				ports.global = &GlobalPorts
-				ports.t = t
+		t.Run(fmt.Sprintf("archiveLevel=%d", archiveLevel), func(t *testing.T) {
+			t.Parallel()
+			for _, fileSize := range fileSizes {
+				t.Run(fmt.Sprintf("fileSize=%d", fileSize), func(t *testing.T) {
+					t.Parallel()
+					for _, full := range []bool{true, false} {
+						t.Run(fmt.Sprintf("full=%v", full), func(t *testing.T) {
+							t.Parallel()
 
-				u, err := driver.NewDoltUser()
-				require.NoError(t, err)
-				t.Cleanup(func() { u.Cleanup() })
-
-				rs, err := u.MakeRepoStore()
-				require.NoError(t, err)
-
-				repoName := "incremental_gc_test"
-				repo, err := rs.MakeRepo(repoName)
-				require.NoError(t, err)
-
-				ctx := context.Background()
-
-				server := MakeServer(t, repo, &driver.Server{
-					Args:        []string{"--port", `{{get_port "server"}}`},
-					DynamicPort: "server",
-				}, &ports)
-				server.DBName = repoName
-
-				// Create the database and run GC
-				func() {
-
-					db, err := server.DB(driver.Connection{User: "root"})
-					require.NoError(t, err)
-					defer db.Close()
-
-					conn, err := db.Conn(ctx)
-					require.NoError(t, err)
-					defer conn.Close()
-					// Create some chunks that will be collected into oldgen because they're referenced by a commit
-					_, err = conn.ExecContext(ctx, "create table vals (id bigint primary key, val bigint)")
-					require.NoError(t, err)
-					vals := []string{}
-					for i := 0; i <= 1024; i++ {
-						vals = append(vals, fmt.Sprintf("(%d,0)", i))
+							runGCIncrementalTest(t, archiveLevel, fileSize, full)
+						})
 					}
-					_, err = conn.ExecContext(ctx, "insert into vals values "+strings.Join(vals, ","))
-					require.NoError(t, err)
-					_, err = conn.ExecContext(ctx, "call dolt_commit('-Am', 'create vals table')")
-					require.NoError(t, err)
-					// Create some chunks that will be collected into newgen because they aren't referenced by a commit
-					_, err = conn.ExecContext(ctx, "create table vals2 (id bigint primary key, val bigint)")
-					require.NoError(t, err)
-					_, err = conn.ExecContext(ctx, "insert into vals2 values "+strings.Join(vals, ","))
-					require.NoError(t, err)
-					gcSQL := "call dolt_gc('--archive-level',?,'--incremental-file-size','1','--full');"
-					_, err = conn.ExecContext(ctx, gcSQL, archiveLevel)
-					require.NoError(t, err)
-				}()
-
-				// Ensure that the server can start and that all data is present
-				func() {
-
-					db, err := server.DB(driver.Connection{User: "root"})
-					require.NoError(t, err)
-					defer db.Close()
-
-					conn, err := db.Conn(ctx)
-					require.NoError(t, err)
-					defer conn.Close()
-					res, err := conn.QueryContext(ctx, "select sum(id) from vals;")
-					var i int
-					require.True(t, res.Next())
-					err = res.Scan(&i)
-					require.NoError(t, err)
-					err = res.Close()
-					require.NoError(t, err)
-					res, err = conn.QueryContext(ctx, "select sum(id) from vals2;")
-					require.NoError(t, err)
-					require.True(t, res.Next())
-					err = res.Scan(&i)
-					require.NoError(t, err)
-					err = res.Close()
-					require.NoError(t, err)
-
-				}()
-
-				repoSize, err := GetRepoSize(repo.Dir)
-				require.NoError(t, err)
-				// Both newgen and oldgen should contain multiple chunk files
-				require.Greater(t, repoSize.NewGenC, 1)
-				require.Greater(t, repoSize.OldGenC, 1)
-			})
-		}
+				})
+			}
+		})
 	}
+}
+
+func runGCIncrementalTest(t *testing.T, archiveLevel chunks.GCArchiveLevel, fileSize int, full bool) {
+	var ports DynamicResources
+	ports.global = &GlobalPorts
+	ports.t = t
+
+	u, err := driver.NewDoltUser()
+	require.NoError(t, err)
+	t.Cleanup(func() { u.Cleanup() })
+
+	rs, err := u.MakeRepoStore()
+	require.NoError(t, err)
+
+	repoName := "incremental_gc_test"
+	repo, err := rs.MakeRepo(repoName)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+
+	server := MakeServer(t, repo, &driver.Server{
+		Args:        []string{"--port", `{{get_port "server"}}`},
+		DynamicPort: "server",
+	}, &ports)
+	server.DBName = repoName
+
+	// Create the database and run GC
+	func() {
+
+		db, err := server.DB(driver.Connection{User: "root"})
+		require.NoError(t, err)
+		defer db.Close()
+
+		conn, err := db.Conn(ctx)
+		require.NoError(t, err)
+		defer conn.Close()
+
+		// Create some chunks that will be collected into oldgen because they're referenced by a commit
+		_, err = conn.ExecContext(ctx, "create table vals (id bigint primary key, val bigint)")
+		require.NoError(t, err)
+		vals := []string{}
+		for i := 0; i <= 1024; i++ {
+			vals = append(vals, fmt.Sprintf("(%d,0)", i))
+		}
+		_, err = conn.ExecContext(ctx, "insert into vals values "+strings.Join(vals, ","))
+		require.NoError(t, err)
+		_, err = conn.ExecContext(ctx, "call dolt_commit('-Am', 'create vals table')")
+		require.NoError(t, err)
+
+		// Create some chunks that will be collected into newgen because they aren't referenced by a commit
+		_, err = conn.ExecContext(ctx, "create table vals2 (id bigint primary key, val bigint)")
+		require.NoError(t, err)
+		_, err = conn.ExecContext(ctx, "insert into vals2 values "+strings.Join(vals, ","))
+		require.NoError(t, err)
+		var gcSQL string
+		if full {
+			gcSQL = "call dolt_gc('--archive-level',?,'--incremental-file-size',?,'--full');"
+		} else {
+			gcSQL = "call dolt_gc('--archive-level',?,'--incremental-file-size',?);"
+		}
+		_, err = conn.ExecContext(ctx, gcSQL, archiveLevel, fileSize)
+		require.NoError(t, err)
+	}()
+
+	// Verify that the DB is in a valid state and there are no dropped chunks.
+	err = repo.DoltExec("fsck")
+	require.NoError(t, err)
+
+	repoSize, err := GetRepoSize(repo.Dir)
+	require.NoError(t, err)
+	// Both newgen and oldgen should contain multiple chunk files
+	require.Greater(t, repoSize.NewGenC, 1)
+	require.Greater(t, repoSize.OldGenC, 1)
 }
 
 type RepoSize struct {
