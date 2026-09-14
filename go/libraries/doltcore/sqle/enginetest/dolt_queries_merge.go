3ddb6da84adf0cc60d2fb7ebd7bf54cf2dd39ab5
Zach Musgrave <zach@dolthub.com>
2026-09-14 21:23:52 +0000

    Place regression coverage in existing test suites

--- go/libraries/doltcore/sqle/enginetest/dolt_queries_merge.go
+++ go/libraries/doltcore/sqle/enginetest/dolt_queries_merge.go
@@ -1675,6 +1675,27 @@ var MergeScripts = []queries.ScriptTest{
 			},
 		},
 	},
+	// https://github.com/dolthub/dolt/issues/8822
+	{
+		Name: "Test concurrent merges with overlapping indexes",
+		SetUpScript: []string{
+			"CREATE TABLE overlap_indexes(pk INT PRIMARY KEY,v INT,UNIQUE KEY uniq(v),KEY idx(v))",
+			"INSERT INTO overlap_indexes VALUES(1,1)",
+			"CALL dolt_commit('-Am','base')",
+			"CALL dolt_checkout('-b','other')",
+			"INSERT INTO overlap_indexes VALUES(2,2)",
+			"CALL dolt_commit('-am','other')",
+			"CALL dolt_checkout('main')",
+			"INSERT INTO overlap_indexes VALUES(3,3)",
+			"CALL dolt_commit('-am','main')",
+			"CALL dolt_merge('other')",
+		},
+		Assertions: []queries.ScriptTestAssertion{
+			{Query: "SELECT * FROM overlap_indexes ORDER BY pk", Expected: []sql.Row{{int32(1), int32(1)}, {int32(2), int32(2)}, {int32(3), int32(3)}}},
+			{Query: "SELECT * FROM overlap_indexes FORCE INDEX(idx) WHERE v=2", Expected: []sql.Row{{int32(2), int32(2)}}},
+			{Query: "SELECT * FROM overlap_indexes FORCE INDEX(uniq) WHERE v=3", Expected: []sql.Row{{int32(3), int32(3)}}},
+		},
+	},
 	{
 		Name: "unique keys, update violation from left",
 		SetUpScript: []string{
