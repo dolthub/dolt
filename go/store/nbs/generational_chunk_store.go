f874616844ff95ce9731a3f7109896461d661d3c
Aaron Son <aaron@dolthub.com>
2026-09-17 16:58:47 +0200

    go/store/nbs: Fix inverted nil check in GenerationalNBS.PersistGhostHashes.
    It called through to ghostGen exactly when ghostGen was nil, and returned the "ghostGen is nil" error otherwise.

--- go/store/nbs/generational_chunk_store.go
+++ go/store/nbs/generational_chunk_store.go
@@ -48,9 +48,9 @@ var ErrGhostChunkRequested = errors.New("requested chunk which is expected to be
 
 func (gcs *GenerationalNBS) PersistGhostHashes(ctx context.Context, refs hash.HashSet) error {
 	if gcs.ghostGen == nil {
-		return gcs.ghostGen.PersistGhostHashes(ctx, refs)
+		return fmt.Errorf("runtime error. ghostGen is nil but an attempt to persist ghost hashes was made")
 	}
-	return fmt.Errorf("runtime error. ghostGen is nil but an attempt to persist ghost hashes was made")
+	return gcs.ghostGen.PersistGhostHashes(ctx, refs)
 }
 
 func (gcs *GenerationalNBS) GhostGen() chunks.GhostChunkStore {
